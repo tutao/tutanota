0ac9411b35e3a25c2677982619d3be1422223226
mpfau <matthias.pfau@tutao.de>
2013-12-06 18:31:57 +0100

    fixed js build

--- js/util/KnockoutObservableAdapter.js
+++ js/util/KnockoutObservableAdapter.js
@@ -15,9 +15,9 @@ tutao.util.KnockoutObservableAdapter = function(entity) {
 	};
 
     ko.subscribable.call(observable);
-    observable.peek = function() { return _latestValue };
-    observable.valueHasMutated = function () { observable["notifySubscribers"](_latestValue); }
-    observable.valueWillMutate = function () { observable["notifySubscribers"](_latestValue, "beforeChange"); }
+    observable.peek = function() { return entity };
+    observable.valueHasMutated = function () { observable["notifySubscribers"](entity); }
+    observable.valueWillMutate = function () { observable["notifySubscribers"](entity, "beforeChange"); }
     ko.utils.extend(observable, ko.observable['fn']);
 
     ko.exportProperty(observable, 'peek', observable.peek);
