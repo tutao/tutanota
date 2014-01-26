95c5094208f7ec68eaa77553d3580f7279d5e804
Arne Moehle <moehle@l3s.de>
2014-01-26 19:24:37 +0100

    fixed iss458: Sender/recipient mail addresses not visible if name is shown

--- js/ctrl/ComposingMail.js
+++ js/ctrl/ComposingMail.js
@@ -568,7 +568,7 @@ tutao.tutanota.ctrl.ComposingMail.prototype._createBubbleFromRecipientInfo = fun
             return "unsecureRecipient";
         }
     }, this);
-	return new tutao.tutanota.ctrl.bubbleinput.Bubble(recipientInfo, recipientInfo.getDisplayText(), state);
+	return new tutao.tutanota.ctrl.bubbleinput.Bubble(recipientInfo, ko.observable(recipientInfo.getDisplayText()), ko.observable(recipientInfo.getMailAddress()), state);
 };
 
 /**
