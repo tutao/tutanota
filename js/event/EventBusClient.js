9325564742bf1bc17ffcd2d932f5849531fe9863
bdeterding <bernd.deterding@tutao.de>
2014-02-05 12:53:03 +0100

    iss365 - New mails can not be received any more after some minutes in IE10

--- js/event/EventBusClient.js
+++ js/event/EventBusClient.js
@@ -2,43 +2,47 @@
 
 goog.provide('tutao.event.EventBusClient');
 
-
 /**
  * The EventBus encapsulates the WebSocket connection to the server. It currently only forwards messages to observers.
- * @implements {tutao.event.ObservableInterface}
  */
-// TODO (before beta) test disconnects, re-connect etc.
 tutao.event.EventBusClient = function() {
 	tutao.util.FunctionUtils.bindPrototypeMethodsToThis(this); // listener methods are invoked from the Websocket
-	this._observable = new tutao.event.Observable();
 	this._socket = null;
-	this._failedConnects = 0;
-	this._lastConnectionAttempt = null;
     this._tryReconnect = false;
+    /** @type {Array.<tutao.event.EventBusListener>} */
+    this._listeners = [];
 };
 
 /**
- * @inheritDoc
+ * Notifies all listeners on reconnect event.
  */
-tutao.event.EventBusClient.prototype.addObserver = function(observer) {
-	this._observable.addObserver(observer);
+tutao.event.EventBusClient.prototype.notifyReconnected = function() {
+    for( var i=0; i < this._listeners.length; i++){
+        this._listeners[i].notifyReconnected();
+    }
 };
 
 /**
- * @inheritDoc
+ * Notifies all listener when receiving new data.
+ * @param {tutao.entity.sys.EntityUpdate} data The update notification
  */
-tutao.event.EventBusClient.prototype.removeObserver = function(observer) {
-	this._observable.removeObserver(observer);
+tutao.event.EventBusClient.prototype.notifyNewDataReceived = function(data) {
+    for( var i=0; i < this._listeners.length; i++){
+        this._listeners[i].notifyNewDataReceived(data);
+    }
 };
+
 
 /**
- * @inheritDoc
+ * Adds a listener to this event bus client.
+ * @param {tutao.event.EventBusListener} listener The listener that is notified for changes.
  */
-tutao.event.EventBusClient.prototype.notifyObservers = function(data) {
-	this._observable.notifyObservers(data);
+tutao.event.EventBusClient.prototype.addListener = function(listener) {
+    this._listeners.push(listener);
 };
 
-tutao.event.EventBusClient.prototype.connect = function() {
+
+tutao.event.EventBusClient.prototype.connect = function(reconnect) {
 	var self = this;
 	var protocol = document.location.protocol === 'http:' ? 'ws' : 'wss';
     var port = document.location.port === '' ? '' : ':' + document.location.port;
@@ -55,6 +59,9 @@ tutao.event.EventBusClient.prototype.connect = function() {
 			.setAuthentication(authentication);
 
 	    self._socket.send(JSON.stringify(wrapper.toJsonData()));
+        if (reconnect) {
+            self.notifyReconnected();
+        }
 	};
 	this._socket.onclose = this._close;
 	this._socket.onerror = this._error;
@@ -82,45 +89,19 @@ tutao.event.EventBusClient.prototype._message = function(message) {
 	if (wrapper.getType() === 'chat') {
 		console.log(wrapper.getChat().getSender() + " > " + wrapper.getChat().getText());
 	} else if (wrapper.getType() === 'entityUpdate') {
-		this.notifyObservers(wrapper.getEntityUpdate());
+		this.notifyNewDataReceived(wrapper.getEntityUpdate());
 	}
 };
 
 tutao.event.EventBusClient.prototype._close = function(event) {
 	console.log("ws close: ", event, new Date());
     if (this._tryReconnect) {
-        this._reconnect();
+        setTimeout(this._reconnect, 30000);
     }
 };
-
-/**
- * Send a message to a recipient group.
- * @param {string} recipient The id of the recipients group.
- * @param {string} message The plain text message to send to the recipient.
- */
-tutao.event.EventBusClient.prototype.sendMessage = function(recipient, message) {
-	//var self = this;
-	//this._checkSocket(function() { self._sendMessage(recipient, message); });
-	this._sendMessage(recipient, message);
-};
-
-tutao.event.EventBusClient.prototype._sendMessage = function(recipient, message) {
-	var chat = new tutao.entity.sys.Chat()
-		.setSender(tutao.locator.userController.getUserGroupId())
-		.setRecipient(recipient)
-		.setText(message);
-	var wrapper = new tutao.entity.sys.WebsocketWrapper()
-		.setType("chat")
-		.setMsgId("0")
-		.setChat(chat);
-	this._socket.send(JSON.stringify(wrapper.toJsonData()));
-};
-
 
 tutao.event.EventBusClient.prototype._reconnect = function() {
     console.log("reconnect socket state: " + this._socket.readyState);
     this._tryReconnect = false;
-    this._reconnectInterval = window.setTimeout(this.connect, 30000 );
+    this.connect(true);
 };
-
-
