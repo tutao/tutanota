922c2408cc87962fe4cf1443be4a89a84ccda165
bdeterding <bernd.deterding@tutao.de>
2014-03-20 12:13:19 +0100

    iss739: Reading external mail in IE 9 and IE 8 not possible on test.tutanota.de

--- js/legacy/Legacy.js
+++ js/legacy/Legacy.js
@@ -8,10 +8,10 @@ goog.provide('tutao.tutanota.legacy.Legacy');
  */
 tutao.tutanota.legacy.Legacy.setup = function(singletons) {
 
-	if (tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.SUPPORTED_TYPE_NOT_SUPPORTED || 
-		tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.SUPPORTED_TYPE_UNKNOWN ||
-        tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.SUPPORTED_TYPE_UPDATE_NEEDED ||
-        tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.SUPPORTED_TYPE_LEGACY_IE) {
+	if (tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.ClientDetector.SUPPORTED_TYPE_NOT_SUPPORTED ||
+		tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.ClientDetector.SUPPORTED_TYPE_UNKNOWN ||
+        tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.ClientDetector.SUPPORTED_TYPE_UPDATE_NEEDED ||
+        tutao.tutanota.util.ClientDetector.getSupportedType() == tutao.tutanota.util.ClientDetector.SUPPORTED_TYPE_LEGACY_IE) {
         delete singletons.clientWorkerProxy;
         delete singletons.rsaCrypter;
         delete singletons.eventBus;
