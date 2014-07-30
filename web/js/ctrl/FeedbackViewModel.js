2f9d27571718b62d21b4f824af485d453666b34c
mpfau <matthias.pfau@tutao.de>
2014-07-30 10:02:49 +0200

    removed screenshot feature from feedback as it does not help on error detection

--- web/js/ctrl/FeedbackViewModel.js
+++ web/js/ctrl/FeedbackViewModel.js
@@ -11,10 +11,7 @@ tutao.tutanota.ctrl.FeedbackViewModel = function() {
 
 	this.message = ko.observable("");
     this.stack = ko.observable("");
-	this.image = null;
-    this.addScreenshot = ko.observable(false);
 	this.showDialog = ko.observable(false);
-    this.showAddScreenshot = ko.observable(false);
 
     var self = this;
     this.isErrorDialog = ko.computed(function() {
@@ -44,49 +41,22 @@ tutao.tutanota.ctrl.FeedbackViewModel = function() {
  * @param {string=} stack
  */
 tutao.tutanota.ctrl.FeedbackViewModel.prototype.open = function(stack) {
-	var self = this;
     if (stack) {
         this.stack(stack);
     } else {
         this.stack("");
     }
     this.message("");
-	html2canvas($('body'), {
-		onrendered: function(canvas) {
-            try{
-                var img = canvas.toDataURL();
-                var string_base64 = img.split(',')[1];
-                var binary_string = window.atob(string_base64);
-                var len = binary_string.length;
-                var bytes = new Uint8Array(len);
-                for (var i = 0; i < len; i++) {
-                    bytes[i] = binary_string.charCodeAt(i);
-                }
-                self.image = bytes.buffer;
-            }catch (e){
-            }
-            self.showAddScreenshot(self.image != null);
-            self.showDialog(true);
-		},
-		allowTaint: true
-	});
+    this.showDialog(true);
 };
 
 tutao.tutanota.ctrl.FeedbackViewModel.prototype.close = function() {
     this.showDialog(false);
-    this.image = null; // only reset image because the view is faded out and text changes would be visible. Message and stack values are reset in open()
 };
 
 tutao.tutanota.ctrl.FeedbackViewModel.prototype.sendFeedback = function() {
     var self = this;
     var attachments = [];
-    if (this.image != null && this.addScreenshot()){
-        var imageFile = new tutao.entity.tutanota.File();
-        imageFile.setMimeType("image/png");
-        imageFile.setName("screenshot.png");
-        imageFile.setSize(this.image.byteLength + "");
-        attachments.push(new tutao.tutanota.util.DataFile(this.image, imageFile));
-    }
     var facade;
     var previousMessageId;
     if (tutao.locator.userController.isExternalUserLoggedIn()) {
