b97df41e908124477816f063775cc0ae6c19c393
mpfau <matthias.pfau@tutao.de>
2014-08-30 23:33:58 +0200

    fixed AesArrayBufferTest

--- web/js/crypto/CryptoError.js
+++ web/js/crypto/CryptoError.js
@@ -9,6 +9,12 @@ tutao.provide('tutao.crypto.CryptoError');
  * @constructor
  */
 tutao.crypto.CryptoError = function(message, error) {
+	if (!error) {
+		this.message = message;
+	} else {
+		this.message = message + ", original message: " + error.message;
+	}
+
     if (Error.captureStackTrace) {
         Error.captureStackTrace(this, tutao.crypto.CryptoError);
     } else {
@@ -23,11 +29,6 @@ tutao.crypto.CryptoError = function(message, error) {
         this.stack = this.name + ". " + this.message + "\n" + error.stack.split("\n").slice(1).join("\n"); // removes first line from stack
     }
 
-	if (!error) {
-		this.message = message;
-	} else {
-		this.message = message + ", original message: " + error.message;
-	}
 	this.name = "CryptoError";
 	this.error = error;
 };
