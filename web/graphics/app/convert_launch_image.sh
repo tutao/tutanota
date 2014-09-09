5d47d974e1260f8d3636eb52971623318739e6ca
bdeterding <bernd.deterding@tutao.de>
2014-09-09 14:05:03 +0200

    Resize launch images for apps

--- web/graphics/app/convert_launch_image.sh
+++ web/graphics/app/convert_launch_image.sh
@@ -14,18 +14,18 @@
 SPLASH=tutanota-logo.svg
 
 mkdir android
-inkscape -z -e android/xlarge_xhdpi -w 960 -h 720 $SPLASH
-inkscape -z -e android/large_hdpi -w 640 -h 480 $SPLASH
-inkscape -z -e android/medium_mdpi -w 470 -h 320 $SPLASH
-inkscape -z -e android/small_ldpi -w 426 -h 320 $SPLASH 
+inkscape -b ffffff00 -z -e android/xlarge_xhdpi --export-area=0:660:1280:1620 -w 960 -h 720 $SPLASH # 1:0,75
+inkscape -b ffffff00 -z -e android/large_hdpi --export-area=0:660:1280:1620 -w 640 -h 480 $SPLASH # 1:0,75
+inkscape -b ffffff00 -z -e android/medium_mdpi --export-area=0:704:1280:1575 -w 470 -h 320 $SPLASH # 1:0,68
+inkscape -b ffffff00 -z -e android/small_ldpi --export-area=0:660:1280:1620 -w 426 -h 320 $SPLASH # 1:0,75
 
 
 
 mkdir ios
-inkscape -z -e ios/Default-568h@2x~iphone.png  -w 640 -h 1136 $SPLASH
-inkscape -z -e ios/Default-Landscape@2x~ipad.png  -w 2048 -h 1496 $SPLASH
-inkscape -z -e ios/Default-Landscape~ipad.png  -w 1024 -h 748 $SPLASH
-inkscape -z -e ios/Default-Portrait@2x~ipad.png  -w 1536 -h 2008 $SPLASH
-inkscape -z -e ios/Default-Portrait~ipad.png  -w 768 -h 1004 $SPLASH
-inkscape -z -e ios/Default@2x~iphone.png  -w 640 -h 960 $SPLASH
-inkscape -z -e ios/Default~iphone.png  -w 320 -h 480 $SPLASH
+inkscape -b ffffff00 -z -e ios/Default-568h@2x~iphone.png  --export-area=0:4:1280:2276 -w 640 -h 1136 $SPLASH # 1:1.775
+inkscape -b ffffff00 -z -e ios/Default-Landscape@2x~ipad.png  --export-area=0:672:1280:1607 -w 2048 -h 1496 $SPLASH # 1:0,73
+inkscape -b ffffff00 -z -e ios/Default-Landscape~ipad.png  --export-area=0:672:1280:1607 -w 1024 -h 748 $SPLASH # 1:0,73
+inkscape -b ffffff00 -z -e ios/Default-Portrait@2x~ipad.png  --export-area=0:301:1280:1978 -w 1536 -h 2008 $SPLASH # 1,31
+inkscape -b ffffff00 -z -e ios/Default-Portrait~ipad.png  --export-area=0:301:1280:1978 -w 768 -h 1004 $SPLASH # 1,31
+inkscape -b ffffff00 -z -e ios/Default@2x~iphone.png  --export-area=0:180:1280:2100 -w 640 -h 960 $SPLASH # 1:1,5
+inkscape -b ffffff00 -z -e ios/Default~iphone.png  --export-area=0:180:1280:2100 -w 320 -h 480 $SPLASH # 1:1,5
