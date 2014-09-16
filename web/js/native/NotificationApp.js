92441922245ab6ceb19a5d78a668463c75fefd37
Arne Möhle <arne.moehle@tutao.de>
2014-09-16 10:56:21 +0200

    update notification app

--- web/js/native/NotificationApp.js
+++ web/js/native/NotificationApp.js
@@ -10,9 +10,11 @@ tutao.native.NotificationApp = function(){
 };
 
 tutao.native.NotificationApp.prototype.add = function(title, message, badge) {
-    window.plugin.notification.local.add({id: this.currentId++, title: title, message: message, badge: badge, autoCancel: true});
+    window.plugin.notification.local.add({id: String(this.currentId++), title: title, message: message, badge: badge, autoCancel: true});
+    navigator.notification.beep(1);
+    navigator.notification.vibrate(300);
 };
 
 tutao.native.NotificationApp.prototype.updateBadge = function(number) {
     cordova.plugins.notification.badge.set(number);
-};
+}
