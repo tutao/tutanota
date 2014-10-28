1986084d30d4a191f2a2877578fda8d3fcdc7f40
bdeterding <bernd.deterding@tutao.de>
2014-10-28 13:56:01 +0100

    new ipad splash screens

--- web/graphics/app/convert_launch_image.sh
+++ web/graphics/app/convert_launch_image.sh
@@ -28,10 +28,10 @@ inkscape -b ffffff00 -z -e android/small_port_ldpi.png --export-area=0:209:1280:
 
 mkdir ios
 inkscape -b ffffff00 -z -e ios/Default-568h@2x~iphone.png  --export-area=0:4:1280:2276 -w 640 -h 1136 $SPLASH # 1:1.775
-inkscape -b ffffff00 -z -e ios/Default-Landscape@2x~ipad.png  --export-area=0:672:1280:1607 -w 2048 -h 1496 $SPLASH # 1:0,73
-inkscape -b ffffff00 -z -e ios/Default-Landscape~ipad.png  --export-area=0:672:1280:1607 -w 1024 -h 748 $SPLASH # 1:0,73
-inkscape -b ffffff00 -z -e ios/Default-Portrait@2x~ipad.png  --export-area=0:301:1280:1978 -w 1536 -h 2008 $SPLASH # 1:1,31
-inkscape -b ffffff00 -z -e ios/Default-Portrait~ipad.png  --export-area=0:301:1280:1978 -w 768 -h 1004 $SPLASH # 1:1,31
+inkscape -b ffffff00 -z -e ios/Default-Landscape@2x~ipad.png  --export-area=0:660:1280:1620 -w 2048 -h 1536 $SPLASH # 1:0,75
+inkscape -b ffffff00 -z -e ios/Default-Landscape~ipad.png  --export-area=0:660:1280:1620 -w 1024 -h 768 $SPLASH # 1:0,75
+inkscape -b ffffff00 -z -e ios/Default-Portrait@2x~ipad.png  --export-area=0:289:1280:1991 -w 1536 -h 2048 $SPLASH # 1:1,33
+inkscape -b ffffff00 -z -e ios/Default-Portrait~ipad.png  --export-area=0:289:1280:1991 -w 768 -h 1024 $SPLASH # 1:1,33
 inkscape -b ffffff00 -z -e ios/Default@2x~iphone.png  --export-area=0:180:1280:2100 -w 640 -h 960 $SPLASH # 1:1,5
 inkscape -b ffffff00 -z -e ios/Default~iphone.png  --export-area=0:180:1280:2100 -w 320 -h 480 $SPLASH # 1:1,5
 inkscape -b ffffff00 -z -e ios/Default-667h.png  --export-area=0:1:1280:2279 -w 750 -h 1334 $SPLASH # 1:1,78
