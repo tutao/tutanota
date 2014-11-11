5c88d571b61a4cfbd16dc8a3ac0491b5435b196a
bdeterding <bernd.deterding@tutao.de>
2014-11-11 11:29:55 +0100

    new launch images for android devices

--- cordova/hooks/after_platform_add/copy_splash.sh
+++ cordova/hooks/after_platform_add/copy_splash.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-if [[ $CORDOVA_PLATFORMS == *android* ]]
-then
-	echo "copy splash screen images for android"
-	cp www/graphics/app/android/xlarge_land_xhdpi.png platforms/android/res/drawable-land-xhdpi/screen.png
-	cp www/graphics/app/android/large_land_hdpi.png  platforms/android/res/drawable-land-hdpi/screen.png
-	cp www/graphics/app/android/medium_land_mdpi.png  platforms/android/res/drawable-land-mdpi/screen.png
-	cp www/graphics/app/android/small_land_ldpi.png  platforms/android/res/drawable-land-ldpi/screen.png
-
-	cp www/graphics/app/android/xlarge_port_xhdpi.png platforms/android/res/drawable-port-xhdpi/screen.png
-	cp www/graphics/app/android/large_port_hdpi.png  platforms/android/res/drawable-port-hdpi/screen.png
-	cp www/graphics/app/android/medium_port_mdpi.png  platforms/android/res/drawable-port-mdpi/screen.png
-	cp www/graphics/app/android/small_port_ldpi.png  platforms/android/res/drawable-port-ldpi/screen.png
-fi
-
 if [[ $CORDOVA_PLATFORMS == *ios* ]]
 then
 	echo "copy splash screen images and app icons for ios"
