5e2d9c67d1fa369475f9d51aa4b7480944619fe3
armhub <arne.moehle@tutao.de>
2014-11-24 12:10:48 +0100

    added close button in modal dialog

--- web/js/ctrl/ModalDialogViewModel.js
+++ web/js/ctrl/ModalDialogViewModel.js
@@ -12,6 +12,7 @@ tutao.tutanota.ctrl.ModalDialogViewModel = function() {
 	this.message = ko.observable("");
     this.buttons = ko.observableArray();
     this.visible = ko.observable(false);
+    this.closeFunction = null;
 };
 
 /**
@@ -20,7 +21,9 @@ tutao.tutanota.ctrl.ModalDialogViewModel = function() {
  * @return {Promise} Resolved when the dialog is closed by the user.
  */
 tutao.tutanota.ctrl.ModalDialogViewModel.prototype.showAlert = function(message) {
-    return this.showDialog(message, ["ok_action"]);
+    return this.showDialog(message, ["ok_action"]).then(function() {
+        // nothing to return
+    });
 };
 
 /**
@@ -35,9 +38,10 @@ tutao.tutanota.ctrl.ModalDialogViewModel.prototype.showConfirm = function(messag
 };
 
 /**
- * Shows the dialog with the given buttons. Returns the index of the button that was selected by the user.
+ * Shows the dialog with the given buttons. Returns the index of the button that was selected by the user. The index of the last button is returned if the user closes the dialog with "X".
  * @param {string} message The text to display in the dialog.
  * @param {Array.<string>} buttonTextIds The texts to show on the buttons. The number text ids is the number of buttons that are shown.
+ * @return {Promise<Number>} Provides the id of the button that was clicked.
  */
 tutao.tutanota.ctrl.ModalDialogViewModel.prototype.showDialog = function(message, buttonTextIds) {
     var self = this;
@@ -54,6 +58,17 @@ tutao.tutanota.ctrl.ModalDialogViewModel.prototype.showDialog = function(message
             })();
         }
         self.buttons(buttons);
+
+        // the last button shall be clicked if the dialog is closed via "X"
+        self.closeFunction = function() {
+            resolve(buttonTextIds.length - 1);
+        };
+
         self.visible(true);
     });
+};
+
+tutao.tutanota.ctrl.ModalDialogViewModel.prototype.close = function() {
+    this.visible(false);
+    this.closeFunction();
 };
