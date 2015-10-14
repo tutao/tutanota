7a6c7d5444add78019899da0638dd44189c887f4
Bernd Deterding <bernd.deterding@tutao.de>
2015-10-14 11:53:06 +0200

    updated build process for iOS9

--- cordova/hooks/after_platform_add/copy_settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $CORDOVA_PLATFORMS == *ios* ]]
-then
-	echo "copy project settings for ios"
-	cp files/ios/Tutanota-Info.plist  platforms/ios/Tutanota/
-	cp -r files/ios/Tutanota.xcodeproj  platforms/ios/
-fi
-
