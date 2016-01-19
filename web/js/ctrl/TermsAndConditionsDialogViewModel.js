9abcca66da127227181bfa1134fd7cbc33594d69
Bernd Deterding <bernd.deterding@tutao.de>
2016-01-19 18:11:50 +0100

    fixed removal of links to support page

--- web/js/ctrl/TermsAndConditionsDialogViewModel.js
+++ web/js/ctrl/TermsAndConditionsDialogViewModel.js
@@ -35,8 +35,7 @@ tutao.tutanota.ctrl.TermsAndConditionsDialogViewModel.prototype.showPrivacy = fu
 };
 
 tutao.tutanota.ctrl.TermsAndConditionsDialogViewModel.prototype._updateText = function() {
-    //this.visible(true);
-    var link = "terms.html" + " #" + this._type + "-" + this.currentLang();
+    var link =  tutao.env.getHttpOrigin()  + "/terms.html" + " #" + this._type + "-" + this.currentLang();
     var element = $("<div>");
     var self = this;
     element.load(link, null, function() {
