cb0de3f40a935957c8875cea4771c1e73531ae4d
bdeterding <bernd.deterding@tutao.de>
2016-02-24 11:06:00 +0100

    refresh tutanota cordova plugin before android build

--- cordova/gulpfile.js
+++ cordova/gulpfile.js
@@ -14,34 +14,36 @@ gulp.task('default', shell.task([
     'cordova run android'
 ]));
 
-gulp.task('createWebRelease', shell.task([
-    'cd ../web; gulp distCordova'
+gulp.task('updateAndroidPlatformAndTutanotaPlugin', shell.task([
+    'cordova platform remove android',
+    'cordova platform add android@5.1.0',
+    // we need to remove and add the native plugin, because changes are not copied from the plugins to the platform folder during build. re-create the link from the plugins native folder to the native project afterwards
+    'cordova plugins remove de.tutanota.native',
+    'cordova plugins add ../native/',
+    'rm -r ./plugins/de.tutanota.native',
+    'ln -s ../../native/ ./plugins/de.tutanota.native'
 ]));
 
-
+gulp.task('createWebRelease',shell.task([
+    'cd ../web; gulp distCordova'
+]));
 
 gulp.task('createWebReleaseTest', shell.task([
     'cd ../web; gulp distCordovaTest'
 ]));
 
-gulp.task('androidProdDistUnsigned', ['createWebRelease'], shell.task([
-    'cordova platform remove android',
-    'cordova platform add android@5.1.0',
+gulp.task('androidProdDistUnsigned', ['createWebRelease', 'updateAndroidPlatformAndTutanotaPlugin'], shell.task([
     // attention: platforms/android/cordova/build does not copy the www content itself, but it was cleared and copied because of platform add/remove
     'platforms/android/cordova/build --release android'
 ]));
 
-gulp.task('androidProdDist', ['createWebRelease'], shell.task([
-    'cordova platform remove android',
-    'cordova platform add android@5.1.0',
+gulp.task('androidProdDist', ['createWebRelease', 'updateAndroidPlatformAndTutanotaPlugin'], shell.task([
     // attention: platforms/android/cordova/build does not copy the www content itself, but it was cleared and copied because of platform add/remove
     // attention: do not use "cordova build" here because it does not respect the --buildConfig
     'platforms/android/cordova/build --buildConfig /opt/next-config/android-keystore/build.json --release android'
 ]));
 
-gulp.task('androidTestDist', ['createWebReleaseTest'], shell.task([
-    'cordova platform remove android',
-    'cordova platform add android@5.1.0',
+gulp.task('androidTestDist', ['createWebReleaseTest', 'updateAndroidPlatformAndTutanotaPlugin'], shell.task([
     // attention: platforms/android/cordova/build does not copy the www content itself, but it was cleared and copied because of platform add/remove
     // attention: do not use "cordova build" here because it does not respect the --buildConfig
     'platforms/android/cordova/build --buildConfig /opt/next-config/android-keystore/build.json --release android'
