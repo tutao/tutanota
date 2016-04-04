0e946630be90fc674b8d80e132608532d52f668f
armhub <arne.moehle@tutao.de>
2016-04-04 11:05:48 +0200

    fixed randomizer

--- web/test/js/crypto/RandomizerTest.js
+++ web/test/js/crypto/RandomizerTest.js
@@ -5,7 +5,7 @@ describe("RandomizerTest", function () {
     var assert = chai.assert;
 
 
-    it(" ", function () {
+    it("RandomData ", function () {
         var r = tutao.locator.randomizer;
         for (var i = 1; i < 20; i++) {
             assert.equal(i * 2, r.generateRandomData(i).length);
