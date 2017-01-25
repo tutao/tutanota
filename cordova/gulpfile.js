58ae517b8cf4c833fe3597447d8c64961bbf7201
Bernd Deterding <bernd.deterding@tutao.de>
2017-01-25 14:51:56 +0100

    increment app version number v2.14.4

--- cordova/gulpfile.js
+++ cordova/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var runSequence = require('run-sequence');
 
-var appVersion = "2.14.0";
+var appVersion = "2.14.4";
 
 gulp.task('default', shell.task([
     // we need to remove and add the native plugin, because changes are not copied from the plugins to the platform folder during build. re-create the link from the plugins native folder to the native project afterwards
