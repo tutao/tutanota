b1d20c70cc7c7432bef7dd07b537c892f63a48d1
amoehle <amoehle@tutao.de>
2017-08-16 15:04:34 +0200

    updated translations

--- src/translations/tl.js
+++ src/translations/tl.js
@@ -1,13 +1,1 @@
-module.exports = {
-	"id": "383e7ffd8cc62e3f2cd8ef07a234fb91",
-	"name": "Tagalog",
-	"code": "tl",
-	"default": false,
-	"main": false,
-	"rtl": false,
-	"plural_forms": ["zero", "one", "other"],
-	"created_at": "2017-02-17T09:37:51Z",
-	"updated_at": "2017-02-17T09:37:51Z",
-	"source_locale": null,
-	"keys": {}
-}
+module.exports = {"id":"383e7ffd8cc62e3f2cd8ef07a234fb91","name":"Tagalog","code":"tl","default":false,"main":false,"rtl":false,"plural_forms":["zero","one","other"],"created_at":"2017-02-17T09:37:51Z","updated_at":"2017-02-17T09:37:51Z","source_locale":null,"keys":{}}
