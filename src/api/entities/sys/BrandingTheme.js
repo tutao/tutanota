9dda57b68c7b4b2a20427694c31ddf348fe4a1fc
bdeterding <bdeterding@tutao.de>
2017-08-30 10:43:22 +0200

    update model

--- src/api/entities/sys/BrandingTheme.js
+++ src/api/entities/sys/BrandingTheme.js
@@ -64,7 +64,7 @@ export const _TypeModel: TypeModel = {
 			"type": "AGGREGATION",
 			"cardinality": "Any",
 			"refType": "DisabledFeature",
-			"final": true
+			"final": false
 		}
 	},
 	"app": "sys",
--- src/api/entities/sys/DisabledFeature.js
+++ src/api/entities/sys/DisabledFeature.js
@@ -1,10 +1,40 @@
 // @flow
+import {create, TypeRef} from "../../common/EntityFunctions"
 
-import {create, TypeRef} from '../../common/EntityFunctions'
+export const DisabledFeatureTypeRef: TypeRef<DisabledFeature> = new TypeRef("sys", "DisabledFeature")
+export const _TypeModel: TypeModel = {
+	"name": "DisabledFeature",
+	"since": 24,
+	"type": "AGGREGATED_TYPE",
+	"id": 1249,
+	"rootId": "A3N5cwAE4Q",
+	"versioned": false,
+	"encrypted": false,
+	"values": {
+		"_id": {
+			"name": "_id",
+			"id": 1250,
+			"since": 24,
+			"type": "CustomId",
+			"cardinality": "One",
+			"final": true,
+			"encrypted": false
+		},
+		"feature": {
+			"name": "feature",
+			"id": 1251,
+			"since": 24,
+			"type": "String",
+			"cardinality": "One",
+			"final": false,
+			"encrypted": false
+		}
+	},
+	"associations": {},
+	"app": "sys",
+	"version": "24"
+}
 
-export const DisabledFeatureTypeRef:TypeRef<DisabledFeature> = new TypeRef("sys", "DisabledFeature")
-export const _TypeModel:TypeModel= {"name":"DisabledFeature","since":24,"type":"AGGREGATED_TYPE","id":1248,"rootId":"A3N5cwAE4A","versioned":false,"encrypted":false,"values":{"_id":{"name":"_id","id":1249,"since":24,"type":"CustomId","cardinality":"One","final":true,"encrypted":false},"feature":{"name":"feature","id":1250,"since":24,"type":"String","cardinality":"One","final":false,"encrypted":false}},"associations":{},"app":"sys","version":"24"}
-
-export function createDisabledFeature():DisabledFeature {
-    return create(_TypeModel)
+export function createDisabledFeature(): DisabledFeature {
+	return create(_TypeModel)
 }
