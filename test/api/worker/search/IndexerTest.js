6d5e2bd45a87fdb0b527e64849c5865a4870c04a
mpfau <mpfau@tutao.de>
2017-12-07 08:29:41 +0100

    test encryptSearchIndexEntries

--- test/api/worker/search/IndexerTest.js
+++ test/api/worker/search/IndexerTest.js
@@ -1,15 +1,20 @@
 // @flow
 import o from "ospec/ospec.js"
-import type {ElementData} from "../../../../src/api/worker/search/SearchTypes"
+import type {
+	ElementData,
+	SearchIndexEntry,
+	EncryptedSearchIndexEntry
+} from "../../../../src/api/worker/search/SearchTypes"
 import {_createNewIndexUpdate} from "../../../../src/api/worker/search/SearchTypes"
 import {Indexer} from "../../../../src/api/worker/search/Indexer"
 import {createContact, _TypeModel as ContactModel, ContactTypeRef} from "../../../../src/api/entities/tutanota/Contact"
 import {GENERATED_MAX_ID, GENERATED_MIN_ID} from "../../../../src/api/common/EntityFunctions"
 import {aes256RandomKey, aes256Decrypt} from "../../../../src/api/worker/crypto/Aes"
-import {uint8ArrayToBase64} from "../../../../src/api/common/utils/Encoding"
+import {uint8ArrayToBase64, utf8Uint8ArrayToString} from "../../../../src/api/common/utils/Encoding"
 import {createMail} from "../../../../src/api/entities/tutanota/Mail"
 import {createMailBody} from "../../../../src/api/entities/tutanota/MailBody"
-import {encryptIndexKey, getAppId} from "../../../../src/api/worker/search/IndexUtils"
+import {encryptIndexKey, getAppId, decryptSearchIndexEntry} from "../../../../src/api/worker/search/IndexUtils"
+import {neverNull} from "../../../../src/api/common/utils/Utils"
 
 o.spec("Indexer test", () => {
 	o("new index update", function () {
@@ -87,6 +92,108 @@ o.spec("Indexer test", () => {
 		}])
 	})
 
+
+	o("encryptSearchIndexEntries", function () {
+		const indexer = new Indexer((null:any), (null:any))
+		indexer.db.key = aes256RandomKey()
+		let id = ["1", "2"]
+		let ownerGroupId = "ownerGroupId"
+		let keyToIndexEntries: Map<string,SearchIndexEntry[]> = new Map([
+			["a", [{
+				id: "2",
+				app: 1,
+				type: 64,
+				attribute: 5,
+				positions: [0],
+			}]],
+			["b", [{
+				id: "2",
+				app: 0,
+				type: 7,
+				attribute: 4,
+				positions: [8, 27],
+			}]],
+		])
+		let indexUpdate = _createNewIndexUpdate(ownerGroupId)
+		indexer.encryptSearchIndexEntries(id, ownerGroupId, keyToIndexEntries, indexUpdate)
+
+		o(indexUpdate.create.encInstanceIdToElementData.size).equals(1)
+		let elementData: ElementData = neverNull(indexUpdate.create.encInstanceIdToElementData.get(uint8ArrayToBase64(encryptIndexKey(indexer.db.key, "2"))))
+		let listId = elementData[0]
+		o(listId).equals(id[0])
+		let words = utf8Uint8ArrayToString(aes256Decrypt(indexer.db.key, elementData[1], true))
+		o(words).equals("a b")
+		o(ownerGroupId).equals(elementData[2])
+
+		o(indexUpdate.create.indexMap.size).equals(2)
+
+		let a: EncryptedSearchIndexEntry[] = neverNull(indexUpdate.create.indexMap.get(uint8ArrayToBase64(encryptIndexKey(indexer.db.key, "a"))))
+		o(a.length).equals(1)
+		let entry = decryptSearchIndexEntry(indexer.db.key, a[0])
+		delete entry.encId
+		o(entry).deepEquals({
+			id: "2",
+			app: 1,
+			type: 64,
+			attribute: 5,
+			positions: [0],
+		})
+
+		let b: EncryptedSearchIndexEntry[] = neverNull(indexUpdate.create.indexMap.get(uint8ArrayToBase64(encryptIndexKey(indexer.db.key, "b"))))
+		o(b.length).equals(1)
+		let entry2 = decryptSearchIndexEntry(indexer.db.key, b[0])
+		delete entry2.encId
+		o(entry2).deepEquals({
+			id: "2",
+			app: 0,
+			type: 7,
+			attribute: 4,
+			positions: [8, 27],
+		})
+
+
+		// add another entry
+		let id2 = ["x", "y"]
+		let keyToIndexEntries2: Map<string,SearchIndexEntry[]> = new Map([
+			["a", [{
+				id: "y",
+				app: 0,
+				type: 34,
+				attribute: 2,
+				positions: [7, 62],
+			}]]
+		])
+		indexer.encryptSearchIndexEntries(id2, ownerGroupId, keyToIndexEntries2, indexUpdate)
+
+		o(indexUpdate.create.encInstanceIdToElementData.size).equals(2)
+		let elementData2: ElementData = neverNull(indexUpdate.create.encInstanceIdToElementData.get(uint8ArrayToBase64(encryptIndexKey(indexer.db.key, "y"))))
+		let listId2 = elementData2[0]
+		o(listId2).equals(id2[0])
+		let words2 = utf8Uint8ArrayToString(aes256Decrypt(indexer.db.key, elementData2[1], true))
+		o(words2).equals("a")
+		o(ownerGroupId).equals(elementData2[2])
+
+		a = neverNull(indexUpdate.create.indexMap.get(uint8ArrayToBase64(encryptIndexKey(indexer.db.key, "a"))))
+		o(a.length).equals(2)
+		entry = decryptSearchIndexEntry(indexer.db.key, a[0])
+		delete entry.encId
+		o(entry).deepEquals({
+			id: "2",
+			app: 1,
+			type: 64,
+			attribute: 5,
+			positions: [0],
+		})
+		let newEntry = decryptSearchIndexEntry(indexer.db.key, a[1])
+		delete newEntry.encId
+		o(newEntry).deepEquals({
+			id: "y",
+			app: 0,
+			type: 34,
+			attribute: 2,
+			positions: [7, 62],
+		})
+	})
 
 	o("createNoMailIndexData", function () {
 		let update = _createNewIndexUpdate("groupId")
