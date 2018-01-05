042627962cb89dd2e6aca2e5c78291d974721f0a
mpfau <mpfau@tutao.de>
2018-01-05 10:40:49 +0100

    fixed test

--- test/client/common/LanguageViewModelTest.js
+++ test/client/common/LanguageViewModelTest.js
@@ -4,9 +4,10 @@ import {lang} from "../../../src/misc/LanguageViewModel"
 import en from "../../../src/translations/en"
 
 o.spec("LanguageViewModelTests", function () {
-	o("en is default language", browser(() => {
+	o("en is default language", browser((done, timeout) => {
+		timeout(500)
 		return lang.init(en).then(() => {
 			o(lang.fallback).equals(en)
-		})
+		}).then(done)
 	}))
 })
