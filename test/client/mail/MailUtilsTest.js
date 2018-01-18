c898ec92c0d5ce66c06b58403a829e9c4d69a798
amoehle <amoehle@tutao.de>
2018-01-18 14:24:29 +0100

    fixed test cases for createRecipientInfo()

--- test/client/mail/MailUtilsTest.js
+++ test/client/mail/MailUtilsTest.js
@@ -1,9 +1,9 @@
 //@flow
 import o from "ospec/ospec.js"
-import {ContactAddressType, GroupType} from "../../../src/api/common/TutanotaConstants"
+import {GroupType} from "../../../src/api/common/TutanotaConstants"
 import {createRecipientInfo, parseMailtoUrl} from "../../../src/mail/MailUtils"
 import {logins} from "../../../src/api/main/LoginController"
-import {neverNull} from "../../../src/api/common/utils/Utils"
+import {recipientInfoType} from "../../../src/api/common/RecipientInfo"
 
 
 o.spec("MailUtils", browser(function () {
@@ -19,39 +19,23 @@ o.spec("MailUtils", browser(function () {
 	})
 
 	o("create contact from recipient info", function () {
-		let r1 = neverNull(createRecipientInfo("schneier@secure.com", "B. Schneier", null, true))
-		o(neverNull(r1.contact)).notEquals(null)
-		o(neverNull(r1.contact)._owner).equals('userId')
-		o(neverNull(r1.contact)._ownerGroup).equals('groupId')
-		o(neverNull(r1.contact).mailAddresses.length).equals(1)
-		o(neverNull(r1.contact).mailAddresses[0].address).equals("schneier@secure.com")
-		o(neverNull(r1.contact).mailAddresses[0].type).equals(ContactAddressType.OTHER)
-		o(neverNull(r1.contact).firstName).equals("B.")
-		o(neverNull(r1.contact).lastName).equals("Schneier")
+		let r1 = createRecipientInfo("schneier@secure.com", "B. Schneier", null, true)
+		o(r1.mailAddress).equals("schneier@secure.com")
+		o(r1.name).equals("B. Schneier")
+		o(r1.type).equals(recipientInfoType.unknown)
+		o(r1.contact).equals(null)
 
-		let r2 = createRecipientInfo("schneier@secure.com", "Bruce", null, true)
-		o(neverNull(r2.contact).firstName).equals("Bruce")
-		o(neverNull(r2.contact).lastName).equals("")
+		let r2 = createRecipientInfo("schneier@tutanota.com", "B. Schneier", null, true)
+		o(r2.mailAddress).equals("schneier@tutanota.com")
+		o(r2.name).equals("B. Schneier")
+		o(r2.type).equals(recipientInfoType.internal)
+		o(r2.contact).equals(null)
 
-		var r3 = createRecipientInfo("schneier@secure.com", "B M A", null, true)
-		o(neverNull(r3.contact).firstName).equals("B")
-		o(neverNull(r3.contact).lastName).equals("M A")
-
-		var r4 = createRecipientInfo("schneier@secure.com", "", null, true)
-		o(neverNull(r4.contact).firstName).equals("Schneier")
-		o(neverNull(r4.contact).lastName).equals("")
-
-		var r5 = createRecipientInfo("bruce.schneier@secure.com", "", null, true)
-		o(neverNull(r5.contact).firstName).equals("Bruce")
-		o(neverNull(r5.contact).lastName).equals("Schneier")
-
-		var r6 = createRecipientInfo("bruce_schneier_schneier@secure.com", "", null, true)
-		o(neverNull(r6.contact).firstName).equals("Bruce")
-		o(neverNull(r6.contact).lastName).equals("Schneier Schneier")
-
-		var r7 = createRecipientInfo("bruce-schneier@secure.com", "", null, true)
-		o(neverNull(r7.contact).firstName).equals("Bruce")
-		o(neverNull(r7.contact).lastName).equals("Schneier")
+		let r3 = createRecipientInfo("schneier@tutanota.com", null, null, true)
+		o(r3.mailAddress).equals("schneier@tutanota.com")
+		o(r3.name).equals("")
+		o(r3.type).equals(recipientInfoType.internal)
+		o(r3.contact).equals(null)
 	})
 
 
