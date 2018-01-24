77729c34e0952dca04d64944b49cb3d751e80ec9
bdeterding <bdeterding@tutao.de>
2018-01-24 12:28:34 +0100

    return new default contact form language if language list is empty,  fixes #219

--- src/contacts/ContactFormUtils.js
+++ src/contacts/ContactFormUtils.js
@@ -1,13 +1,18 @@
 //@flow
 import {lang} from "../misc/LanguageViewModel"
+import {createContactFormLanguage} from "../api/entities/tutanota/ContactFormLanguage"
 
-export function getDefaultContactFormLanguage(languages: ContactFormLanguage[]):ContactFormLanguage {
+export function getDefaultContactFormLanguage(languages: ContactFormLanguage[]): ContactFormLanguage {
 	let language = languages.find(l => l.code == lang.code)
 	if (!language) {
 		language = languages.find(l => l.code == 'en')
 	}
 	if (!language) {
 		language = languages[0]
+	}
+	if (!language) { // FIXME: only needed for existing contact forms remove after all existing contact forms have been saved.
+		language = createContactFormLanguage()
+		language.code = lang.code
 	}
 	return language
 }
