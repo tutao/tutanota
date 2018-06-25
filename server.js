e3e59278cea0e9696a483375b3ac42db337ebcc2
ivk <ivk@tutao.de>
2018-06-25 13:40:10 +0200

    Local server improvements

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const port = 9000
 const nodeStatic = require('node-static')
 const file = new nodeStatic.Server('build', {cache: false, gzip: true});
 const http = require('http')
-
-var fs = require('fs');
+const fs = require('fs')
+const os = require('os')
 
 const server = http.createServer(function (req, res) {
 	file.serve(req, res, (err, result) => {
+		console.log("req from " + req.connection.remoteAddress)
 		if (err && err.status === 404) {
 			console.log(req.url + " not found -> reset to root url")
 			res.statusCode = 302;
-			res.setHeader('Location', `http://localhost:${port}/?r=${req.url.replace(/\?/g, "&")}`);
+			res.setHeader('Location', `http://${os.hostname()}:${port}/?r=${req.url.replace(/\?/g, "&")}`);
 			res.end();
 		}
 	});
@@ -22,4 +23,5 @@ const server = http.createServer(function (req, res) {
 
 require('chokidar-socket-emitter')({app: server, path: 'build', relativeTo: 'build'})
 
+console.log(`Static server for build is running on ${os.hostname()}:${port}`)
 server.listen(port)
