41db9fce351fb4cfa672c53db5670ffbcdea3ff0
ivk <ivk@tutao.de>
2018-07-18 16:07:25 +0200

    Add noscript message.

--- buildSrc/LaunchHtml.js
+++ buildSrc/LaunchHtml.js
@@ -22,50 +22,60 @@ module.exports.renderHtml = function (scripts, env) {
 	const m = require('mithril')
 	const render = require('mithril-node-render')
 	let html = '<!DOCTYPE html>\n' + render(
-			m("html", [
-				m("head", [
-					m("meta[charset=utf-8]"),
-					env.dist && env.mode === "App" ? m(`meta[http-equiv=Content-Security-Policy][content=default-src 'self'; img-src http: data: *; style-src 'unsafe-inline'; connect-src 'self' ${getUrls(env)};]`) : null,
-					m("meta[name=apple-mobile-web-app-capable][content=yes]"),
-					m("meta[name=mobile-web-app-capable][content=yes]"),
-					m("meta[name=referrer][content=no-referrer]"),
-					scripts.map(script => m(`script[src=${script}][defer]`)),
-					m.trust("<!-- TutanotaTags -->"), // everything from here to </head> is replaced at runtime for custom domains with defined metaTags
-					m("title", "Tutanota"),
-					m("meta[name=description][content=Secure email for everyone: Get your encrypted mailbox for free. Now you can show the online spies that you won&#39;t make it easy for them.]"),
-					m("link [rel=shortcut icon][type=image/x-icon][href=/images/logo-favicon-152.png]"),
-					m("meta[name=application-name][content=Tutanota]"),
-					m("link[rel=apple-touch-icon][sizes=152x152][href=/images/logo-favicon-152.png]"),
-					m("link[rel=icon][sizes=192x192][href=/images/logo-favicon-192.png]"),
-					m("meta[name=viewport][content=width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no]"),
+		m("html", [
+			m("head", [
+				m("meta[charset=utf-8]"),
+				cors(env),
+				m("meta[name=apple-mobile-web-app-capable][content=yes]"),
+				m("meta[name=mobile-web-app-capable][content=yes]"),
+				m("meta[name=referrer][content=no-referrer]"),
+				scripts.map(script => m(`script[src=${script}][defer]`)),
+				m.trust("<!-- TutanotaTags -->"), // everything from here to </head> is replaced at runtime for custom domains with defined metaTags
+				m("title", "Tutanota"),
+				m("meta[name=description][content=Secure email for everyone: Get your encrypted mailbox for free. Now you can show the online spies that you won&#39;t make it easy for them.]"),
+				m("link [rel=shortcut icon][type=image/x-icon][href=/images/logo-favicon-152.png]"),
+				m("meta[name=application-name][content=Tutanota]"),
+				m("link[rel=apple-touch-icon][sizes=152x152][href=/images/logo-favicon-152.png]"),
+				m("link[rel=icon][sizes=192x192][href=/images/logo-favicon-192.png]"),
+				m("meta[name=viewport][content=width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no]"),
 
-					// twitter
-					m("meta[name=twitter:card][content=summary]"),
-					m("meta[name=twitter:site][content=@TutanotaTeam]"),
-					m("meta[name=twitter:domain][content=tutanota.com]"),
-					m("meta[name=twitter:image][content=https://tutanota.com/images/share_image.png]"),
+				// twitter
+				m("meta[name=twitter:card][content=summary]"),
+				m("meta[name=twitter:site][content=@TutanotaTeam]"),
+				m("meta[name=twitter:domain][content=tutanota.com]"),
+				m("meta[name=twitter:image][content=https://tutanota.com/images/share_image.png]"),
 
-					// facebook
-					m("meta[stream=og:site_name][content=Tutanota]"),
-					m("meta[stream=og:title][content=Secure Emails Become a Breeze]"),
-					m("meta[stream=og:description][content=Get your encrypted mailbox for free and show the Internet spies that you won&#39;t make it easy for them! Why? Because you simply can.]"),
-					m("meta[stream=og:locale][content=en_US]"),
-					m("meta[stream=og:url][content=https://tutanota.com/]"),
-					m("meta[stream=og:image][content=https://tutanota.com/images/share_image.png]"),
-					m("meta[stream=article:publisher][content=https://www.facebook.com/tutanota]"),
+				// facebook
+				m("meta[stream=og:site_name][content=Tutanota]"),
+				m("meta[stream=og:title][content=Secure Emails Become a Breeze]"),
+				m("meta[stream=og:description][content=Get your encrypted mailbox for free and show the Internet spies that you won&#39;t make it easy for them! Why? Because you simply can.]"),
+				m("meta[stream=og:locale][content=en_US]"),
+				m("meta[stream=og:url][content=https://tutanota.com/]"),
+				m("meta[stream=og:image][content=https://tutanota.com/images/share_image.png]"),
+				m("meta[stream=article:publisher][content=https://www.facebook.com/tutanota]"),
 
-					// google +
-					m("meta[itemprop=name][content=Secure Emails Become a Breeze.]"),
-					m("meta[itemprop=description][content=Get your encrypted mailbox for free and show the Internet spies that you won&#39;t make it easy for them! Why? Because you simply can.]"),
-					m("meta[itemprop=image][content=https://tutanota.com/images/share_image.png]"),
+				// google +
+				m("meta[itemprop=name][content=Secure Emails Become a Breeze.]"),
+				m("meta[itemprop=description][content=Get your encrypted mailbox for free and show the Internet spies that you won&#39;t make it easy for them! Why? Because you simply can.]"),
+				m("meta[itemprop=image][content=https://tutanota.com/images/share_image.png]"),
 
-					m("meta[name=apple-itunes-app][content=app-id=id922429609, affiliate-data=10lSfb]"),
-				]),
-				m("body")
-			])
-		)
+				m("meta[name=apple-itunes-app][content=app-id=id922429609, affiliate-data=10lSfb]"),
+			]),
+			m("body", m("noscript",
+				"Tutanota requires javascript to be enabled. Please, activate it in the settings of your browser."))
+		])
+	)
 	global.window = undefined // we have to reset the window stream as it leads to problems with system js builder, otherwise
 	return html
+}
+
+const cors = (env) => {
+	if (env.dist && env.mode === "App") {
+		m("meta[http-equiv=Content-Security-Policy][content=default-src 'self'; img-src http: data: *; " +
+			`style-src 'unsafe-inline'; connect-src 'self' ${getUrls(env)};]`)
+	} else {
+		null
+	}
 }
 
 module.exports.renderTestHtml = function (scripts) {
@@ -73,15 +83,15 @@ module.exports.renderTestHtml = function (scripts) {
 	const m = require('mithril')
 	const render = require('mithril-node-render')
 	let html = '<!DOCTYPE html>\n' + render(
-			m("html", [
-				m("head", [
-					m("meta[charset=utf-8]"),
-					m("title", "Test"),
-					scripts.map(script => m(`script[src=${script}]`))
-				]),
-				m("body", "Open the console (F12) for test output!")
-			])
-		)
+		m("html", [
+			m("head", [
+				m("meta[charset=utf-8]"),
+				m("title", "Test"),
+				scripts.map(script => m(`script[src=${script}]`))
+			]),
+			m("body", "Open the console (F12) for test output!")
+		])
+	)
 	global.window = undefined // we have to reset the window stream as it leads to problems with system js builder, otherwise
 	return html
 }
