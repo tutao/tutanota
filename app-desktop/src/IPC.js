f387f07dae4705e071b48ed22559737253a39a89
nig <nig@tutao.de>
2018-10-10 09:12:27 +0200

    merged remote into local

--- app-desktop/src/IPC.js
+++
@@ -1,8 +0,0 @@
-const ipc = require('electron').ipcMain
-
-exports.init = (window) => {
-	exports.send = (...args) => window.webContents.send.apply(window.webContents, args)
-	exports.on = (...args) => ipc.on.apply(ipc, args)
-}
-exports.send = () => {console.log("ipc not initialized!")}
-exports.on = () => {console.log("ipc not initialized!")}
