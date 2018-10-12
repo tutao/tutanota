02ed6f1996308c2651993084f475966d27f3b767
mpfau <mpfau@tutao.de>
2018-10-12 13:36:59 +0200

    fixed update url for snapshot

--- dist.js
+++ dist.js
@@ -180,12 +180,12 @@ function buildDesktopClient() {
 	if (options.desktop) {
 		if (options.host === undefined) {
 			return createHtml(env.create(SystemConfig.distRuntimeConfig(bundles), "https://mail.tutanota.com", version, "Desktop", true), bundles)
-				.then(() => desktopBuilder.build(__dirname, packageJSON.version, options.desktop, "https://mail.tutanota.com", ""))
+				.then(() => desktopBuilder.build(__dirname, packageJSON.version, options.desktop, "https://mail.tutanota.com", "https://mail.tutanota.com/desktop", ""))
 				.then(() => createHtml(env.create(SystemConfig.distRuntimeConfig(bundles), "https://test.tutanota.com", version, "Desktop", true), bundles))
-				.then(() => desktopBuilder.build(__dirname, packageJSON.version, options.desktop, "https://test.tutanota.com", "-test"))
+				.then(() => desktopBuilder.build(__dirname, packageJSON.version, options.desktop, "https://test.tutanota.com", "https://test.tutanota.com/desktop-test", "-test"))
 		} else {
 			return createHtml(env.create(SystemConfig.distRuntimeConfig(bundles), targetUrl, version, "Desktop", true), bundles)
-				.then(() => desktopBuilder.build(__dirname, packageJSON.version, options.desktop, targetUrl, "-snapshot"))
+				.then(() => desktopBuilder.build(__dirname, packageJSON.version, options.desktop, targetUrl, "https://next.tutao.de/desktop-snapshot", "-snapshot"))
 		}
 	}
 }
