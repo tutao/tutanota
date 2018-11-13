4a7336e8a575f81b2bfe7a3bff550f263f56345f
ivk <ivk@tutao.de>
2018-11-13 11:19:17 +0100

    Fix dropdown resize double animation, #761

--- src/gui/base/Dropdown.js
+++ src/gui/base/Dropdown.js
@@ -286,20 +286,20 @@ export class Dropdown {
 			)
 
 			// We would prefer to cancel current animation but we don't have infrastructure for this yet
-			animations.add(this._domDropdown, [
+			return animations.add(this._domDropdown, [
 				width(0, this._width),
 				height(0, this.maxHeight)
 			], {easing: ease.out, duration: animate ? DefaultAnimationTime : 0})
-			          .then(() => {
-				          if (this.maxHeight - this._getFilterHeight() < contentsHeight) {
-					          // do not show the scrollbar during the animation.
-					          this._domContents.style.maxHeight = px(this.maxHeight - this._getFilterHeight())
-					          this._domContents.style.overflowY = client.overflowAuto
-				          }
-				          if (this._domInput) {
-					          this._domInput.focus()
-				          }
-			          })
+			                 .then(() => {
+				                 if (this.maxHeight - this._getFilterHeight() < contentsHeight) {
+					                 // do not show the scrollbar during the animation.
+					                 this._domContents.style.maxHeight = px(this.maxHeight - this._getFilterHeight())
+					                 this._domContents.style.overflowY = client.overflowAuto
+				                 }
+				                 if (this._domInput) {
+					                 this._domInput.focus()
+				                 }
+			                 })
 		}
 	}
 
