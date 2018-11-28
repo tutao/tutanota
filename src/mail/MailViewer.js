b99b1d38603e85ab83e70dd599cc35aa2a2446de
nig <nig@tutao.de>
2018-11-28 11:12:08 +0100

    added tests for window.print actually being a function. closes #850

--- src/mail/MailViewer.js
+++ src/mail/MailViewer.js
@@ -237,7 +237,7 @@ export class MailViewer {
 				if (!this._isAnnouncement() && !client.isMobileDevice() && !logins.isEnabled(FeatureType.DisableMailExport)) {
 					moreButtons.push(new Button("export_action", () => exportAsEml(this.mail, this._htmlBody), () => Icons.Download).setType(ButtonType.Dropdown))
 				}
-				if (!client.isMobileDevice() && !logins.isEnabled(FeatureType.DisableMailExport)) {
+				if (!client.isMobileDevice() && !logins.isEnabled(FeatureType.DisableMailExport) && typeof window.print === "function") {
 					moreButtons.push(new Button("print_action", () => window.print(), () => Icons.Print).setType(ButtonType.Dropdown))
 				}
 				if (this.mail.listUnsubscribe) {
