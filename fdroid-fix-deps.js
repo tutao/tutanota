131e95397280e81b177f3da6e0f950795b9ba6d6
ivk <ivk@tutao.de>
2019-02-05 14:17:42 +0100

    [android] Add script to remove binary dependencies for F-Droid

---
+++ fdroid-fix-deps.js
@@ -0,0 +1,25 @@
+// Script which removes dependencies with binaries which raise F-Droid alarms.
+
+const fs = require('fs')
+const packageJson = require('./package.json')
+
+;[
+	"electron-builder",
+	'electron',
+	"electron-localshortcut",
+	"electron-packager",
+	"electron-updater",
+	"express",
+	"flow-bin",
+	"request",
+	"chokidar",
+	"body-parser",
+	"rcedit",
+	"winreg",
+	"node-forge",
+	"js-yaml"
+].forEach((dep) => {
+	delete packageJson.devDependencies[dep]
+})
+
+fs.writeFileSync("package.json", JSON.stringify(packageJson, null, "  "))
