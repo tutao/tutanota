05810d579f97aa40714734fca18a932803eb16eb
nig <nig@tutao.de>
2019-02-26 12:35:15 +0100

    count bytes while encrypting files, #1007

--- src/native/AesApp.js
+++ src/native/AesApp.js
@@ -4,13 +4,19 @@ import {nativeApp} from "./NativeWrapper"
 import {Request} from "../api/common/WorkerProtocol"
 import {keyToBase64} from "../api/worker/crypto/CryptoUtils"
 
+export type EncryptedFileInfo = {|
+	uri: string,
+	unencSize: number
+|}
+
 /**
  * Encrypts a file with the provided key
  * @return Returns the URI of the decrypted file. Resolves to an exception if the encryption failed.
  */
-export function aesEncryptFile(key: Aes128Key, fileUrl: string, iv: Uint8Array): Promise<string> {
+export function aesEncryptFile(key: Aes128Key, fileUrl: string, iv: Uint8Array): Promise<EncryptedFileInfo> {
 	let encodedKey = keyToBase64(key)
 	return nativeApp.invokeNative(new Request('aesEncryptFile', [encodedKey, fileUrl, uint8ArrayToBase64(iv)]))
+	                .then(json => JSON.parse(json))
 }
 
 /**
