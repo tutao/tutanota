eb9b953ef03949b11e8f56b67fb6dfd2d8339592
ivk <ivk@tutao.de>
2019-04-05 17:10:00 +0200

    Fix overlays colliding with each other, fix #1206

--- src/gui/base/Overlay.js
+++ src/gui/base/Overlay.js
@@ -22,11 +22,12 @@ type OverlayAttrs = {
 	closeAnimation?: AnimationProvider;
 }
 
-const overlays: Array<[OverlayAttrs, ?HTMLElement]> = []
+const overlays: Array<[OverlayAttrs, ?HTMLElement, number]> = []
 const boxShadow = (() => {
 	const {r, g, b} = hexToRgb(theme.modal_bg)
 	return `0 2px 12px rgba(${r}, ${g}, ${b}, 0.4), 0 10px 40px rgba(${r}, ${g}, ${b}, 0.3)`
 })()
+let key = 0
 
 export function displayOverlay(position: PositionRect, component: Component, createAnimation?: AnimationProvider,
                                closeAnimation?: AnimationProvider): () => void {
@@ -36,7 +37,7 @@ export function displayOverlay(position: PositionRect, component: Component, cre
 		createAnimation,
 		closeAnimation
 	}
-	const pair = [newAttrs, null]
+	const pair = [newAttrs, null, key++]
 	overlays.push(pair)
 	return () => {
 		const dom = pair[1];
@@ -53,9 +54,10 @@ export const overlay = {
 		style: {
 			display: overlays.length > 0 ? "" : 'none' // display: null not working for IE11
 		}
-	}, overlays.map((pair) => {
-		const [attrs] = pair
+	}, overlays.map((overlayAttrs) => {
+		const [attrs, dom, key] = overlayAttrs
 		return m(".abs.list-bg", {
+			key,
 			style: {
 				width: attrs.position.width,
 				top: attrs.position.top,
@@ -67,13 +69,13 @@ export const overlay = {
 				'box-shadow': boxShadow,
 			},
 			oncreate: (vnode: Vnode<any>) => {
-				pair[1] = vnode.dom
+				overlayAttrs[1] = vnode.dom
 				if (attrs.createAnimation) {
 					animations.add(vnode.dom, attrs.createAnimation(vnode.dom))
 				}
 			},
 			onremove: () => {
-				pair[1] = null
+				overlayAttrs[1] = null
 			}
 		}, m(attrs.component))
 	}))
