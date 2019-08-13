0628b21ec125ce4285d3367ed1aa4482e7c1ff68
bdeterding <bdeterding@tutao.de>
2019-08-13 17:26:45 +0200

    added missing translation to edit calendar dialog.

--- src/translations/en.js
+++ src/translations/en.js
@@ -1006,6 +1006,7 @@ module.exports = {
 		"yes_label": "Yes",
 		"yourCalendars_label": "Your calendars",
 		"yourFolders_action": "YOUR FOLDERS",
-		"yourMessage_label": "Your message"
+		"yourMessage_label": "Your message",
+		"color_label": "Color"
 	}
 }
