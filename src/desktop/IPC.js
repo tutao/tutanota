5bf7ae58f9a0f43e8394a1f15c4d4269370d0c7a
ivk <ivk@tutao.de>
2019-09-26 10:51:42 +0200

    Add logs for the desktop client, #1510

--- src/desktop/IPC.js
+++ src/desktop/IPC.js
@@ -168,6 +168,9 @@ export class IPC {
 				this._sock.sendSocketMessage(args[0])
 				d.resolve()
 				break
+			case 'getLog':
+				d.resolve(global.logger.getEntries())
+				break
 			default:
 				d.reject(new Error(`Invalid Method invocation: ${method}`))
 				break
