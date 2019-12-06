e89b730cef85239d39a27e8f92ad5035f505740d
ivk <ivk@tutao.de>
2019-12-06 17:29:52 +0100

    Do not change header title when selecting item in right column

--- src/gui/base/Header.js
+++ src/gui/base/Header.js
@@ -168,10 +168,8 @@ class Header {
 		if (this._searchBarVisible()) {
 			return this._searchBar()
 		} else if (viewSlider) {
-			// We to keep the title of the first visible background column so that it doesn't change when opening drawer
-			const title = viewSlider.focusedColumn.columnType === 1 // background
-				? viewSlider.focusedColumn.getTitle()
-				: viewSlider.getBackgroundColumns()[0].getTitle()
+			const fistVisibleBgColumn = viewSlider.getBackgroundColumns().find(c => c.visible)
+			const title = fistVisibleBgColumn ? fistVisibleBgColumn.getTitle() : ""
 			return header(title)
 		} else if (m.route.get().startsWith('/login')) {
 			return header(lang.get("login_label"))
