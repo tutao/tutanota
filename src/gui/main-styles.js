96c419dab9f50ac3b81b6be4c443e0656d4e80dd
ivk <ivk@tutao.de>
2020-05-26 11:20:51 +0200

    Adjust the font stack, #1909

--- src/gui/main-styles.js
+++ src/gui/main-styles.js
@@ -16,7 +16,11 @@ export function requiresStatusBarHack() {
 }
 
 function getFonts(): string {
-	const fonts: Array<string> = ['-apple-system', 'BlinkMacSystemFont', 'Helvetica', 'Arial', 'sans-serif']
+	// see https://bitsofco.de/the-new-system-font-stack/
+	const fonts: Array<string> = [
+		'-apple-system', 'system-ui', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'
+	]
+	// workaround for incorrect Japanese font see https://github.com/tutao/tutanota/issues/1909
 	if (env.platformId === 'win32' && lang.code === 'ja') fonts.push('SimHei', "黑体")
 	fonts.push("Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol")
 	return fonts.join(', ')
