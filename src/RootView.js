0bc89577e751d41d779f004a9a9509300ded1b84
pas <pas@tutao.de>
2020-12-28 13:40:29 +0100

    fix "disabling outline" being console logged infinitely, fix #2486

--- src/RootView.js
+++ src/RootView.js
@@ -16,18 +16,20 @@ class RootView {
 
 		// On first mouse event disable outline. This is a compromise between keyboard navigation users and mouse users.
 		let onmousedown = (e) => {
-			console.log("disabling outline")
-			styles.registerStyle("outline", () => ({
-				"*": {
-					outline: "none",
-				}
-			}))
-			// remove event listener after the first click to not re-register style
-			onmousedown = null
-			// It is important to not redraw at this point because click event may be lost otherwise and saved login button would not be
-			// actually pressed. It's unclear why but preventing redraw (this way or setting listener manually) helps.
-			// It's also useless to redraw for this click handler because we just want to add a global style definition.
-			e.redraw = false
+			if (onmousedown) {
+				console.log("disabling outline")
+				styles.registerStyle("outline", () => ({
+					"*": {
+						outline: "none",
+					}
+				}))
+				// remove event listener after the first click to not re-register style
+				onmousedown = null
+				// It is important to not redraw at this point because click event may be lost otherwise and saved login button would not be
+				// actually pressed. It's unclear why but preventing redraw (this way or setting listener manually) helps.
+				// It's also useless to redraw for this click handler because we just want to add a global style definition.
+				e.redraw = false
+			}
 		}
 
 
