480028d3b8c5b7af08f119bbe4b907341c602391
ivk <ivk@tutao.de>
2021-01-28 13:32:45 +0100

    Fix bump-version.js

--- bump-version.js
+++ bump-version.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-const options = require('commander')
-const {spawnSync} = require("child_process")
-const fs = require("fs")
+import options from "commander"
+
+import {spawnSync} from "child_process"
+import fs from "fs"
 
 options
 	.usage('<minor|patch>', "major.minor.patch, default is patch")
@@ -15,32 +16,36 @@ if (!["minor", "patch"].includes(which)) {
 	process.exit(1)
 }
 
-const currentVersionString = require("./package.json").version
-const currentVersion = currentVersionString.split(".").map((n) => parseInt(n, 10))
-const newVersion = currentVersion.slice()
-if (which === "patch") {
-	newVersion[2]++
-} else {
-	newVersion[1]++
-	newVersion[2] = 0
+run()
+
+async function run() {
+	const currentVersionString = JSON.parse(await fs.promises.readFile("./package.json", {encoding: "utf8"})).version
+	const currentVersion = currentVersionString.split(".").map((n) => parseInt(n, 10))
+	const newVersion = currentVersion.slice()
+	if (which === "patch") {
+		newVersion[2]++
+	} else {
+		newVersion[1]++
+		newVersion[2] = 0
+	}
+	const newVersionString = newVersion.join(".")
+
+	spawnSync("npm", ["version", "--no-git-tag-version", which])
+
+	const infoPlistName = "app-ios/tutanota/Info.plist"
+	const infoPlistContents = fs.readFileSync(infoPlistName, "utf8")
+	const newInfoPlistContents = infoPlistContents.replace(new RegExp(currentVersion.join("\\."), "g"), newVersionString)
+	fs.writeFileSync(infoPlistName, newInfoPlistContents)
+
+	const buildGradleName = "app-android/app/build.gradle"
+	const newAndroidVersion = versionToBuildNumber(newVersion)
+	const buildGradleString = fs.readFileSync(buildGradleName, "utf8")
+	                            .replace(new RegExp(currentVersion.join("\\.")), newVersionString)
+	                            .replace(new RegExp(versionToBuildNumber(currentVersion)), newAndroidVersion)
+	fs.writeFileSync(buildGradleName, buildGradleString)
+
+	console.log(`Bumped version to ${newVersionString} ${newAndroidVersion} (was ${currentVersionString})`)
 }
-const newVersionString = newVersion.join(".")
-
-spawnSync("npm", ["version", "--no-git-tag-version", which])
-
-const infoPlistName = "app-ios/tutanota/Info.plist"
-const infoPlistContents = fs.readFileSync(infoPlistName, "utf8")
-const newInfoPlistContents = infoPlistContents.replace(new RegExp(currentVersion.join("\\."), "g"), newVersionString)
-fs.writeFileSync(infoPlistName, newInfoPlistContents)
-
-const buildGradleName = "app-android/app/build.gradle"
-const newAndroidVersion = versionToBuildNumber(newVersion)
-const buildGradleString = fs.readFileSync(buildGradleName, "utf8")
-                            .replace(new RegExp(currentVersion.join("\\.")), newVersionString)
-                            .replace(new RegExp(versionToBuildNumber(currentVersion)), newAndroidVersion)
-fs.writeFileSync(buildGradleName, buildGradleString)
-
-console.log(`Bumped version to ${newVersionString} ${newAndroidVersion} (was ${currentVersionString})`)
 
 function pad(num, size) {
 	let s = num + "";
