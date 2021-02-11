tutao/tutanota

commit 14755b1ac215a905a4fe53452e0cdc174b15fbb9

fix worker initialization error on old chromium #2694

src/app.js
@@ -52,6 +52,12 @@ client.init(navigator.userAgent, navigator.platform)
 
 export const state: {prefix: ?string, prefixWithoutFile: ?string} = (module.hot && module.hot.data)
 	? downcast(module.hot.data.state) : {prefix: null, prefixWithoutFile: null}
+if (state.prefix == null) {
+	const prefix = state.prefix = location.pathname[location.pathname.length - 1] !== '/'
+		? location.pathname
+		: location.pathname.substring(0, location.pathname.length - 1)
+	state.prefixWithoutFile = prefix.includes(".") ? prefix.substring(0, prefix.lastIndexOf("/")) : prefix
+}
 
 // Write it here for the WorkerClient so that it can load relative worker easily. Should do it here so that it doesn't break after HMR.
 window.tutao.appState = state
@@ -183,11 +189,6 @@ let initialized = import("./translations/en").then((en) => lang.init(en.default)
 
 	let start = "/"
 	if (state.prefix == null) {
-		const prefix = state.prefix = location.pathname[location.pathname.length - 1] !== '/'
-			? location.pathname
-			: location.pathname.substring(0, location.pathname.length - 1)
-		state.prefixWithoutFile = prefix.includes(".") ? prefix.substring(0, prefix.lastIndexOf("/")) : prefix
-
 		let query = m.parseQueryString(location.search)
 		let redirectTo = query['r'] // redirection triggered by the server (e.g. the user reloads /mail/id by pressing F5)
 		if (redirectTo) {

dist.js
@@ -144,7 +144,7 @@ async function buildWebapp(version) {
 	await fs.copy(path.join(__dirname, '/resources/favicon'), path.join(__dirname, 'build/dist/images'))
 	await fs.copy(path.join(__dirname, '/src/braintree.html'), path.join(__dirname, '/build/dist/braintree.html'))
 
-	console.log("stared bundling", measure())
+	console.log("started bundling", measure())
 	const bundle = await rollup({
 		input: ["src/app.js", "src/api/worker/worker.js"],
 		preserveEntrySignatures: false,

commit 4926f47944b87abaa00cba4d7676131b141d3895

Fix redirect processing
was broken by 14755b1a

src/app.js
@@ -52,11 +52,29 @@ client.init(navigator.userAgent, navigator.platform)
 
 export const state: {prefix: ?string, prefixWithoutFile: ?string} = (module.hot && module.hot.data)
 	? downcast(module.hot.data.state) : {prefix: null, prefixWithoutFile: null}
+
+let startRoute = "/"
 if (state.prefix == null) {
 	const prefix = state.prefix = location.pathname[location.pathname.length - 1] !== '/'
 		? location.pathname
 		: location.pathname.substring(0, location.pathname.length - 1)
 	state.prefixWithoutFile = prefix.includes(".") ? prefix.substring(0, prefix.lastIndexOf("/")) : prefix
+
+	let query = m.parseQueryString(location.search)
+	let redirectTo = query['r'] // redirection triggered by the server (e.g. the user reloads /mail/id by pressing F5)
+	if (redirectTo) {
+		delete query['r']
+	} else {
+		redirectTo = ""
+	}
+	let newQueryString = m.buildQueryString(query)
+	if (newQueryString.length > 0) {
+		newQueryString = "?" + newQueryString
+	}
+	let target = redirectTo + newQueryString + location.hash
+	if (target === "" || target[0] !== "/") target = "/" + target
+	history.replaceState(null, "", neverNull(state.prefix) + target)
+	startRoute = target
 }
 
 // Write it here for the WorkerClient so that it can load relative worker easily. Should do it here so that it doesn't break after HMR.
@@ -187,29 +205,11 @@ let initialized = import("./translations/en").then((en) => lang.init(en.default)
 	const calendarViewResolver = createViewResolver(() => import("./calendar/view/CalendarView.js")
 		.then(module => new module.CalendarView()), true)
 
-	let start = "/"
-	if (state.prefix == null) {
-		let query = m.parseQueryString(location.search)
-		let redirectTo = query['r'] // redirection triggered by the server (e.g. the user reloads /mail/id by pressing F5)
-		if (redirectTo) {
-			delete query['r']
-		} else {
-			redirectTo = ""
-		}
-		let newQueryString = m.buildQueryString(query)
-		if (newQueryString.length > 0) {
-			newQueryString = "?" + newQueryString
-		}
-		let target = redirectTo + newQueryString + location.hash
-		if (target === "" || target[0] !== "/") target = "/" + target
-		history.replaceState(null, "", neverNull(state.prefix) + target)
-		start = target
-	}
 	m.route.prefix = neverNull(state.prefix)
 	styles.init()
 
 	// keep in sync with RewriteAppResourceUrlHandler.java
-	m.route(neverNull(document.body), start, {
+	m.route(neverNull(document.body), startRoute, {
 		"/": {
 			onmatch: (args, requestedPath) => forceLogin(args, requestedPath)
 		},