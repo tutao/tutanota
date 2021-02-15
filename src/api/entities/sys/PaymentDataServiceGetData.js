bab620014a1ef2df300ce55c248ac90a4ceb3d63
jow <jow@tutao.de>
2021-02-15 14:36:51 +0100

    Add PaymentDataServiceGetData

---
+++ src/api/entities/sys/PaymentDataServiceGetData.js
@@ -0,0 +1,46 @@
+// @flow
+
+import {create} from "../../common/utils/EntityUtils"
+import {TypeRef} from "../../common/utils/TypeRef"
+
+
+export const PaymentDataServiceGetDataTypeRef: TypeRef<PaymentDataServiceGetData> = new TypeRef("sys", "PaymentDataServiceGetData")
+export const _TypeModel: TypeModel = {
+	"name": "PaymentDataServiceGetData",
+	"since": 67,
+	"type": "DATA_TRANSFER_TYPE",
+	"id": 1861,
+	"rootId": "A3N5cwAHRQ",
+	"versioned": false,
+	"encrypted": false,
+	"values": {
+		"_format": {
+			"id": 1862,
+			"type": "Number",
+			"cardinality": "One",
+			"final": false,
+			"encrypted": false
+		},
+		"clientType": {
+			"id": 1863,
+			"type": "Number",
+			"cardinality": "ZeroOrOne",
+			"final": false,
+			"encrypted": false
+		}
+	},
+	"associations": {},
+	"app": "sys",
+	"version": "67"
+}
+
+export function createPaymentDataServiceGetData(values?: $Shape<$Exact<PaymentDataServiceGetData>>): PaymentDataServiceGetData {
+	return Object.assign(create(_TypeModel, PaymentDataServiceGetDataTypeRef), values)
+}
+
+export type PaymentDataServiceGetData = {
+	_type: TypeRef<PaymentDataServiceGetData>;
+
+	_format: NumberString;
+	clientType: ?NumberString;
+}
