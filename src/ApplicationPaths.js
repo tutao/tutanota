bea6fd706784db6deb99566e94a4519153fae47d
jom <jom@tutao.de>
2021-02-15 09:26:49 +0100

    simplify paths list

--- src/ApplicationPaths.js
+++ src/ApplicationPaths.js
@@ -1,6 +1,6 @@
 // @flow
 
-export type ApplicationPaths = Array<{root: string, params?: Array<string>, resolver: RouteResolver}>
+export type ApplicationPaths = {[path: string]: RouteResolverMatch}
 
 type ViewResolvers = {
 	loginViewResolver: RouteResolverMatch,
@@ -25,55 +25,34 @@ export function applicationPaths(
 		calendarViewResolver
 	}: ViewResolvers
 ): ApplicationPaths {
-	return [
-		{
-			root: "/login", resolver: loginViewResolver,
-		},
-		{
-			root: "/signup", resolver: loginViewResolver,
-		},
-		{
-			root: "/recover", resolver: loginViewResolver,
-		},
-		{
-			root: "/takeover", resolver: loginViewResolver,
-		},
-		{
-			root: "/giftcard", resolver: loginViewResolver
-		},
-		{
-			root: "/mailto", resolver: mailViewResolver,
-		},
-		{
-			root: "/mail", resolver: mailViewResolver,
-			params: ["/:listId", "/:listId/:mailId"],
-		},
-		{
-			root: "/ext", resolver: externalLoginViewResolver,
-		},
-		{
-			root: "/contact", resolver: contactViewResolver,
-			params: ["/:listId", "/:listId/:contactId"]
-		},
-		{
-			root: "/search/:category", resolver: searchViewResolver,
-			params: ["/:id"]
-		},
-		{
-			root: "/settings", resolver: settingsViewResolver,
-			params: ["/:folder"]
-		},
-		{
-			root: "/contactform/:formId", resolver: contactFormViewResolver,
-		},
-		{
-			root: "/calendar", resolver: calendarViewResolver,
-			params: ["/:view", "/:view/:date"]
-		},
-	]
+	return {
+		"/login": loginViewResolver,
+		"/signup": loginViewResolver,
+		"/recover": loginViewResolver,
+		"/takeover": loginViewResolver,
+		"/mailto": mailViewResolver,
+		"/mail": mailViewResolver,
+		"/mail/:listId": mailViewResolver,
+		"/mail/:listId/:mailId": mailViewResolver,
+		"/ext": externalLoginViewResolver,
+		"/contact": contactViewResolver,
+		"/contact/:listId": contactViewResolver,
+		"/contact/:listId/:contactId": contactViewResolver,
+		"/search/:category": searchViewResolver,
+		"/search/:category/:id": searchViewResolver,
+		"/settings": settingsViewResolver,
+		"/settings/:folder": settingsViewResolver,
+		"/contactform/:formId": contactFormViewResolver,
+		"/calendar": calendarViewResolver,
+		"/calendar/:view": calendarViewResolver,
+		"/calendar/:view/:date": calendarViewResolver,
+		"/giftcard/": loginViewResolver,
+	}
 }
 
 
 export function getPathBases(): Array<string> {
-	return applicationPaths(({}: any)).map(path => path.root.split("/")[1])
+	const paths = Object.keys(applicationPaths(({}: any)))
+	const uniquePathBases = new Set(paths.map(path => path.split("/")[1]))
+	return Array.from(uniquePathBases)
 }
