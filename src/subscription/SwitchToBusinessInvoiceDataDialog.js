7dd6bdc3684f604cee5583051f488cca6abf2911
vaf <vaf@tutao.de>
2021-02-23 11:18:53 +0100

    fix BadRequestError in SwitchToBusinessInvoiceDataDialog if business is ordered

--- src/subscription/SwitchToBusinessInvoiceDataDialog.js
+++ src/subscription/SwitchToBusinessInvoiceDataDialog.js
@@ -7,9 +7,14 @@ import {InvoiceDataInput} from "./InvoiceDataInput"
 import {updatePaymentData} from "./InvoiceAndPaymentDataPage"
 import {BadRequestError} from "../api/common/error/RestError"
 import type {AccountingInfo} from "../api/entities/sys/AccountingInfo"
-import {update} from "../api/main/Entity"
 import type {Customer} from "../api/entities/sys/Customer"
+import {CustomerTypeRef} from "../api/entities/sys/Customer"
 import {showBusinessBuyDialog} from "./BuyDialog"
+import {locator} from "../api/main/MainLocator"
+import type {EntityUpdateData} from "../api/main/EventController"
+import {isUpdateForTypeRef} from "../api/main/EventController"
+import {defer} from "../api/common/utils/Utils"
+import {showProgressDialog} from "../gui/ProgressDialog"
 
 /**
  * Shows a dialog to update the invoice data for business use. Switches the account to business use before actually saving the new invoice data
@@ -17,6 +22,20 @@ import {showBusinessBuyDialog} from "./BuyDialog"
  */
 export function show(customer: Customer, invoiceData: InvoiceData, accountingInfo: AccountingInfo, currentlyBusinessOrdered: boolean, headingId: ?TranslationKey, infoMessageId: ?TranslationKey): Dialog {
 	const invoiceDataInput = new InvoiceDataInput(true, invoiceData)
+	const entityEventUpdateForCustomer = defer() // required if business is booked because the customer is then changed
+	const entityEventListener = (updates: $ReadOnlyArray<EntityUpdateData>, eventOwnerGroupId: Id): Promise<void> => {
+		return Promise.each(updates, update => {
+			if (isUpdateForTypeRef(CustomerTypeRef, update)) {
+				return locator.entityClient.load(CustomerTypeRef, customer._id)
+				              .then(updatedCustomer => {
+					              customer = updatedCustomer
+					              entityEventUpdateForCustomer.resolve()
+				              })
+			}
+		}).return()
+	}
+	locator.eventController.addEntityListener(entityEventListener)
+
 	const confirmAction = () => {
 		let error = invoiceDataInput.validateInvoiceData()
 		if (error) {
@@ -25,21 +44,30 @@ export function show(customer: Customer, invoiceData: InvoiceData, accountingInf
 			let p = Promise.resolve(false)
 			if (!currentlyBusinessOrdered) {
 				p = showBusinessBuyDialog(true)
+			} else {
+				entityEventUpdateForCustomer.resolve()
 			}
 			p.then(failed => {
 				if (failed) {
 					return
 				}
-				customer.businessUse = true
-				update(customer).then(() => {
-					updatePaymentData(Number(accountingInfo.paymentInterval), invoiceDataInput.getInvoiceData(), null, null, false, "0", accountingInfo).then(success => {
-						if (success) {
-							dialog.close()
-						}
-					}).catch(BadRequestError, e => {
-						Dialog.error("paymentMethodNotAvailable_msg")
-					})
-				})
+				showProgressDialog("pleaseWait_msg", entityEventUpdateForCustomer.promise.then(() => {
+					customer.businessUse = true
+				}).then(() => {
+					locator.entityClient.update(customer)
+					       .then(() => {
+						       updatePaymentData(Number(accountingInfo.paymentInterval), invoiceDataInput.getInvoiceData(), null, null, false, "0", accountingInfo)
+							       .then(success => {
+								       if (success) {
+									       locator.eventController.removeEntityListener(entityEventListener)
+									       dialog.close()
+								       }
+							       })
+							       .catch(BadRequestError, e => {
+								       Dialog.error("paymentMethodNotAvailable_msg")
+							       })
+					       })
+				}))
 			})
 		}
 	}
