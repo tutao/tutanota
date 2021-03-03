c67d7db14b5c6773ae4cf17eb309b298637a3369
jom <jom@tutao.de>
2021-03-03 15:52:42 +0100

    write bundler tests

--- test/client/mail/export/BundlerTest.js
+++ test/client/mail/export/BundlerTest.js
@@ -14,12 +14,12 @@ import {isSameId} from "../../../../src/api/common/utils/EntityUtils"
 import {FileTypeRef} from "../../../../src/api/entities/tutanota/File"
 
 o.spec("Bundler", function () {
-	o.only("make mail bundle", async function () {
-
+	o("make mail bundle non compressed headers", async function () {
 		const mailId = ["maillistid", "maillid"]
 		const subject = "hello"
 		const mailBodyId = "mailbodyid"
 		const body = "This is the body text of the body of the email"
+		const sanitizedBodyText = "this is the sanitized body text of the email"
 		const sender = {address: "sender@mycoolsite.co.uk", name: "the sender"}
 		const to = [{address: "to@mycoolsite.co.uk", name: "the to"}]
 		const cc = [{address: "cc@mycoolsite.co.uk", name: "the cc"}]
@@ -33,11 +33,11 @@ o.spec("Bundler", function () {
 		}
 
 		const worker = {
-			downloadFileContent: o.spy(file => Promise.resolve([]))
+			downloadFileContent: o.spy(file => Promise.resolve("attachment"))
 		}
 
 		const sanitizer = {
-			sanitize: o.spy(text => ({text}))
+			sanitize: o.spy(text => ({text: sanitizedBodyText}))
 		}
 		const mailHeadersId = "mailheadersid"
 		const attachmentIds = [["listid", "attachid1"], ["listid", "attachid2"], ["listid", "attachid3"]]
@@ -64,12 +64,15 @@ o.spec("Bundler", function () {
 		o(bundle.mailId).deepEquals(mailId)
 		o(bundle.subject).equals(subject)
 		o(bundle.sender).deepEquals(sender)
+		o(bundle.body).equals(sanitizedBodyText)
 		o(bundle.to).deepEquals(to)
 		o(bundle.cc).deepEquals(cc)
 		o(bundle.bcc).deepEquals(bcc)
 		o(bundle.replyTo).deepEquals(replyTo)
 		o(bundle.isDraft).deepEquals(false)
 		o(bundle.isRead).deepEquals(true)
+		o(bundle.headers).equals(headers)
+		o(bundle.attachments).deepEquals(["attachment", "attachment", "attachment"])
 		const compareCall = (typeRef, id) => call => isSameTypeRef(call.args[0], typeRef) && isSameId(call.args[1], id)
 		const assertLoaded = (typeRef, id) => o(entityClient.load.calls.find(compareCall(typeRef, id))).notEquals(undefined)(`assertLoaded TypeRef(${typeRef.app}, ${typeRef.type}) ${id.toString()}`)
 		assertLoaded(MailBodyTypeRef, mailBodyId)
