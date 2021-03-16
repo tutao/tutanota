d7d53943bf98dccc55ca6c4f721793495d7239c5
nig <nig@tutao.de>
2021-03-16 11:16:48 +0100

    add possibility to use self-signed cert for desktop update signatures
    to create these signatures, run the dist script with the environment
    variable DEBUG_SIGN set to a directory containing a self-signed code
    signing cert file test.p12
    
    close #2918

--- buildSrc/installerSigner.js
+++ buildSrc/installerSigner.js
@@ -43,6 +43,7 @@ import path from "path"
 import fs from "fs-extra"
 import {spawnSync} from "child_process"
 import jsyaml from "js-yaml"
+import forge from "node-forge"
 
 /**
  * Creates a signature on the given application file, writes it to signatureFileName and adds the signature to the yaml file.
@@ -54,21 +55,10 @@ import jsyaml from "js-yaml"
 export function sign(filePath, signatureFileName, ymlFileName) {
 	console.log("Signing", path.basename(filePath), '...')
 	const dir = path.dirname(filePath)
-	const sigOutPath = path.join(dir, signatureFileName)
-	const result = spawnSync("/usr/bin/pkcs11-tool", [
-		"-s",
-		"-m", "SHA512-RSA-PKCS",
-		"--id", "10",
-		"--pin", "env:HSM_USER_PIN",
-		"-i", path.basename(filePath),
-		"-o", signatureFileName
-	], {
-		cwd: dir,
-		stdio: [process.stdin, process.stdout, process.stderr]
-	})
-	if (result.status !== 0) {
-		throw new Error("error invoking process" + JSON.stringify(result))
-	}
+
+	const sigOutPath = process.env.DEBUG_SIGN
+		? signWithSelfSignedCertificate(filePath, signatureFileName, dir)
+		: signWithHSM(filePath, signatureFileName, dir)
 
 	if (ymlFileName) {
 		console.log(`attaching signature to yml...`, ymlFileName)
@@ -80,5 +70,56 @@ export function sign(filePath, signatureFileName, ymlFileName) {
 	} else {
 		console.log("Not attaching signature to yml")
 	}
+}
 
+function signWithHSM(filePath, signatureFileName, dir) {
+	let result = spawnSync("/usr/bin/pkcs11-tool", [
+		"-s",
+		"-m", "SHA512-RSA-PKCS",
+		"--id", "10",
+		"--pin", "env:HSM_USER_PIN",
+		"-i", path.basename(filePath),
+		"-o", signatureFileName
+	], {
+		cwd: dir,
+		stdio: [process.stdin, process.stdout, process.stderr]
+	})
+
+	if (result.status !== 0) {
+		throw new Error("error during hsm signing process" + JSON.stringify(result))
+	}
+	return path.join(dir, signatureFileName)
+}
+
+function signWithSelfSignedCertificate(filePath, signatureFileName, dir) {
+	const sigOutPath = path.join(dir, signatureFileName)
+
+	// copy cert to webserver so it can be easily put into a VM
+	fs.copyFileSync(path.join(process.env.DEBUG_SIGN, "ca.crt"), path.join(dir, "ca.crt"))
+
+	try {
+		const fileData = fs.readFileSync(filePath) //binary format
+		const lnk = path.join(process.env.DEBUG_SIGN, "test.p12")
+		const privateKey = getPrivateKeyFromCert(lnk)
+		const md = forge.md.sha512.create()
+		md.update(fileData.toString('binary'))
+		const sig = Buffer.from(privateKey.sign(md), 'binary')
+		fs.writeFileSync(sigOutPath, sig, null)
+	} catch (e) {
+		console.log('Error:', e.message)
+	}
+	return sigOutPath
+}
+
+function getPrivateKeyFromCert(lnk) {
+	if (!lnk) {
+		throw new Error("can't sign client, no certificate file name")
+	}
+	const p12b64 = fs.readFileSync(lnk).toString('base64')
+	const p12Der = forge.util.decode64(p12b64)
+	const p12Asn1 = forge.asn1.fromDer(p12Der)
+	const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, "")
+	const bag = p12.getBags({friendlyName: 'user'})["friendlyName"]
+		.find(b => b.key != null)
+	return bag.key
 }
--- buildSrc/winsigner.cjs
+++ buildSrc/winsigner.cjs
@@ -4,11 +4,33 @@ const fs = Promise.promisifyAll(require("fs-extra"))
 const spawn = require('child_process').spawn
 
 function signer(args) {
-	const certificateFile = process.env["WIN_CSC_FILE"]
-	const hsmPin = process.env["HSM_USER_PIN"]
 	const extension = "." + args.path.split(".").pop()
 	const unsignedFileName = args.path.replace(extension, "-unsigned" + extension)
-	const command = "/usr/bin/osslsigncode"
+
+	const commandArguments = process.env.DEBUG_SIGN
+		? getSelfSignedArgs(unsignedFileName, args.hash, args.path)
+		: getHsmArgs(unsignedFileName, args.hash, args.path)
+
+	return signWithArgs(commandArguments, args.path, unsignedFileName)
+}
+
+function getSelfSignedArgs(unsignedFileName, hash, file_to_sign) {
+	const certificateFile = path.join(process.env.DEBUG_SIGN, "test.p12")
+
+	return [
+		"sign",
+		"-in", unsignedFileName,
+		"-out", file_to_sign,
+		"-pkcs12", certificateFile,
+		"-h", hash ? hash : "sha256",
+		"-t", "http://timestamp.comodoca.com",
+		"-n", "tutanota-desktop"
+	]
+}
+
+function getHsmArgs(unsignedFileName, hash, file_to_sign) {
+	const certificateFile = process.env["WIN_CSC_FILE"]
+	const hsmPin = process.env["HSM_USER_PIN"]
 
 	//  Timestamping:
 	//  1. The client application creates a hashed value of the data to the timestamp server.
@@ -22,38 +44,40 @@ function signer(args) {
 	//  http://www.startssl.com/timestamp
 
 	if (!certificateFile) {
-		console.error("ERROR: " + args.path.split(path.sep).pop() + "\" not signed! The NSIS installer may not work.")
+		console.error("ERROR: " + file_to_sign.split(path.sep).pop() + "\" not signed! The NSIS installer may not work.")
 		console.log("\t• set WIN_CSC_FILE env var")
-		return Promise.reject(new Error(args.path))
+		throw new Error(file_to_sign)
 	}
 
 	if (!hsmPin) {
-		console.log("ERROR: " + args.path.split(path.sep).pop() + "\" not signed! The NSIS installer may not work.")
+		console.log("ERROR: " + file_to_sign.split(path.sep).pop() + "\" not signed! The NSIS installer may not work.")
 		console.log("\t• set  HSM_USER_PIN env var")
-		return Promise.reject(new Error(args.path))
+		throw new Error(file_to_sign)
 	}
 
-	if (!fs.existsSync(command)) {
-		console.log("ERROR: " + args.path.split(path.sep).pop() + "\" not signed! The NSIS installer may not work.")
-		console.log("\t• install osslsigncode")
-		return Promise.reject(new Error(args.path))
-	}
-
-	const commandArguments = [
+	return [
 		"-in", unsignedFileName,
-		"-out", args.path,
+		"-out", file_to_sign,
 		"-pkcs11engine", "/usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so",
 		"-pkcs11module", "/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so",
 		"-certs", certificateFile,
 		"-key", "10",
 		"-pass", hsmPin,
-		"-h", args.hash ? args.hash : "sha256",
+		"-h", hash ? hash : "sha256",
 		"-t", "http://timestamp.comodoca.com",
 		"-n", "tutanota-desktop"
 	]
+}
 
-	fs.renameSync(args.path, unsignedFileName)
-	console.log(`spawning "${command}" -h ${args.hash}`)
+function signWithArgs(commandArguments, file_to_sign, unsignedFileName) {
+	const command = "/usr/bin/osslsigncode"
+
+	if (!fs.existsSync(command)) {
+		console.log("ERROR: " + file_to_sign.split(path.sep).pop() + "\" not signed! The NSIS installer may not work.")
+		console.log("\t• install osslsigncode")
+		return Promise.reject(new Error(file_to_sign))
+	}
+	fs.renameSync(file_to_sign, unsignedFileName)
 	// only for testing, would print certificate password to logs, otherwise
 	//console.log(`spawning "${command} ${commandArguments.join(" ")}"`)
 	let child = spawn(command, commandArguments, {
@@ -67,7 +91,7 @@ function signer(args) {
 				cb(exitCode)
 			} else {
 				fs.removeSync(unsignedFileName)
-				cb(null, args.path)
+				cb(null, file_to_sign)
 			}
 		})
 	})
