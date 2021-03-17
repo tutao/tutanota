9eed13a942bdc59f90759d0dc39bef09c98fdfb5
ivk <ivk@tutao.de>
2021-03-17 16:11:11 +0100

    [android] Handle suspension when downloading missed notification, #2858

--- app-android/app/src/main/java/de/tutao/tutanota/push/TutanotaNotificationsHandler.java
+++ app-android/app/src/main/java/de/tutao/tutanota/push/TutanotaNotificationsHandler.java
@@ -96,6 +96,14 @@ public class TutanotaNotificationsHandler {
 				Log.w(TAG, e);
 				localNotificationsFacade.showErrorNotification(R.string.scheduleAlarmError_msg, e);
 				return null;
+			} catch (ServiceUnavailableException e) {
+				Log.d(TAG, "ServiceUnavailable when downloading missed notification, waiting " +
+						e.getSuspensionSeconds() + "s");
+				try {
+					Thread.sleep(TimeUnit.SECONDS.toMillis(e.getSuspensionSeconds()));
+				} catch (InterruptedException ignored) {
+				}
+				// tries are not decremented and we don't return, we just wait and try again.
 			} catch (ServerResponseException e) {
 				triesLeft--;
 				Log.w(TAG, e);
@@ -134,13 +142,7 @@ public class TutanotaNotificationsHandler {
 			int responseCode = urlConnection.getResponseCode();
 			Log.d(TAG, "MissedNotification response code " + responseCode);
 
-			if (responseCode == 404) {
-				throw new FileNotFoundException("Missed notification not found: " + 404);
-			} else if (400 <= responseCode && responseCode < 500) {
-				throw new ClientRequestException(responseCode);
-			} else if (500 <= responseCode && responseCode <= 600) {
-				throw new ServerResponseException(responseCode);
-			}
+			handleResponseCode(urlConnection, responseCode);
 
 			try (InputStream inputStream = urlConnection.getInputStream()) {
 				String responseString = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
@@ -149,6 +151,29 @@ public class TutanotaNotificationsHandler {
 			}
 		} catch (MalformedURLException | JSONException e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	private void handleResponseCode(HttpURLConnection urlConnection, int responseCode)
+			throws FileNotFoundException, ServerResponseException, ClientRequestException, ServiceUnavailableException {
+		if (responseCode == 404) {
+			throw new FileNotFoundException("Missed notification not found: " + 404);
+		} else if (responseCode == ServiceUnavailableException.CODE) {
+			String retryAfterHeader = urlConnection.getHeaderField("Retry-After");
+			if (retryAfterHeader == null) {
+				retryAfterHeader = urlConnection.getHeaderField("Suspension-Time");
+			}
+			int suspensionTime;
+			try {
+				suspensionTime = Integer.parseInt(retryAfterHeader);
+			} catch (NumberFormatException e) {
+				suspensionTime = 0;
+			}
+			throw new ServiceUnavailableException(suspensionTime);
+		} else if (400 <= responseCode && responseCode < 500) {
+			throw new ClientRequestException(responseCode);
+		} else if (500 <= responseCode && responseCode <= 600) {
+			throw new ServerResponseException(responseCode);
 		}
 	}
 
@@ -199,6 +224,20 @@ public class TutanotaNotificationsHandler {
 	static class ServerResponseException extends HttpException {
 		ServerResponseException(int code) {
 			super(code);
+		}
+	}
+
+	static class ServiceUnavailableException extends HttpException {
+		static final int CODE = 503;
+		private final int suspensionSeconds;
+
+		ServiceUnavailableException(int suspensionSeconds) {
+			super(CODE);
+			this.suspensionSeconds = suspensionSeconds;
+		}
+
+		public int getSuspensionSeconds() {
+			return suspensionSeconds;
 		}
 	}
 
