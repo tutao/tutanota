835c16694efa871b0364055df84a0c195e07f224
ivk <ivk@tutao.de>
2021-03-24 10:35:08 +0100

    Cleanup LineChart

--- src/gui/base/LineChart.js
+++ src/gui/base/LineChart.js
@@ -2,6 +2,7 @@
 import m from "mithril"
 import {theme} from "../theme"
 import {size} from "../size"
+import {NBSP} from "../../api/common/utils/StringUtils"
 
 type AxisLabel = {
 	interval: number;
@@ -11,7 +12,7 @@ type AxisLabel = {
 
 export type LineChartAttrs = {
 	data: {
-		name: string, color: string, line: boolean, scatter: boolean, coordinates: [number, number][], // x,y
+		name: string, color: string, line: boolean, scatter: boolean, coordinates: Array<[number, number]>, // x,y
 		maxX: ?number, maxY: ?number, minX: ?number, minY: ?number,
 	}[];
 	maxX: number;
@@ -25,26 +26,11 @@ export type LineChartAttrs = {
 
 const coordinateSystemWidth = 3840 // UHD-1
 
-/*  the svg coordinate system starts at 0,0 (x,y) in the upper left corner. Negative values are out
- *  of the visible area. The y-axis is inverted in comparison to our diagram data (top is 0, largest
- *  value is at the bottom. That is why y-values are inverted in the visible range
- */
-function calculateX(x: number, a: LineChartAttrs, xFactor: number) {
-	//console.log(x, xFactor, a)
-	return Math.round((x - a.minX) * xFactor)
-}
-
-function calculateY(height: number, y: number, a: LineChartAttrs, yFactor: number) {
-	return Math.round((height) - ((y - a.minY) * yFactor))
-}
-
-
-class _LineChart {
+export class LineChart implements MComponent<LineChartAttrs> {
 	_domWrapper: HTMLElement;
 	_domSvg: HTMLElement;
 	_domAll: HTMLElement;
 	_scale: number;
-	_mousePosition: ?number[];
 	_domPositions: HTMLElement;
 	_zoom: number;
 	_pos: [number, number];
@@ -80,128 +66,126 @@ class _LineChart {
 		e.preventDefault()
 	}
 
-	view(vnode: Vnode<LineChartAttrs>) {
-		try {
-			console.log("updating linechart")
-			//let start = performance.now()
+	view(vnode: Vnode<LineChartAttrs>): Children {
+		const a = vnode.attrs
+		this._attrs = a
+		const height = coordinateSystemWidth * a.heightFactor
+		const xFactor = coordinateSystemWidth / ((a.maxX - a.minX))
+		const yFactor = height / ((a.maxY - a.minY))
 
-			const a = vnode.attrs
-			this._attrs = a
-			const height = coordinateSystemWidth * a.heightFactor
-			const xFactor = coordinateSystemWidth / ((a.maxX - a.minX))
-			const yFactor = height / ((a.maxY - a.minY))
-
-			return m("div.line-chart-example.mlr-l.mt-l.mb-l", {
-					oncreate: vnode => {
-						this._domWrapper = vnode.dom
-					},
-				}, [
-					m("div.positions.abs.mlr-l.mt-l", {
-						oncreate: vnode => this._domPositions = vnode.dom,
-						style: {right: '5px', top: 0}
-					}),
-					m("svg.line-chart", {
-							style: {border: `1px solid ${theme.content_border}`},
-							oncreate: vnode => {
-								this.init(vnode)
-								const moveListener = (e: MouseEvent) => {
-									//console.log(e.movementX, this._scale)
-									if (this._moving) {
-										let x = this._pos[0] - e.movementX * this._scale / this._zoom
-										let y = this._pos[1] - e.movementY * this._scale / this._zoom
-										this.setPosition(x, y)
-									}
-
-									// update position labels (right top)
-									let {x, y} = this.getMousePosition(e)
-									let a = this._attrs
-									const height = coordinateSystemWidth * a.heightFactor
-									const xFactor = coordinateSystemWidth / ((a.maxX - a.minX))
-									const yFactor = height / ((a.maxY - a.minY))
-									this._domPositions.innerHTML = a.yLabel.formatExact(a.maxY - (y / yFactor)) + "<br>"
-										+ a.xLabel.formatExact(a.minX + (x / xFactor))
+		return m("div.line-chart-example.mlr-l.mt-l.mb-l", {
+				oncreate: vnode => {
+					this._domWrapper = vnode.dom
+				},
+			}, [
+				m("div.positions.abs.mlr-l.mt-l.white-space-pre", {
+					oncreate: vnode => this._domPositions = vnode.dom,
+					style: {right: '5px', top: 0}
+				}),
+				m("svg.line-chart", {
+						style: {border: `1px solid ${theme.content_border}`},
+						oncreate: vnode => {
+							this.onDomUpdated(vnode)
+							this._domSvg.addEventListener("mousemove", (e: MouseEvent) => this._onMove(e))
+							this._domSvg.addEventListener("mousedown", (e: MouseEvent) => this._move(e))
+							this._domSvg.addEventListener("mouseup", (e: MouseEvent) => this._stopMove(e))
+							this._domSvg.addEventListener("mouseenter", (e: MouseEvent) => {
+								if (e.buttons === 1) this._move(e)
+							})
+							this._domSvg.addEventListener("mouseleave", (e: MouseEvent) => this._stopMove(e))
+							this._domSvg.addEventListener("wheel", (e: any) => {
+								if (e.ctrlKey) {
+									e.deltaY > 0 ? this.zoom(e, -0.3) : this.zoom(e, 0.3)
+									e.preventDefault()
 								}
-								this._domSvg.addEventListener("mousemove", moveListener)
-								this._domSvg.addEventListener("mousedown", (e: MouseEvent) => this._move(e))
-								this._domSvg.addEventListener("mouseup", (e: MouseEvent) => this._stopMove(e))
-								this._domSvg.addEventListener("mouseenter", (e: MouseEvent) => {
-									if (e.buttons === 1) this._move(e)
-								})
-								this._domSvg.addEventListener("mouseleave", (e: MouseEvent) => this._stopMove(e))
-								this._domSvg.addEventListener("wheel", (e: any) => {
-									if (e.ctrlKey) {
-										e.deltaY > 0 ? this.zoom(e, -0.3) : this.zoom(e, 0.3)
-										e.preventDefault()
-									}
-								})
+							})
 
-							},
-							onupdate: vnode => {
-								this.init(vnode)
-							},
-							// good viewbox introduction: https://sarasoueidan.com/blog/svg-coordinate-systems/
-							//viewBox: `${this.data.minX} ${this.data.minY} ${this.data.maxX - this.data.minX} ${this.data.maxY - this.data.minY}`
-							viewBox: `${this._pos[0]} ${this._pos[1]} ${this._viewBox.width / this._zoom} ${this._viewBox.height
-							/ this._zoom}`,
 						},
-						this._domSvg === null ? [] : m("g",
-							{
-								oncreate: vnode => {
-									this._domAll = vnode.dom
-								},
-							}, [
-								m("circle", {
-									oncreate: vnode => this._circle = vnode.dom,
-									style: {display: 'none'},
-									x: 0,
-									y: 0,
-									r: 2.5 * this._scale / this._zoom
+						onupdate: vnode => {
+							this.onDomUpdated(vnode)
+						},
+						// good viewbox introduction: https://sarasoueidan.com/blog/svg-coordinate-systems/
+						viewBox:
+							`${this._pos[0]} ${this._pos[1]} ${this._viewBox.width / this._zoom} ${this._viewBox.height / this._zoom}`,
+					},
+					this._domSvg === null
+						? []
+						: m("g",
+						{
+							oncreate: vnode => {
+								this._domAll = vnode.dom
+							},
+						}, [
+							m("circle", {
+								oncreate: vnode => this._circle = vnode.dom,
+								style: {display: 'none'},
+								x: 0,
+								y: 0,
+								r: 2.5 * this._scale / this._zoom
+							}),
+							m("g.coordinate-system", [
+								m("path", {
+									stroke: theme.content_accent,
+									fill: 'none',
+									'stroke-width': 2 * this._scale / this._zoom,
+									d: `M0,0 L0,${height} L${coordinateSystemWidth},${height}`
 								}),
-								m("g.coordinate-system", [
-									m("path", {
-										stroke: theme.content_accent,
-										fill: 'none',
-										'stroke-width': 2 * this._scale / this._zoom,
-										d: `M0,0 L0,${height} L${coordinateSystemWidth},${height}`
-									}),
-									m("g.y-labels", this.renderYLabel(a, height, yFactor)),
-									m("g.x-labels", this.renderXLabel(a, height, xFactor)),
-								]),
-								a.data.map((d, index) => {
-									return d.coordinates.length == 0 ? null : [
-										d.line ? m("g", {
-												stroke: d.color,
-												'stroke-width': 1 * this._scale / this._zoom,
-												fill: 'none',
-											},
-											m("path", {
-												d: d.coordinates.map((xy, i) => {
-													return [
-														(i === 0 ? 'M' : 'L') + calculateX(xy[0], a, xFactor) + ','
-														+ calculateY(height, xy[1], a, yFactor)
-													]
-												}).join(' ')
-											}, m("title", d.name)),
-										) : null,
-										d.scatter ? m("g", d.coordinates.map(xy => m("circle", {
+								m("g.y-labels", this.renderYLabel(a, height, yFactor)),
+								m("g.x-labels", this.renderXLabel(a, height, xFactor)),
+							]),
+							a.data.map((d) => {
+								return d.coordinates.length === 0 ? null : [
+									d.line
+										? m("g", {
+											stroke: d.color,
+											'stroke-width': 1 * this._scale / this._zoom,
+											fill: 'none',
+										},
+										m("path", {
+											d: d.coordinates.map((xy, i) => {
+												return [
+													(i === 0 ? 'M' : 'L') + calculateX(xy[0], a, xFactor) + ','
+													+ calculateY(height, xy[1], a, yFactor)
+												]
+											}).join(' ')
+										}, m("title", d.name)),
+										)
+										: null,
+									d.scatter
+										? m("g", d.coordinates.map(xy => m("circle", {
 											fill: d.color,
 											cx: calculateX(xy[0], a, xFactor),
 											cy: calculateY(height, xy[1], a, yFactor),
 											r: 2.5 * this._scale / this._zoom
-										}, m("title", d.name)))) : null,
-									]
-								}),
-							])
-					)
-				]
-			)
-		}
-		finally {
-			//console.log(performance.now() - start)
-		}
+										}, m("title", d.name))))
+										: null,
+								]
+							}),
+						])
+				)
+			]
+		)
 	}
 
-	renderYLabel(a: LineChartAttrs, height: number, yFactor: number) {
+	_onMove(e: MouseEvent) {
+		if (this._moving) {
+			let x = this._pos[0] - e.movementX * this._scale / this._zoom
+			let y = this._pos[1] - e.movementY * this._scale / this._zoom
+			this.setPosition(x, y)
+		}
+
+		// update position labels (right top)
+		let {x, y} = this.getMousePosition(e)
+		let a = this._attrs
+		const height = coordinateSystemWidth * a.heightFactor
+		const xFactor = coordinateSystemWidth / ((a.maxX - a.minX))
+		const yFactor = height / ((a.maxY - a.minY))
+		const formattedY = a.yLabel.formatExact(a.maxY - (y / yFactor))
+		const formattedX = a.xLabel.formatExact(a.minX + (x / xFactor))
+		this._domPositions.innerText = formattedY + "\n" + formattedX
+	}
+
+	renderYLabel(a: LineChartAttrs, height: number, yFactor: number): Children {
 		return [...Array(Math.round((a.maxY - a.minY) / a.yLabel.interval)).keys()].map(i => {
 			let y = calculateY(height, a.minY + i * a.yLabel.interval, a, yFactor)
 			return [
@@ -217,12 +201,12 @@ class _LineChart {
 					'font-size': size.font_size_base * this._scale / this._zoom,
 					'alignment-baseline': 'middle',
 					'text-anchor': 'end'
-				}, " " + a.yLabel.formatLabel(a.minY + i * a.yLabel.interval) + " "),
+				}, NBSP + a.yLabel.formatLabel(a.minY + i * a.yLabel.interval) + NBSP),
 			]
 		})
 	}
 
-	renderXLabel(a: LineChartAttrs, height: number, xFactor: number) {
+	renderXLabel(a: LineChartAttrs, height: number, xFactor: number): Children {
 		return [...Array(Math.round((a.maxX - a.minX) / a.xLabel.interval)).keys()].map(i => {
 			let x = calculateX(a.minX + i * a.xLabel.interval, a, xFactor)
 			return i > 0 ? [
@@ -239,12 +223,12 @@ class _LineChart {
 					'font-size': size.font_size_base * this._scale / this._zoom,
 					'alignment-baseline': 'hanging',
 					'text-anchor': 'middle'
-				}, " " + a.xLabel.formatLabel(i * a.xLabel.interval) + " "),
+				}, NBSP + a.xLabel.formatLabel(i * a.xLabel.interval) + NBSP),
 			] : null
 		})
 	}
 
-	getMousePosition(e: MouseEvent) {
+	getMousePosition(e: MouseEvent): {x: number, y: number} {
 		let offset = (e.currentTarget: any).getBoundingClientRect()
 		return {
 			x: ((e.clientX - offset.left) * this._scale / this._zoom) + this._pos[0],
@@ -252,11 +236,9 @@ class _LineChart {
 		}
 	}
 
-	setPosition(x, y) {
+	setPosition(x: number, y: number) {
 		this._pos = [x, y]
-		//this._domAll.style.transform = `translate(${x}px,${y}px) scale(${this._zoom})`
-		this._domSvg.setAttribute("viewBox", `${x} ${y} ${this._viewBox.width / this._zoom} ${this._viewBox.height
-		/ this._zoom}`)
+		this._domSvg.setAttribute("viewBox", `${x} ${y} ${this._viewBox.width / this._zoom} ${this._viewBox.height / this._zoom}`)
 	}
 
 	zoom(e: MouseEvent, step: number) {
@@ -282,9 +264,9 @@ class _LineChart {
 		}
 	}
 
-	init(vnode: Vnode<LineChartAttrs>) {
-		const a = vnode.attrs
+	onDomUpdated(vnode: Vnode<LineChartAttrs>) {
 		this._domSvg = vnode.dom
+		// _domSvg is actually SVGGraphicsElement but flow doesn't have the definition
 		this._bounds = (this._domSvg: any).getBBox()
 		this._pos = [this._bounds.x, this._bounds.y]
 		this._viewBox = this._bounds
@@ -292,14 +274,22 @@ class _LineChart {
 		const newScale = this._viewBox.width / this._domSvg.clientWidth
 		if (Math.round(newScale * 100) / 100 !== Math.round(this._scale * 100) / 100) {
 			this._scale = newScale
-			window.requestAnimationFrame(m.redraw)
+			m.redraw()
 		}
 	}
-
 }
 
+/*  the svg coordinate system starts at 0,0 (x,y) in the upper left corner. Negative values are out
+ *  of the visible area. The y-axis is inverted in comparison to our diagram data (top is 0, largest
+ *  value is at the bottom. That is why y-values are inverted in the visible range
+ */
+function calculateX(x: number, a: LineChartAttrs, xFactor: number) {
+	//console.log(x, xFactor, a)
+	return Math.round((x - a.minX) * xFactor)
+}
 
-export const LineChart: Class<MComponent<LineChartAttrs>> = _LineChart
-
+function calculateY(height: number, y: number, a: LineChartAttrs, yFactor: number) {
+	return Math.round((height) - ((y - a.minY) * yFactor))
+}
 
 
