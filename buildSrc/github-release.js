a2fcddc4b7aba6fd52e06bc57b9bfa005ff5c9dc
ivk <ivk@tutao.de>
2021-04-07 16:08:31 +0200

    Use @octokit/rest to create the release

---
+++ buildSrc/github-release.js
@@ -0,0 +1,23 @@
+import {Octokit} from "@octokit/rest"
+import {createTokenAuth} from "@octokit/auth-token";
+
+(async function () {
+	const releaseToken = process.env.GITHUB_TOKEN
+	if (!releaseToken) {
+		throw new Error("No GITHUB_TOKEN set!")
+	}
+
+	const auth = createTokenAuth(releaseToken)
+	const authentication = await auth()
+	const octokit = new Octokit({
+		authentication,
+		userAgent: 'tuta-github-release-v0.0.1'
+	})
+	await octokit.repos.createRelease({
+		owner: "tutao",
+		repo: "tutanota",
+		draft: true,
+		name: "test-draft"
+	})
+})()
+
