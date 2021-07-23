03b6ac5beb6ef5102158e1fd6d9bd85d3f9c7ef5
vis <vis@tutao.de>
2021-07-23 11:43:19 +0200

    Update translations

--- src/translations/de.js
+++ src/translations/de.js
@@ -11,7 +11,7 @@ export default {
 		"other"
 	],
 	"created_at": "2015-01-13T20:40:31Z",
-	"updated_at": "2021-07-15T13:28:30Z",
+	"updated_at": "2021-07-23T07:17:12Z",
 	"source_locale": {
 		"id": "fcd7471b347c8e517663e194dcddf237",
 		"name": "en",
