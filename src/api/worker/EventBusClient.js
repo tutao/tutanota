5e7a20710990617ef347a504139cedd5b4d25b58
ivk <ivk@tutao.de>
2021-08-23 09:44:29 +0200

    Fix reconnecting websocket, fix #3411
    Check for socket in _close() was broken with Flow update (it was
    checking if socket is a function which is never the case). This code is
    not needed because the function is stubbed in tests anyway.

--- src/api/worker/EventBusClient.js
+++ src/api/worker/EventBusClient.js
@@ -291,9 +291,7 @@ export class EventBusClient {
 				break;
 		}
 
-		if (this._socket && typeof this._socket == "function") { // close is undefined in node tests
-			this._socket.close()
-		}
+		this._socket?.close()
 	}
 
 	_unsubscribeFromOldWebsocket() {
