7909b6def885a0ec8f102e686402aabbcf06d715
bedhub <bedhub@users.noreply.github.com>
2021-09-08 13:46:18 +0200

    use plain text search function for templates and knowledgebase, fixes #3462

--- src/knowledgebase/model/KnowledgeBaseSearchFilter.js
+++ src/knowledgebase/model/KnowledgeBaseSearchFilter.js
@@ -1,30 +1,10 @@
 // @flow
 
 import type {KnowledgeBaseEntry} from "../../api/entities/tutanota/KnowledgeBaseEntry"
+import {search} from "../../api/common/utils/PlainTextSearch"
 
-export function knowledgeBaseSearch(input: string, allEntries: $ReadOnlyArray<KnowledgeBaseEntry>): Array<KnowledgeBaseEntry> {
-	let matchedEntries = []
-	let queryWords = input.toLowerCase().trim().split(" ")
-	for (const queryString of queryWords) {
-		if (queryString) {
-			allEntries.forEach(entry => {
-				let entryTitle = entry.title.toLowerCase()
-				let entryKeywords = entry.keywords
-				//search in title
-				if (entryTitle.includes(queryString) && !matchedEntries.includes(entry)) {
-					matchedEntries.push(entry)
-				}
-				//search in keywords
-				entryKeywords.forEach(k => {
-					let keyword = k.keyword.toLowerCase()
-					if (keyword.includes(queryString) && !matchedEntries.includes(entry)) {
-						matchedEntries.push(entry)
-					}
-				})
-			})
-		}
-	}
-	return matchedEntries
+export function knowledgeBaseSearch(input: string, allEntries: $ReadOnlyArray<KnowledgeBaseEntry>): $ReadOnlyArray<KnowledgeBaseEntry> {
+	return search(input, allEntries, ["title", "description", "keywords.keyword"], false)
 }
 
 
--- src/templates/model/TemplateSearchFilter.js
+++ src/templates/model/TemplateSearchFilter.js
@@ -1,32 +1,14 @@
 //@flow
 import type {EmailTemplate} from "../../api/entities/tutanota/EmailTemplate"
 import {TEMPLATE_SHORTCUT_PREFIX} from "./TemplatePopupModel"
+import {search} from "../../api/common/utils/PlainTextSearch"
 
 
 export function searchInTemplates(input: string, allTemplates: $ReadOnlyArray<EmailTemplate>): $ReadOnlyArray<EmailTemplate> {
-	// filter out whitespaces and prepare case insensitive search.
-	let allQueryWords = input.toLowerCase().trim().split(" ").filter(word => word.trim().length > 0)
-	if (allQueryWords.length === 0) {
-		return allTemplates
-	}
-	const firstWord = allQueryWords[0];
-	if (firstWord.startsWith(TEMPLATE_SHORTCUT_PREFIX)) { // search in tag only
-		const newQueryString = firstWord.substring(TEMPLATE_SHORTCUT_PREFIX.length)
-		return allTemplates.filter(template => template.tag.toLowerCase().startsWith(newQueryString))
+	if (input.startsWith(TEMPLATE_SHORTCUT_PREFIX)) { // search in tag only
+		const newQueryString = input.substring(TEMPLATE_SHORTCUT_PREFIX.length)
+		return search(newQueryString, allTemplates, ["tag"], false)
 	} else {
-		// full text search in all other attributes
-		// all words must match in either title, tag or text of content
-		return allTemplates.filter(template => {
-			return findMatch(allQueryWords, template.title) //search in title
-				|| findMatch(allQueryWords, template.tag) //search in tag
-				|| template.contents.some(content => findMatch(allQueryWords, content.text)) //search in contents
-		})
+		return search(input, allTemplates, ["tag", "title", "contents.text"], false)
 	}
-}
-
-/**
- * Checks if all given words are included in the given content and returns true in this case. Returns false otherwise.
- */
-function findMatch(queryWords: Array<string>, content: string): boolean {
-	return queryWords.every(queryWord => content.toLowerCase().includes(queryWord))
 }
