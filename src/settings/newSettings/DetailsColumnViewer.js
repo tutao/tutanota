a2af7951ccc756a2548b8f11662a35e5185d6ff4
tih <tih@tutao.de>
2021-12-15 12:17:05 +0100

    add different background for found settings while searching

--- src/settings/newSettings/DetailsColumnViewer.js
+++ src/settings/newSettings/DetailsColumnViewer.js
@@ -1,20 +1,38 @@
 // @flow
 import m from "mithril"
+import type {SettingsSection, SettingsValue} from "./SettingsModel"
 import {lang} from "../../misc/LanguageViewModel"
-import type {SettingsSection} from "./SettingsModel"
 
 export type DetailsColumnViewerAttrs = {
-	section: SettingsSection
+	section: SettingsSection,
+	searchValue: string
 }
 
-
+/**
+ * render the current selected section settings
+ */
 export class DetailsColumnViewer implements MComponent<DetailsColumnViewerAttrs> {
 
+	/**
+	 * @returns true if the currently rendering setting includes the search value
+	 */
+	isEqualToSearchValue(setting: SettingsValue<any>, value: string): boolean {
+		let result = false
+		if(value && value !== "") {
+			if (setting.attrs.label) {
+				result = lang.get(setting.attrs.label).toLowerCase().includes(value.toLowerCase())
+			} else if (lang.get(setting.attrs.tableHeading)) {
+				result = lang.get(setting.attrs.tableHeading).toLowerCase().includes(value.toLowerCase())
+			}
+		}
+		return result
+	}
+
 	view(vnode: Vnode<DetailsColumnViewerAttrs>): Children {
-		const {section} = vnode.attrs
+		const {section, searchValue} = vnode.attrs
 		return m(".fill-absolute.scroll.plr-l.pb-floating", [
 			section.settingsValues.map(value => {
-				return m(".mt-l", [
+				return m(".mt-l" + (this.isEqualToSearchValue(value, searchValue) ? ".elevated-bg" : ""), [
 					m(value.component, value.attrs)
 				])
 			})
