cf4bcf0b4c5ecc970715c4ca59e57cfa2c4246af
nig <nig@tutao.de>
2022-02-08 14:56:10 +0100

    [desktop/linux] Fix saving credentials key, fix #3875
    We had concurrent keychain accesses during startup:
     * device key for sse
     * credential key for encrypting saved credentials
    
    Either keytar or libsecret can't handle concurrent accesses, which led
    to invalid credential encryption down the line. We could store the key
    but when used it wouldn't work anymore.
    
    Waiting for one operation to return before calling keytar again fixed
    this.
    
    Co-authored-by: ivk <ivk@tutao.de>

--- src/desktop/sse/SecretStorage.ts
+++ src/desktop/sse/SecretStorage.ts
@@ -1,5 +1,6 @@
 import {CANCELLED, getPassword, setPassword} from "keytar"
 import {CancelledError} from "../../api/common/error/CancelledError"
+import {noOp} from "@tutao/tutanota-utils"
 
 export interface SecretStorage {
 	getPassword(service: string, account: string): Promise<string | null>
@@ -8,17 +9,34 @@ export interface SecretStorage {
 }
 
 export class KeytarSecretStorage implements SecretStorage {
+
+	/**
+	 * keytar can't handle concurrent accesses to the keychain, so we need to sequence
+	 * calls to getPassword and setPassword.
+	 * this promise chain stores pending operations.
+	 */
+	private lastOp: Promise<unknown> = Promise.resolve()
+
 	getPassword(service: string, account: string): Promise<string | null> {
-		return getPassword(service, account)
-			.catch(e => {
-				if (e.message === CANCELLED) {
-					throw new CancelledError("user cancelled keychain unlock")
-				}
-				throw e
-			})
+		const newOp = this.lastOp
+						  .catch(noOp)
+						  .then(() => getPassword(service, account)
+							  .catch(e => {
+								  if (e.message === CANCELLED) {
+									  throw new CancelledError("user cancelled keychain unlock")
+								  }
+								  throw e
+							  }))
+		this.lastOp = newOp
+
+		return newOp
 	}
 
 	setPassword(service: string, account: string, password: string): Promise<void> {
-		return setPassword(service, account, password)
+		const newOp = this.lastOp
+						  .catch(noOp)
+						  .then(() => setPassword(service, account, password))
+		this.lastOp = newOp
+		return newOp
 	}
 }
