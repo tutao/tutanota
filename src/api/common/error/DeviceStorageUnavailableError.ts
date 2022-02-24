82f16f95e081b049a9a7bbe372c9445ec2ff7b71
nig <nig@tutao.de>
2022-02-24 11:21:28 +0100

    fix DeviceStorageUnavailableError.ts types and documentation

--- src/api/common/error/DeviceStorageUnavailableError.ts
+++ src/api/common/error/DeviceStorageUnavailableError.ts
@@ -1,9 +1,16 @@
 //@bundleInto:common-min
 
-import {TutanotaError} from "./TutanotaError"
+import {TutanotaError} from "./TutanotaError.js"
 
+/**
+ * this error is thrown when the client fails to get access to a safe storage for
+ * credentials, pushIdentifiers and alarms.
+ */
 export class DeviceStorageUnavailableError extends TutanotaError {
-	constructor(msg: string, error: Error) {
-		super("DeviceStorageUnavailableError", error ? msg + "> " + (error.stack ? error.stack : error.message) : msg)
+	constructor(msg: string, error: Error | null) {
+		const message = error
+			? msg + "> " + (error.stack ? error.stack : error.message)
+			: msg
+		super("DeviceStorageUnavailableError", message)
 	}
 }
