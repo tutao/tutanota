3aafc5f83c43bf08a5c149cd5badd339eee264a9
now <now@tutao.de>
2022-03-25 17:00:40 +0100

    Add new models for enum UsageTestMetric

--- src/api/common/TutanotaConstants.ts
+++ src/api/common/TutanotaConstants.ts
@@ -945,6 +945,7 @@ export const UsageTestStateToName = reverse(UsageTestState)
 
 export enum UsageTestMetricType {
 	Number = "0",
+	Enum = "1",
 }
 
 export const UsageTestMetricTypeToName = reverse(UsageTestMetricType)
