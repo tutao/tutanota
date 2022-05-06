2b6f1b69bf73ba5cb6f831f3078aae196ea8dc60
jom <jom@tutao.de>
2022-05-06 14:38:09 +0200

    refactor InterWindowEventBus interface
    we wanted messages to be defined as a single type to make it easier to know which messages are available, improve type inference and to avoid having to downcast

--- src/native/common/NativeInterface.ts
+++ src/native/common/NativeInterface.ts
@@ -1,7 +1,8 @@
 import type {Request} from "../../api/common/MessageDispatcher"
 import type {OfflineDbFacade} from "../../desktop/db/OfflineDbFacade"
 import {IWebauthn} from "../../misc/2fa/webauthn/IWebauthn.js"
-import {IInterWindowEventHandler, IInterWindowEventSender} from "../../desktop/ipc/IInterWindowEventBus"
+import {InterWindowEventHandler, InterWindowEventSender} from "./InterWindowEventBus.js";
+import {InterWindowEventTypes} from "./InterWindowEventTypes"
 import {IPostLoginAction} from "../../api/main/LoginController"
 
 export interface NativeInterface {
@@ -12,11 +13,11 @@ export interface NativeInterface {
 export interface ExposedNativeInterface {
 	webauthn: IWebauthn
 	offlineDbFacade: OfflineDbFacade
-	interWindowEventSender: IInterWindowEventSender
+	interWindowEventSender: InterWindowEventSender<InterWindowEventTypes>
 	postLoginActions: IPostLoginAction
 }
 
 /** What web interfaces can be accessed by the native part. */
 export interface ExposedWebInterface {
-	interWindowEventHandler: IInterWindowEventHandler,
+	interWindowEventHandler: InterWindowEventHandler<InterWindowEventTypes>,
 }
