3827fcbc7751804f15a7c92d9e62d001fda2a4e1
jom <jom@tutao.de>
2022-05-10 17:47:00 +0200

    move `exposeNativeInterface`
    it was in the wrong chunk

---
+++ src/api/common/ExposeNativeInterface.ts
@@ -0,0 +1,6 @@
+import {exposeRemote} from "./WorkerProxy.js";
+import type {ExposedNativeInterface, NativeInterface} from "../../native/common/NativeInterface.js";
+
+export function exposeNativeInterface(native: NativeInterface): ExposedNativeInterface {
+    return exposeRemote((request) => native.invokeNative(request))
+}
