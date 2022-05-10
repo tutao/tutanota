1d829e95973e823701907adf2f4eacb65bc85277
ivk <ivk@tutao.de>
2022-05-10 10:58:11 +0200

    [build] esbuild: do not inline translations

--- buildSrc/DevBuild.js
+++ buildSrc/DevBuild.js
@@ -4,13 +4,14 @@ import {prepareAssets} from "./Builder.js"
 import {build} from "esbuild"
 import {getTutanotaAppVersion} from "./buildUtils.js"
 import {$} from "zx"
+import "zx/globals"
 import {keytarNativePlugin, sqliteNativePlugin} from "./nativeLibraryEsbuildPlugin.js"
 import * as env from "./env.js"
 import {dependencyMap} from "./RollupConfig.js"
 import {esbuildPluginAliasPath} from "esbuild-plugin-alias-path"
 import {runStep} from "./runStep.js"
 
-export async function runDevBuild({stage, host, desktop, clean, watch}) {
+export async function runDevBuild({stage, host, desktop, clean}) {
 	if (clean) {
 		await runStep("Clean", async () => {
 			await fs.emptyDir("build")
@@ -40,14 +41,20 @@ async function buildWebPart({stage, host, version}) {
 	await runStep("Web: Assets", async () => {
 		await prepareAssets(stage, host, version)
 		await fs.promises.writeFile("build/worker-bootstrap.js", `importScripts("./polyfill.js")
-importScripts("./api/worker/worker.js")
+importScripts("./worker.js")
 `)
 	})
 
 	await runStep("Web: Esbuild", async () => {
 		await build({
-			entryPoints: ["src/app.ts", "src/api/worker/worker.ts"],
+			// Using named entry points so that it outputs build/worker.js and not build/api/worker/worker.js
+			entryPoints: {app: "src/app.ts", worker: "src/api/worker/worker.ts"},
 			outdir: "./build/",
+			// Why bundle at the moment:
+			// - We need to include all the imports: everything in src + libs. We could use wildcard in the future.
+			// - We can't have imports or dynamic imports in the worker because we can't start it as a module because of Firefox.
+			//     (see https://bugzilla.mozilla.org/show_bug.cgi?id=1247687)
+			//     We can theoretically compile it separately but it will be slower and more confusing.
 			bundle: true,
 			format: 'esm',
 			sourcemap: "linked",
@@ -57,6 +64,7 @@ importScripts("./api/worker/worker.js")
 			},
 			plugins: [
 				libDeps(),
+				externalTranslationsPlugin(),
 			],
 		})
 	})
@@ -67,6 +75,8 @@ async function buildDesktopPart({version}) {
 		await build({
 			entryPoints: ["src/desktop/DesktopMain.ts"],
 			outdir: "./build/desktop",
+			// Why we bundle at the moment:
+			// - We need to include all the imports: we currently use some node_modules directly, without pre-bundling them like rest of libs we can't avoid it
 			bundle: true,
 			format: 'cjs',
 			sourcemap: "linked",
@@ -86,7 +96,8 @@ async function buildDesktopPart({version}) {
 					nativeBindingPath: './keytar.node',
 					platform: process.platform,
 				}),
-				preludeEnvPlugin(env.create({staticUrl: null, version, mode: "Desktop", dist: false}))
+				preludeEnvPlugin(env.create({staticUrl: null, version, mode: "Desktop", dist: false})),
+				externalTranslationsPlugin(),
 			],
 		})
 	})
@@ -136,5 +147,31 @@ export function preludeEnvPlugin(env) {
 			const bannerStart = options.banner["js"] ? options.banner["js"] + "\n" : ""
 			options.banner["js"] = bannerStart + `globalThis.env = ${JSON.stringify(env, null, 2)};`
 		},
+	}
+}
+
+/** Do not embed translations in the source, compile them separately so that ouput is not as huge. */
+export function externalTranslationsPlugin() {
+	return {
+		name: "skip-translations",
+		setup(build) {
+			build.onResolve({filter: /\.\.\/translations\/.+/, namespace: "file"}, () => {
+				return {
+					external: true,
+				}
+			})
+			build.onEnd(async () => {
+				const translations = await globby("src/translations/*.ts")
+				await build.esbuild.build({
+					...build.initialOptions,
+					// No need for plugins there, also we don't want *this* plugin to be called again
+					plugins: [],
+					entryPoints: translations,
+					// So that it outputs build/translations/de.js instead of build/de.js
+					// (or build/desktop/translations/de.js instead of build/desktop/de.js)
+					outbase: "src",
+				})
+			})
+		}
 	}
 }
