f9c99216b7248960d022c16cc241b85706db82b7
ivk <ivk@tutao.de>
2022-05-18 10:42:38 +0200

    [android] Fix credential encryption after Android upgrade, fix #4144
    For keys generated on previous Android versions (with old parameters)
    we need to keep using the same auth mechanisms as before otherwise we
    just fail. We fall back to them now as we get an exception and old
    key is the only reason it should be thrown.

--- app-android/app/src/main/java/de/tutao/tutanota/credentials/CredentialsEncryptionFromAPI30.java
+++ app-android/app/src/main/java/de/tutao/tutanota/credentials/CredentialsEncryptionFromAPI30.java
@@ -1,6 +1,8 @@
 package de.tutao.tutanota.credentials;
 
 import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.UserNotAuthenticatedException;
+import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 import androidx.biometric.BiometricManager;
@@ -21,6 +23,8 @@ import de.tutao.tutanota.Utils;
 
 @RequiresApi(30)
 public final class CredentialsEncryptionFromAPI30 implements ICredentialsEncryption {
+	private static final String TAG = "CredEnc30";
+
 	private final AndroidKeyStoreFacade keyStoreFacade;
 	private final FragmentActivity activity;
 	private final AuthenticationPrompt authenticationPrompt;
@@ -34,8 +38,7 @@ public final class CredentialsEncryptionFromAPI30 implements ICredentialsEncrypt
 	@Override
 	public String encryptUsingKeychain(String base64EncodedData, CredentialEncryptionMode encryptionMode) throws KeyStoreException, CryptoError, CredentialAuthenticationException, KeyPermanentlyInvalidatedException {
 		byte[] dataToEncrypt = Utils.base64ToBytes(base64EncodedData);
-		Cipher cipher = keyStoreFacade.getCipherForEncryptionMode(encryptionMode);
-		authenticateCipher(cipher, encryptionMode);
+		Cipher cipher = this.getAuthenticatedCipherForEncryptionModelWithFallback(encryptionMode);
 		byte[] encryptedBytes = keyStoreFacade.encryptData(dataToEncrypt, cipher);
 		return Utils.bytesToBase64(encryptedBytes);
 	}
@@ -43,8 +46,8 @@ public final class CredentialsEncryptionFromAPI30 implements ICredentialsEncrypt
 	@Override
 	public String decryptUsingKeychain(String base64EncodedEncryptedData, CredentialEncryptionMode encryptionMode) throws KeyStoreException, CryptoError, CredentialAuthenticationException, KeyPermanentlyInvalidatedException {
 		byte[] dataToDecrypt = Utils.base64ToBytes(base64EncodedEncryptedData);
-		Cipher cipher = keyStoreFacade.getCipherForDecryptionMode(encryptionMode, dataToDecrypt);
-		authenticateCipher(cipher, encryptionMode);
+		Cipher cipher = this.getAuthenticatedCipherForDecryptionModelWithFallback(dataToDecrypt, encryptionMode);
+
 		byte[] decryptedBytes = keyStoreFacade.decryptData(dataToDecrypt, cipher);
 		return Utils.bytesToBase64(decryptedBytes);
 	}
@@ -61,6 +64,46 @@ public final class CredentialsEncryptionFromAPI30 implements ICredentialsEncrypt
 			supportedModes.add(CredentialEncryptionMode.ENCRYPTION_MODE_SYSTEM_PASSWORD);
 		}
 		return supportedModes;
+	}
+
+	private Cipher getAuthenticatedCipherForEncryptionModelWithFallback(CredentialEncryptionMode encryptionMode) throws CredentialAuthenticationException, KeyStoreException, KeyPermanentlyInvalidatedException {
+		Cipher cipher;
+		try {
+			cipher = keyStoreFacade.getCipherForEncryptionMode(encryptionMode);
+		} catch (KeyStoreException e) {
+			// If the key was created with the old Android version then we need to use old mechanism
+			if (e.getCause() instanceof UserNotAuthenticatedException) {
+				String message = "Got UserNotAuthenticatedException for enc w/ mode " + encryptionMode + " , likely an old key, falling back to old auth";
+				Log.i(TAG, message, e);
+				this.authenticationPrompt.authenticate(activity, createPromptInfo(encryptionMode));
+				return keyStoreFacade.getCipherForEncryptionMode(encryptionMode);
+			} else {
+				throw e;
+			}
+		}
+		// If we got here then we use "modern" auth method
+		authenticateCipher(cipher, encryptionMode);
+		return cipher;
+	}
+
+	private Cipher getAuthenticatedCipherForDecryptionModelWithFallback(byte[] dataToDecrypt, CredentialEncryptionMode encryptionMode) throws CredentialAuthenticationException, KeyStoreException, KeyPermanentlyInvalidatedException, CryptoError {
+		Cipher cipher;
+		try {
+			cipher = keyStoreFacade.getCipherForDecryptionMode(encryptionMode, dataToDecrypt);
+		} catch (KeyStoreException e) {
+			// If the key was created with the old Android version then we need to use old mechanism
+			if (e.getCause() instanceof UserNotAuthenticatedException) {
+				String message = "Got UserNotAuthenticatedException for dec w/ mode " + encryptionMode + " , likely an old key, falling back to old auth";
+				Log.i(TAG, message, e);
+				this.authenticationPrompt.authenticate(activity, createPromptInfo(encryptionMode));
+				return keyStoreFacade.getCipherForDecryptionMode(encryptionMode, dataToDecrypt);
+			} else {
+				throw e;
+			}
+		}
+		// If we got here then we use "modern" auth method
+		authenticateCipher(cipher, encryptionMode);
+		return cipher;
 	}
 
 	private void authenticateCipher(Cipher cipher, CredentialEncryptionMode encryptionMode) throws CredentialAuthenticationException {
@@ -95,5 +138,29 @@ public final class CredentialsEncryptionFromAPI30 implements ICredentialsEncrypt
 		BiometricPrompt.PromptInfo promptInfo = promptInfoBuilder.build();
 
 		this.authenticationPrompt.authenticateCryptoObject(activity, promptInfo, cryptoObject);
+	}
+
+	/**
+	 * Prepare prompt info to authenticate the "legacy" way. Once we don't need to support old keys we can get rid of
+	 * it.
+	 */
+	private BiometricPrompt.PromptInfo createPromptInfo(CredentialEncryptionMode mode) {
+		if (mode == CredentialEncryptionMode.ENCRYPTION_MODE_BIOMETRICS) {
+			BiometricPrompt.PromptInfo.Builder promptInfoBuilder = new BiometricPrompt.PromptInfo.Builder()
+					.setTitle(activity.getString(R.string.unlockCredentials_action))
+					// see AuthentorUtils#isSupportedCombination from androidx.biometrics
+					.setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+					.setNegativeButtonText(activity.getString(android.R.string.cancel));
+			return promptInfoBuilder.build();
+
+		} else if (mode == CredentialEncryptionMode.ENCRYPTION_MODE_SYSTEM_PASSWORD) {
+			BiometricPrompt.PromptInfo.Builder promptInfoBuilder = new BiometricPrompt.PromptInfo.Builder()
+					.setTitle(activity.getString(R.string.unlockCredentials_action))
+					// see AuthentorUtils#isSupportedCombination from androidx.biometrics
+					.setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL | BiometricManager.Authenticators.BIOMETRIC_WEAK);
+			return promptInfoBuilder.build();
+		} else {
+			throw new AssertionError("");
+		}
 	}
 }
