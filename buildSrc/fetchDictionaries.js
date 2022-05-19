7a9347ba8bf29eac333cd0f896f3adfc05b88e42
nig <nig@tutao.de>
2022-05-19 10:20:09 +0200

    changes after review by ivk & jom

--- buildSrc/fetchDictionaries.js
+++ buildSrc/fetchDictionaries.js
@@ -64,14 +64,13 @@ async function publishDebPackage() {
 
 	console.log("create", deb)
 	exitOnFail(spawnSync("/usr/local/bin/fpm", `${commonArgs} -n tutanota-desktop-dicts -v ${electronVersion} dictionaries/=${target}-desktop/dictionaries`.split(" "), {
-		cwd: path.join(process.cwd(), "build"),
+		cwd: "build",
 		stdio: [process.stdin, process.stdout, process.stderr]
 	}))
 
 	// copy spell checker dictionaries.
 	console.log("copying dictionaries")
 	exitOnFail(spawnSync("/bin/cp", `-f build/${deb} /opt/repository/tutanota/`.split(" "), {
-		cwd: process.cwd(),
 		stdio: [process.stdin, process.stdout, process.stderr]
 	}))
 	exitOnFail(spawnSync("/bin/chmod", `o+r /opt/repository/tutanota/${deb}`.split(" "), {
