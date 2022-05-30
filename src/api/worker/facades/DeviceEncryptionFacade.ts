40153d79509d3fec9c84acd1f938ec05f0b127ea
jom <jom@tutao.de>
2022-05-30 10:14:14 +0200

    Collapse DeviceEncryptionFacade.ts
    #4211

--- src/api/worker/facades/DeviceEncryptionFacade.ts
+++ src/api/worker/facades/DeviceEncryptionFacade.ts
@@ -1,39 +1,29 @@
-/* Facade to interact with encryption mechanisms using device capabilities. You can use this facade if you need to encrypt data from the
- *  main thread - the facade will delegate all the actual encryption operations to the native thread.
- * */
 import {aes256Decrypt, aes256Encrypt, aes256RandomKey, bitArrayToUint8Array, CryptoError, generateIV, uint8ArrayToBitArray} from "@tutao/tutanota-crypto"
 import {CryptoError as TutanotaCryptoError} from "../../common/error/CryptoError.js"
 
-export interface DeviceEncryptionFacade {
+export class DeviceEncryptionFacade {
+
 	/**
 	 * Generates an encryption key.
 	 */
-	generateKey(): Promise<Uint8Array>
+	async generateKey(): Promise<Uint8Array> {
+		return bitArrayToUint8Array(aes256RandomKey())
+	}
 
 	/**
 	 * Encrypts {@param data} using {@param deviceKey}.
 	 * @param deviceKey Key used for encryption
 	 * @param data Data to encrypt.
 	 */
-	encrypt(deviceKey: Uint8Array, data: Uint8Array): Promise<Uint8Array>
+	async encrypt(deviceKey: Uint8Array, data: Uint8Array): Promise<Uint8Array> {
+		return aes256Encrypt(uint8ArrayToBitArray(deviceKey), data, generateIV())
+	}
 
 	/**
 	 * Decrypts {@param encryptedData} using {@param deviceKey}.
 	 * @param deviceKey Key used for encryption
 	 * @param encryptedData Data to be decrypted.
 	 */
-	decrypt(deviceKey: Uint8Array, encryptedData: Uint8Array): Promise<Uint8Array>
-}
-
-export class Aes256DeviceEncryptionFacade implements DeviceEncryptionFacade {
-	async generateKey(): Promise<Uint8Array> {
-		return bitArrayToUint8Array(aes256RandomKey())
-	}
-
-	async encrypt(deviceKey: Uint8Array, data: Uint8Array): Promise<Uint8Array> {
-		return aes256Encrypt(uint8ArrayToBitArray(deviceKey), data, generateIV())
-	}
-
 	async decrypt(deviceKey: Uint8Array, encryptedData: Uint8Array): Promise<Uint8Array> {
 		try {
 			return aes256Decrypt(uint8ArrayToBitArray(deviceKey), encryptedData)
