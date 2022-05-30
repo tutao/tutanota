5068c5c77986fb7d2813709f6919f9f5223adf83
jom <jom@tutao.de>
2022-05-30 14:16:58 +0200

    load all calendars sequentially
    when doing an initial load of all a user's calendars, it is happening as a bunch of parallel requests. This can lead to the user getting suspended or worse, blocked.

--- src/offline/CachePostLoginAction.ts
+++ src/offline/CachePostLoginAction.ts
@@ -3,9 +3,9 @@ import {CalendarModel} from "../calendar/model/CalendarModel.js"
 import {CalendarEventTypeRef} from "../api/entities/tutanota/TypeRefs.js"
 import {CUSTOM_MIN_ID} from "../api/common/utils/EntityUtils.js"
 import {EntityClient} from "../api/common/EntityClient.js"
-import {DesktopConfigKey} from "../desktop/config/ConfigKeys.js"
 import {ProgressTracker} from "../api/main/ProgressTracker.js"
-import {SettingsFacade} from "../native/common/generatedipc/SettingsFacade.js"
+import {promiseMap} from "@tutao/tutanota-utils"
+import {NoopProgressMonitor} from "../api/common/utils/ProgressMonitor.js"
 
 
 export class CachePostLoginAction implements IPostLoginAction {
@@ -15,27 +15,25 @@ export class CachePostLoginAction implements IPostLoginAction {
 		private readonly entityClient: EntityClient,
 		private readonly progressTracker: ProgressTracker,
 		private readonly logins: LoginController,
-		private readonly desktopSettingsFacade: SettingsFacade | null
 	) {
 
 	}
 
 	async onFullLoginSuccess(loggedInEvent: LoggedInEvent): Promise<void> {
-		if (await this.desktopSettingsFacade?.getBooleanConfigValue(DesktopConfigKey.offlineStorageEnabled)) {
 			// 3 work to load calendar info, 2 work to load short and long events
-			const workPerCalendar = 3 + 2
-			const totalWork = this.logins.getUserController().getCalendarMemberships().length * workPerCalendar
-			const monitorHandle = this.progressTracker.registerMonitor(totalWork)
-			const progressMonitor = this.progressTracker.getMonitor(monitorHandle)
-			const calendarInfos = await this.calendarModel.loadCalendarInfos(progressMonitor!)
-			const loadingPromises = []
-			for (const {groupRoot} of calendarInfos.values()) {
-				loadingPromises.push(this.entityClient.loadAll(CalendarEventTypeRef, groupRoot.longEvents, CUSTOM_MIN_ID).then(() => progressMonitor?.workDone(1)))
-				loadingPromises.push(this.entityClient.loadAll(CalendarEventTypeRef, groupRoot.shortEvents, CUSTOM_MIN_ID).then(() => progressMonitor?.workDone(1)))
-			}
-			await Promise.all(loadingPromises)
-			progressMonitor?.completed()
-		}
+		const workPerCalendar = 3 + 2
+		const totalWork = this.logins.getUserController().getCalendarMemberships().length * workPerCalendar
+		const monitorHandle = this.progressTracker.registerMonitor(totalWork)
+		const progressMonitor = this.progressTracker.getMonitor(monitorHandle) ?? new NoopProgressMonitor()
+		const calendarInfos = await this.calendarModel.loadCalendarInfos(progressMonitor)
+
+		await promiseMap(calendarInfos.values(), async ({groupRoot}) => {
+			await Promise.all([
+				this.entityClient.loadAll(CalendarEventTypeRef, groupRoot.longEvents, CUSTOM_MIN_ID).then(() => progressMonitor.workDone(1)),
+				this.entityClient.loadAll(CalendarEventTypeRef, groupRoot.shortEvents, CUSTOM_MIN_ID).then(() => progressMonitor.workDone(1))
+			])
+		})
+		progressMonitor.completed()
 	}
 
 	async onPartialLoginSuccess(loggedInEvent: LoggedInEvent): Promise<void> {
