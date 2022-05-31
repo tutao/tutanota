fdfa06b922a5f02e6b3681b73b8220e639f352c1
nig <nig@tutao.de>
2022-05-31 13:12:34 +0200

    [iOS] Fix warning in TUTEncodingConverter
    Co-authored-by: ivk <ivk@tutao.de>

--- app-ios/tutanota/Sources/Utils/TUTEncodingConverter.m
+++ app-ios/tutanota/Sources/Utils/TUTEncodingConverter.m
@@ -5,13 +5,14 @@
 }
 
 + (NSData*)hexToBytes:(NSString*)hex {
+  const char* utf8string = hex.UTF8String;
 	NSMutableData *bytes= [[NSMutableData alloc] init];
-	unsigned char whole_byte;
+	unsigned long whole_byte;
 	char byte_chars[3] = {'\0','\0','\0'};
 	for (int i = 0; i < ([hex length] / 2); i++) {
-		byte_chars[0] = [hex characterAtIndex:i*2];
-		byte_chars[1] = [hex characterAtIndex:i*2+1];
-		whole_byte = strtol(byte_chars, NULL, 16);
+		byte_chars[0] = utf8string[i*2];
+		byte_chars[1] = utf8string[i*2+1];
+		whole_byte = strtoul(byte_chars, NULL, 16);
 		[bytes appendBytes:&whole_byte length:1];
 	}
 	return bytes;
