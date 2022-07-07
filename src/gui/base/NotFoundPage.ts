bc6c1f13bea20983a6e34cab733e0ac4ad87075b
jom <jom@tutao.de>
2022-07-07 13:04:49 +0200

    Replace InfoView with NotFoundPage
    There are 3 usages of InfoView  (one in the admin client)
    
    the app.ts and admin client usages were identical, then only difference in ContactFormView was that no back button was rendered. I collapsed all three to use the same implementation and made InfoView into a more specific NotFoundPage component. The main difference from before is that ContactFormView now shows a back button in the 404 page, which seems fine to have at the benefit of a simpler component
    
    #2559

---
+++ src/gui/base/NotFoundPage.ts
@@ -0,0 +1,31 @@
+import m, {Children, Component} from "mithril"
+import {px} from "../size"
+import {AriaLandmarks, landmarkAttrs} from "../AriaUtils"
+import {assertMainOrNode} from "../../api/common/Env"
+import {lang} from "../../misc/LanguageViewModel.js"
+import {Button, ButtonType} from "./Button.js"
+
+assertMainOrNode()
+
+export class NotFoundPage implements Component<void> {
+	view(): Children {
+		return m(".main-view.flex.items-center.justify-center.mlr" + landmarkAttrs(AriaLandmarks.Main),
+			{
+				style: {
+					"max-height": px(450),
+				},
+			},
+			m(".message.center.max-width-l", [
+				m("h2", "404"),
+				[
+					m("p", lang.get("notFound404_msg")),
+					m(Button, {
+						label: "back_action",
+						click: () => window.history.back(),
+						type: ButtonType.Primary,
+					}),
+				]
+			]),
+		)
+	}
+}
