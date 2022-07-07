f6329ccb3158118de30a9b57ce78ea1fa68a12d9
jom <jom@tutao.de>
2022-07-07 13:09:46 +0200

    Rename LabelN to Label
    It is used in Admin client so we can't delete it as suggested in the issue (may or may not want to move it there)
    
    #2559

--- src/gui/base/Label.ts
+++ src/gui/base/Label.ts
@@ -1,28 +1,26 @@
 import m, {Component, Vnode} from "mithril"
 import {px, size} from "../size"
 import {inputLineHeight} from "./TextFieldN"
-import {lang} from "../../misc/LanguageViewModel"
 import type {TranslationKey} from "../../misc/LanguageViewModel"
+import {lang} from "../../misc/LanguageViewModel"
 import type {lazy} from "@tutao/tutanota-utils"
 
 export type LabelAttrs = {
 	label: TranslationKey | lazy<string>
 }
 
-export class LabelN implements Component<LabelAttrs> {
+export class Label implements Component<LabelAttrs> {
 	view(vnode: Vnode<LabelAttrs>) {
 		return m(".rel.pt", [
 			m("label.abs.text-ellipsis.noselect.backface_fix.z1.i.pr-s.small", lang.getMaybeLazy(vnode.attrs.label)),
-			m(
-				".flex.flex-column.flex-end",
+			m(".flex.flex-column.flex-end",
 				{
 					style: {
 						"min-height": px(size.button_height + 2),
 					},
 				},
 				[
-					m(
-						".mt-form",
+					m(".mt-form",
 						{
 							style: {
 								lineHeight: px(inputLineHeight),
