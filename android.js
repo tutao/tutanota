48d3fe455f44dd7620844ed31182f26be6b8a892
ganthern <nig@tutao.de>
2022-07-15 09:56:16 +0200

    [android] use zx in android.js build script
    zx uses bash as returned by node's "which" module by default
    all platforms should have it available since it comes with git
    for windows.

--- android.js
+++ android.js
@@ -9,16 +9,14 @@
  *  'ANDROID_HOME'
  */
 import {Argument, Option, program} from "commander"
-import fs from "fs"
-import {execFileSync} from "child_process"
 import {runDevBuild} from "./buildSrc/DevBuild.js"
 import {prepareMobileBuild} from "./buildSrc/prepareMobileBuild.js"
 import {buildWebapp} from "./buildSrc/buildWebapp.js"
 import {getTutanotaAppVersion, measure} from "./buildSrc/buildUtils.js"
 import path from "path"
+import {$, cd} from 'zx'
 
-const log = (...messages) => console.log("\nBUILD:", ...messages, "\n")
-
+const log = (...messages) => console.log(chalk.green("\nBUILD:"), ...messages, "\n")
 
 await program
 	.usage('[options] [test|prod|local|host <url>] ')
@@ -30,23 +28,28 @@ await program
 	.addOption(new Option('-b, --buildtype <type>', 'gradle build type')
 		.choices(["debugDist", "debug", "release", "releaseTest"])
 		.default("release"))
+	.addOption(new Option('-i, --install', "call adb install after build"))
 	.addOption(new Option('-w --webclient <client>', 'choose web client build')
 		.choices(["make", "dist"])
 		.default("dist"))
-	.action(async (stage, host, options) => {
+	.action(async (stage, host, {webclient, buildtype}) => {
 		if (stage === "host" && host == null || stage !== "host" && host != null) {
 			program.outputHelp()
 			process.exit(1)
 		}
 
-		const {webclient, buildtype} = options
-
-		await buildAndroid({
+		const apk = await buildAndroid({
 			stage: stage ?? 'prod',
 			host: host,
 			webClient: webclient,
 			buildType: buildtype,
 		})
+
+		if (options.install) {
+			await $`adb install ${apk}`
+			// would be cool, but needs to figure out the correct app to start:
+			// await $`adb shell am start -n de.tutao.tutanota/de.tutao.tutanota.MainActivity`
+		}
 	})
 	.parseAsync(process.argv)
 
@@ -79,40 +82,22 @@ async function buildAndroid({stage, host, buildType, webClient}) {
 	await prepareMobileBuild(webClient)
 
 	try {
-		log("cleaning 'build/app-android'")
-		await fs.promises.rm("build/app-android", {recursive: true})
+		await $`rm -r build/app-android`
 	} catch (e) {
 		// Ignoring the error if the folder is not there
 	}
 
-	log("Starting build: ", buildType)
-
-	const {version} = JSON.parse(await fs.promises.readFile("package.json", "utf8"))
+	const {version} = JSON.parse(await $`cat package.json`.quiet())
 	const apkName = `tutanota-tutao-${buildType}-${version}.apk`
 	const apkPath = `app-android/app/build/outputs/apk/tutao/${buildType}/${apkName}`
-
-	runCommand('./gradlew', [`assembleTutao${buildType}`], {
-		cwd: './app-android/',
-	})
-
-	await fs.promises.mkdir("build/app-android", {recursive: true})
-
 	const outPath = `./build/app-android/${apkName}`
-	await fs.promises.rename(apkPath, outPath)
+	cd("./app-android")
+	await $`./gradlew assembleTutao${buildType}`
+	cd("..")
+	await $`mkdir -p build/app-android`
+	await $`mv ${apkPath} ${outPath}`
 
 	log(`Build complete. The APK is located at: ${outPath}`)
 
-	// runDevBuild spawns some child processes from the BuildServerClient,
-	// ideally we would detach from them inside as needed but for now we just hard exit
-	process.exit(0)
-}
-
-function runCommand(command, args, options) {
-	try {
-		log("command:", `${command} ${args.join(" ")}`)
-		return execFileSync(command, args, options)
-	} catch (e) {
-		// original e contains lots of noise. `e.stack` has enough for debugging
-		throw new Error(e.stack)
-	}
+	return outPath
 }
