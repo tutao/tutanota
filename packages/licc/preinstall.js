2bc76340eeca9340193913911ac6685facbfcd12
ivk <ivk@tutao.de>
2022-07-21 15:50:50 +0200

    [build] Make licc postinstall script platform-independent

---
+++ packages/licc/preinstall.js
@@ -0,0 +1,9 @@
+/** @file little script to prepare licc install. Implemented as a node script to please Windows. */
+import * as fs from "fs"
+
+// mkdir -p dist
+fs.mkdirSync("dist", {recursive: true})
+// touch dist/cli.js
+fs.closeSync(fs.openSync("dist/cli.js", 'w'))
+
+
