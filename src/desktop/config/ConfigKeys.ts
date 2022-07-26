603579b1e82c85cc775792301201af1824815a56
nig <nig@tutao.de>
2022-07-26 15:16:22 +0200

    [desktop] migrate old localStorage to the new origin
    before opening the actual web app:
    * create an invisible window without content at the file:// origin
    * b64-encode the "tutanotaConfig" object from its localstorage if there
    * return it or the string "null" to the main thread
    * close the window
    * delete the complete localStorage of the file:// origin
    * if the extracted string is "null", we're finished, otherwise:
    * open an invisible window without content at the asset:// origin
    * send an executeJavascript call with the encoded config object
    * if there are no credentials in the asset:// localstorage, decode the
      sent config object and replace the asset:// localstorage one with it.
    * this will be attempted once for each install
    
    #4412

--- src/desktop/config/ConfigKeys.ts
+++ src/desktop/config/ConfigKeys.ts
@@ -16,6 +16,7 @@ export enum DesktopConfigKey {
 	spellcheck = "spellcheck",
 	selectedTheme = "selectedTheme",
 	themes = "themes",
+	webConfigLocation = "webConfigLocation"
 }
 
 export enum DesktopConfigEncKey {
