15a48818c6d61ff2f4d9dfcc0975f2da8d9a6dc6
ivk <ivk@tutao.de>
2022-08-25 13:35:36 +0200

    [android] Fix temp root directory
    the `root` temp directory was interpolated incorrectly. This generally
    worked but it means that clearing app data was not done correctly

--- app-android/app/src/main/java/de/tutao/tutanota/AndroidFileFacade.kt
+++ app-android/app/src/main/java/de/tutao/tutanota/AndroidFileFacade.kt
@@ -40,7 +40,7 @@ class TempDir(
 	private val randomTempDirectory = random.bytes(16).toBase64().base64ToBase64Url()
 
 	// We can only read from the temp/ subdir as configured in paths.xml
-	val root get() = File(context.filesDir, "temp/$this.randomTempDirectory").apply { mkdirs() }
+	val root get() = File(context.filesDir, "temp/$randomTempDirectory").apply { mkdirs() }
 	val encrypt get() = File(context.filesDir, "temp/$randomTempDirectory/encrypted").apply { mkdirs() }
 	val decrypt get() = File(context.filesDir, "temp/$randomTempDirectory/decrypted").apply { mkdirs() }
 }
