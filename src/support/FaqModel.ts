25a1a475a9d55ec911dc4a744aa0c7ec2249fecc
nig <nig@tutao.de>
2022-12-02 16:42:22 +0100

    [refactor] cleanup FaqModel.ts
    pi #40

--- src/support/FaqModel.ts
+++ src/support/FaqModel.ts
@@ -1,11 +1,10 @@
 import type {LanguageViewModelType} from "../misc/LanguageViewModel"
 import {lang, LanguageViewModel} from "../misc/LanguageViewModel"
-import {assert, downcast} from "@tutao/tutanota-utils"
+import {assert, delay, downcast, LazyLoaded, promiseMap} from "@tutao/tutanota-utils"
 import {search} from "../api/common/utils/PlainTextSearch"
-import {LazyLoaded} from "@tutao/tutanota-utils"
-import {htmlSanitizer} from "../misc/HtmlSanitizer"
-import {delay, promiseMap} from "@tutao/tutanota-utils"
 import type {SanitizedHTML} from "../misc/HtmlSanitizer"
+import {htmlSanitizer} from "../misc/HtmlSanitizer"
+import {ProgrammingError} from "../api/common/error/ProgrammingError.js"
 
 export type FaqEntry = {
 	id: string
@@ -23,39 +22,56 @@ const MARKDOWN_SUFFIX = "_markdown"
 /**
  * Loads FAQ entries from tutanota.com for different languages and allows searching
  *
- *
+ * NOTE: it's only exported for testing!
  */
-// visibility only for testing
 export class FaqModel {
-	private _list: Array<FaqEntry> | null = null
-	private _currentLanguageCode: string | null = null
-	private _faqLanguages: LanguageViewModelType | null = null
-	private _lazyLoaded: LazyLoaded<void>
+	private list: Array<FaqEntry> | null = null
+	private currentLanguageCode: string | null = null
+	private faqLanguages: LanguageViewModelType | null = null
+	private lazyLoaded: LazyLoaded<void>
 
 	private get faqLang(): LanguageViewModel {
-		if (this._faqLanguages == null) {
-			throw new Error("Not initialized!")
+		if (this.faqLanguages == null) {
+			throw new ProgrammingError("faq not initialized!")
 		}
-		return this._faqLanguages
+		return this.faqLanguages
 	}
 
 	constructor() {
-		this._lazyLoaded = new LazyLoaded(() => {
+		this.lazyLoaded = new LazyLoaded(() => {
 			return Promise.all([this.fetchFAQ("en"), this.fetchFAQ(lang.code)]).then(([defaultTranslations, currentLanguageTranslations]) => {
 				if (defaultTranslations != null || currentLanguageTranslations != null) {
 					const faqLanguageViewModel = new LanguageViewModel()
 					faqLanguageViewModel.initWithTranslations(lang.code, lang.languageTag, defaultTranslations, currentLanguageTranslations)
-					this._faqLanguages = faqLanguageViewModel
+					this.faqLanguages = faqLanguageViewModel
 				}
 			})
 		})
 	}
 
-	init(): Promise<void> {
-		return this._lazyLoaded.getAsync()
+	async init(): Promise<void> {
+		await this.lazyLoaded.getAsync()
+		this.getList()
 	}
 
-	async fetchFAQ(langCode: string): Promise<Translation> {
+	/**
+	 * will return all faq entries that contain the given query and mark the query occurrences
+	 * with <mark> tags.
+	 */
+	search(query: string): ReadonlyArray<FaqEntry> {
+		const cleanQuery = query.trim()
+
+		if (cleanQuery === "") {
+			return []
+		} else {
+			return search(cleanQuery, this.getList(), ["tags", "title", "text"], true)
+		}
+	}
+
+	/**
+	 * fetch the entries for the given lang code from the web site
+	 */
+	private async fetchFAQ(langCode: string): Promise<Translation> {
 		const faqPath = `https://tutanota.com/faq-entries/${langCode}.json`
 		const keys = await fetch(faqPath)
 			.then(response => response.json())
@@ -73,7 +89,7 @@ export class FaqModel {
 				blockExternalContent: false,
 			})
 			// Delay to spread sanitize() calls between event loops.
-			// Otherwise we stop main thread for way too long and UI gets laggy.
+			// otherwise, we stop main thread for way too long and UI gets laggy.
 			await delay(1)
 			return [key, sanitized.html]
 		})
@@ -84,34 +100,31 @@ export class FaqModel {
 		}
 	}
 
-	getList(): Array<FaqEntry> {
-		if (this._list == null && this._faqLanguages == null) {
+	/**
+	 * return the current faqEntry list if it fits the current language code
+	 * otherwise, recreate the list for current lang and then return it
+	 */
+	private getList(): Array<FaqEntry> {
+		if (this.list == null && this.faqLanguages == null) {
 			return []
 		}
 
-		if (this._list == null || this._currentLanguageCode !== lang.code) {
-			this._currentLanguageCode = lang.code
+		if (this.list == null || this.currentLanguageCode !== lang.code) {
+			this.currentLanguageCode = lang.code
 			const faqNames = Object.keys(this.faqLang.fallback.keys)
-			this._list = faqNames
+			this.list = faqNames
 				.filter(key => key.startsWith(FAQ_PREFIX) && key.endsWith(MARKDOWN_SUFFIX))
 				.map((titleKey: string) => titleKey.substring(FAQ_PREFIX.length, titleKey.indexOf(MARKDOWN_SUFFIX)))
 				.map((name: string) => this.createFAQ(name))
 		}
 
-		return this._list
+		return this.list
 	}
 
-	search(query: string): ReadonlyArray<FaqEntry> {
-		const cleanQuery = query.trim()
-
-		if (cleanQuery === "") {
-			return []
-		} else {
-			return search(cleanQuery, this.getList(), ["tags", "title", "text"], true)
-		}
-	}
-
-	createFAQ(id: string): FaqEntry {
+	/**
+	 * convert the raw translations for an id to a structured FaqEntry
+	 */
+	private createFAQ(id: string): FaqEntry {
 		return {
 			id: id,
 			title: this.faqLang.get(downcast(`faq.${id}_title`)),
@@ -120,7 +133,7 @@ export class FaqModel {
 		}
 	}
 
-	getTags(id: string): string {
+	private getTags(id: string): string {
 		try {
 			return this.faqLang.get(downcast(id))
 		} catch (e) {
