51cf2bd9996a1e623187184b660b2703801a1cd4
nig <nig@tutao.de>
2022-12-21 15:11:16 +0100

    [android] catch error after failing to create mail editor from native.
    the error message may contain personal data, so we're not logging
    the entire error but try to extract the name of the underlying error.
    
    close #4874

--- app-android/app/src/main/java/de/tutao/tutanota/MainActivity.kt
+++ app-android/app/src/main/java/de/tutao/tutanota/MainActivity.kt
@@ -40,6 +40,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.File
@@ -86,19 +88,19 @@ class MainActivity : FragmentActivity() {
 		val fileFacade = AndroidFileFacade(this, LocalNotificationsFacade(this), SecureRandom())
 		val cryptoFacade = AndroidNativeCryptoFacade(this, fileFacade.tempDir)
 		sseStorage = SseStorage(
-			AppDatabase.getDatabase(this, false),
-			createAndroidKeyStoreFacade(cryptoFacade)
+				AppDatabase.getDatabase(this, false),
+				createAndroidKeyStoreFacade(cryptoFacade)
 		)
 		val alarmNotificationsManager = AlarmNotificationsManager(
-			sseStorage,
-			cryptoFacade,
-			SystemAlarmFacade(this),
-			LocalNotificationsFacade(this)
+				sseStorage,
+				cryptoFacade,
+				SystemAlarmFacade(this),
+				LocalNotificationsFacade(this)
 		)
 		val nativePushFacade = AndroidNativePushFacade(
-			this,
-			sseStorage,
-			alarmNotificationsManager
+				this,
+				sseStorage,
+				alarmNotificationsManager
 		)
 
 		val ipcJson = Json { ignoreUnknownKeys = true }
@@ -112,22 +114,22 @@ class MainActivity : FragmentActivity() {
 		val webauthnFacade = AndroidWebauthnFacade(this, ipcJson)
 
 		val globalDispatcher = AndroidGlobalDispatcher(
-			ipcJson,
-			commonSystemFacade,
-			fileFacade,
-			AndroidMobileSystemFacade(contact, fileFacade, this),
-			CredentialsEncryptionFactory.create(this, cryptoFacade),
-			cryptoFacade,
-			nativePushFacade,
-			sqlCipherFacade,
-			themeFacade,
-			webauthnFacade,
+				ipcJson,
+				commonSystemFacade,
+				fileFacade,
+				AndroidMobileSystemFacade(contact, fileFacade, this),
+				CredentialsEncryptionFactory.create(this, cryptoFacade),
+				cryptoFacade,
+				nativePushFacade,
+				sqlCipherFacade,
+				themeFacade,
+				webauthnFacade,
 		)
 		remoteBridge = RemoteBridge(
-			ipcJson,
-			this,
-			globalDispatcher,
-			commonSystemFacade,
+				ipcJson,
+				this,
+				globalDispatcher,
+				commonSystemFacade,
 		)
 
 		themeFacade.applyCurrentTheme()
@@ -175,7 +177,7 @@ class MainActivity : FragmentActivity() {
 					startActivity(intent)
 				} catch (e: ActivityNotFoundException) {
 					Toast.makeText(this@MainActivity, "Could not open link: $url", Toast.LENGTH_SHORT)
-						.show()
+							.show()
 				}
 				return true
 			}
@@ -185,16 +187,16 @@ class MainActivity : FragmentActivity() {
 				return if (request.method == "OPTIONS") {
 					Log.v(TAG, "replacing OPTIONS response to $url")
 					WebResourceResponse(
-						"text/html",
-						"UTF-8",
-						200,
-						"OK",
-						mutableMapOf(
-							"Access-Control-Allow-Origin" to "*",
-							"Access-Control-Allow-Methods" to "POST, GET, PUT, DELETE",
-							"Access-Control-Allow-Headers" to "*"
-						),
-						null
+							"text/html",
+							"UTF-8",
+							200,
+							"OK",
+							mutableMapOf(
+									"Access-Control-Allow-Origin" to "*",
+									"Access-Control-Allow-Methods" to "POST, GET, PUT, DELETE",
+									"Access-Control-Allow-Headers" to "*"
+							),
+							null
 					)
 				} else if (request.method == "GET" && url.toString().startsWith(BASE_WEB_VIEW_URL)) {
 					Log.v(TAG, "replacing asset GET response to ${url.path}")
@@ -205,22 +207,22 @@ class MainActivity : FragmentActivity() {
 						if (!assetPath.startsWith(BuildConfig.RES_ADDRESS)) throw IOException("can't find this")
 						val mimeType = getMimeTypeForUrl(url.toString())
 						WebResourceResponse(
-							mimeType,
-							null,
-							200,
-							"OK",
-							null,
-							assets.open(assetPath)
+								mimeType,
+								null,
+								200,
+								"OK",
+								null,
+								assets.open(assetPath)
 						)
 					} catch (e: IOException) {
 						Log.w(TAG, "Resource not found ${url.path}")
 						WebResourceResponse(
-							null,
-							null,
-							404,
-							"Not Found",
-							null,
-							null
+								null,
+								null,
+								404,
+								"Not Found",
+								null,
+								null
 						)
 					}
 				} else {
@@ -282,7 +284,7 @@ class MainActivity : FragmentActivity() {
 	/** @return "result" extra value */
 	suspend fun startWebauthn(uri: Uri): String {
 		val customIntent = CustomTabsIntent.Builder()
-			.build()
+				.build()
 		val intent = customIntent.intent.apply {
 			data = uri
 			// close custom tabs activity as soon as user navigates away from it, otherwise it will linger as a separate
@@ -337,16 +339,16 @@ class MainActivity : FragmentActivity() {
 			try {
 				Log.d(TAG, "migrating credentials")
 				val oldTutanotaConfig = evalJavaScriptForResult(
-					"let s = localStorage.getItem('tutanotaConfig'); s ? btoa(s) : null",
-					"file:///dummy.html"
+						"let s = localStorage.getItem('tutanotaConfig'); s ? btoa(s) : null",
+						"file:///dummy.html"
 				)
 				WebStorage.getInstance().deleteAllData()
 				if (oldTutanotaConfig == "null") {
 					Log.d(TAG, "no credentials to migrate")
 				} else {
 					evalJavaScriptForResult(
-						"localStorage.setItem('tutanotaConfig', atob($oldTutanotaConfig))",
-						"https://assets.tutanota.com/dummy.html"
+							"localStorage.setItem('tutanotaConfig', atob($oldTutanotaConfig))",
+							"https://assets.tutanota.com/dummy.html"
 					)
 				}
 			} finally {
@@ -410,7 +412,7 @@ class MainActivity : FragmentActivity() {
 		if (intent.action != null) {
 			when (intent.action) {
 				Intent.ACTION_SEND, Intent.ACTION_SEND_MULTIPLE, Intent.ACTION_SENDTO, Intent.ACTION_VIEW -> share(
-					intent
+						intent
 				)
 				OPEN_USER_MAILBOX_ACTION -> openMailbox(intent)
 				OPEN_CALENDAR_ACTION -> openCalendar(intent)
@@ -445,8 +447,8 @@ class MainActivity : FragmentActivity() {
 		val preferences = android.preference.PreferenceManager.getDefaultSharedPreferences(this)
 
 		if (
-			!preferences.getBoolean(ASKED_BATTERY_OPTIMIZATIONS_PREF, false)
-			&& !powerManager.isIgnoringBatteryOptimizations(packageName)
+				!preferences.getBoolean(ASKED_BATTERY_OPTIMIZATIONS_PREF, false)
+				&& !powerManager.isIgnoringBatteryOptimizations(packageName)
 		) {
 
 			commonNativeFacade.showAlertDialog("allowPushNotification_msg")
@@ -455,8 +457,8 @@ class MainActivity : FragmentActivity() {
 				saveAskedBatteryOptimizations(preferences)
 				@SuppressLint("BatteryLife")
 				val intent = Intent(
-					Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
-					Uri.parse("package:$packageName")
+						Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+						Uri.parse("package:$packageName")
 				)
 				startActivity(intent)
 			}
@@ -526,13 +528,13 @@ class MainActivity : FragmentActivity() {
 	}
 
 	suspend fun startActivityForResult(@RequiresPermission intent: Intent?): ActivityResult =
-		suspendCoroutine { continuation ->
-			val requestCode = getNextRequestCode()
-			activityRequests[requestCode] = continuation
-			// we need requestCode to identify the request which is not possible with new API
-			@Suppress("DEPRECATION")
-			super.startActivityForResult(intent, requestCode)
-		}
+			suspendCoroutine { continuation ->
+				val requestCode = getNextRequestCode()
+				activityRequests[requestCode] = continuation
+				// we need requestCode to identify the request which is not possible with new API
+				@Suppress("DEPRECATION")
+				super.startActivityForResult(intent, requestCode)
+			}
 
 	@Deprecated("Deprecated in Java")
 	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -549,9 +551,9 @@ class MainActivity : FragmentActivity() {
 	fun setupPushNotifications() {
 		try {
 			val serviceIntent = PushNotificationService.startIntent(
-				this,
-				"MainActivity#setupPushNotifications",
-				attemptForeground = true,
+					this,
+					"MainActivity#setupPushNotifications",
+					attemptForeground = true,
 			)
 			startService(serviceIntent)
 		} catch (e: IllegalStateException) {
@@ -560,10 +562,10 @@ class MainActivity : FragmentActivity() {
 		}
 		val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
 		jobScheduler.schedule(
-			JobInfo.Builder(1, ComponentName(this, PushNotificationService::class.java))
-				.setPeriodic(TimeUnit.MINUTES.toMillis(15))
-				.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-				.setPersisted(true).build()
+				JobInfo.Builder(1, ComponentName(this, PushNotificationService::class.java))
+						.setPeriodic(TimeUnit.MINUTES.toMillis(15))
+						.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+						.setPersisted(true).build()
 		)
 	}
 
@@ -602,14 +604,23 @@ class MainActivity : FragmentActivity() {
 		} else {
 			""
 		}
-
-		commonNativeFacade.createMailEditor(
-			files,
-			text ?: "",
-			addresses,
-			subject ?: "",
-			mailToUrlString
-		)
+		try {
+			commonNativeFacade.createMailEditor(
+					files,
+					text ?: "",
+					addresses,
+					subject ?: "",
+					mailToUrlString
+			)
+		} catch (e: RemoteExecutionException) {
+			val name = if (e.message != null) {
+				val element = Json.parseToJsonElement(e.message)
+				element.jsonObject["name"]?.jsonPrimitive?.content
+			} else {
+				null
+			}
+			Log.d(TAG, "failed to create a mail editor because of a ${name ?: "unknown error"}")
+		}
 	}
 
 	private fun getFilesFromIntent(intent: Intent): List<String> {
@@ -657,10 +668,10 @@ class MainActivity : FragmentActivity() {
 		val addresses = ArrayList<String>(1)
 		addresses.add(address)
 		startService(
-			notificationDismissedIntent(
-				this, addresses,
-				"MainActivity#openMailbox", isSummary
-			)
+				notificationDismissedIntent(
+						this, addresses,
+						"MainActivity#openMailbox", isSummary
+				)
 		)
 
 		commonNativeFacade.openMailBox(userId, address, null)
@@ -707,7 +718,7 @@ class MainActivity : FragmentActivity() {
 			menu.setHeaderTitle(link)
 			menu.add(0, 0, 0, "Copy link").setOnMenuItemClickListener {
 				(getSystemService(CLIPBOARD_SERVICE) as ClipboardManager)
-					.setPrimaryClip(ClipData.newPlainText(link, link))
+						.setPrimaryClip(ClipData.newPlainText(link, link))
 				true
 			}
 			menu.add(0, 2, 0, "Share").setOnMenuItemClickListener {
@@ -745,43 +756,43 @@ class MainActivity : FragmentActivity() {
 
 	@SuppressLint("SetJavascriptEnabled")
 	private suspend fun evalJavaScriptForResult(javaScript: String, origin: String): String =
-		withContext(Dispatchers.Main) {
-			val webView = WebView(this@MainActivity)
-			Log.d(TAG, "Created webview for evaluation!")
+			withContext(Dispatchers.Main) {
+				val webView = WebView(this@MainActivity)
+				Log.d(TAG, "Created webview for evaluation!")
 
-			webView.settings.apply {
-				javaScriptEnabled = true
-				domStorageEnabled = true
-				javaScriptCanOpenWindowsAutomatically = false
-				allowFileAccess = true
-				allowContentAccess = false
-				cacheMode = WebSettings.LOAD_NO_CACHE
-				mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
-			}
+				webView.settings.apply {
+					javaScriptEnabled = true
+					domStorageEnabled = true
+					javaScriptCanOpenWindowsAutomatically = false
+					allowFileAccess = true
+					allowContentAccess = false
+					cacheMode = WebSettings.LOAD_NO_CACHE
+					mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
+				}
 
-			suspendCoroutine { cb ->
-				webView.webViewClient = object : WebViewClient() {
-					override fun onPageFinished(view: WebView?, url: String?) {
-						super.onPageFinished(view, url)
-						Log.d(TAG, "finished page: $url")
-						webView.evaluateJavascript(javaScript) { res ->
-							Log.d(TAG, "got a response for $origin")
-							webView.destroy()
-							cb.resume(res)
+				suspendCoroutine { cb ->
+					webView.webViewClient = object : WebViewClient() {
+						override fun onPageFinished(view: WebView?, url: String?) {
+							super.onPageFinished(view, url)
+							Log.d(TAG, "finished page: $url")
+							webView.evaluateJavascript(javaScript) { res ->
+								Log.d(TAG, "got a response for $origin")
+								webView.destroy()
+								cb.resume(res)
+							}
+						}
+
+						override fun onReceivedError(
+								view: WebView?,
+								request: WebResourceRequest?,
+								error: WebResourceError?
+						) {
+							Log.d(TAG, "received error: ${error?.description}")
+							super.onReceivedError(view, request, error)
 						}
 					}
 
-					override fun onReceivedError(
-						view: WebView?,
-						request: WebResourceRequest?,
-						error: WebResourceError?
-					) {
-						Log.d(TAG, "received error: ${error?.description}")
-						super.onReceivedError(view, request, error)
-					}
+					webView.loadDataWithBaseURL(origin, " ", "text/plain", null, null)
 				}
-
-				webView.loadDataWithBaseURL(origin, " ", "text/plain", null, null)
 			}
-		}
 }
