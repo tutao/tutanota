a005e59784c7e9739d45821a38ad6aa66fdaf311
nok <nok@tutao.de>
2022-12-21 15:26:18 +0100

    fix show progress for download all in browser
    previously only downloading a single file would show a blocking progress
    indication. Now this progress indication is also shown for the save all
    operation. If the download of a single file fails, an error message is
    displayed, mentioning this specific file. Nevertheless, the downloading
    process for the other files will continue in the background.
    Furthermore, the progress indication also shows a completeness
    indication in form of a bar.
    
    #4811

--- src/file/FileControllerBrowser.ts
+++ src/file/FileControllerBrowser.ts
@@ -8,6 +8,7 @@ import { File as TutanotaFile } from "../api/entities/tutanota/TypeRefs.js"
 import { BlobFacade } from "../api/worker/facades/BlobFacade"
 import { FileFacade } from "../api/worker/facades/FileFacade"
 import { downloadAndDecryptDataFile, FileController, handleDownloadErrors, openDataFileInBrowser, zipDataFiles } from "./FileController.js"
+import stream from "mithril/stream"
 
 assertMainOrNode()
 
@@ -28,15 +29,29 @@ export class FileControllerBrowser implements FileController {
 
 	async downloadAll(tutanotaFiles: Array<TutanotaFile>): Promise<void> {
 		const downloadedFiles: Array<DataFile> = []
-		for (const file of tutanotaFiles) {
-			try {
-				const downloadedFile = await this.downloadAndDecrypt(file)
-				downloadedFiles.push(downloadedFile)
-			} catch (e) {
-				await handleDownloadErrors(e, (msg) => Dialog.message(() => lang.get(msg) + " " + file.name))
-			}
+		try {
+			const progress = stream(0)
+			await showProgressDialog(
+				"pleaseWait_msg",
+				new Promise<void>(async (resolve) => {
+					for (const file of tutanotaFiles) {
+						try {
+							const downloadedFile = await this.downloadAndDecrypt(file)
+							downloadedFiles.push(downloadedFile)
+							progress(((tutanotaFiles.indexOf(file) + 1) / tutanotaFiles.length) * 100)
+						} catch (e) {
+							await handleDownloadErrors(e, (msg) => Dialog.message(() => lang.get(msg) + " " + file.name))
+						}
+					}
+					await openDataFileInBrowser(await zipDataFiles(downloadedFiles, `${sortableTimestamp()}-attachments.zip`))
+					resolve()
+				}),
+				progress,
+			)
+		} catch (e) {
+			console.log("downloadAndOpen error", e.message)
+			await handleDownloadErrors(e, Dialog.message)
 		}
-		await openDataFileInBrowser(await zipDataFiles(downloadedFiles, `${sortableTimestamp()}-attachments.zip`))
 	}
 
 	async open(file: TutanotaFile) {
