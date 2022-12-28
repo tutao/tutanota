97eb6b4520d50aedb48506114145ef4eaa18bebf
ivk <ivk@tutao.de>
2022-12-28 13:50:54 +0100

    Move leader status to connectivity model

--- src/misc/WebsocketConnectivityModel.ts
+++ src/misc/WebsocketConnectivityModel.ts
@@ -3,19 +3,30 @@ import stream from "mithril/stream"
 import { identity } from "@tutao/tutanota-utils"
 import { CloseEventBusOption } from "../api/common/TutanotaConstants.js"
 import { ExposedEventBus } from "../api/worker/WorkerImpl.js"
+import {WebsocketLeaderStatus} from "../api/entities/sys/TypeRefs.js"
 
 export interface WebsocketConnectivityListener {
 	updateWebSocketState(wsConnectionState: WsConnectionState): Promise<void>
+	updateLeaderStatus(leaderStatus: WebsocketLeaderStatus): Promise<void>
 }
 
 /** A web page thread view on websocket/event bus. */
 export class WebsocketConnectivityModel implements WebsocketConnectivityListener {
 	private readonly _wsConnection = stream<WsConnectionState>(WsConnectionState.terminated)
+	private _leaderStatus: boolean = false
 
 	constructor(private readonly eventBus: ExposedEventBus) {}
 
 	async updateWebSocketState(wsConnectionState: WsConnectionState): Promise<void> {
 		this._wsConnection(wsConnectionState)
+	}
+
+	async updateLeaderStatus(leaderStatus: WebsocketLeaderStatus): Promise<void> {
+		this._leaderStatus = leaderStatus.leaderStatus
+	}
+
+	isLeader(): boolean {
+		return this._leaderStatus
 	}
 
 	wsConnection(): stream<WsConnectionState> {
