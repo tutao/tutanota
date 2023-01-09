2b1c4953de38729337235a8b31714da6993a5448
wrd <wrd@tutao.de>
2023-01-09 14:58:45 +0100

    Implement folder hierarchy indicators
    close #4943
    
    Co-authored-by: ivk <ivk@tutao.de>

--- src/mail/view/MailFolderRow.ts
+++ src/mail/view/MailFolderRow.ts
@@ -1,7 +1,6 @@
-import m, { Children, Component, Vnode, VnodeDOM } from "mithril"
+import m, { Children, Component, Vnode } from "mithril"
 import type { NavButtonAttrs } from "../../gui/base/NavButton.js"
 import { isNavButtonSelected, NavButton } from "../../gui/base/NavButton.js"
-import { animations, opacity } from "../../gui/animation/Animations"
 import { CounterBadge } from "../../gui/base/CounterBadge"
 import { getNavButtonIconBackground, theme } from "../../gui/theme"
 import { px, size } from "../../gui/size"
@@ -18,12 +17,16 @@ export type MailFolderRowAttrs = {
 	onExpanderClick: () => unknown
 	icon: AllIcons
 	hasChildren: boolean
+	onSelectedPath: boolean
+	numberOfPreviousRows: number
+	isLastSibling: boolean
 }
 
 export class MailFolderRow implements Component<MailFolderRowAttrs> {
 	view(vnode: Vnode<MailFolderRowAttrs>): Children {
 		const { count, button, rightButton, expanded, indentationLevel, icon, hasChildren } = vnode.attrs
 		const indentationMargin = indentationLevel * size.hpad
+
 		return m(".folder-row.pr-l.flex.flex-row", [
 			hasChildren && !expanded
 				? m(Icon, {
@@ -42,6 +45,7 @@ export class MailFolderRow implements Component<MailFolderRowAttrs> {
 					marginLeft: px(indentationMargin),
 				},
 			}),
+			this.renderHierarchyLine(vnode.attrs, indentationMargin),
 			m(
 				"button.flex.items-center.justify-end",
 				{
@@ -70,5 +74,47 @@ export class MailFolderRow implements Component<MailFolderRowAttrs> {
 						background: getNavButtonIconBackground(),
 				  }),
 		])
+	}
+
+	private renderHierarchyLine(
+		{ indentationLevel, numberOfPreviousRows, isLastSibling, onSelectedPath }: MailFolderRowAttrs,
+		indentationMargin: number,
+	) {
+		const lineSize = 2
+		const border = `${lineSize}px solid ${onSelectedPath ? theme.content_accent : theme.content_border}`
+		const verticalOffsetInsideRow = size.button_height / 2 + 1
+		const verticalOffsetForParent = size.button_height / 4
+		const lengthOfHorizontalLine = size.hpad - 2
+		return indentationLevel !== 0
+			? [
+					isLastSibling || onSelectedPath
+						// draw both vertical and horizontal lines
+						? m(".abs", {
+								style: {
+									width: px(lengthOfHorizontalLine),
+									borderBottomLeftRadius: "3px",
+									// there's some subtle difference between border we use here and the height for the other element and this +1 is to
+									// accommodate it
+									height: px(1 + verticalOffsetInsideRow + verticalOffsetForParent + numberOfPreviousRows * size.button_height),
+									top: px(-verticalOffsetForParent - numberOfPreviousRows * size.button_height),
+									left: px(indentationMargin + size.hpad),
+									borderLeft: border,
+									borderBottom: border,
+									// we need to draw selected lines over everything else, even things that are drawn later
+									zIndex: onSelectedPath ? 1 : "",
+								},
+						  })
+						// draw only the horizontal line
+						: m(".abs", {
+								style: {
+									height: px(lineSize),
+									top: px(verticalOffsetInsideRow),
+									left: px(indentationMargin + size.hpad),
+									width: px(lengthOfHorizontalLine),
+									backgroundColor: onSelectedPath ? theme.content_accent : theme.content_border,
+								},
+						  }),
+			  ]
+			: null
 	}
 }
