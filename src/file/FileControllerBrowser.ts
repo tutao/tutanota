03edcbd0e1b317da637ef390baf567bec259974e
nok <nok@tutao.de>
2023-01-09 17:57:42 +0100

    changes after review: use template method pattern

--- src/file/FileControllerBrowser.ts
+++ src/file/FileControllerBrowser.ts
@@ -1,13 +1,11 @@
-import { Dialog } from "../gui/base/Dialog"
 import { DataFile } from "../api/common/DataFile"
 import { assertMainOrNode } from "../api/common/Env"
-import { sortableTimestamp } from "@tutao/tutanota-utils"
-import { showProgressDialog } from "../gui/dialogs/ProgressDialog"
 import { File as TutanotaFile } from "../api/entities/tutanota/TypeRefs.js"
 import { BlobFacade } from "../api/worker/facades/BlobFacade"
 import { FileFacade } from "../api/worker/facades/FileFacade"
-import { doDownload, downloadAndDecryptDataFile, FileController, handleDownloadErrors, openDataFileInBrowser, zipDataFiles } from "./FileController.js"
+import { downloadAndDecryptDataFile, FileController, guiDownload, openDataFileInBrowser } from "./FileController.js"
 import stream from "mithril/stream"
+import { DownloadStrategyBrowserMultiple, DownloadStrategyBrowserSingle } from "./DownloadStrategy.js"
 
 assertMainOrNode()
 
@@ -15,35 +13,12 @@ export class FileControllerBrowser implements FileController {
 	constructor(private readonly blobFacade: BlobFacade, private readonly fileFacade: FileFacade) {}
 
 	async download(file: TutanotaFile) {
-		try {
-			await showProgressDialog(
-				"pleaseWait_msg",
-				this.downloadAndDecrypt(file).then((file) => this.saveDataFile(file)),
-			)
-		} catch (e) {
-			console.log("downloadAndOpen error", e.message)
-			await handleDownloadErrors(e, Dialog.message)
-		}
+		await guiDownload(new DownloadStrategyBrowserSingle(this).doDownload([file]))
 	}
 
 	async downloadAll(tutanotaFiles: Array<TutanotaFile>): Promise<void> {
 		const progress = stream(0)
-		try {
-			await showProgressDialog(
-				"pleaseWait_msg",
-				doDownload({
-					tutanotaFiles,
-					downloadAction: (file) => this.downloadAndDecrypt(file),
-					processDownloadedFiles: async (files) => openDataFileInBrowser(await zipDataFiles(files, `${sortableTimestamp()}-attachments.zip`)),
-					cleanUp: async () => {},
-					progress,
-				}),
-				progress,
-			)
-		} catch (e) {
-			console.log("downloadAndOpen error", e.message)
-			await handleDownloadErrors(e, Dialog.message)
-		}
+		await guiDownload(new DownloadStrategyBrowserMultiple(this).doDownload(tutanotaFiles, progress), progress)
 	}
 
 	async open(file: TutanotaFile) {
