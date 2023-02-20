936a272ef3f64464d13c9107ac2a41ac531dbce6
nig <nig@tutao.de>
2023-02-20 14:42:51 +0100

    changes after review

--- src/mail/editor/SendMailModel.ts
+++ src/mail/editor/SendMailModel.ts
@@ -407,8 +407,13 @@ export class SendMailModel {
 
 		const recipientsFilter = (recipientList: Array<PartialRecipient>) =>
 			deduplicate(
-				recipientList.filter((r) => isMailAddress(r.address, false)),
-				(a, b) => cleanMailAddress(a.address) === cleanMailAddress(b.address),
+				recipientList
+					.filter((r) => isMailAddress(r.address, false))
+					.map((a) => {
+						a.address = cleanMailAddress(a.address)
+						return a
+					}),
+				(a, b) => a.address === b.address,
 			)
 
 		this.recipientsResolved = Promise.all([
@@ -545,7 +550,8 @@ export class SendMailModel {
 	 */
 	removeRecipient(recipient: Recipient, type: RecipientField, notify: boolean = true): boolean {
 		const recipients = this.recipients.get(type) ?? []
-		const didRemove = findAndRemove(recipients, (r) => cleanMailAddress(r.address) === cleanMailAddress(recipient.address))
+		const cleanRecipientAddress = cleanMailAddress(recipient.address)
+		const didRemove = findAndRemove(recipients, (r) => cleanMailAddress(r.address) === cleanRecipientAddress)
 		this.markAsChangedIfNecessary(didRemove)
 
 		if (didRemove && notify) {
