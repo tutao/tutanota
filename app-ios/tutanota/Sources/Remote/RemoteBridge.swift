1c5cb28dc9d3addc58e840bfeaf59235beb1c0b5
ivk <ivk@tutao.de>
2023-03-17 14:48:53 +0100

    [ios] Addres review feedback for sorted alarms

--- app-ios/tutanota/Sources/Remote/RemoteBridge.swift
+++ app-ios/tutanota/Sources/Remote/RemoteBridge.swift
@@ -27,7 +27,7 @@ class RemoteBridge : NSObject, NativeInterface {
     themeFacade: ThemeFacade,
     appDelegate: AppDelegate,
     alarmManager: AlarmManager,
-    userPreferences: UserPreferenceFacade,
+    notificationStorage: NotificationStorage,
     keychainManager: KeychainManager,
     webAuthnFacade: WebAuthnFacade,
     sqlCipherFacade: IosSqlCipherFacade
@@ -46,7 +46,7 @@ class RemoteBridge : NSObject, NativeInterface {
     let nativePushFacade = IosNativePushFacade(
       appDelegate: appDelegate,
       alarmManager: alarmManager,
-      userPreferences: userPreferences,
+      notificationStorage: notificationStorage,
       keychainManager: keychainManager,
       commonNativeFacade: self.commonNativeFacade
     )
