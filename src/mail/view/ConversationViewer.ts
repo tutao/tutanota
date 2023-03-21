ffcbf5c2839bcdfa634d82315d0b3c1238529122
wrd <wrd@tutao.de>
2023-03-21 12:29:08 +0100

    Display mail subject in header on mobile
    close #5251

--- src/mail/view/ConversationViewer.ts
+++ src/mail/view/ConversationViewer.ts
@@ -14,6 +14,7 @@ import { px, size } from "../../gui/size.js"
 import { Keys } from "../../api/common/TutanotaConstants.js"
 import { keyManager, Shortcut } from "../../misc/KeyManager.js"
 import { styles } from "../../gui/styles.js"
+import { client } from "../../misc/ClientDetector"
 
 export interface ConversationViewerAttrs {
 	viewModel: ConversationViewModel
@@ -86,7 +87,7 @@ export class ConversationViewer implements Component<ConversationViewerAttrs> {
 				this.renderLoadingState(viewModel),
 				this.renderFooter(),
 			),
-			this.renderFloatingSubject(),
+			client.isMobileDevice() ? null : this.renderFloatingSubject(),
 		])
 	}
 
