5ba756224af656326a94937fcf1fe1e860c3ef53
nig <nig@tutao.de>
2023-06-08 13:08:32 +0200

    memoize pre-processed template contents
    #5484

--- src/settings/TemplateDetailsViewer.ts
+++ src/settings/TemplateDetailsViewer.ts
@@ -1,4 +1,4 @@
-import m from "mithril"
+import m, { Children } from "mithril"
 import type { lazy } from "@tutao/tutanota-utils"
 import { assertNotNull } from "@tutao/tutanota-utils"
 import { TextField } from "../gui/base/TextField.js"
@@ -7,7 +7,7 @@ import { Icons } from "../gui/base/icons/Icons"
 import { getLanguageCode } from "./TemplateEditorModel"
 import { showTemplateEditor } from "./TemplateEditor"
 import { Dialog } from "../gui/base/Dialog"
-import { lang, languageByCode } from "../misc/LanguageViewModel"
+import { lang, languageByCode, TranslationKey } from "../misc/LanguageViewModel"
 import type { EmailTemplate } from "../api/entities/tutanota/TypeRefs.js"
 import { TemplateGroupRootTypeRef } from "../api/entities/tutanota/TypeRefs.js"
 import { locator } from "../api/main/MainLocator"
@@ -18,64 +18,65 @@ import { ActionBar } from "../gui/base/ActionBar.js"
 import { htmlSanitizer } from "../misc/HtmlSanitizer.js"
 
 export class TemplateDetailsViewer implements UpdatableSettingsDetailsViewer {
-	readonly renderView: UpdatableSettingsDetailsViewer["renderView"]
+	// we're not memoizing the translated language name since this is not a proper mithril component and may stick around after a language
+	// switch.
+	private readonly sanitizedContents: Array<{ text: string; languageCodeTextId: TranslationKey }>
 
 	constructor(private readonly template: EmailTemplate, private readonly entityClient: EntityClient, readonly isReadOnly: lazy<boolean>) {
-		this.renderView = () => {
-			return m("#user-viewer.fill-absolute.scroll.plr-l.pb-floating", [
-				m(".flex.mt-l.center-vertically", [
-					m(".h4.text-ellipsis", template.title),
-					!this.isReadOnly()
-						? m(ActionBar, {
-								buttons: [
-									{
-										title: "edit_action",
-										icon: Icons.Edit,
-										click: () => this.editTemplate(template),
-									},
-									{
-										title: "remove_action",
-										icon: Icons.Trash,
-										click: () => this.deleteTemplate(entityClient, template),
-									},
-								],
-						  })
-						: null,
-				]),
-				m("", [
-					m(TextField, {
-						label: "shortcut_label",
-						value: TEMPLATE_SHORTCUT_PREFIX + assertNotNull(template.tag, "template without tag!"),
-						disabled: true,
-					}),
-					template.contents.map((emailTemplateContent) => {
-						const language = languageByCode[getLanguageCode(emailTemplateContent)]
-						return m(".flex.flex-column", [
-							m(".h4.mt-l", lang.get(language.textId)),
-							m(".editor-border.text-break", m.trust(this.sanitize(emailTemplateContent.text))),
-						])
-					}),
-				]),
-			])
-		}
+		this.sanitizedContents = template.contents.map((emailTemplateContent) => ({
+			text: htmlSanitizer.sanitizeHTML(emailTemplateContent.text, { blockExternalContent: false, allowRelativeLinks: true }).html,
+			languageCodeTextId: languageByCode[getLanguageCode(emailTemplateContent)].textId,
+		}))
 	}
 
-	private deleteTemplate(entityClient: EntityClient, template: EmailTemplate) {
-		Dialog.confirm("deleteTemplate_msg").then((confirmed) => {
-			if (confirmed) {
-				entityClient.erase(template)
-			}
-		})
+	readonly renderView: UpdatableSettingsDetailsViewer["renderView"] = () => {
+		return m("#user-viewer.fill-absolute.scroll.plr-l.pb-floating", [this.renderTitleLine(), this.renderContent()])
 	}
 
-	private editTemplate(template: EmailTemplate) {
-		locator.entityClient.load(TemplateGroupRootTypeRef, assertNotNull(template._ownerGroup, "template without ownerGroup!")).then((groupRoot) => {
-			showTemplateEditor(template, groupRoot)
-		})
+	private renderTitleLine(): Children {
+		return m(".flex.mt-l.center-vertically", [
+			m(".h4.text-ellipsis", this.template.title),
+			!this.isReadOnly()
+				? m(ActionBar, {
+						buttons: [
+							{
+								title: "edit_action",
+								icon: Icons.Edit,
+								click: () => this.editTemplate(),
+							},
+							{
+								title: "remove_action",
+								icon: Icons.Trash,
+								click: () => this.deleteTemplate(),
+							},
+						],
+				  })
+				: null,
+		])
 	}
 
-	private sanitize(html: string): string {
-		return htmlSanitizer.sanitizeHTML(html, { blockExternalContent: false, allowRelativeLinks: true }).html
+	private renderContent(): Children {
+		return m("", [
+			m(TextField, {
+				label: "shortcut_label",
+				value: TEMPLATE_SHORTCUT_PREFIX + assertNotNull(this.template.tag, "template without tag!"),
+				disabled: true,
+			}),
+			this.sanitizedContents.map(({ text, languageCodeTextId }) => {
+				return m(".flex.flex-column", [m(".h4.mt-l", lang.get(languageCodeTextId)), m(".editor-border.text-break", m.trust(text))])
+			}),
+		])
+	}
+
+	private async deleteTemplate(): Promise<void> {
+		if (!(await Dialog.confirm("deleteTemplate_msg"))) return
+		await this.entityClient.erase(this.template)
+	}
+
+	private async editTemplate() {
+		const { template } = this
+		const groupRoot = await locator.entityClient.load(TemplateGroupRootTypeRef, assertNotNull(template._ownerGroup, "template without ownerGroup!"))
+		showTemplateEditor(template, groupRoot)
 	}
 
 	entityEventsReceived(updates: ReadonlyArray<EntityUpdateData>): Promise<void> {
