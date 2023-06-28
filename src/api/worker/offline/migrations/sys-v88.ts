1a1cb8d89777a641e54231d923ddf41d0af88b27
bedhub <bedhub@users.noreply.github.com>
2023-06-28 16:01:35 +0200

    delete File instances from offline database, fixes tutadb 1537

--- src/api/worker/offline/migrations/sys-v88.ts
+++ src/api/worker/offline/migrations/sys-v88.ts
@@ -1,10 +1,13 @@
 import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
+import { deleteInstancesOfType } from "../StandardMigrations.js"
+import { FileTypeRef } from "../../../entities/tutanota/TypeRefs.js"
 
 export const sys88: OfflineMigration = {
 	app: "sys",
 	version: 88,
 	async migrate(storage: OfflineStorage) {
-		// we only changed data transfer types
+		// Delete File instances to ensure that only File instances with references to Blobs exist
+		await deleteInstancesOfType(storage, FileTypeRef)
 	},
 }
