76b29a86623c48ff2bd2927d71d6afe4a7313932
ivk <ivk@tutao.de>
2023-06-28 10:47:31 +0200

    Restore url routing changes

--- src/gui/ScopedRouter.ts
+++ src/gui/ScopedRouter.ts
@@ -1,21 +1,37 @@
 import { throttleRoute } from "../misc/RouteChange.js"
 import m from "mithril"
+import { debounceStart } from "@tutao/tutanota-utils"
 
 /** URL-related functions */
 export interface Router {
+	getFullPath(): string
 	/** will do parameter substitution like mithril route */
 	routeTo(path: string, params: Record<string, any>): void
 }
 
-/** router that is scoped to a specific prefix and will ignore the path changes outside of it */
-export class ScopedRouter<Scope extends string> implements Router {
-	private thorttledRoute = throttleRoute()
+export class ThrottledRouter implements Router {
+	private readonly throttledRoute = debounceStart(32, throttleRoute())
 
-	constructor(readonly scope: Scope) {}
+	getFullPath(): string {
+		return m.route.get()
+	}
 
 	routeTo(path: string, params: Record<string, any>) {
-		if (m.route.get().startsWith(this.scope)) {
-			this.thorttledRoute(path, params)
+		this.throttledRoute(path, params)
+	}
+}
+
+/** router that is scoped to a specific prefix and will ignore the path changes outside of it */
+export class ScopedRouter<Scope extends string> implements Router {
+	constructor(private readonly router: Router, readonly scope: Scope) {}
+
+	getFullPath(): string {
+		return this.router.getFullPath()
+	}
+
+	routeTo(path: string, params: Record<string, any>) {
+		if (this.router.getFullPath().startsWith(this.scope)) {
+			this.router.routeTo(path, params)
 		}
 	}
 }
