2e42a53d4372e466fcb0c49f5f3970c4c674ab17
nig <nig@tutao.de>
2023-07-26 15:50:08 +0200

    migrate ProtocolProxy to the new handleProtocol electron API
    since fetch is available in electron 25s node version, we can
    also get rid of the http and https requests
    
    close #5670

--- test/tests/desktop/net/ProtocolProxyTest.ts
+++ test/tests/desktop/net/ProtocolProxyTest.ts
@@ -1,185 +1,160 @@
 import o from "ospec"
-import http from "node:http"
-import https from "node:https"
 import path from "node:path"
-import { constructor, matchers, object, replace, verify, when } from "testdouble"
+import { OutgoingHttpHeader } from "node:http"
+import { func, matchers, object, verify, when } from "testdouble"
 import { doHandleProtocols, handleProtocols } from "../../../../src/desktop/net/ProtocolProxy.js"
 
 o.spec("ProtocolProxy", function () {
+	let fetchMock
+
+	o.beforeEach(function () {
+		fetchMock = func()
+	})
+
 	o("will only be intercepted once on a session", function () {
 		const protocol: any = object()
 		const ses: any = { protocol }
-		when(protocol.isProtocolIntercepted("http")).thenReturn(true)
-		when(protocol.isProtocolIntercepted("https")).thenReturn(true)
-		when(protocol.isProtocolRegistered("asset")).thenReturn(true)
+		when(protocol.isProtocolHandled("http")).thenReturn(true)
+		when(protocol.isProtocolHandled("https")).thenReturn(true)
+		when(protocol.isProtocolHandled("asset")).thenReturn(true)
 		handleProtocols(ses, "none")
+		verify(protocol.handle("https", matchers.anything()), { times: 0 })
+		verify(protocol.handle("http", matchers.anything()), { times: 0 })
+		verify(protocol.handle("asset", matchers.anything()), { times: 0 })
 	})
 
-	o.spec("will be intercepted if it wasn't before", function () {
-		o("http", function () {
-			const protocol: any = object()
-			const ses: any = { protocol }
-			when(protocol.isProtocolIntercepted("http")).thenReturn(false)
-			when(protocol.isProtocolIntercepted("https")).thenReturn(true)
-			when(protocol.isProtocolRegistered("asset")).thenReturn(true)
-			when(protocol.interceptStreamProtocol("http", matchers.anything())).thenReturn(true)
-			handleProtocols(ses, "none")
-		})
-
-		o("https", function () {
-			const protocol: any = object()
-			const ses: any = { protocol }
-			when(protocol.isProtocolIntercepted("http")).thenReturn(true)
-			when(protocol.isProtocolIntercepted("https")).thenReturn(false)
-			when(protocol.isProtocolRegistered("asset")).thenReturn(true)
-			when(protocol.interceptStreamProtocol("https", matchers.anything())).thenReturn(true)
-			handleProtocols(ses, "none")
-		})
-
-		o("asset", function () {
-			const protocol: any = object()
-			const ses: any = { protocol }
-			when(protocol.isProtocolIntercepted("http")).thenReturn(true)
-			when(protocol.isProtocolIntercepted("https")).thenReturn(true)
-			when(protocol.isProtocolRegistered("asset")).thenReturn(false)
-			when(protocol.registerFileProtocol("asset", matchers.anything())).thenReturn(true)
-			handleProtocols(ses, "none")
-		})
+	o("protocol will be handled if it wasn't before", function () {
+		const protocol: any = object()
+		const ses: any = { protocol }
+		when(protocol.isProtocolHandled("http")).thenReturn(false, true)
+		when(protocol.isProtocolHandled("https")).thenReturn(false, true)
+		when(protocol.isProtocolHandled("asset")).thenReturn(false, true)
+		handleProtocols(ses, "none")
+		verify(protocol.handle("http", matchers.anything()), { times: 1 })
+		verify(protocol.handle("https", matchers.anything()), { times: 1 })
+		verify(protocol.handle("asset", matchers.anything()), { times: 1 })
 	})
 
 	o.spec("OPTIONS gets intercepted", function () {
-		o("http", function () {
+		o("fetch", async function () {
 			const protocol: any = object()
 			const ses: any = { protocol }
-			const captor = matchers.captor()
-			when(protocol.isProtocolIntercepted(matchers.anything())).thenReturn(false)
-			when(protocol.isProtocolRegistered(matchers.anything())).thenReturn(false)
-			when(protocol.interceptStreamProtocol("http", captor.capture())).thenReturn(true)
-			when(protocol.interceptStreamProtocol("https", matchers.anything())).thenReturn(true)
-			when(protocol.registerFileProtocol("asset", matchers.anything())).thenReturn(true)
-			const agent: any = constructor(http.Agent)
-			const httpModule: any = object()
-			const httpsModule: any = object()
-			replace(httpModule, "Agent", agent)
-			replace(httpsModule, "Agent", agent)
+			const httpCaptor = matchers.captor()
+			const httpsCaptor = matchers.captor()
+			when(protocol.isProtocolHandled("http")).thenReturn(false, true)
+			when(protocol.isProtocolHandled("https")).thenReturn(false, true)
+			when(protocol.isProtocolHandled("asset")).thenReturn(true)
+			when(protocol.handle("http", httpCaptor.capture())).thenReturn(undefined)
+			when(protocol.handle("https", httpsCaptor.capture())).thenReturn(undefined)
+			when(protocol.handle("asset", matchers.anything())).thenReturn(undefined)
 			const path: any = object()
-			doHandleProtocols(ses, "none", httpModule, httpsModule, path)
-			const request = {
+			const fs: any = object()
+			doHandleProtocols(ses, "none", fetchMock, path, fs)
+			const request = (url) => ({
 				method: "OPTIONS",
 				headers: {},
-				url: "http://no/where",
-				uploadData: null,
-				referrer: "",
-			}
-			const response = {
-				statusCode: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
-					"Access-Control-Allow-Headers": "*",
-				},
-			}
-			const responseReceiver = (resp) => o(resp).deepEquals(response)
-			captor.value(request, responseReceiver)
-		})
+				url,
+			})
 
-		o("https", function () {
-			const protocol: any = object()
-			const ses: any = { protocol }
-			const captor = matchers.captor()
-			when(protocol.isProtocolIntercepted(matchers.anything())).thenReturn(false)
-			when(protocol.isProtocolRegistered(matchers.anything())).thenReturn(false)
-			when(protocol.interceptStreamProtocol("http", matchers.anything())).thenReturn(true)
-			when(protocol.interceptStreamProtocol("https", captor.capture())).thenReturn(true)
-			when(protocol.registerFileProtocol("asset", matchers.anything())).thenReturn(true)
-			const agent: any = constructor(http.Agent)
-			const httpModule: any = object()
-			const httpsModule: any = object()
-			replace(httpModule, "Agent", agent)
-			replace(httpsModule, "Agent", agent)
-			const path: any = object()
-			doHandleProtocols(ses, "none", httpModule, httpsModule, path)
-			const request = {
-				method: "OPTIONS",
-				headers: {},
-				url: "https://no/where",
-				uploadData: null,
-				referrer: "",
+			const responseHeaders = {
+				"access-control-allow-origin": "*",
+				"access-control-allow-methods": "POST, GET, PUT, DELETE",
+				"access-control-allow-headers": "*",
 			}
-			const response = {
-				statusCode: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
-					"Access-Control-Allow-Headers": "*",
-				},
-			}
-			const responseReceiver = (resp) => o(resp).deepEquals(response)
-			captor.value(request, responseReceiver)
+			const responseHttp = await httpCaptor.value(request("http://no/where"))
+			const responseHttps = await httpsCaptor.value(request("https://no/where"))
+			o(responseHttp.status).equals(200)
+			o(headersToObject(responseHttp.headers)).deepEquals(responseHeaders)
+			o(responseHttps.status).equals(200)
+			o(headersToObject(responseHttps.headers)).deepEquals(responseHeaders)
 		})
 	})
 
 	o.spec("methods other than OPTIONS get proxied", function () {
-		o("GET http", function () {
+		o("GET http", async function () {
 			const protocol: any = object()
 			const ses: any = { protocol }
 			const captor = matchers.captor()
-			when(protocol.isProtocolIntercepted(matchers.anything())).thenReturn(false)
-			when(protocol.isProtocolRegistered(matchers.anything())).thenReturn(false)
-			when(protocol.interceptStreamProtocol("http", captor.capture())).thenReturn(true)
-			when(protocol.interceptStreamProtocol("https", matchers.anything())).thenReturn(true)
-			when(protocol.registerFileProtocol("asset", matchers.anything())).thenReturn(true)
-			const agent: any = constructor(http.Agent)
-			const httpModule: any = object(http)
-			const httpsModule: any = object(https)
+			when(protocol.isProtocolHandled(matchers.anything())).thenReturn(false, true)
+			when(protocol.isProtocolHandled(matchers.anything())).thenReturn(false, true)
+			when(protocol.handle("http", captor.capture())).thenReturn(undefined)
+			when(protocol.handle("https", matchers.anything())).thenReturn(undefined)
+			when(protocol.handle("asset", matchers.anything())).thenReturn(undefined)
 			const request = {
+				arrayBuffer: () => Promise.resolve(new Uint8Array()),
 				method: "GET",
 				headers: {
 					"some-header": "header-value",
 				},
 				url: "http://no/where",
-				uploadData: null,
 				referrer: "",
 			}
-			const proxiedClientRequest: any = object(["request", "once", "end", "destroy"])
-			replace(httpModule, "Agent", agent)
-			replace(httpsModule, "Agent", agent)
-			when(httpModule.request("http://no/where", { method: request.method, headers: request.headers, agent: matchers.anything() })).thenReturn(
-				proxiedClientRequest,
-			)
 			const path: any = object()
-			doHandleProtocols(ses, "none", httpModule, httpsModule, path)
-
-			const response = { statusCode: 200, headers: {}, data: null }
-			const responseReceiver = (resp) => o(resp).deepEquals(response)
-			captor.value(request, responseReceiver)
-			verify(httpModule.request("http://no/where", { method: request.method, headers: request.headers, agent: matchers.anything() }))
+			const fs: any = object()
+			const responseSymbol = "abc"
+			when(fetchMock("http://no/where", matchers.anything())).thenResolve(responseSymbol)
+			doHandleProtocols(ses, "none", fetchMock, path, fs)
+			const responseFromSubject = await captor.value(request)
+			o(responseFromSubject).deepEquals(responseSymbol)
 		})
 	})
 
 	o.spec("asset protocol works", function () {
-		function testAssetResolution(name, url, expected) {
-			o(name, function () {
-				const protocol: any = object()
-				const ses: any = { protocol }
-				const captor = matchers.captor()
-				when(protocol.isProtocolIntercepted(matchers.anything())).thenReturn(true)
-				when(protocol.isProtocolRegistered(matchers.anything())).thenReturn(false)
-				when(protocol.registerFileProtocol("asset", captor.capture())).thenReturn(true)
-				const httpModule: any = {}
-				const httpsModule: any = {}
-				doHandleProtocols(ses, "/tutanota/assets", httpModule, httpsModule, path)
-				const responseReceiver = (resp) => o(resp).deepEquals(expected)
-				const request = { url }
-				captor.value(request, responseReceiver)
-			})
-		}
-
-		testAssetResolution("returns files that can be found", "asset://app/noodles.txt", { path: "/tutanota/assets/noodles.txt" })
-		testAssetResolution("normalizes pathname before resolving", "asset://app/subfolder/../../../win32/noodles.exe", {
-			path: "/tutanota/assets/win32/noodles.exe",
+		let protocol: any = object()
+		let ses: any
+		let promises: any = object()
+		let fs: any = { promises }
+		o.beforeEach(function () {
+			when(protocol.isProtocolHandled("http")).thenReturn(true)
+			when(protocol.isProtocolHandled("https")).thenReturn(true)
+			when(protocol.isProtocolHandled("asset")).thenReturn(false, true)
+			ses = { protocol }
 		})
-		testAssetResolution("rejects invalid protocol", "basset://app/noodles.txt", { error: -6 })
-		testAssetResolution("rejects non-app hostname", "asset://bop/noodles.txt", { error: -6 })
+
+		o("returns files that can be found", async function () {
+			const captor = matchers.captor()
+			when(protocol.handle("asset", captor.capture())).thenReturn(undefined)
+			when(fs.promises.readFile("/tutanota/assets/noodles.txt")).thenResolve("hello OK")
+			doHandleProtocols(ses, "/tutanota/assets", fetchMock, path, fs)
+			const request = { url: "asset://app/noodles.txt" }
+			const responseFromSubject = await captor.value(request)
+			o(await responseFromSubject.text()).deepEquals("hello OK")
+		})
+
+		o("normalizes pathname before resolving", async function () {
+			const captor = matchers.captor()
+			when(protocol.handle("asset", captor.capture())).thenReturn(undefined)
+			when(fs.promises.readFile("/tutanota/assets/win32/noodles.exe")).thenResolve("hello OK")
+			doHandleProtocols(ses, "/tutanota/assets", fetchMock, path, fs)
+			const request = { url: "asset://app/subfolder/../../../win32/noodles.exe" }
+			const responseFromSubject = await captor.value(request)
+			o(await responseFromSubject.text()).deepEquals("hello OK")
+		})
+
+		o("rejects invalid protocol", async function () {
+			const captor = matchers.captor()
+			when(protocol.handle("asset", captor.capture())).thenReturn(undefined)
+			doHandleProtocols(ses, "/tutanota/assets", fetchMock, path, fs)
+			const request = { url: "basset://app/noodles.txt" }
+			const responseFromSubject = await captor.value(request)
+			o(await responseFromSubject.status).deepEquals(404)
+		})
+
+		o("rejects non-app hostname", async function () {
+			const captor = matchers.captor()
+			when(protocol.handle("asset", captor.capture())).thenReturn(undefined)
+			doHandleProtocols(ses, "/tutanota/assets", fetchMock, path, fs)
+			const request = { url: "asset://bop/noodles.txt" }
+			const responseFromSubject = await captor.value(request)
+			o(await responseFromSubject.status).deepEquals(404)
+		})
 	})
 })
+
+function headersToObject(headers: Headers): Record<string, OutgoingHttpHeader> {
+	const returnObject: Record<string, OutgoingHttpHeader> = {}
+	headers.forEach((value: OutgoingHttpHeader, key: string) => {
+		returnObject[key] = value
+	})
+	return returnObject
+}
