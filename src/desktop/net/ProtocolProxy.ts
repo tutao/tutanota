88a41c6af46b16871374504a29ceb0ab9a341b6f
nig <nig@tutao.de>
2023-07-26 15:50:08 +0200

    migrate ProtocolProxy to the new handleProtocol electron API
    since fetch is available in electron 25s node version, we can
    also get rid of the http and https requests
    
    close #5670

--- src/desktop/net/ProtocolProxy.ts
+++ src/desktop/net/ProtocolProxy.ts
@@ -1,30 +1,16 @@
-import http from "node:http"
-import https from "node:https"
 import path from "node:path"
+import fs from "node:fs"
 import { log } from "../DesktopLog.js"
-import { ProtocolRequest, ProtocolResponse, Session } from "electron"
-import { ProgrammingError } from "../../api/common/error/ProgrammingError.js"
+import { Session } from "electron"
 import { errorToObj } from "../../api/common/MessageDispatcher.js"
-import { Callback, makeSingleUse } from "@tutao/tutanota-utils"
+import { lazyMemoized } from "@tutao/tutanota-utils"
+import { getMimeTypeForFile } from "../DesktopFileFacade.js"
+import { ServiceUnavailableError } from "../../api/common/error/RestError.js"
 
 const TAG = "[ProtocolProxy]"
 
 export const ASSET_PROTOCOL = "asset"
 const PROXIED_REQUEST_READ_TIMEOUT = 20000
-
-// https://source.chromium.org/chromium/chromium/src/+/main:net/base/net_error_list.h
-const enum NetErrorCode {
-	OTHER = -2,
-	INVALID_ARGUMENT = -4,
-	NOT_FOUND = -6,
-	TIMED_OUT = -7,
-}
-
-class ProxyError extends Error {
-	constructor(readonly code: NetErrorCode) {
-		super(`net operation failed with code ${code}`)
-	}
-}
 
 /**
  * intercept & proxy https, http and asset requests on a session
@@ -32,16 +18,16 @@ class ProxyError extends Error {
  * @param assetDir the base directory of allowable scripts, images and other resources that the app may load.
  */
 export function handleProtocols(session: Session, assetDir: string): void {
-	doHandleProtocols(session, assetDir, http, https, path)
+	doHandleProtocols(session, assetDir, fetch, path, fs)
 }
 
 /**
  *  exported for testing
  */
-export function doHandleProtocols(session: Session, assetDir: string, httpModule: typeof http, httpsModule: typeof https, pathModule: typeof path): void {
-	if (!interceptProtocol("http", session, httpModule)) throw new Error("could not intercept http protocol")
-	if (!interceptProtocol("https", session, httpsModule)) throw new Error("could not intercept https protocol")
-	if (!handleAssetProtocol(session, assetDir, pathModule)) throw new Error("could not register asset protocol")
+export function doHandleProtocols(session: Session, assetDir: string, fetchImpl: typeof fetch, pathModule: typeof path, fsModule: typeof fs): void {
+	if (!interceptProtocol("http", session, fetchImpl)) throw new Error("could not intercept http protocol")
+	if (!interceptProtocol("https", session, fetchImpl)) throw new Error("could not intercept https protocol")
+	if (!handleAssetProtocol(session, assetDir, pathModule, fsModule)) throw new Error("could not register asset protocol")
 }
 
 /**
@@ -49,111 +35,96 @@ export function doHandleProtocols(session: Session, assetDir: string, httpModule
  * answered immediately without actually calling the server.
  * @param session the session to intercept requests for
  * @param protocol http and https use different modules, so we need to intercept them separately.
- * @param net the net module to use (http if protocol is http, https if protocol is https)
+ * @param fetchImpl an implementation of the fetch API (Request) => Promise<Response>
  */
-function interceptProtocol(protocol: string, session: Session, net: typeof http | typeof https): boolean {
-	if (session.protocol.isProtocolIntercepted(protocol)) return true
-
-	const agent = new net.Agent({
-		keepAlive: true,
-		maxSockets: 4,
-		keepAliveMsecs: 7000 * 1000, // server has a 7200s tls session ticket timeout, so we keep the socket for 7000s
-		timeout: PROXIED_REQUEST_READ_TIMEOUT, // this is an idle timeout (empirically determined)
-	})
-
-	const handler = async ({ method, headers, url, uploadData, referrer }: ProtocolRequest, sendResponse: Callback<ProtocolResponse>) => {
-		// we never want to call this more than once
-		// * error, then anything: something already failed, telling caller about further errors or sending a response makes little sense.
-		// * timeout, then anything: see above.
-		// * response, then anything: we're already done, don't need to tell the caller about the error or send another response.
-		// using this simplifies bookkeeping.
-		sendResponse = makeSingleUse(sendResponse)
+function interceptProtocol(protocol: string, session: Session, fetchImpl: typeof fetch): boolean {
+	if (session.protocol.isProtocolHandled(protocol)) return true
+	session.protocol.handle(protocol, async (request: Request): Promise<Response> => {
+		const { method, url, headers } = request
 		const startTime: number = Date.now()
-		const handleError = (e: Error) => {
-			const parsedUrl = new URL(url)
-			const noQueryUrl = `${parsedUrl.protocol}//${parsedUrl.host}${parsedUrl.pathname}`
-			log.debug(TAG, `error for ${method} ${noQueryUrl}:`)
-			log.debug(TAG, e)
-			log.debug(TAG, JSON.stringify(errorToObj(e)))
-			log.debug(TAG, `failed after ${Date.now() - startTime}ms`)
-			// Passing anything but the codes mentioned in https://source.chromium.org/chromium/chromium/src/+/main:net/base/net_error_list.h
-			// will lead to an immediate crash of the renderer without warning.
-			if (e instanceof ProxyError) {
-				sendResponse({ error: e.code })
-			} else {
-				sendResponse({ error: NetErrorCode.OTHER })
-			}
-		}
 
 		if (!url.startsWith(protocol)) {
-			handleError(new ProxyError(NetErrorCode.INVALID_ARGUMENT))
-			return
-		}
-		if (method == "OPTIONS") {
+			return new Response(null, { status: 400 })
+		} else if (method == "OPTIONS") {
 			// this actually doesn't seem to be called when the actual request is intercepted,
 			// but we'll handle it anyway.
 			log.debug(TAG, "intercepted options request, returning canned response")
-			return sendResponse({
-				statusCode: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
-					"Access-Control-Allow-Headers": "*",
-				},
-			})
-		}
-		let actualData: Buffer | null = null
-		if (uploadData) {
-			const firstData = uploadData[0]
-			if (firstData.blobUUID) {
-				actualData = await session.getBlobData(firstData.blobUUID)
-			} else if (firstData.file) {
-				log.error(TAG, "Uploading files by path is not implemented")
-				throw new ProgrammingError("Uploading files by path is not implemented")
-			} else {
-				actualData = firstData.bytes
+			return optionsResponse()
+		} else {
+			try {
+				const options: RequestInit = {
+					headers,
+					method,
+					keepalive: true,
+					signal: AbortSignal.timeout(PROXIED_REQUEST_READ_TIMEOUT),
+				}
+				const body = await request.arrayBuffer()
+				if (body.byteLength > 0) {
+					headers.set("Content-Length", String(body.byteLength))
+					options.body = body
+				}
+				return await fetchImpl(url, options)
+			} catch (e) {
+				const parsedUrl = new URL(url)
+				const noQueryUrl = `${parsedUrl.protocol}//${parsedUrl.host}${parsedUrl.pathname}`
+				log.debug(TAG, `error for ${method} ${noQueryUrl}:`)
+				log.debug(TAG, e)
+				log.debug(TAG, JSON.stringify(errorToObj(e)))
+				log.debug(TAG, `failed after ${Date.now() - startTime}ms`)
+				return new Response(null, { status: ServiceUnavailableError.CODE })
 			}
-			headers["Content-Length"] = String(actualData.length)
 		}
-		const clientRequest: http.ClientRequest = net.request(url, { method, headers, agent })
-		clientRequest.once("response", (res: http.IncomingMessage) => {
-			res.on("error", (e) => {
-				// we don't need to do anything else because whatever is on the other side of
-				// sendResponse will handle errors as it sees fit.
-				log.debug(TAG, "response error", url)
-			})
-			// casting because typescript doesn't accept http.IncomingHttpHeaders as a Record even though it's just an object.
-			const resHeaders: Record<string, string | string[]> = res.headers as unknown as Record<string, string | string[]>
-			sendResponse({ statusCode: res.statusCode, headers: resHeaders, data: res })
-		})
-		clientRequest.once("error", (e) => handleError(e))
-		clientRequest.once("timeout", () => clientRequest.destroy(new ProxyError(NetErrorCode.TIMED_OUT)))
-		clientRequest.end(actualData ?? undefined)
-	}
-	return session.protocol.interceptStreamProtocol(protocol, handler)
+	})
+	return session.protocol.isProtocolHandled(protocol)
 }
 
 /**
  * assign a custom handler the asset protocol scheme on the session if it has not been done yet.
  * the handler will handle all relative requests for resources and ensures that only
  */
-function handleAssetProtocol(session: Session, assetDir: string, pathModule: typeof path): boolean {
-	if (session.protocol.isProtocolRegistered(ASSET_PROTOCOL)) return true
-	return session.protocol.registerFileProtocol(ASSET_PROTOCOL, (request, sendResponse) => {
+function handleAssetProtocol(session: Session, assetDir: string, pathModule: typeof path, fsModule: typeof fs): boolean {
+	if (session.protocol.isProtocolHandled(ASSET_PROTOCOL)) return true
+	session.protocol.handle(ASSET_PROTOCOL, async (request: Request): Promise<Response> => {
 		const fail = (msg: string) => {
 			log.debug(TAG, msg)
-			return sendResponse({ error: NetErrorCode.NOT_FOUND })
+			return new Response(null, { status: 404 })
 		}
 		// in node, new URL will normalize the path and remove /.. and /. elements.
 		// this doesn't work in browsers, so the startsWith check below should stay just to be sure
 		const url = new URL(request.url)
-		if (url.protocol.slice(0, -1) !== ASSET_PROTOCOL) return fail(`passed non-asset url to asset handler: ${url}`)
-		if (url.hostname !== "app" || !url.pathname.startsWith("/")) return fail(`Invalid asset:// URL: ${request.url}`)
-		const filePath = pathModule.resolve(assetDir, url.pathname.substring(1))
-		if (!filePath.startsWith(assetDir)) {
-			return fail(`Invalid asset URL ${request.url} w/ pathname ${url.pathname} got resolved to ${filePath})`)
+		if (url.protocol.slice(0, -1) !== ASSET_PROTOCOL) {
+			return fail(`passed non-asset url to asset handler: ${url}`)
+		} else if (url.hostname !== "app" || !url.pathname.startsWith("/")) {
+			return fail(`Invalid asset:// URL: ${request.url}`)
 		} else {
-			return sendResponse({ path: filePath })
+			const filePath = pathModule.resolve(assetDir, url.pathname.substring(1))
+			if (!filePath.startsWith(assetDir)) {
+				return fail(`Invalid asset URL ${request.url} w/ pathname ${url.pathname} got resolved to ${filePath})`)
+			} else {
+				// fetch for file:/// is not implemented in node 18 apparently, so we're getting it by hand.
+				try {
+					const content = await fsModule.promises.readFile(filePath)
+					const headers = new Headers({
+						"Content-Length": String(content.byteLength),
+						"Content-Type": await getMimeTypeForFile(filePath),
+					})
+					return new Response(content, { status: 200, headers })
+				} catch (e) {
+					return fail(`failed to read asset at ${request.url}: ${e.message}`)
+				}
+			}
 		}
 	})
+	return session.protocol.isProtocolHandled(ASSET_PROTOCOL)
 }
+
+const optionsResponse = lazyMemoized<Response>(() => {
+	const headers = new Headers()
+	headers.set("Access-Control-Allow-Origin", "*")
+	headers.set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+	headers.set("Access-Control-Allow-Headers", "*")
+	return new Response(null, {
+		status: 200,
+		headers,
+	})
+})
