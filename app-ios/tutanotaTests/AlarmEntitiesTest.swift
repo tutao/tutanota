2683d02056ee37c57125bbf522def31d3a92f36d
wrd <wrd@tutao.de>
2023-09-13 10:45:43 +0200

    [ios] Implement arbitrary alarm times
    Co-authored-by: ivk <ivk@tutao.de>

---
+++ app-ios/tutanotaTests/AlarmEntitiesTest.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import tutanota
+
+class AlarmEntitiesTest : XCTestCase {
+  func testParsingAlarmInterval() {
+    XCTAssertEqual(AlarmInterval(string: "3M"), AlarmInterval(unit: .minute, value: 3))
+    XCTAssertEqual(AlarmInterval(string: "4H"), AlarmInterval(unit: .hour, value: 4))
+    XCTAssertEqual(AlarmInterval(string: "11D"), AlarmInterval(unit: .day, value: 11))
+    XCTAssertEqual(AlarmInterval(string: "22W"), AlarmInterval(unit: .week, value: 22))
+  }
+}
