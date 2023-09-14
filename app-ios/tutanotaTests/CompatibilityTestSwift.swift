1e5087340b5ccd9871d9ab081c20a138798bc9ab
tutao <hello@tutao.de>
2023-09-14 11:13:22 +0200

    Rewrite iOS argon2id glue in Swift

---
+++ app-ios/tutanotaTests/CompatibilityTestSwift.swift
@@ -0,0 +1,33 @@
+import Foundation
+import XCTest
+@testable import tutanota
+
+// used for testing Swift code
+class CompatibilityTestSwift: XCTestCase {
+  var testData: [String: Any]?
+  
+  override func setUp() async throws {
+    try await super.setUp()
+    
+    let jsonUrl = Bundle(for: self.classForCoder).url(forResource: "CompatibilityTestData", withExtension: "json")!
+    let jsonData = try Data.init(contentsOf: jsonUrl)
+    self.testData = try JSONSerialization.jsonObject(with: jsonData) as? [String: Any]
+  }
+  
+  func testArgon2id() {
+    let ARGON2ID_HASH_LENGTH: Int = 32
+    let ARGON2ID_ITERATIONS: UInt = 4
+    let ARGON2ID_PARALLELISM: UInt = 1
+    let ARGON2ID_MEMORY_COST: UInt = 32 * 1024
+    
+    let tests = (testData!["argon2idTests"] as? [[String: String]])!
+    for test in tests {
+      let password = TUTEncodingConverter.string(toBytes: test["password"]!)
+      let expectedHash = TUTEncodingConverter.hex(toBytes: test["keyHex"]!)
+      let salt = TUTEncodingConverter.hex(toBytes: test["saltHex"]!)
+      let result = try! Argon2idFacade().generateHash(ofPassword: password, ofHashLength: ARGON2ID_HASH_LENGTH, withSalt: salt, withIterations: ARGON2ID_ITERATIONS, withParallelism: ARGON2ID_PARALLELISM, withMemoryCost: ARGON2ID_MEMORY_COST)
+      
+      XCTAssertEqual(expectedHash, result)
+    }
+  }
+}
