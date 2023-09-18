29379d26f7da66d0e299e4a86d0115095ec7506e
tutao <hello@tutao.de>
2023-09-18 12:03:19 +0200

    Clean up Argon2id iOS glue slightly

--- app-ios/tutanota/Sources/Crypto/Argon2.swift
+++ app-ios/tutanota/Sources/Crypto/Argon2.swift
@@ -18,29 +18,27 @@ func generateArgon2idHash(
   withParallelism parallelism: UInt,
   withMemoryCost memoryCostInKibibytes: UInt
 ) throws -> Data {
-  var hashOutput = Data(count: length)
+  var hashOutput = [UInt8](repeating: 0, count: length)
   
-  // we need to pass pointers directly to C, of which they have a limited lifetime (hence why we have three closures here!)
+  // we need to pass pointers directly to C, of which they have a limited lifetime (hence why we have two closures here!)
   let errorCode = password.withUnsafeBytes{(passwordBytePtr: UnsafeRawBufferPointer) in
     salt.withUnsafeBytes{(saltBytePtr: UnsafeRawBufferPointer) in
-      hashOutput.withUnsafeMutableBytes{(hashOutputBytePtr: UnsafeMutableRawBufferPointer) in
-        return argon2id_hash_raw(UInt32(iterations),
-                                 UInt32(memoryCostInKibibytes),
-                                 UInt32(parallelism),
-                                 passwordBytePtr.bindMemory(to: UInt8.self).baseAddress,
-                                 password.count,
-                                 saltBytePtr.bindMemory(to: UInt8.self).baseAddress,
-                                 salt.count,
-                                 hashOutputBytePtr.bindMemory(to: UInt8.self).baseAddress,
-                                 length)
-      }
+      return argon2id_hash_raw(UInt32(iterations),
+                               UInt32(memoryCostInKibibytes),
+                               UInt32(parallelism),
+                               passwordBytePtr.baseAddress,
+                               password.count,
+                               saltBytePtr.baseAddress,
+                               salt.count,
+                               &hashOutput,
+                               hashOutput.count)
     }
   }
   
   // handle error case
   switch Argon2_ErrorCodes(errorCode) {
   case ARGON2_OK:
-    return hashOutput
+    return Data(hashOutput)
   default:
     let errorMessage = argon2_error_message(errorCode)!
     throw TUTErrorFactory.createError(withDomain: TUT_CRYPTO_ERROR, message: "argon2id_hash_raw returned \(errorCode): \(errorMessage)")
