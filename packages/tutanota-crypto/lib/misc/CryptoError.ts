ffff88fb7a0e5b7364ebe8244651c41dab602f7e
ivk <ivk@tutao.de>
2023-09-29 15:20:57 +0200

    Fix handling corrupted credentials key
    We did not expect encryption errors from fetching the credentials key
    before because we didn't check HMAC in those cases. Now that we do
    check it more consistently the errors show up sooner and must be
    handled properly.
    
    fix #5916

--- packages/tutanota-crypto/lib/misc/CryptoError.ts
+++ packages/tutanota-crypto/lib/misc/CryptoError.ts
@@ -1,6 +1,9 @@
 // TODO reconcile with CryptoError in tutanota-3
+//    currently will get deserialized to the CryptoError from tutanota-3
 export class CryptoError extends Error {
 	constructor(message: string, error?: Error) {
 		super(error ? message + "> " + (error.stack ? error.stack : error.message) : message)
+		// This is needed to correctly deserialize the error.
+		this.name = "CryptoError"
 	}
 }
