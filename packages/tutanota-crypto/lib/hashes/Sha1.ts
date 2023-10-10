42547ab091be1c1d2d8d572b83a404f07b877da3
ivk <ivk@tutao.de>
2023-10-10 18:38:23 +0200

    Do not use the whole buffer for sjcl toBits()
    We've been passing the whole underlying buffer of Uint8Array to toBits()
    which works for most cases but can break if Uint8Array points only to
    the part of the underlying buffer. This is easy to run it with node's
    Buffer.from() which allocates a big chunk of data at once so it's
    underlying buffer will in many cases bigger than the byteLength.

--- packages/tutanota-crypto/lib/hashes/Sha1.ts
+++ packages/tutanota-crypto/lib/hashes/Sha1.ts
@@ -9,7 +9,7 @@ const sha1 = new sjcl.hash.sha1()
  */
 export function sha1Hash(uint8Array: Uint8Array): Uint8Array {
 	try {
-		sha1.update(sjcl.codec.arrayBuffer.toBits(uint8Array.buffer))
+		sha1.update(sjcl.codec.arrayBuffer.toBits(uint8Array.buffer, uint8Array.byteOffset, uint8Array.byteLength))
 		return new Uint8Array(sjcl.codec.arrayBuffer.fromBits(sha1.finalize(), false))
 	} finally {
 		sha1.reset()
--- packages/tutanota-crypto/lib/hashes/Sha512.ts
+++ packages/tutanota-crypto/lib/hashes/Sha512.ts
@@ -10,7 +10,7 @@ export const SHA512_HASH_LENGTH_BYTES = 64
  */
 export function sha512Hash(uint8Array: Uint8Array): Uint8Array {
 	try {
-		sha512.update(sjcl.codec.arrayBuffer.toBits(uint8Array.buffer))
+		sha512.update(sjcl.codec.arrayBuffer.toBits(uint8Array.buffer, uint8Array.byteOffset, uint8Array.byteLength))
 		return new Uint8Array(sjcl.codec.arrayBuffer.fromBits(sha512.finalize(), false))
 	} finally {
 		sha512.reset()
