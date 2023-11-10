a910109c7c82b2a6a8174442c4c8ea5f4b6d41a9
jat <jat@tutao.de>
2023-11-10 17:25:31 +0100

    Put back comment on why setTimeout is used to delay hiding the more button

--- src/mail/view/MailFolderRow.ts
+++ src/mail/view/MailFolderRow.ts
@@ -48,6 +48,8 @@ export class MailFolderRow implements Component<MailFolderRowAttrs> {
 		// these functions can be used to handle keyboard navigation
 		const handleForwardsTab = (event: KeyboardEvent) => {
 			if (event.key === "Tab" && !event.shiftKey) {
+				// The setTimeout is so that there is some time to tab to the rightButton
+				// otherwise it disappears immediately and is unreachable on keyboard
 				setTimeout(() => {
 					this.hovered = false
 				}, 5)
