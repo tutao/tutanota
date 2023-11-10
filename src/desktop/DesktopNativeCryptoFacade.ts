37e6ff133afbdd90fdb0a1f9c99405d85c78fab1
nig <nig@tutao.de>
2023-11-10 15:25:57 +0100

    add aesDecryptBytes and aesEncryptBytes to DesktopNativeCryptoFacade
    the ability to easily encrypt arbitrary length data without the
    roundtrip through aesEncryptObject is sorely lacking

--- src/desktop/DesktopNativeCryptoFacade.ts
+++ src/desktop/DesktopNativeCryptoFacade.ts
@@ -79,6 +79,14 @@ export class DesktopNativeCryptoFacade implements NativeCryptoFacade {
 		return this.cryptoFns.aesEncrypt(encryptionKey, keyToEncrypt, undefined, false)
 	}
 
+	aesDecryptBytes(encryptionKey: Aes256Key, data: Uint8Array): Uint8Array {
+		return this.cryptoFns.aesDecrypt(encryptionKey, data, true)
+	}
+
+	aesEncryptBytes(encryptionKey: Aes256Key, data: Uint8Array): Uint8Array {
+		return this.cryptoFns.aesEncrypt(encryptionKey, data, undefined, true, true)
+	}
+
 	decryptAndMapToInstance<T>(model: TypeModel, instance: Record<string, any>, piSessionKey: Uint8Array, piSessionKeyEncSessionKey: Uint8Array): Promise<T> {
 		const sk = this.cryptoFns.decryptKey(uint8ArrayToKey(piSessionKey), piSessionKeyEncSessionKey)
 		return this.cryptoFns.decryptAndMapToInstance(model, instance, sk)
