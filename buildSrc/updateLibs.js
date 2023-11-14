1d4afc689115c9804c7e49d89476106b0418a6ad
nig <nig@tutao.de>
2023-11-14 11:14:22 +0100

    vendor undici 5.27.2
    fix 803bbf05b06c321659e518d56c02e27bc9935fa1
    close #6150
    #6053

--- buildSrc/updateLibs.js
+++ buildSrc/updateLibs.js
@@ -34,6 +34,7 @@ const clientDependencies = [
 	{ src: "../node_modules/better-sqlite3/lib/index.js", target: "better-sqlite3.mjs", rollup: rollDesktopDep },
 	{ src: "../node_modules/keytar/lib/keytar.js", target: "keytar.mjs", rollup: rollDesktopDep },
 	{ src: "../node_modules/winreg/lib/registry.js", target: "winreg.mjs", rollup: rollDesktopDep },
+	{ src: "../node_modules/undici/index.js", target: "undici.mjs", rollup: rollDesktopDep },
 ]
 
 run()
