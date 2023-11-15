c561d27558754d580157ae3f48abf2875bc60bbe
mup <mup@tutao.de>
2023-11-15 14:16:55 +0100

    Changes after review

--- src/misc/TranslationKey.ts
+++ src/misc/TranslationKey.ts
@@ -1585,6 +1585,8 @@ export type TranslationKeyType =
 	| "you_label"
 	| "emptyString_msg"
 	| "failedToExport_label"
+	| "failedToExport_msg"
+	| "failedToExport_title"
 	| "eventNoLongerExists_msg"
 	| "credentialsEncryptionModeAppPassword_label"
 	| "credentialsEncryptionModeAppPasswordHelp_msg"
