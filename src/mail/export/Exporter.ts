27cbea08fe516f28dbe8d6e274dd7a68f2fb66e7
jat <jat@tutao.de>
2023-11-23 15:56:23 +0100

    Fix the day name being wrong when exporting an email as a `.eml`
    The day name was always the day after the date. This is due to
    `Date.getUTCDay()` returning 0 for Sunday but the `dayNames` array
    having "Mon" at index 0.

--- src/mail/export/Exporter.ts
+++ src/mail/export/Exporter.ts
@@ -219,7 +219,7 @@ function escapeSpecialCharacters(name: string): string {
 }
 
 export function _formatSmtpDateTime(date: Date): string {
-	const dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+	const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 	const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 	return (
 		dayNames[date.getUTCDay()] +
