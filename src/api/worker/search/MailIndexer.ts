235610b4356b74d1fc2e367a5b74b75dc093dfa8
bed <bed@tutao.de>
2023-12-11 22:17:33 +0100

    only invoke indexMailboxes once when extending or creating search index
    #fixes 6278
    
    This fix only removes the duplicate invocation of indexMailboxes
    which already improves the performance for my case by factor 10.

--- src/api/worker/search/MailIndexer.ts
+++ src/api/worker/search/MailIndexer.ts
@@ -285,10 +285,7 @@ export class MailIndexer {
 	async extendIndexIfNeeded(user: User, newOldestTimestamp: number): Promise<void> {
 		if (this.currentIndexTimestamp > FULL_INDEXED_TIMESTAMP && this.currentIndexTimestamp > newOldestTimestamp) {
 			this.mailboxIndexingPromise = this.mailboxIndexingPromise
-				.then(
-					async () => await this.indexMailboxes(user, newOldestTimestamp),
-					async () => await this.indexMailboxes(user, newOldestTimestamp),
-				)
+				.then(async () => await this.indexMailboxes(user, newOldestTimestamp))
 				.catch(
 					ofClass(CancelledError, (e) => {
 						console.log("extend mail index has been cancelled", e)
