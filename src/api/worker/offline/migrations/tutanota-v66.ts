649e9b5e31c734c01f67ca0d2abca8e8e5c5a499
paw <paw-hub@users.noreply.github.com>
2024-01-05 15:42:30 +0100

    Add Tutanota model v66 migrations
    We forgot to do this, and thus it results in emails being broken once
    they are put onto the server as undefined.
    
    tutadb#1738

--- src/api/worker/offline/migrations/tutanota-v66.ts
+++ src/api/worker/offline/migrations/tutanota-v66.ts
@@ -1,13 +1,12 @@
 import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
-import { deleteInstancesOfType, migrateAllElements, migrateAllListElements, removeValue } from "../StandardMigrations.js"
-import { CustomerTypeRef } from "../../../entities/sys/TypeRefs.js"
-import { MailboxGroupRootTypeRef, MailTypeRef } from "../../../entities/tutanota/TypeRefs.js"
+import { migrateAllListElements } from "../StandardMigrations.js"
+import { createMail, MailTypeRef } from "../../../entities/tutanota/TypeRefs.js"
 
 export const tutanota66: OfflineMigration = {
 	app: "tutanota",
 	version: 66,
 	async migrate(storage: OfflineStorage) {
-		// don't need to migrate InternalGroupData as it is never stored in the offline db
+		migrateAllListElements(MailTypeRef, storage, [createMail])
 	},
 }
