26cfae87c150a3dacca69cc0831d2a42fd174312
paw <paw-hub@users.noreply.github.com>
2024-01-25 16:50:20 +0100

    [ios] Sync contacts to iOS
    Add support for syncing Tuta contacts to iOS.
    
    closes #6382

---
+++ app-ios/tutanota/Sources/Contacts/StructuredContactTypes.swift
@@ -0,0 +1,48 @@
+import Contacts
+
+enum ContactAddressType: String {
+  case _private = "0"
+  case work = "1"
+  case other = "2"
+  case custom = "3"
+  
+  func toCNLabel() -> String {
+    switch self {
+    case ._private: return CNLabelHome
+    case .work: return CNLabelWork
+    default: return CNLabelOther
+    }
+  }
+}
+
+enum ContactPhoneNumberType: String {
+  case _private = "0"
+  case work = "1"
+  case mobile = "2"
+  case fax = "3"
+  case other = "4"
+  
+  func toCNLabel() -> String {
+    switch self {
+    case ._private: return CNLabelHome
+    case .work: return CNLabelWork
+    case .mobile: return CNLabelPhoneNumberMobile
+    case .fax: return CNLabelPhoneNumberOtherFax
+    default: return CNLabelOther
+    }
+  }
+}
+
+extension ContactAddressType: Codable {
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(self)
+  }
+}
+
+extension ContactPhoneNumberType: Codable {
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(self)
+  }
+}
