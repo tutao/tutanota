5be343bdd57b1c0364b4e6fc9ce5e157a0d6bf93
nig <nig@tutao.de>
2024-02-01 15:24:00 +0100

    delete User instead of UserGroupRoot to disable offline login

--- src/api/worker/offline/migrations/sys-v94.ts
+++ src/api/worker/offline/migrations/sys-v94.ts
@@ -3,7 +3,7 @@ import { OfflineStorage } from "../OfflineStorage.js"
 import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade.js"
 import { deleteInstancesOfType, migrateAllListElements } from "../StandardMigrations.js"
 import { MailBodyTypeRef, MailTypeRef } from "../../../entities/tutanota/TypeRefs.js"
-import { createCustomerInfo, CustomerInfoTypeRef, UserGroupRootTypeRef } from "../../../entities/sys/TypeRefs.js"
+import { createCustomerInfo, CustomerInfoTypeRef, UserTypeRef } from "../../../entities/sys/TypeRefs.js"
 
 export const sys94: OfflineMigration = {
 	app: "sys",
@@ -12,7 +12,7 @@ export const sys94: OfflineMigration = {
 		// these are due to the mailbody migration
 		await deleteInstancesOfType(storage, MailTypeRef)
 		await deleteInstancesOfType(storage, MailBodyTypeRef)
-		await deleteInstancesOfType(storage, UserGroupRootTypeRef)
+		await deleteInstancesOfType(storage, UserTypeRef)
 		// this is to add the customerInfo.supportInfo field (sys94)
 		await migrateAllListElements(CustomerInfoTypeRef, storage, [createCustomerInfo])
 	},
