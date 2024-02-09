d247c568099152e19e538ae93e0c08bcce0c4e84
paw <paw-hub@users.noreply.github.com>
2024-02-09 09:56:40 +0100

    [ios] Pass --strict to lint/format
    We want to make sure that the commands return a non-zero exit code when
    issues are found. This will allow us to later return our own non-zero
    exit code for CI.

--- app-ios/lint.sh
+++ app-ios/lint.sh
@@ -2,6 +2,12 @@
 # Use this script to format and/or lint the swift code. It has the same usage as the NPM scripts.
 set -eux -o pipefail
 
+# Run a command while allowing errors. This will change the final exit code of the script so CI fails.
+ERRORS_FOUND=false
+capture_errors() {
+  ${@} || ERRORS_FOUND=true
+}
+
 set_homebrew_path() {
   if [[ "$(uname -m)" == arm64 ]]; then
     export PATH="/opt/homebrew/bin:$PATH"
@@ -11,15 +17,15 @@ set_homebrew_path() {
 format() {
   set_homebrew_path
   
-  # Write changes to the files if $2 is true
-  local fix_command="lint"
+  # Write changes to the files if $2 is true; otherwise, use --strict so the exit code is non-zero if issues are found
+  local fix_command="lint --strict"
   if [ "$2" == true ]; then
     fix_command="format --in-place"
   fi
   
   if which swift-format > /dev/null; then
     FILES=$(find "${1}" -name "*.swift" -type f -not -path "*/GeneratedIpc/*")
-    swift-format $fix_command --configuration .swift-format.json --recursive --parallel $FILES
+    capture_errors swift-format $fix_command --configuration .swift-format.json --recursive --parallel $FILES
   else
     echo "warning: swift-format not installed, download from https://github.com/apple/swift-format"
   fi
@@ -28,14 +34,14 @@ format() {
 lint() {
   set_homebrew_path
   
-  # Write changes to the files if $2 is true
-  local fix_command=""
+  # Write changes to the files if $2 is true; otherwise, use --strict so the exit code is non-zero if issues are found
+  local arg="--strict"
   if [ "$2" == true ]; then
-    fix_command="--fix"
+    arg="--fix"
   fi
   
   if which swiftlint > /dev/null; then
-    swiftlint lint "$1" $fix_command
+    capture_errors swiftlint lint "$1" $arg
   else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
   fi
@@ -59,4 +65,15 @@ elif [ "$1" == "style:fix" ]; then
   format "$directory" true
 elif [ "$1" == "lint:fix" ]; then
   lint "$directory" true
+else
+  echo "Failed to match \`$1\`; exiting"
+  exit 1
+fi
+
+if [ "$ERRORS_FOUND" == true ]; then
+  echo "Error: Warnings/errors found with linting/formatting. Please fix them!"
+  exit 1
+else
+  echo "Looks good to me!"
+  exit 0
 fi
