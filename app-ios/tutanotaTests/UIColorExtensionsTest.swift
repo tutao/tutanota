231ce29ac6698708274fe289b0a19ba04935331f
paw <paw-hub@users.noreply.github.com>
2024-02-09 09:56:37 +0100

    [ios] Fix empty_xctest_method

--- app-ios/tutanotaTests/UIColorExtensionsTest.swift
+++ app-ios/tutanotaTests/UIColorExtensionsTest.swift
@@ -2,15 +2,6 @@ import XCTest
 import tutanota
 
 class UIColorExtensionsTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testIsColorLightPinkDark() throws {
       XCTAssertEqual(UIColor.init(hex: "#B73A9A")?.isLight(), false)
     }
