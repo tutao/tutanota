988dd3229dea1b64544cdf951b959e9c0e1d3903
Bothim_TV <development@bothimtv.com>
2024-02-13 15:46:52 +0100

    Minor change to completeness bar

--- src/gui/CompletenessIndicator.ts
+++ src/gui/CompletenessIndicator.ts
@@ -1,5 +1,6 @@
 import m, { Children, Component, Vnode } from "mithril"
 import { theme } from "./theme.js"
+import { DefaultAnimationTime } from "./animation/Animations.js"
 
 export interface CompletenessIndicatorAttrs {
 	percentageCompleted: number
@@ -8,26 +9,28 @@ export interface CompletenessIndicatorAttrs {
 
 export class CompletenessIndicator implements Component<CompletenessIndicatorAttrs> {
 	view({ attrs }: Vnode<CompletenessIndicatorAttrs>): Children {
+		const mediumStrengthPercentage: number = 30
+		const goodStrengthPercentage: number = 75
 		return m(
 			"",
 			{
 				style: {
 					border: `1px solid ${theme.content_button}`,
 					width: attrs.width ?? "100px",
-					height: "10px",
+					height: "8px",
 				},
 			},
 			m("", {
 				style: {
-					"background-color": attrs.percentageCompleted < 50 ? "red" : attrs.percentageCompleted < 75 ? "yellow" : "green",
-					// red to yellow to green
+					"background-color":
+						attrs.percentageCompleted < mediumStrengthPercentage ? "red" : attrs.percentageCompleted < goodStrengthPercentage ? "yellow" : "green",
 					"background-image":
-						attrs.percentageCompleted < 50
-							? "linear-gradient(90deg, rgba(150,50,50,1) 0%, rgba(255,0,0,1) 100%)" // black to red
-							: attrs.percentageCompleted < 75
-							? "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,255,0,1) 100%)" // red to yellow
-							: "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,245,0,1) 50%, rgba(0,255,0,1) 100%)",
-					transition: "width 1s ease 0s",
+						attrs.percentageCompleted < mediumStrengthPercentage
+							? "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,0,0,1) 100%)"
+							: attrs.percentageCompleted < goodStrengthPercentage
+							? "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,255,0,1) 100%)"
+							: "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,245,0,1) 75%, rgba(0,255,0,1) 100%)",
+					transition: `width ${DefaultAnimationTime * 3}ms ease 0s`,
 					width: attrs.percentageCompleted + "%",
 					height: "100%",
 				},
