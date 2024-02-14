8da63d63ab8ed3d6cc6e7a917570480231f200f1
mup <mrex@tuta.io>
2024-02-14 12:55:54 +0100

    Get Dirty info from contact on Android
    This commit adds the isDirty property to a contact and return Dirty
    contacts to the web part of the app, allowing the app to deal with
    native updates.

--- app-ios/tutanota/Sources/GeneratedIpc/MobileSystemFacade.swift
+++ app-ios/tutanota/Sources/GeneratedIpc/MobileSystemFacade.swift
@@ -8,6 +8,33 @@ import Foundation
  */
 public protocol MobileSystemFacade {
 	/**
+	 * Find suggestions in the OS contact provider.
+	 */
+	func findSuggestions(
+		_ query: String
+	) async throws -> [NativeContact]
+	/**
+	 * Store one or more contacts in system's contact book
+	 */
+	func saveContacts(
+		_ username: String,
+		_ contacts: [StructuredContact]
+	) async throws -> Void
+	/**
+	 * Sync all Tuta contacts with system's contact book, this operation includes Inserts, Updates and Deletions
+	 */
+	func syncContacts(
+		_ username: String,
+		_ contacts: [StructuredContact]
+	) async throws -> [StructuredContact]
+	/**
+	 * Delete all or a specific Tuta contact from system's contact book
+	 */
+	func deleteContacts(
+		_ username: String,
+		_ contactId: String?
+	) async throws -> Void
+	/**
 	 * Redirect the user to Phone's Settings
 	 */
 	func goToSettings(
