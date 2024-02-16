b4f58b664ef1babdb4405997dc80f6ae965c4d6d
paw <paw-hub@users.noreply.github.com>
2024-02-16 15:29:37 +0100

    Don't show incorrect mail address info on legacy plans
    It is not true that legacy plans can have unlimited custom domains. Make
    it not display anything here to prevent confusion.
    
    Fixes #6540

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -1641,6 +1641,7 @@ export default {
 		"yourFolders_action": "YOUR FOLDERS",
 		"yourMessage_label": "Your message",
 		"you_label": "You",
+		"mailAddressInfoLegacy_msg": "Disabled email addresses can be reassigned to another user or mailbox within your account.",
 		"vcardInSharingFiles_msg": "We detected one or more contact files, do you want to import or attach them?",
 		"importFromContactBook_label": "Import contacts from your device",
 		"importContacts_label": "Import contacts",
