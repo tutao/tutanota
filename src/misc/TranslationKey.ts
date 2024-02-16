fa80560f2c99f8e196e48a84c851b8122dc2635e
paw <paw-hub@users.noreply.github.com>
2024-02-16 15:29:37 +0100

    Don't show incorrect mail address info on legacy plans
    It is not true that legacy plans can have unlimited custom domains. Make
    it not display anything here to prevent confusion.
    
    Fixes #6540

--- src/misc/TranslationKey.ts
+++ src/misc/TranslationKey.ts
@@ -1650,6 +1650,7 @@ export type TranslationKeyType =
 	| "yourMessage_label"
 	| "you_label"
 	| "emptyString_msg"
+	| "mailAddressInfoLegacy_msg"
 	| "vcardInSharingFiles_msg"
 	| "importFromContactBook_label"
 	| "importContacts_label"
