ac75a7c47ffe7ce02626ad9300b1521a24145526
jat <jat@tutao.de>
2024-02-22 13:39:00 +0100

    Fix `licc` generating incorrect code for enums

--- app-android/app/src/main/java/de/tutao/tutanota/generated_ipc/PermissionType.kt
+++ app-android/app/src/main/java/de/tutao/tutanota/generated_ipc/PermissionType.kt
@@ -1,9 +1,19 @@
 /* generated file, don't edit. */
 
+
 package de.tutao.tutanota.ipc
 
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
 enum class PermissionType {
+	@SerialName("0")
 	CONTACTS,
+	
+	@SerialName("1")
 	IGNORE_BATTERY_OPTIMIZATION,
-	NOTIFICATION,
+	
+	@SerialName("2")
+	NOTIFICATION;
 }
--- src/native/common/generatedipc/PermissionType.ts
+++ src/native/common/generatedipc/PermissionType.ts
@@ -1,7 +1,7 @@
 /* generated file, don't edit. */
 
 export const enum PermissionType {
-	Contacts = 0,
-	IgnoreBatteryOptimization = 1,
-	Notification = 2,
+	Contacts = "0",
+	IgnoreBatteryOptimization = "1",
+	Notification = "2",
 }
