89f5975b0d3ec36268ca3ac4e40c20c64a58b2bf
paw <paw-hub@users.noreply.github.com>
2024-02-29 18:17:12 +0100

    Fix client-side captcha verification; fix SignupFormTest
    Fix Captcha matching hours 25-29 and allows it to match times that
    will never be correct (but can be checked locally).
    
    Add missing SignupFormTest to the test suite.
    
    Fixes #6316

--- test/tests/subscription/SignupFormTest.ts
+++ test/tests/subscription/SignupFormTest.ts
@@ -1,22 +1,31 @@
-// Note: This file is not included yet because importing SingupForm breaks test and also because test does not work
 import o from "@tutao/otest"
 import { parseCaptchaInput } from "../../../src/subscription/Captcha.js"
+
 o.spec("CaptchaInputParse", function () {
-	o("Hour", function () {
+	o("invalid input", function () {
 		o(parseCaptchaInput("nonsense")).equals(null)
 		o(parseCaptchaInput("2:")).equals(null)
 		o(parseCaptchaInput(":::")).equals(null)
 		o(parseCaptchaInput("")).equals(null)
 		o(parseCaptchaInput("25:01")).equals(null)
 		o(parseCaptchaInput("08:84")).equals(null)
+		o(parseCaptchaInput("08:60")).equals(null)
+	})
+	o("single digit hour or minute", function () {
 		o(parseCaptchaInput("1:1")).equals("01:01")
 		o(parseCaptchaInput("13:1")).equals("01:01")
-		o(parseCaptchaInput("13:01")).equals("01:01")
 		o(parseCaptchaInput("01:1")).equals("01:01")
 		o(parseCaptchaInput("1:01")).equals("01:01")
+		o(parseCaptchaInput("2:00")).equals("02:00")
+	})
+	o("hour 0", function () {
 		o(parseCaptchaInput("24:00")).equals("00:00")
 		o(parseCaptchaInput("12:00")).equals("00:00")
+		o(parseCaptchaInput("00:00")).equals("00:00")
 		o(parseCaptchaInput("24:14")).equals("00:14")
-		o(parseCaptchaInput("2:00")).equals("02:00")
+	})
+	o("24 hour and 12 hour match", function () {
+		o(parseCaptchaInput("13:01")).equals("01:01")
+		o(parseCaptchaInput("01:01")).equals("01:01")
 	})
 })
