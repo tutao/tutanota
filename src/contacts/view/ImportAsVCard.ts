b6c49d05c7f203573052ed99928960210c994bab
jat <jat@tutao.de>
2024-05-14 14:38:20 +0200

    Use the contact import dialog when importing vCards via the more button

--- src/contacts/view/ImportAsVCard.ts
+++ src/contacts/view/ImportAsVCard.ts
@@ -1,68 +1,27 @@
 import { showFileChooser } from "../../file/FileController.js"
-import { assertNotNull, utf8Uint8ArrayToString } from "@tutao/tutanota-utils"
-import { vCardFileToVCards, vCardListToContacts } from "../VCardImporter.js"
+import { utf8Uint8ArrayToString } from "@tutao/tutanota-utils"
 import { showProgressDialog } from "../../gui/dialogs/ProgressDialog.js"
 import { locator } from "../../api/main/MainLocator.js"
-import { GroupType } from "../../api/common/TutanotaConstants.js"
 import { Dialog } from "../../gui/base/Dialog.js"
-import { lang } from "../../misc/LanguageViewModel.js"
-import { SetupMultipleError } from "../../api/common/error/SetupMultipleError.js"
 import { ContactModel } from "../model/ContactModel.js"
 import { ContactTypeRef } from "../../api/entities/tutanota/TypeRefs.js"
 import { exportContacts } from "../VCardExporter.js"
 
 export function importAsVCard() {
-	showFileChooser(true, ["vcf"]).then((contactFiles) => {
-		let numberOfContacts: number
-
-		try {
-			if (contactFiles.length > 0) {
-				let vCardsList = contactFiles.map((contactFile) => {
-					let vCardFileData = utf8Uint8ArrayToString(contactFile.data)
-					let vCards = vCardFileToVCards(vCardFileData)
-
-					if (vCards == null) {
-						throw new Error("no vcards found")
-					} else {
-						return vCards
-					}
+	showFileChooser(true, ["vcf"]).then(async (contactFiles) => {
+		if (contactFiles.length <= 0) return
+		return showProgressDialog(
+			"pleaseWait_msg",
+			(async () => {
+				const contactImporter = await locator.contactImporter()
+				const contactListId = await locator.contactModel.getContactListId()
+				// If multiple vCard files where selected, combine the data within them
+				const vCardList = contactFiles.flatMap((contactFile) => {
+					return utf8Uint8ArrayToString(contactFile.data)
 				})
-				return showProgressDialog(
-					"pleaseWait_msg",
-					Promise.resolve().then(() => {
-						const flatvCards = vCardsList.flat()
-						const contactMembership = assertNotNull(
-							locator.logins.getUserController().user.memberships.find((m) => m.groupType === GroupType.Contact),
-						)
-						const contactList = vCardListToContacts(flatvCards, contactMembership.group)
-						numberOfContacts = contactList.length
-						return locator.contactModel.getContactListId().then((contactListId) =>
-							locator.entityClient.setupMultipleEntities(contactListId, contactList).then(() => {
-								// actually a success message
-								Dialog.message(() =>
-									lang.get("importVCardSuccess_msg", {
-										"{1}": numberOfContacts,
-									}),
-								)
-							}),
-						)
-					}),
-				)
-			}
-		} catch (e) {
-			console.log(e)
-
-			if (e instanceof SetupMultipleError) {
-				Dialog.message(() =>
-					lang.get("importContactsError_msg", {
-						"{amount}": e.failedInstances.length + "",
-						"{total}": numberOfContacts + "",
-					}),
-				)
-			} else {
-				Dialog.message("importVCardError_msg")
-			}
-		}
+				await contactImporter.importContactsFromFile(vCardList, contactListId!)
+			})(),
+		)
 	})
 }
 
