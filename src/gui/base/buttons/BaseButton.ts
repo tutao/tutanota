dc9d064605cb5ca05d8bd9ef8a74339b75d478d1
wrd <wrd@tutao.de>
2024-05-29 13:59:29 +0200

    Use title instead of aria-label in BaseButton
    Title causes the hover text, and aria-label should derive from title.
    
    fix #7031

--- src/gui/base/buttons/BaseButton.ts
+++ src/gui/base/buttons/BaseButton.ts
@@ -33,7 +33,7 @@ export class BaseButton implements ClassComponent<BaseButtonAttrs> {
 		return m(
 			"button",
 			{
-				"aria-label": attrs.label,
+				title: attrs.label,
 				disabled,
 				"aria-disabled": disabled,
 				pressed,
