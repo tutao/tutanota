80126f73032234d679a76c90d27609c0358a7fe2
ivk <ivk@tutao.de>
2024-06-04 21:02:34 +0200

    [sdk] InstanceMapper serialize

--- tuta-sdk/rust/src/instance_mapper.rs
+++ tuta-sdk/rust/src/instance_mapper.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+use std::fmt::Display;
 use std::sync::Arc;
 
-use serde::{de, Deserialize, Deserializer};
+use serde::{de, Deserialize, Deserializer, ser, Serialize, Serializer};
 use serde::de::{DeserializeSeed, IntoDeserializer, MapAccess, Visitor};
 use serde::de::value::{MapDeserializer, SeqDeserializer};
+use serde::ser::{SerializeMap, SerializeSeq, SerializeStruct, SerializeStructVariant, SerializeTuple, SerializeTupleStruct, SerializeTupleVariant};
 use thiserror::Error;
 
 use crate::element_value::{ElementValue, ParsedEntity};
@@ -11,12 +14,21 @@ use crate::entities::Entity;
 use crate::IdTuple;
 
 /// Converter between untyped representations of API Entities and generated structures
-pub struct InstanceMapper {
-}
+pub struct InstanceMapper {}
 
 impl InstanceMapper {
     fn new() -> Self {
         InstanceMapper {}
+    }
+}
+
+#[derive(Error, Debug)]
+#[error("Error while deserializing entity, {0}")]
+pub struct SerError(String);
+
+impl ser::Error for SerError {
+    fn custom<T>(msg: T) -> Self where T: Display {
+        SerError(msg.to_string())
     }
 }
 
@@ -28,6 +40,10 @@ impl InstanceMapper {
     pub fn parse_entity<'a, T: Entity + Deserialize<'a>>(&self, map: ParsedEntity) -> Result<T, DeError> {
         let de = ParsedEntityDeserializer::from_parsed_entity(map);
         T::deserialize(de).map_err(|e| e.into())
+    }
+
+    pub fn serialize_entity<'a, T: Entity + Serialize>(&self, entity: T) -> Result<ParsedEntity, SerError> {
+        entity.serialize(ElementValueSerializer).map(|v| v.assert_dict())
     }
 }
 
@@ -196,10 +212,182 @@ impl<'de> IntoDeserializer<'de> for ElementValue {
     }
 }
 
+/// Serialize Entity into ElementValue variant.
+// See serde_json::value::ser for an example.
+struct ElementValueSerializer;
+
+struct ElementValueStructSerializer {
+    map: HashMap<String, ElementValue>,
+}
+
+struct ElementValueSeqSerializer {
+    vec: Vec<ElementValue>
+}
+
+impl Serializer for ElementValueSerializer {
+    type Ok = ElementValue;
+    type Error = SerError;
+    type SerializeSeq = ElementValueSeqSerializer;
+    type SerializeTuple = ser::Impossible<ElementValue, SerError>;
+    type SerializeTupleStruct = ser::Impossible<ElementValue, SerError>;
+    type SerializeTupleVariant = ser::Impossible<ElementValue, SerError>;
+    type SerializeMap = ser::Impossible<ElementValue, SerError>;
+    type SerializeStruct = ElementValueStructSerializer;
+    type SerializeStructVariant = ser::Impossible<ElementValue, SerError>;
+
+    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+        Ok(ElementValue::Bool(v))
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
+        Ok(ElementValue::Number(v))
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
+        // FIXME: this is only called for binary data, we don't want to repack it into ElementValue,
+        //   we should do something about it
+        //   see serialize_bytes
+        Ok(ElementValue::Number(v.into()))
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        Ok(ElementValue::String(v.to_string()))
+    }
+
+    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
+        // FIXME this is not getting called, should use serde_bytes in generated types
+        Ok(ElementValue::Bytes(v.into()))
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        Ok(ElementValue::Null)
+    }
+
+    fn serialize_some<T>(self, value: &T) -> Result<Self::Ok, Self::Error> where T: ?Sized + Serialize {
+        value.serialize(self)
+    }
+
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_unit_variant(self, name: &'static str, variant_index: u32, variant: &'static str) -> Result<Self::Ok, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_newtype_struct<T>(self, name: &'static str, value: &T) -> Result<Self::Ok, Self::Error> where T: ?Sized + Serialize {
+        todo!()
+    }
+
+    fn serialize_newtype_variant<T>(self, name: &'static str, variant_index: u32, variant: &'static str, value: &T) -> Result<Self::Ok, Self::Error> where T: ?Sized + Serialize {
+        todo!()
+    }
+
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        let vec = match len {
+            None => Vec::new(),
+            Some(l) => Vec::with_capacity(l),
+        };
+        Ok(ElementValueSeqSerializer { vec })
+    }
+
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_tuple_struct(self, name: &'static str, len: usize) -> Result<Self::SerializeTupleStruct, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_tuple_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> Result<Self::SerializeTupleVariant, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_struct(self, name: &'static str, len: usize) -> Result<Self::SerializeStruct, Self::Error> {
+        Ok(ElementValueStructSerializer { map: HashMap::with_capacity(len) })
+    }
+
+    fn serialize_struct_variant(self, name: &'static str, variant_index: u32, variant: &'static str, len: usize) -> Result<Self::SerializeStructVariant, Self::Error> {
+        todo!()
+    }
+}
+
+impl SerializeSeq for ElementValueSeqSerializer {
+    type Ok = ElementValue;
+    type Error = SerError;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<(), Self::Error> where T: ?Sized + Serialize {
+        self.vec.push(value.serialize(ElementValueSerializer)?);
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(ElementValue::Array(self.vec))
+    }
+}
+
+impl SerializeStruct for ElementValueStructSerializer {
+    type Ok = ElementValue;
+    type Error = SerError;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<(), Self::Error> where T: ?Sized + Serialize {
+        self.map.insert(key.to_string(), value.serialize(ElementValueSerializer)?);
+        Ok(())
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(ElementValue::Dict(self.map))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::entities::sys::Group;
-    use crate::entities::tutanota::MailboxGroupRoot;
+    use crate::entities::sys::{ArchiveRef, ArchiveType, Group, TypeInfo};
+    use crate::entities::tutanota::{MailboxGroupRoot, OutOfOfficeNotificationRecipientList};
     use crate::json_element::RawEntity;
     use crate::json_serializer::JsonSerializer;
     use crate::type_model_provider::init_type_model_provider;
@@ -207,7 +395,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_group() {
+    fn test_de_group() {
         let email_string = include_str!("../test_data/group_response.json");
         let parsed_entity = get_parsed_entity::<Group>(email_string);
         let mapper = InstanceMapper::new();
@@ -218,11 +406,79 @@ mod tests {
     }
 
     #[test]
-    fn test_mailbox_group_root() {
+    fn test_de_mailbox_group_root() {
         let email_string = include_str!("../test_data/mailbox_group_root_response.json");
         let parsed_entity = get_parsed_entity::<MailboxGroupRoot>(email_string);
         let mapper = InstanceMapper::new();
         let _group_root: MailboxGroupRoot = mapper.parse_entity(parsed_entity).unwrap();
+    }
+
+    #[test]
+    fn test_ser_mailbox_group_root() {
+        let group_root = MailboxGroupRoot {
+            _format: 0,
+            _id: "".to_string(),
+            _ownerGroup: None,
+            _permissions: "".to_string(),
+            calendarEventUpdates: None,
+            mailbox: "mailboxId".to_string(),
+            mailboxProperties: None,
+            outOfOfficeNotification: None,
+            outOfOfficeNotificationRecipientList: Some(OutOfOfficeNotificationRecipientList {
+                _id: "oof_id".to_string(),
+                list: "oof_list".to_string(),
+            }),
+            serverProperties: "serverPropertiesId".to_string(),
+            whitelistRequests: "whitelistRequests".to_string(),
+        };
+        let mapper = InstanceMapper::new();
+        let result = mapper.serialize_entity(group_root.clone()).unwrap();
+        assert_eq!(&ElementValue::Number(0), result.get("_format").unwrap());
+    }
+
+    #[test]
+    fn test_ser_group() {
+        let group_root = Group {
+            _format: 0,
+            _id: "".to_string(),
+            _ownerGroup: None,
+            _permissions: "".to_string(),
+            groupInfo: IdTuple::new("list_id".to_owned(), "element_id".to_owned()),
+            administratedGroups: None,
+            archives: vec![ArchiveType {
+                _id: "archive_ref_id".to_string(),
+                active: ArchiveRef {
+                    _id: "_".to_string(),
+                    archiveId: "archive_id".to_string(),
+                },
+                inactive: vec![],
+                r#type: TypeInfo {
+                    _id: "_".to_string(),
+                    application: "app".to_string(),
+                    typeId: 1,
+                },
+            }],
+            currentKeys: None,
+            customer: None,
+            formerGroupKeys: None,
+            invitations: "_".to_string(),
+            members: "_".to_string(),
+            groupKeyVersion: 1,
+            admin: None,
+            r#type: 46,
+            adminGroupEncGKey: None,
+            adminGroupKeyVersion: None,
+            enabled: true,
+            external: false,
+            pubAdminGroupEncGKey: Some(vec![1, 2, 3]),
+            storageCounter: None,
+            user: None,
+
+        };
+        let mapper = InstanceMapper::new();
+        let result = mapper.serialize_entity(group_root.clone()).unwrap();
+        assert_eq!(&ElementValue::Number(0), result.get("_format").unwrap());
+        assert_eq!(&ElementValue::Bytes(vec![1, 2, 3]), result.get("pubAdminGroupEncGKey").unwrap())
     }
 
     fn get_parsed_entity<T: Entity>(email_string: &str) -> ParsedEntity {
