d6885807eb8fcec7841d1e0fc2aa76d67559e92f
vis <vis@tutao.de>
2024-06-11 14:57:11 +0200

    Introduce SystemModelV102
    Purge Group and UserGroupRoot from offline database.
    
    tutadb#1814

--- src/api/worker/offline/migrations/sys-v102.ts
+++ src/api/worker/offline/migrations/sys-v102.ts
@@ -2,12 +2,13 @@ import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
 import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade.js"
 import { deleteInstancesOfType } from "../StandardMigrations.js"
-import { UserGroupRootTypeRef } from "../../../entities/sys/TypeRefs.js"
+import { GroupTypeRef, UserGroupRootTypeRef } from "../../../entities/sys/TypeRefs.js"
 
 export const sys102: OfflineMigration = {
 	app: "sys",
 	version: 102,
 	async migrate(storage: OfflineStorage, sqlCipherFacade: SqlCipherFacade) {
-		await deleteInstancesOfType(storage, UserGroupRootTypeRef) // we need to do it again after the migration
+		await deleteInstancesOfType(storage, UserGroupRootTypeRef) // to ensure keyRotations is populated
+		await deleteInstancesOfType(storage, GroupTypeRef) // to ensure formerGroupKeys is populated
 	},
 }
