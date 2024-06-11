fc501a996eb509859eb1648a9861481c15fdde2b
vis <vis@tutao.de>
2024-06-11 16:01:35 +0200

    Disable offline login after migration
    We do this by deleting the User instances.
    
    With the other migrations, the data would be impossible to read anyway, and would leave the mailbox in an in-between state, showing nothing.

--- src/api/worker/offline/migrations/sys-v102.ts
+++ src/api/worker/offline/migrations/sys-v102.ts
@@ -2,7 +2,7 @@ import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
 import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade.js"
 import { deleteInstancesOfType } from "../StandardMigrations.js"
-import { GroupTypeRef, UserGroupRootTypeRef } from "../../../entities/sys/TypeRefs.js"
+import { GroupTypeRef, UserGroupRootTypeRef, UserTypeRef } from "../../../entities/sys/TypeRefs.js"
 
 export const sys102: OfflineMigration = {
 	app: "sys",
@@ -10,5 +10,7 @@ export const sys102: OfflineMigration = {
 	async migrate(storage: OfflineStorage, sqlCipherFacade: SqlCipherFacade) {
 		await deleteInstancesOfType(storage, UserGroupRootTypeRef) // to ensure keyRotations is populated
 		await deleteInstancesOfType(storage, GroupTypeRef) // to ensure formerGroupKeys is populated
+		// We also delete UserType ref to disable offline login. Otherwise, clients will see an unexpected error message with pure offline login.
+		await deleteInstancesOfType(storage, UserTypeRef)
 	},
 }
