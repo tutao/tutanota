1257ac1b7794d2505eed1da9b61ff736ca632ab6
jop <jop@tutao.de>
2024-06-17 10:14:01 +0200

    Add fixes and test cases
    Added some additional fixes for all possibilties of glpyhs used and
    existing/lacking canvas support. Outsourced some code into methods for
    better compartmentalization and testing.
    Added one test case for the detection of "invalid" glyphs

---
+++ test/tests/api/worker/pdf/PdfDocumentTest.ts
@@ -0,0 +1,13 @@
+import o from "@tutao/otest"
+import { areStringPartsOneByteLength } from "../../../../../src/api/worker/pdf/PdfDocument.js"
+
+o.spec("PdfDocument", function () {
+	o("Chars with over one byteLength are detected in StringParts", async function () {
+		o(areStringPartsOneByteLength("Hello World".split(""))).equals(true)
+		o(areStringPartsOneByteLength("ÄäÖöÜü".split(""))).equals(true)
+		o(areStringPartsOneByteLength(`"'That's Wrong!'";.,`.split(""))).equals(true)
+		o(areStringPartsOneByteLength("Wybieram się w podróż do Stambułu".split(""))).equals(false)
+		o(areStringPartsOneByteLength("我要去伊斯坦布尔旅行".split(""))).equals(false)
+		o(areStringPartsOneByteLength("𱁬".split(""))).equals(false)
+	})
+})
