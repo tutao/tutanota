b0838076fd37b17ed78f80d04afc91e9696cbbc1
jat <jat@tutao.de>
2024-06-28 16:57:03 +0200

    Remove unused code from iOS client xcodegen project

--- app-ios/tutanota/Sources/tutanota-Bridging-Header.h
+++ app-ios/tutanota/Sources/tutanota-Bridging-Header.h
@@ -2,9 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#define KYBER_K 4
-//#define OQS_ENABLE_KEM_kyber_1024 1
-
 #import "Utils/TUTIcons.h"
 #import <TutanotaSharedFramework/TUTEncodingConverter.h>
 #import <TutanotaSharedFramework/TUTLog.h>
