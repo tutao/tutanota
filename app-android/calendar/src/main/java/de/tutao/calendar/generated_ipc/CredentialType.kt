a631caddc60441c72821c780db80e806dbe054c8
and <and@tutao.de>
2024-07-01 17:08:06 +0200

    [CALENDAR][Android] Creates Android base project
    This commit creates the base android project to the Calendar
    App.
    
    It's the same as the mail app with less permission and without
    mailto and vcard handling
    
    Closes #7145
    
    Co-authored-by: Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>

---
+++ app-android/calendar/src/main/java/de/tutao/calendar/generated_ipc/CredentialType.kt
@@ -0,0 +1,5 @@
+/* generated file, don't edit. */
+
+
+package de.tutao.calendar.ipc
+typealias CredentialType = de.tutao.calendar.CredentialType
---
+++ app-android/calendar/src/main/java/de/tutao/calendar/generated_ipc/TaggedSqlValue.kt
@@ -0,0 +1,5 @@
+/* generated file, don't edit. */
+
+
+package de.tutao.calendar.ipc
+typealias TaggedSqlValue = de.tutao.calendar.offline.TaggedSqlValue
