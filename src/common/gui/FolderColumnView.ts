926888e710e87444f29f728ab37d2d6d90e57d49
and <and@tutao.de>
2024-07-01 09:36:14 +0200

    Show new event button in Agenda view when it's empty (for single and two columns layout)

--- src/common/gui/FolderColumnView.ts
+++ src/common/gui/FolderColumnView.ts
@@ -6,8 +6,7 @@ import { lang } from "../misc/LanguageViewModel.js"
 import { AriaLandmarks, landmarkAttrs } from "./AriaUtils.js"
 import type { ClickHandler } from "./base/GuiUtils.js"
 import type { lazy } from "@tutao/tutanota-utils"
-import { BaseButton, BaseButtonAttrs } from "./base/buttons/BaseButton.js"
-import { px, size } from "./size.js"
+import { MainCreateButton } from "./MainCreateButton.js"
 
 export type Attrs = {
 	/** Button to be displayed on top of the column*/
@@ -44,22 +43,7 @@ export class FolderColumnView implements Component<Attrs> {
 
 	private renderMainButton(attrs: Attrs): Children {
 		if (attrs.button) {
-			const label = lang.get(attrs.button.label)
-			return m(
-				".plr-button-double.scrollbar-gutter-stable-or-fallback.scroll",
-				m(BaseButton, {
-					label,
-					text: label,
-					onclick: attrs.button.click,
-					class: "full-width border-radius-big center b flash",
-					style: {
-						border: `2px solid ${theme.content_accent}`,
-						// matching toolbar
-						height: px(size.button_height + size.vpad_xs * 2),
-						color: theme.content_accent,
-					},
-				} satisfies BaseButtonAttrs),
-			)
+			return m(".plr-button-double.scrollbar-gutter-stable-or-fallback.scroll", MainCreateButton(attrs.button))
 		} else {
 			return null
 		}
