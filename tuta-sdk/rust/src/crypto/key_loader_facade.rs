a8d736ca564c9a6e1b70990f045d92bf1f810c3f
jat <jat@tutao.de>
2024-07-02 12:19:45 +0200

    [sdk] Use `mockall_double` instead of passing in with `dyn`
    Co-authored-by: paw <paw-hub@users.noreply.github.com>

--- tuta-sdk/rust/src/crypto/key_loader_facade.rs
+++ tuta-sdk/rust/src/crypto/key_loader_facade.rs
@@ -7,28 +7,21 @@ pub struct VersionedAesKey {
     pub version: i64,
 }
 
-#[cfg_attr(test, mockall::automock)]
-pub trait KeyLoaderFacade: Send + Sync {
-    fn get_current_group_key(&self, group: &GeneratedId) -> Result<VersionedAesKey, KeyLoadError>;
-    fn get_current_asymmetric_key_pair(&self, group: &GeneratedId) -> Result<AsymmetricKeyPair, KeyLoadError>;
-    fn get_group_key(&self, group: &GeneratedId, version: i64) -> Result<GenericAesKey, KeyLoadError>;
-    fn get_asymmetric_key_pair(&self, group: &GeneratedId, version: i64) -> Result<AsymmetricKeyPair, KeyLoadError>;
-}
-
 #[derive(uniffi::Object)]
-pub(crate) struct KeyLoaderFacadeImpl {}
+pub(crate) struct KeyLoaderFacade {}
 
-impl KeyLoaderFacade for KeyLoaderFacadeImpl {
-    fn get_current_group_key(&self, group: &GeneratedId) -> Result<VersionedAesKey, KeyLoadError> {
+#[cfg_attr(test, mockall::automock)]
+impl KeyLoaderFacade {
+    pub fn get_current_group_key(&self, group: &GeneratedId) -> Result<VersionedAesKey, KeyLoadError> {
         todo!()
     }
-    fn get_current_asymmetric_key_pair(&self, group: &GeneratedId) -> Result<AsymmetricKeyPair, KeyLoadError> {
+    pub fn get_current_asymmetric_key_pair(&self, group: &GeneratedId) -> Result<AsymmetricKeyPair, KeyLoadError> {
         todo!()
     }
-    fn get_group_key(&self, group: &GeneratedId, version: i64) -> Result<GenericAesKey, KeyLoadError> {
+    pub fn get_group_key(&self, group: &GeneratedId, version: i64) -> Result<GenericAesKey, KeyLoadError> {
         todo!()
     }
-    fn get_asymmetric_key_pair(&self, group: &GeneratedId, version: i64) -> Result<AsymmetricKeyPair, KeyLoadError> {
+    pub fn get_asymmetric_key_pair(&self, group: &GeneratedId, version: i64) -> Result<AsymmetricKeyPair, KeyLoadError> {
         todo!()
     }
 }
