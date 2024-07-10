df7f5dcc55018bff3231b79f8fc24334989d473e
mup <mup@tutao.de>
2024-07-10 11:54:05 +0200

    [Calendar] Moves ActionBar to common and fix search selection and filter bugs
    Fixes calendar not selecting an event when the event ID is present in the URL.
    Also fixes the calendar selection that was throwing an error when moving back
    to All calendars
    
    Co-authored-by: André Dias <and@tutao.de>

--- src/calendar-app/calendar/search/view/SearchListView.ts
+++ src/calendar-app/calendar/search/view/SearchListView.ts
@@ -1,122 +1,121 @@
-import m, {Children, Component, Vnode} from "mithril"
-import {assertMainOrNode} from "../../../../common/api/common/Env"
-import {downcast, TypeRef} from "@tutao/tutanota-utils"
-import {ListModel} from "../../../../common/misc/ListModel.js"
-import {List, ListAttrs, MultiselectMode, RenderConfig} from "../../../../common/gui/base/List.js"
-import {size} from "../../../../common/gui/size.js"
-import {CalendarEvent,} from "../../../../common/api/entities/tutanota/TypeRefs.js"
+import m, { Children, Component, Vnode } from "mithril"
+import { assertMainOrNode } from "../../../../common/api/common/Env"
+import { downcast, TypeRef } from "@tutao/tutanota-utils"
+import { ListModel } from "../../../../common/misc/ListModel.js"
+import { List, ListAttrs, MultiselectMode, RenderConfig } from "../../../../common/gui/base/List.js"
+import { size } from "../../../../common/gui/size.js"
+import { CalendarEvent } from "../../../../common/api/entities/tutanota/TypeRefs.js"
 import ColumnEmptyMessageBox from "../../../../common/gui/base/ColumnEmptyMessageBox.js"
-import {BootIcons} from "../../../../common/gui/base/icons/BootIcons.js"
-import {lang} from "../../../../common/misc/LanguageViewModel.js"
-import {theme} from "../../../../common/gui/theme.js"
-import {VirtualRow} from "../../../../common/gui/base/ListUtils.js"
-import {styles} from "../../../../common/gui/styles.js"
-import {KindaCalendarRow} from "../../view/CalendarRow.js"
+import { BootIcons } from "../../../../common/gui/base/icons/BootIcons.js"
+import { lang } from "../../../../common/misc/LanguageViewModel.js"
+import { theme } from "../../../../common/gui/theme.js"
+import { VirtualRow } from "../../../../common/gui/base/ListUtils.js"
+import { styles } from "../../../../common/gui/styles.js"
+import { KindaCalendarRow } from "../../view/CalendarRow.js"
 
 assertMainOrNode()
 
 export class SearchResultListEntry {
-    constructor(readonly entry: CalendarEvent) {
-    }
+	constructor(readonly entry: CalendarEvent) {}
 
-    get _id(): IdTuple {
-        return this.entry._id
-    }
+	get _id(): IdTuple {
+		return this.entry._id
+	}
 }
 
 export interface SearchListViewAttrs {
-    listModel: ListModel<SearchResultListEntry>
-    onSingleSelection: (item: SearchResultListEntry) => unknown
-    isFreeAccount: boolean
-    cancelCallback: () => unknown | null
+	listModel: ListModel<SearchResultListEntry>
+	onSingleSelection: (item: SearchResultListEntry) => unknown
+	isFreeAccount: boolean
+	cancelCallback: () => unknown | null
 }
 
 export class SearchListView implements Component<SearchListViewAttrs> {
-    private listModel: ListModel<SearchResultListEntry>
+	private listModel: ListModel<SearchResultListEntry>
 
-    constructor({attrs}: Vnode<SearchListViewAttrs>) {
-        this.listModel = attrs.listModel
-    }
+	constructor({ attrs }: Vnode<SearchListViewAttrs>) {
+		this.listModel = attrs.listModel
+	}
 
-    view({attrs}: Vnode<SearchListViewAttrs>): Children {
-        this.listModel = attrs.listModel
-        const icon = BootIcons.Calendar;
-        const renderConfig = this.calendarRenderConfig;
+	view({ attrs }: Vnode<SearchListViewAttrs>): Children {
+		this.listModel = attrs.listModel
+		const icon = BootIcons.Calendar
+		const renderConfig = this.calendarRenderConfig
 
-        return attrs.listModel.isEmptyAndDone()
-            ? m(ColumnEmptyMessageBox, {
-                icon,
-                message: () =>
-                    lang.get("searchNoResults_msg") + "\n" + (attrs.isFreeAccount ? lang.get("goPremium_msg") : lang.get("switchSearchInMenu_label")),
-                color: theme.list_message_bg,
-            })
-            : m(List, {
-                state: attrs.listModel.state,
-                renderConfig,
-                onLoadMore: () => {
-                    attrs.listModel?.loadMore()
-                },
-                onRetryLoading: () => {
-                    attrs.listModel?.retryLoading()
-                },
-                onSingleSelection: (item: SearchResultListEntry) => {
-                    attrs.listModel?.onSingleSelection(item)
-                    attrs.onSingleSelection(item)
-                },
-                onSingleTogglingMultiselection: (item: SearchResultListEntry) => {
-                    attrs.listModel.onSingleInclusiveSelection(item, styles.isSingleColumnLayout())
-                },
-                onRangeSelectionTowards: (item: SearchResultListEntry) => {
-                    attrs.listModel.selectRangeTowards(item)
-                },
-                onStopLoading() {
-                    if (attrs.cancelCallback != null) {
-                        attrs.cancelCallback()
-                    }
+		return attrs.listModel.isEmptyAndDone()
+			? m(ColumnEmptyMessageBox, {
+					icon,
+					message: () =>
+						lang.get("searchNoResults_msg") + "\n" + (attrs.isFreeAccount ? lang.get("goPremium_msg") : lang.get("switchSearchInMenu_label")),
+					color: theme.list_message_bg,
+			  })
+			: m(List, {
+					state: attrs.listModel.state,
+					renderConfig,
+					onLoadMore: () => {
+						attrs.listModel?.loadMore()
+					},
+					onRetryLoading: () => {
+						attrs.listModel?.retryLoading()
+					},
+					onSingleSelection: (item: SearchResultListEntry) => {
+						attrs.listModel?.onSingleSelection(item)
+						attrs.onSingleSelection(item)
+					},
+					onSingleTogglingMultiselection: (item: SearchResultListEntry) => {
+						attrs.listModel.onSingleInclusiveSelection(item, styles.isSingleColumnLayout())
+					},
+					onRangeSelectionTowards: (item: SearchResultListEntry) => {
+						attrs.listModel.selectRangeTowards(item)
+					},
+					onStopLoading() {
+						if (attrs.cancelCallback != null) {
+							attrs.cancelCallback()
+						}
 
-                    attrs.listModel.stopLoading()
-                },
-            } satisfies ListAttrs<SearchResultListEntry, SearchResultListRow>)
-    }
+						attrs.listModel.stopLoading()
+					},
+			  } satisfies ListAttrs<SearchResultListEntry, SearchResultListRow>)
+	}
 
-    private readonly calendarRenderConfig: RenderConfig<SearchResultListEntry, SearchResultListRow> = {
-        itemHeight: size.list_row_height,
-        multiselectionAllowed: MultiselectMode.Disabled,
-        swipe: null,
-        createElement: (dom: HTMLElement) => {
-            const row: SearchResultListRow = new SearchResultListRow(new KindaCalendarRow(dom))
-            m.render(dom, row.render())
-            return row
-        },
-    }
+	private readonly calendarRenderConfig: RenderConfig<SearchResultListEntry, SearchResultListRow> = {
+		itemHeight: size.list_row_height,
+		multiselectionAllowed: MultiselectMode.Disabled,
+		swipe: null,
+		createElement: (dom: HTMLElement) => {
+			const row: SearchResultListRow = new SearchResultListRow(new KindaCalendarRow(dom))
+			m.render(dom, row.render())
+			return row
+		},
+	}
 }
 
 export class SearchResultListRow implements VirtualRow<SearchResultListEntry> {
-    top: number
-    // set from List
-    domElement: HTMLElement | null = null
+	top: number
+	// set from List
+	domElement: HTMLElement | null = null
 
-    // this is our own entry which we need for some reason (probably easier to deal with than a lot of sum type entries)
-    private _entity: SearchResultListEntry | null = null
-    get entity(): SearchResultListEntry | null {
-        return this._entity
-    }
+	// this is our own entry which we need for some reason (probably easier to deal with than a lot of sum type entries)
+	private _entity: SearchResultListEntry | null = null
+	get entity(): SearchResultListEntry | null {
+		return this._entity
+	}
 
-    private _delegate: KindaCalendarRow
+	private _delegate: KindaCalendarRow
 
-    constructor(delegate: KindaCalendarRow) {
-        this._delegate = delegate
-        this.top = 0
-    }
+	constructor(delegate: KindaCalendarRow) {
+		this._delegate = delegate
+		this.top = 0
+	}
 
-    update(entry: SearchResultListEntry, selected: boolean, isInMultiSelect: boolean): void {
-        this._delegate.domElement = this.domElement!
-        this._entity = entry
+	update(entry: SearchResultListEntry, selected: boolean, isInMultiSelect: boolean): void {
+		this._delegate.domElement = this.domElement!
+		this._entity = entry
 
-        this._delegate.update(downcast(entry.entry), selected, isInMultiSelect)
-    }
+		this._delegate.update(downcast(entry.entry), selected, isInMultiSelect)
+	}
 
-    render(): Children {
-        return this._delegate.render()
-    }
+	render(): Children {
+		return this._delegate.render()
+	}
 }
