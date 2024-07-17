0642928c3b85da0c9d26995fd83f9bbd467b841a
paw <paw-hub@users.noreply.github.com>
2024-07-17 14:41:32 +0200

    [sdk] Implement Android notifications with subject
    Co-authored-by: paw <paw-hub@users.noreply.github.com>

--- app-android/app/src/main/java/de/tutao/tutanota/push/NotificationInfo.kt
+++ app-android/app/src/main/java/de/tutao/tutanota/push/NotificationInfo.kt
@@ -1,16 +1,25 @@
 package de.tutao.tutanota.push
 
+import de.tutao.tutasdk.IdTuple
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class NotificationInfo(
-	val mailAddress: String,
-	val userId: String,
-	val mailId: IdTupleWrapper?,
+		val mailAddress: String,
+		val userId: String,
+		val mailId: IdTupleWrapper?,
 )
 
 @Serializable
 data class IdTupleWrapper(
-	val listId: String,
-	val listElementId: String,
+		val listId: String,
+		val listElementId: String,
 )
+
+// The name is to avoid confusion between `tutasdk.IdTuple` and `tutanota.IdTuple`
+fun IdTupleWrapper.toSdkIdTuple(): IdTuple {
+	return IdTuple(
+			this.listId,
+			this.listElementId
+	)
+}
