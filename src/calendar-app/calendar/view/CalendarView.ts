fc74f44e33ee9f84432202a86ed75cf9cf51ec7e
wrd <wrd@tutao.de>
2024-07-24 17:32:30 +0200

    New event made through agenda view new event has next half hour time

--- src/calendar-app/calendar/view/CalendarView.ts
+++ src/calendar-app/calendar/view/CalendarView.ts
@@ -306,7 +306,7 @@ export class CalendarView extends BaseTopLevelView implements TopLevelView<Calen
 									scrollPosition: this.viewModel.getScrollPosition(),
 									onScrollPositionChange: (newPosition: number) => this.viewModel.setScrollPosition(newPosition),
 									onViewChanged: (vnode) => this.viewModel.setViewParameters(vnode.dom as HTMLElement),
-									onCreateEvent: () => this.createNewEventDialog(),
+									onNewEvent: (date) => this.createNewEventDialog(date),
 								} satisfies CalendarAgendaViewAttrs),
 							})
 
