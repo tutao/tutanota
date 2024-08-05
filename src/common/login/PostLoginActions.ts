87f6d627652e51f04105e53dfb02b031cc192449
jat <jat@tutao.de>
2024-08-05 14:08:14 +0200

    Fix the `RichNotificationsNews` news item showing on added accounts
    This stops the news counter displaying '1' when logged into a newly
    added account. It was caused by the initial call to
    `getExtendedNotificationMode()` inside `RichNotificationNews.ts`
    returning `DEFAULT_EXTENDED_NOTIFCATION_MODE` (`NO_SENDER_OR_SUBJECT`)
    thus causing `RichNotificationNews.isShown()` to return `true` instead
    of the default inside `storePushIdentifierSessionKey()`
    (`SENDER_AND_SUBJECT`).
    
    Co-authored-by: wec43 <162116343+wec43@users.noreply.github.com>

--- src/common/login/PostLoginActions.ts
+++ src/common/login/PostLoginActions.ts
@@ -131,13 +131,15 @@ export class PostLoginActions implements PostLoginAction {
 		}
 
 		if (isApp() || isDesktop()) {
+			// Do not try to register for notifications while the setup dialog
+			// is being shown because we might not have a permission yet and
+			// we don't want to ask for it while dialog is shown, we will ask in
+			// the dialog anyway.
+			// After dialog is finished or dismissed the setup is "complete".
 			if ((isApp() && deviceConfig.getIsSetupComplete()) || isDesktop()) {
-				// Do not try to register for notifications while the setup dialog
-				// is being shown because we might not have a permission yet and
-				// we don't want to ask for it while dialog is shown, we will ask in
-				// the dialog anyway.
-				// After dialog is finished or dismissed the setup is "complete".
-				locator.pushService.register()
+				// Await the push service registration so `storePushIdentifierLocally()` can set the extended notification mode on Android
+				// before `loadNewsIds()` runs the `isShown()` check of the `RichNotificationsNews` news item
+				await locator.pushService.register()
 			} else {
 				console.log("Skipping registering for notifications while setup dialog is shown")
 			}
