112de541367505d783f837227f96688c01ba50fa
jhm <17314077+jomapp@users.noreply.github.com>
2024-08-20 16:58:38 +0200

    fix offline migrations for MailSet support (TutanotaModelV74)
    Closes #7429

--- src/common/api/worker/offline/migrations/tutanota-v74.ts
+++ src/common/api/worker/offline/migrations/tutanota-v74.ts
@@ -1,16 +1,32 @@
 import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
 import { addValue, migrateAllElements, migrateAllListElements } from "../StandardMigrations.js"
-import { createMail, createMailBox, MailBoxTypeRef, MailFolderTypeRef, MailTypeRef } from "../../../entities/tutanota/TypeRefs.js"
+import {
+	createMail,
+	createMailBox,
+	createMailFolder,
+	createUserSettingsGroupRoot,
+	MailBoxTypeRef,
+	MailFolderTypeRef,
+	MailTypeRef,
+	UserSettingsGroupRootTypeRef,
+} from "../../../entities/tutanota/TypeRefs.js"
 import { GENERATED_MIN_ID } from "../../../common/utils/EntityUtils.js"
 
 export const tutanota74: OfflineMigration = {
 	app: "tutanota",
 	version: 74,
 	async migrate(storage: OfflineStorage) {
-		// the TutanotaModelV75 introduces MailSets to support import and labels
-		await migrateAllListElements(MailFolderTypeRef, storage, [addValue("isLabel", "0"), addValue("isMailSet", "0"), addValue("entries", GENERATED_MIN_ID)])
+		// the TutanotaModelV74 introduces MailSets to support import and labels
+		await migrateAllListElements(MailFolderTypeRef, storage, [
+			addValue("isLabel", false),
+			addValue("isMailSet", false),
+			addValue("entries", GENERATED_MIN_ID),
+		])
 		await migrateAllElements(MailBoxTypeRef, storage, [createMailBox]) // initialize mailbags
 		await migrateAllListElements(MailTypeRef, storage, [createMail]) // initialize sets
+
+		// we need to re-initialize the UserSettingsGroupRoot to add a default value for defaultAlarmsList on GroupSettings
+		await migrateAllElements(UserSettingsGroupRootTypeRef, storage, [createUserSettingsGroupRoot])
 	},
 }
