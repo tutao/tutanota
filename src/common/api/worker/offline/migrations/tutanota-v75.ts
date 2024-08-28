3cfaa7f00f1ebd2af29427f7077ad6868b3a3f67
nig <nig@tutao.de>
2024-08-28 15:05:45 +0200

    fix offline migrations for tutanota 75
    when logging in, the app assumes that the
    UserSettingsGroupRoot is cached if the UserGroupInfo
    is cached. It is required for partialLogin to work at all, so
    we have to redownload it after the migration.
    
    Deleting UserGroupInfos as well makes sure that we
    fall back to the sync login to redownload the changed entities
    
    Co-authored-by: mup <34790144+murilopereirame@users.noreply.github.com>

--- src/common/api/worker/offline/migrations/tutanota-v75.ts
+++ src/common/api/worker/offline/migrations/tutanota-v75.ts
@@ -2,6 +2,9 @@ import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
 import { deleteInstancesOfType } from "../StandardMigrations.js"
 import { UserSettingsGroupRootTypeRef } from "../../../entities/tutanota/TypeRefs.js"
+import { GroupInfoTypeRef } from "../../../entities/sys/TypeRefs.js"
+import { GroupType } from "../../../common/TutanotaConstants.js"
+import { getElementId, getListId } from "../../../common/utils/EntityUtils.js"
 import { AuditLogEntryTypeRef } from "../../../entities/sys/TypeRefs.js"
 
 export const tutanota75: OfflineMigration = {
@@ -9,6 +12,12 @@ export const tutanota75: OfflineMigration = {
 	version: 75,
 	async migrate(storage: OfflineStorage) {
 		await deleteInstancesOfType(storage, UserSettingsGroupRootTypeRef)
+		// required to throw the LoginIncompleteError when trying async login
+		const groupInfos = await storage.getListElementsOfType(GroupInfoTypeRef)
+		for (const groupInfo of groupInfos) {
+			if (groupInfo.groupType !== GroupType.User) continue
+			await storage.deleteIfExists(GroupInfoTypeRef, getListId(groupInfo), getElementId(groupInfo))
+		}
 		await deleteInstancesOfType(storage, AuditLogEntryTypeRef)
 	},
 }
