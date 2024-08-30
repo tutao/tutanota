2ab8b8387636872bef5e2df584d04360cfd70d0c
rih <rih@tutao.de>
2024-08-30 13:15:07 +0200

    import the right SearchBar based on app type
    Co-authored-by: Murilo Rocha Pereira <34790144+murilopereirame@users.noreply.github.com>

--- src/common/misc/LazySearchBar.ts
+++ src/common/misc/LazySearchBar.ts
@@ -1,6 +1,7 @@
 import m, { Children, ClassComponent, Vnode } from "mithril"
 import type { SearchBar, SearchBarAttrs } from "../../mail-app/search/SearchBar.js"
 import { LazyLoaded } from "@tutao/tutanota-utils"
+import { client } from "./ClientDetector.js"
 
 /**
  * Lazy wrapper around SearchBar which unfortunately resides in the search chunk right now and cannot be imported from some files.
@@ -9,7 +10,8 @@ import { LazyLoaded } from "@tutao/tutanota-utils"
  */
 export class LazySearchBar implements ClassComponent<SearchBarAttrs> {
 	private static searchBar: LazyLoaded<SearchBar> = new LazyLoaded(async () => {
-		const { searchBar } = await import("../../mail-app/search/SearchBar.js")
+		const searchbarPath = client.isCalendarApp() ? "../../calendar-app/search/CalendarSearchBar.js" : "../../mail-app/search/SearchBar.js"
+		const { searchBar } = await import(searchbarPath)
 		m.redraw()
 		return searchBar
 	})
