0e166425d7617323486b07a2bd56efc407968676
ivk <ivk@tutao.de>
2024-09-09 14:57:20 +0200

    v244.240909.0

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 21
-		versionName = "244.240905.0"
+		versionCode = 22
+		versionName = "244.240909.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
