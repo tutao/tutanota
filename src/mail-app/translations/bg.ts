e17b711c281955f53566872f8903a4730275ea3d
jenkins build server <jenkins@next.tutao.de>
2024-09-17 17:12:57 +0200

    update translations

--- src/mail-app/translations/bg.ts
+++ src/mail-app/translations/bg.ts
@@ -11,7 +11,7 @@ export default {
 		"other"
 	],
 	"created_at": "2015-03-12T09:25:20Z",
-	"updated_at": "2024-08-29T16:04:52Z",
+	"updated_at": "2024-09-04T12:36:18Z",
 	"source_locale": null,
 	"fallback_locale": null,
 	"keys": {
--- src/mail-app/translations/id.ts
+++ src/mail-app/translations/id.ts
@@ -10,7 +10,7 @@ export default {
 		"other"
 	],
 	"created_at": "2015-04-07T07:56:36Z",
-	"updated_at": "2024-08-29T16:04:52Z",
+	"updated_at": "2024-09-04T12:36:18Z",
 	"source_locale": null,
 	"fallback_locale": null,
 	"keys": {
