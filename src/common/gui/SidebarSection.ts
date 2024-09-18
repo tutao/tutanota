61bf5968936cd522b283ae01a14b64b18eca7a8e
and <and@tutao.de>
2024-09-18 13:32:05 +0200

    [Calendar] Changes at external calendars subscriptions
    This commit modifies the creation dialog used to subscribe to external
    calendars.
    Also, fix a few bugs such as the external calendar appearing as an
    available calendar during event creation and private calendars being
    listed as external after editing it on the sidebar.

--- src/common/gui/SidebarSection.ts
+++ src/common/gui/SidebarSection.ts
@@ -2,16 +2,13 @@ import m, { Child, Children, Component, Vnode } from "mithril"
 import type { TranslationKey } from "../misc/LanguageViewModel"
 import { lang } from "../misc/LanguageViewModel"
 import { theme } from "./theme"
-import { isNotNull, lazy } from "@tutao/tutanota-utils"
-import { ExpanderButton, ExpanderPanel } from "./base/Expander.js"
+import { lazy } from "@tutao/tutanota-utils"
 import Stream from "mithril/stream"
 import stream from "mithril/stream"
-import { px, size } from "./size.js"
 
 export type SidebarSectionAttrs = {
 	name: TranslationKey | lazy<string>
 	button?: Child
-	expandable?: true
 	hideIfEmpty?: true
 }
 
@@ -19,7 +16,7 @@ export class SidebarSection implements Component<SidebarSectionAttrs> {
 	expanded: Stream<boolean> = stream(true)
 
 	view(vnode: Vnode<SidebarSectionAttrs>): Children {
-		const { name, button, expandable, hideIfEmpty } = vnode.attrs
+		const { name, button, hideIfEmpty } = vnode.attrs
 		const content = vnode.children
 		if (hideIfEmpty && content == false) return null // Using loose equality to check if children has any contents
 		return m(
@@ -31,34 +28,10 @@ export class SidebarSection implements Component<SidebarSectionAttrs> {
 			},
 			[
 				m(".folder-row.flex-space-between.plr-button.pt-s.button-height", [
-					expandable
-						? m(".flex", [
-								m(ExpanderButton, {
-									label: () => "",
-									expanded: this.expanded(),
-									onExpandedChange: this.expanded,
-									isBig: true,
-									style: {
-										minWidth: "auto",
-										paddingTop: 0,
-										paddingLeft: px(size.hpad_small),
-										paddingRight: px(size.hpad_small),
-									},
-								}),
-								m("small.b.align-self-center.text-ellipsis.plr-button", lang.getMaybeLazy(name).toLocaleUpperCase()),
-						  ])
-						: m("small.b.align-self-center.text-ellipsis.plr-button", lang.getMaybeLazy(name).toLocaleUpperCase()),
+					m("small.b.align-self-center.text-ellipsis.plr-button", lang.getMaybeLazy(name).toLocaleUpperCase()),
 					button ?? null,
 				]),
-				expandable
-					? m(
-							ExpanderPanel,
-							{
-								expanded: this.expanded(),
-							},
-							content,
-					  )
-					: content,
+				content,
 			],
 		)
 	}
