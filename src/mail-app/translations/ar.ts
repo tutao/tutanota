156707d8c7e3d22f9702e6adb51b445ff198733b
and <and@tutao.de>
2024-09-20 16:43:38 +0200

    update translations

--- src/mail-app/translations/ar.ts
+++ src/mail-app/translations/ar.ts
@@ -14,7 +14,7 @@ export default {
 		"other"
 	],
 	"created_at": "2015-01-27T13:11:44Z",
-	"updated_at": "2024-08-29T16:04:52Z",
+	"updated_at": "2024-09-20T14:27:32Z",
 	"source_locale": {
 		"id": "fcd7471b347c8e517663e194dcddf237",
 		"name": "en",
