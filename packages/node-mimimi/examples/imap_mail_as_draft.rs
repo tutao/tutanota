34049a3b83b90d4a1b4c8455727d367116c77483
jhm <17314077+jomapp@users.noreply.github.com>
2024-10-16 14:07:05 +0200

    improve documentation in imap_mail_as_draft.rs

--- packages/node-mimimi/examples/imap_mail_as_draft.rs
+++ packages/node-mimimi/examples/imap_mail_as_draft.rs
@@ -4,34 +4,33 @@ use tutao_node_mimimi::imap::import_client::{ImapImport, ImapImportConfig};
 use tutasdk::net::native_rest_client::NativeRestClient;
 use tutasdk::{Sdk, CLIENT_VERSION};
 
-/// 1. Start greenmail server
-/// 	* Greenmail version atleast 2.1 is required earlier version had a bug of sending invalid `\*` flag
-/// `java -Dgreenmail.setup.test.all -Dgreenmail.verbose -jar ./greenmail-standalone-2.1.0.jar`
+/// 1. Start GreenMail server
+/// 	* A greenmail version >= 2.1 is required, as earlier versions had a bug sending an invalid `\*` flag
+/// 	`java -Dgreenmail.setup.test.all -Dgreenmail.verbose -jar ./greenmail-standalone-2.1.0.jar`
 ///
-/// 2. create default user
-/// ```bash
-/// curl -X POST "http://localhost:8080/api/user" \
-///  -H 'accept: application/json'\
-///  -H 'content-type: application/json' \
-///  -d '{"email":"sug@localhost","login":"sug@localhost","password":"sug"}'
-/// ```
+/// 2. Create default user
+/// 	```
+/// 	curl -X POST "http://localhost:8080/api/user" -H 'accept: application/json' -H 'content-type: application/json' -d '{"email":"sug@localhost","login":"sug@localhost","password":"sug"}'
+/// 	```
 ///
-/// 3. Add account in thunderbird with manual configuration.
-/// 	* SMTP with host: .localhost, port: 3025 ( is smtp without ssl )
-/// 	* IMAP: with host: .localhost, port: 3993 ( is imap with ssl i.e imaps )
+/// 3. Add account in Thunderbird with manual configuration (type in user credentials and then click on "configure manually").
+/// 	* IMAP: with host: .localhost, port: 3143, connection security: None, authentication method: Normal password,  (imap without SSL)
+/// 	* SMTP with host: .localhost, port: 3025, connection security: None, authentication method: Normal password (smtp without SSL)
 ///
-/// 4. Send some mail ( by creating another account following step 2 ) or to sug@localhost
-/// 	Can use html and all formatting
-/// 	This example will always try to import the first mail. importing multiple is not implemented yet
+/// 4. Send Some Mail (by creating another account following step 2) to sug@localhost
+/// 	* We can use HTML and all kinds of formatting.
+/// 	* This example will always try to import the first mail. Importing multiple mails is not implemented yet.
 ///
 /// 5. Start tutadb server on `http://localhost:9000` which should have this user `map-free@tutanota.de:map`
 ///
-/// 6. Check the list of mails in `Draft` folder and run this example. Now the mail you wrote in step 4
-/// 	should appear in draft folder.
-/// 	* running this example multiple times will always import first mail of imap `SEARCH` command
-///	6.1 in case of error, say "oops"
+/// 6. Check the list of mails in the `Draft` folder and run this example.
+/// 	* Now the mail you wrote in step 4 should appear in draft folder.
+/// 	* Running this example multiple times will always import first mail retrieved by the imap `SEARCH` command.
 ///
-/// 7. smile :)
+///	6.1 in case of an error, say "oops" :D
+///
+/// 7. Smile :)
+
 #[tokio::main]
 async fn main() {
 	let sdk = Sdk::new(
