84ccb7bc9ad3680bc90d8f7f9c14208f2e51010c
mab <mab@tutao.de>
2024-10-22 16:46:43 +0200

    Implement verification pool (structure + ui)
    The verification pool manages a collection of email addresses which are
    mapped to a bundle containing their (expected) public key fingerprint
    and the verification status. This commit also includes a primitive UI
    for managing this pool: the user can insert and delete entries. Rocket
    science, basically.

--- src/common/api/worker/facades/lazy/KeyVerificationFacade.ts
+++ src/common/api/worker/facades/lazy/KeyVerificationFacade.ts
@@ -2,10 +2,41 @@ import { assertWorkerOrNode } from "../../../common/Env"
 
 assertWorkerOrNode()
 
-export class KeyVerificationFacade {
-	constructor() {}
+// TODO: does this type exist anywhere else maybe?
+export type MailAddress = string
 
-	async meow(): Promise<string> {
-		return Promise.resolve("meow")
+export interface KeyVerificationDetails {
+	fingerprint: string
+	verified: boolean
+}
+
+export class KeyVerificationFacade {
+	/**
+	 * Mail addresses in this pool are eligible for key verification.
+	 * For each address, this pool keeps track of its fingerprint and
+	 * verification status (the "details").
+	 */
+	verificationPool = new Map<MailAddress, KeyVerificationDetails>()
+
+	constructor() {
+		// TODO: this should not be hardcoded
+		this.verificationPool.set("freepancakes@tutanota.com", {
+			fingerprint: "a69589448040836f526eb01263605b2c2d58b849f796ab1ee96e4bd87010e849",
+			verified: false,
+		})
+	}
+
+	async getPool(): Promise<Map<MailAddress, KeyVerificationDetails>> {
+		return Promise.resolve(this.verificationPool)
+	}
+
+	async addToPool(mailAddress: string, fingerprint: string) {
+		this.verificationPool.set(mailAddress, { fingerprint: fingerprint, verified: false })
+		return Promise.resolve()
+	}
+
+	async removeFromPool(mailAddress: string) {
+		this.verificationPool.delete(mailAddress)
+		return Promise.resolve()
 	}
 }
--- src/common/settings/keymanagement/KeyManagementSettingsViewer.ts
+++ src/common/settings/keymanagement/KeyManagementSettingsViewer.ts
@@ -5,10 +5,15 @@ import { UserController } from "../../api/main/UserController.js"
 import { assertNotNull } from "@tutao/tutanota-utils"
 import { CryptoFacade } from "../../api/worker/crypto/CryptoFacade.js"
 import { lang } from "../../misc/LanguageViewModel"
-import { KeyVerificationFacade } from "../../api/worker/facades/lazy/KeyVerificationFacade"
+import { IconButton } from "../../gui/base/IconButton"
+import { Icons } from "../../gui/base/icons/Icons"
+import { ButtonSize } from "../../gui/base/ButtonSize"
+import { VerifyMailAddressDialog } from "./VerifyMailAddressDialog"
+import { KeyVerificationDetails, KeyVerificationFacade, MailAddress } from "../../api/worker/facades/lazy/KeyVerificationFacade"
 
 export class KeyManagementSettingsViewer implements UpdatableSettingsViewer {
 	publicKeyHash: string | null
+	verificationPool: Map<MailAddress, KeyVerificationDetails>
 
 	constructor(
 		private readonly keyVerificationFacade: KeyVerificationFacade,
@@ -16,12 +21,13 @@ export class KeyManagementSettingsViewer implements UpdatableSettingsViewer {
 		private readonly userController: UserController,
 	) {
 		this.publicKeyHash = null
+		this.verificationPool = new Map<MailAddress, KeyVerificationDetails>()
 	}
 
 	async init() {
 		this.publicKeyHash = await this.cryptoFacade.getPublicKeyHash(assertNotNull(this.userController.userGroupInfo.mailAddress))
-		const blah = await this.keyVerificationFacade.meow()
-		console.log("KV-Facade: ", blah)
+		this.verificationPool = await this.keyVerificationFacade.getPool()
+
 		m.redraw()
 	}
 
@@ -30,12 +36,59 @@ export class KeyManagementSettingsViewer implements UpdatableSettingsViewer {
 		return Promise.resolve()
 	}
 
+	async reload() {
+		this.verificationPool = await this.keyVerificationFacade.getPool()
+		m.redraw()
+	}
+
+	renderVerificationStatus(verified: boolean): m.Vnode<any, any> {
+		return m("", verified ? "✔ (verified) " : "✘ (unverified) ")
+	}
+
 	view(): Children {
+		const obj = this
+
+		const addressRows = Array.from(this.verificationPool.entries()).map(([mailAddress, details]: [string, KeyVerificationDetails]) => {
+			return [
+				m(".flex.items-center.selectable", [
+					m("span.b.text-break.selectable", mailAddress),
+					m(".flex-grow"),
+					this.renderVerificationStatus(details.verified),
+					m(IconButton, {
+						title: "keyManagement.verifyMailAddress_action",
+						click: async () => {
+							await this.keyVerificationFacade.removeFromPool(mailAddress)
+							await obj.reload()
+						},
+						icon: Icons.Trash,
+						size: ButtonSize.Compact,
+					}),
+				]),
+				m(".small.text-break.monospace.selectable", details.fingerprint),
+			]
+		})
+
 		return m("", [
 			m(".fill-absolute.scroll.plr-l.pb-xl", [
 				m(".h4.mt-l", lang.get("keyManagement.publicKeyFingerprint_label")),
 				m("p", [m(".small.text-break.monospace.selectable", this.publicKeyHash)]),
 				m(".small.text-break", lang.get("keyManagement.publicKeyFingerprintInfo_msg") + " "),
+
+				m(".h4.mt-l", lang.get("keyManagement.verificationPool_label")),
+				m(".full-width.flex-space-between.items-center.mb-s", [
+					lang.get("keyManagement.verifyMailAddress_action"),
+					m(IconButton, {
+						title: "keyManagement.verifyMailAddress_action",
+						click: async () => {
+							const dialog = new VerifyMailAddressDialog(this.keyVerificationFacade, () => obj.reload())
+							dialog.show()
+						},
+						icon: Icons.Add,
+						size: ButtonSize.Compact,
+					}),
+				]),
+
+				m("", ...addressRows),
 			]),
 		])
 	}
