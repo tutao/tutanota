9ec810f75cfc24176e31ad86b3e4f2400ca61af9
rih <rih@tutao.de>
2024-10-28 14:45:41 +0100

    Created generic card component
    Co-authored-by: and <and@tutao.de>

---
+++ src/common/gui/base/Card.ts
@@ -0,0 +1,27 @@
+import m, { Children, ClassComponent, Vnode } from "mithril"
+
+export interface CardAttrs {
+	rootElementType?: "div" | "section"
+	class?: string
+	style?: Pick<CSSStyleDeclaration, "padding">
+}
+
+type HTMLElementWithAttrs = Partial<Omit<HTMLElement, "style"> & CardAttrs>
+
+/**
+ * Simple card component
+ * @see Component attributes: {CardAttrs}
+ * @example
+ * m(Card, {
+ *     rootElementType: "section", // Changing the default root element
+ *     class: "custom-font-size", // Adding new styles
+ *     style: {
+ *         "font-size": px(size.font_size_base * 1.25) // Overriding the component style
+ *     }
+ * }),
+ */
+export class Card implements ClassComponent<CardAttrs> {
+	view({ attrs, children }: Vnode<CardAttrs, this>): Children | void | null {
+		return m(`${attrs.rootElementType ?? "div"}.card-container`, { class: attrs.class, style: attrs.style } satisfies HTMLElementWithAttrs, children)
+	}
+}
