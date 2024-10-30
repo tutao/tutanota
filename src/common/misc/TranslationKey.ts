9f6459092515c91d63b3afb47a6530d6d1ba1684
hrb-hub <181954414+hrb-hub@users.noreply.github.com>
2024-10-30 19:16:27 +0100

    Make new Color Picker accessible on screen readers
    Close #7842

--- src/common/misc/TranslationKey.ts
+++ src/common/misc/TranslationKey.ts
@@ -955,7 +955,6 @@ export type TranslationKeyType =
 	| "moveToTop_action"
 	| "moveUp_action"
 	| "move_action"
-	| "assignLabel_action"
 	| "nameSuffix_placeholder"
 	| "name_label"
 	| "nativeShareGiftCard_label"
