f859f6833a233823ae1160ecbafbe4d47594fcb7
nig <nig@tutao.de>
2024-11-05 13:08:45 +0100

    [tutasdk] use tokio::main macro for examples

--- tuta-sdk/rust/sdk/examples/create_session.rs
+++ tuta-sdk/rust/sdk/examples/create_session.rs
@@ -3,17 +3,8 @@ use std::sync::Arc;
 use tutasdk::net::native_rest_client::NativeRestClient;
 use tutasdk::Sdk;
 
-fn main() -> Result<(), Box<dyn Error>> {
-	let mut runtime_builder = tokio::runtime::Builder::new_current_thread();
-	let runtime_builder = runtime_builder.enable_all();
-	let Ok(runtime) = runtime_builder.build() else {
-		panic!("could not initialize tokio runtime");
-	};
-
-	runtime.block_on(async_main())
-}
-
-async fn async_main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
 	let rest_client = Arc::new(NativeRestClient::try_new().unwrap());
 
 	// this test expect local server with matching model versions to be live at: http://localhost:9000
