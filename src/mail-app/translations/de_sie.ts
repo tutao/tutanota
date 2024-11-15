38600c8be5eec09c26ad216783165e2bca003a43
wrd <wrd@tutao.de>
2024-11-15 17:33:20 +0100

    Add confirmation dialog before deleting label
    Close #7934

--- src/mail-app/translations/de_sie.ts
+++ src/mail-app/translations/de_sie.ts
@@ -1819,6 +1819,7 @@ export default {
 		"yourCalendars_label": "Deine Kalender",
 		"yourFolders_action": "Ihre ORDNER",
 		"yourMessage_label": "Ihre Nachricht",
-		"you_label": "Sie"
+        "you_label": "Sie",
+		"confirmDeleteLabel_msg": "Möchten Sie wirklich das Label \"{1}\" löschen?"
 	}
 }
