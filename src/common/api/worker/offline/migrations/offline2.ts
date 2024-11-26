ee0daf142e1851a42381f845e23097081d0b2f67
ivk <ivk@tutao.de>
2024-11-26 17:03:12 +0100

    Fix login after migrating to tutanota-v77
    TutanotaProperties cannot be deleted on their own as offline login
    fails. TutanotaProperties will be reloaded eventually in any case so we
    should add the default value instead.
    
    Close #8039
    
    Co-authored-by: wrd <wrd@tutao.de>

--- src/common/api/worker/offline/migrations/offline2.ts
+++ src/common/api/worker/offline/migrations/offline2.ts
@@ -1,16 +1,19 @@
 import { OfflineStorage } from "../OfflineStorage.js"
 import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade.js"
-import { deleteInstancesOfType } from "../StandardMigrations.js"
+import { addValue, deleteInstancesOfType, migrateAllElements } from "../StandardMigrations.js"
 import { TutanotaPropertiesTypeRef } from "../../../entities/tutanota/TypeRefs.js"
 import { OfflineMigration } from "../OfflineStorageMigrator.js"
 
 /**
- * Migration to patch up the broken tutanota-v77 migration which didn't delete TutanotaProperties initially.
+ * Migration to patch up the broken tutanota-v77 migration.
+ *
+ * We write default value which might be out of sync with the server but we have an extra check for that where
+ * we use this property.
  */
 export const offline2: OfflineMigration = {
 	app: "offline",
 	version: 2,
 	async migrate(storage: OfflineStorage, _: SqlCipherFacade): Promise<void> {
-		await deleteInstancesOfType(storage, TutanotaPropertiesTypeRef)
+		await migrateAllElements(TutanotaPropertiesTypeRef, storage, [addValue("defaultLabelCreated", false)])
 	},
 }
