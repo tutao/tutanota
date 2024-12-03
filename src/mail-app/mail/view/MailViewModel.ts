6af22c1c08ffa3efd76227a23138a1e776ac59e4
paw <paw-hub@users.noreply.github.com>
2024-12-03 10:48:43 +0100

    Change CacheMode enum value names
    Change CacheMode.Cache to CacheMode.ReadAndWrite, and CacheMode.Bypass
    to CacheMode.WriteOnly.
    
    All three values use the cache, so having one value be called 'Cache'
    and another 'Bypass' might lead to the wrong idea of what these fields
    actually do in the future, since we now have the new CacheMode.ReadOnly
    mode.
    
    Co-authored-by: BijinDev <BijinDev@users.noreply.github.com>

--- src/mail-app/mail/view/MailViewModel.ts
+++ src/mail-app/mail/view/MailViewModel.ts
@@ -274,7 +274,7 @@ export class MailViewModel {
 
 		let mail: Mail | null
 		try {
-			mail = await this.entityClient.load(MailTypeRef, [listId, mailId], { cacheMode: CacheMode.Bypass })
+			mail = await this.entityClient.load(MailTypeRef, [listId, mailId], { cacheMode: CacheMode.WriteOnly })
 		} catch (e) {
 			if (isOfflineError(e)) {
 				return
