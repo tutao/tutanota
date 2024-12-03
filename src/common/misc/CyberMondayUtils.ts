16fef51f3195c65574bb5f07ed4a17dddd60c062
jug <jug@tutao.de>
2024-12-03 13:18:52 +0100

    Make cyber monday last a day longer

--- src/common/misc/CyberMondayUtils.ts
+++ src/common/misc/CyberMondayUtils.ts
@@ -2,7 +2,7 @@ import { DateTime, Interval } from "luxon"
 
 export function isReferenceDateWithinCyberMondayCampaign(referenceDate: Date) {
 	const startIso = "2024-11-15T10:00:00+01:00"
-	const endIso = "2024-12-04T10:00:00+01:00"
+	const endIso = "2024-12-05T10:00:00+01:00"
 
 	const cyberMonday2024Interval = Interval.fromDateTimes(DateTime.fromISO(startIso), DateTime.fromISO(endIso))
 
