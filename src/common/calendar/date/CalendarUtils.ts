cfa405559ae71c798ae956db4efd76f3abe99a2c
mup <mup@tutao.de>
2024-12-17 12:10:52 +0100

    Fixes SETPOS filtering

--- src/common/calendar/date/CalendarUtils.ts
+++ src/common/calendar/date/CalendarUtils.ts
@@ -930,7 +930,16 @@ function bySetPosContainsEventOccurance(posRulesValues: string[], event: Calenda
 	}
 	const { repeatRule } = event
 	switch (repeatRule?.frequency) {
-		case RepeatPeriod.DAILY || RepeatPeriod.ANNUALLY:
+		case RepeatPeriod.DAILY:
+			if (
+				negativeValues.some((value) => Number(value) < -366) ||
+				positiveValues.some((value) => Number(value) > 366) ||
+				positiveValues.includes(eventCount.toString())
+			) {
+				return true
+			}
+			break
+		case RepeatPeriod.ANNUALLY:
 			if (
 				negativeValues.some((value) => Number(value) < -366) ||
 				positiveValues.some((value) => Number(value) > 366) ||
@@ -986,7 +995,7 @@ export function addDaysForRecurringEvent(
 		? repeatRule.excludedDates.map(({ date }) => createDateWrapper({ date: getAllDayDateForTimezone(date, timeZone) }))
 		: repeatRule.excludedDates
 	const generatedEvents = generateEventOccurrences(event, timeZone, new Date(range.end))
-	const allEvents: CalendarEvent[] = []
+	const allEvents: Map<number, CalendarEvent> = new Map()
 
 	for (const { startTime, endTime } of generatedEvents) {
 		if (startTime.getTime() > range.end) break
@@ -1003,7 +1012,7 @@ export function addDaysForRecurringEvent(
 				eventClone.startTime = new Date(startTime)
 				eventClone.endTime = new Date(endTime)
 			}
-			allEvents.push(eventClone)
+			allEvents.set(eventClone.startTime.getTime(), eventClone)
 		}
 	}
 
@@ -1011,9 +1020,9 @@ export function addDaysForRecurringEvent(
 	const setPosRulesValues = setPosRules.map((rule) => rule.interval)
 	const shouldApplySetPos = isNotEmpty(setPosRules) && setPosRules.length < repeatRule.advancedRules.length
 	let eventCount = 0
-	console.log({ allEvents })
-	for (const event of allEvents) {
-		if (shouldApplySetPos && !bySetPosContainsEventOccurance(setPosRulesValues, event, ++eventCount, allEvents)) {
+	const events = Array.from(allEvents.values())
+	for (const event of events) {
+		if (shouldApplySetPos && !bySetPosContainsEventOccurance(setPosRulesValues, event, ++eventCount, events)) {
 			continue
 		}
 		addDaysForEventInstance(daysToEvents, event, range, timeZone)
