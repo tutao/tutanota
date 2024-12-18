bc2faa72ec52df8dc45a3d0922cb47a9825a125b
mab <mab@tutao.de>
2024-12-18 10:47:29 +0100

    Allow for more customization of `MonospaceTextDisplay`
    This adds two optional attributes:
    - `border` (boolean)
    - `classes` (single string of extra CSS classes)

--- src/common/gui/base/MonospaceTextDisplay.ts
+++ src/common/gui/base/MonospaceTextDisplay.ts
@@ -5,11 +5,14 @@ export type MonospaceTextDisplayAttrs = {
 	text: string
 	placeholder?: string
 	chunkSize?: number
+	border?: boolean
+	classes?: string
 }
 
 export class MonospaceTextDisplay implements Component<MonospaceTextDisplayAttrs> {
 	view(vnode: Vnode<MonospaceTextDisplayAttrs>): Children {
-		const { text, placeholder, chunkSize } = vnode.attrs
+		const { text, placeholder, chunkSize, classes } = vnode.attrs
+		const border = vnode.attrs.border ?? true
 
 		let formattedText: string
 		if (chunkSize !== undefined && text !== "") {
@@ -18,9 +21,15 @@ export class MonospaceTextDisplay implements Component<MonospaceTextDisplayAttrs
 			formattedText = text
 		}
 
+		let extraClasses = classes ?? ""
+
+		if (border) {
+			extraClasses += ".border.pt.pb.plr"
+		}
+
 		// Display `formattedText` unless it is empty and a placeholder is given
 		const contents = formattedText != "" ? formattedText : placeholder !== undefined ? placeholder : ""
 
-		return m(".text-break.monospace.selectable.flex.flex-wrap.border.pt.pb.plr", contents)
+		return m(".text-break.monospace.selectable.flex.flex-wrap" + extraClasses, contents)
 	}
 }
