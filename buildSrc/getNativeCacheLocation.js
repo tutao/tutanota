4e2bfba91e8be15143739b8b85e84fe61db7509d
map <mpfau@users.noreply.github.com>
2024-12-23 17:31:00 +0100

    fix for merge to master

--- buildSrc/getNativeCacheLocation.js
+++ buildSrc/getNativeCacheLocation.js
@@ -8,14 +8,14 @@
  * (see .github/workflows/test.yml)
  * */
 import fs from "node:fs/promises"
-import { buildCachedLibPath } from "./nativeLibraryProvider.js"
+import { buildCachedLibPaths } from "./nativeLibraryProvider.js"
 
 const packageJson = JSON.parse(await fs.readFile("package-lock.json", "utf-8"))
 const module = process.argv[2]
 // we have a git commit as a version in dependencies, we want the actually resolved version number
 const version = packageJson.packages[`node_modules/${module}`].version
 console.log(
-	await buildCachedLibPath({
+	await buildCachedLibPaths({
 		rootDir: "test",
 		platform: "linux",
 		environment: "node",
