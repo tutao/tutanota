8aac8ba0d77881558cdf19b21f3ade99d27a32ff
jenkins build server <jenkins@next.tutao.de>
2025-01-06 11:12:53 +0100

    update translations

--- src/mail-app/translations/ko.ts
+++ src/mail-app/translations/ko.ts
@@ -12,7 +12,7 @@ export default {
 		"other"
 	],
 	"created_at": "2015-03-13T09:48:48Z",
-	"updated_at": "2024-12-05T05:39:11Z",
+	"updated_at": "2025-01-04T07:40:16Z",
 	"source_locale": null,
 	"fallback_locale": null,
 	"keys": {
