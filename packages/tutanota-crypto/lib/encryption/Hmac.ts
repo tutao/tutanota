45ff3abf39e4b5120f6e931ea0636c6087175791
vis <vis@tutao.de>
2025-01-07 19:05:47 +0100

    Expose HMAC-SHA-256 interface
    Allows creating and verifying tags. This is useful to manually verify
    authenticity of non-confidential data.
    
    tutadb#1937

---
+++ packages/tutanota-crypto/lib/encryption/Hmac.ts
@@ -0,0 +1,26 @@
+import { AesKey } from "./Aes.js"
+import sjcl from "../internal/sjcl.js"
+import { bitArrayToUint8Array, uint8ArrayToBitArray } from "../misc/Utils.js"
+import { arrayEquals } from "@tutao/tutanota-utils"
+import { CryptoError } from "../misc/CryptoError.js"
+
+export type MacTag = Uint8Array & { __brand: "macTag" }
+
+/**
+ * Create an HMAC-SHA-256 tag over the given data using the given key.
+ */
+export function hmacSha256(key: AesKey, data: Uint8Array): MacTag {
+	const hmac = new sjcl.misc.hmac(key, sjcl.hash.sha256)
+	return bitArrayToUint8Array(hmac.encrypt(uint8ArrayToBitArray(data))) as MacTag
+}
+
+/**
+ * Verify an HMAC-SHA-256 tag against the given data and key.
+ * @throws CryptoError if the tag does not match the data and key.
+ */
+export function verifyHmacSha256(key: AesKey, data: Uint8Array, tag: MacTag) {
+	const computedTag = hmacSha256(key, data)
+	if (!arrayEquals(computedTag, tag)) {
+		throw new CryptoError("invalid mac")
+	}
+}
