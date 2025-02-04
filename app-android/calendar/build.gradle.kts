2873bd22f0b744af0945a0f0b31e3c8c3e5696fb
jug <jug@tutao.de>
2025-02-04 13:08:06 +0100

    v266.250204.0
    Co-authored-by: yoy <yoy@tutao.de>

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 89
-		versionName = "266.250203.0"
+		versionCode = 90
+		versionName = "266.250204.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
