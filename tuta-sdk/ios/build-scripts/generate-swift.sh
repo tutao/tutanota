15c3641af5891ffea0cf0de786c37e9617349e1f
nig <nig@tutao.de>
2025-02-04 10:12:17 +0100

    use the repository root as a cargo workspace
    this will improve incremental compilation times since
    we don't use multiple target folders anymore.
    
    Co-authored-by: sug <sug@tutao.de>

--- tuta-sdk/ios/build-scripts/generate-swift.sh
+++ tuta-sdk/ios/build-scripts/generate-swift.sh
@@ -32,23 +32,23 @@ createFolderStructure() {
     mkdir "${SRC_ROOT}/target/combined/"
     mkdir "${SRC_ROOT}/target/combined/${RELFLAG}"
 
-    if [ -d "${SRC_ROOT}/../ios/tutasdk/generated-src" ]; then
-        rm -r "${SRC_ROOT}/../ios/tutasdk/generated-src"
+    if [ -d "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src" ]; then
+        rm -r "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src"
     fi
 
-    mkdir "${SRC_ROOT}/../ios/tutasdk/generated-src"
-    mkdir "${SRC_ROOT}/../ios/tutasdk/generated-src/headers"
-    mkdir "${SRC_ROOT}/../ios/tutasdk/generated-src/Sources"
+    mkdir "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src"
+    mkdir "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src/headers"
+    mkdir "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src/Sources"
 }
 
 generateLibrary() {
     lipo -create $ARCH_LIST -output "${SRC_ROOT}/target/combined/${RELFLAG}/libtutasdk.a"
 
-    cp "${SRC_ROOT}/target/combined/${RELFLAG}/libtutasdk.a" "${SRC_ROOT}/../ios/tutasdk/generated-src/tutasdk.a"
+    cp "${SRC_ROOT}/target/combined/${RELFLAG}/libtutasdk.a" "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src/tutasdk.a"
 
-    mv $SRC_ROOT/bindings/*.h "${SRC_ROOT}/../ios/tutasdk/generated-src/headers/"
-    mv $SRC_ROOT/bindings/*.modulemap "${SRC_ROOT}/../ios/tutasdk/generated-src/headers/"
-    mv $SRC_ROOT/bindings/*.swift "${SRC_ROOT}/../ios/tutasdk/generated-src/Sources/"
+    mv $SRC_ROOT/bindings/*.h "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src/headers/"
+    mv $SRC_ROOT/bindings/*.modulemap "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src/headers/"
+    mv $SRC_ROOT/bindings/*.swift "${SRC_ROOT}/tuta-sdk/ios/tutasdk/generated-src/Sources/"
 }
 
 cd $SRC_ROOT
@@ -66,18 +66,18 @@ for arch in $ARCHS; do
       fi
 
       # Intel iOS simulator
-    cargo run --bin uniffi-bindgen generate --library "${SRC_ROOT}/target/x86_64-apple-ios/${RELFLAG}/libtutasdk.dylib" --out-dir "$SRC_ROOT/bindings" --language=swift
+    cargo run --package uniffi-bindgen -- generate --library "${SRC_ROOT}/target/x86_64-apple-ios/${RELFLAG}/libtutasdk.dylib" --out-dir "$SRC_ROOT/bindings" --language=swift
     includeArch "${SRC_ROOT}/target/x86_64-apple-ios/${RELFLAG}/libtutasdk.a";
       ;;
 
     arm64)
       if [ $IS_SIMULATOR -eq 0 ]; then
         # Hardware iOS targets
-        cargo run --bin uniffi-bindgen generate --library "${SRC_ROOT}/target/aarch64-apple-ios/${RELFLAG}/libtutasdk.dylib" --out-dir "$SRC_ROOT/bindings" --language=swift
+        cargo run --package uniffi-bindgen -- generate --library "${SRC_ROOT}/target/aarch64-apple-ios/${RELFLAG}/libtutasdk.dylib" --out-dir "$SRC_ROOT/bindings" --language=swift
         includeArch "${SRC_ROOT}/target/aarch64-apple-ios/${RELFLAG}/libtutasdk.a";
       else
         # M1 iOS simulator
-        cargo run --bin uniffi-bindgen generate --library "${SRC_ROOT}/target/aarch64-apple-ios-sim/${RELFLAG}/libtutasdk.dylib" --out-dir "$SRC_ROOT/bindings" --language=swift
+        cargo run --package uniffi-bindgen -- generate --library "${SRC_ROOT}/target/aarch64-apple-ios-sim/${RELFLAG}/libtutasdk.dylib" --out-dir "$SRC_ROOT/bindings" --language=swift
         includeArch "${SRC_ROOT}/target/aarch64-apple-ios-sim/${RELFLAG}/libtutasdk.a";
       fi
   esac
