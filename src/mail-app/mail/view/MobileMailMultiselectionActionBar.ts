8798a6fd4eeceb7c7fed18bcc9866061b4e4c0ef
paw <paw-hub@users.noreply.github.com>
2025-02-13 15:45:40 +0100

    Pass move/delete
    Co-authored-by: ivk <ivk@tutao.de>

--- src/mail-app/mail/view/MobileMailMultiselectionActionBar.ts
+++ src/mail-app/mail/view/MobileMailMultiselectionActionBar.ts
@@ -2,19 +2,19 @@ import { Mail } from "../../../common/api/entities/tutanota/TypeRefs.js"
 import m, { Children, Vnode } from "mithril"
 import { IconButton } from "../../../common/gui/base/IconButton.js"
 import { Icons } from "../../../common/gui/base/icons/Icons.js"
-import { promptAndDeleteMails, showMoveMailsDropdown } from "./MailGuiUtils.js"
-import { DROPDOWN_MARGIN } from "../../../common/gui/base/Dropdown.js"
+import { DROPDOWN_MARGIN, PosRect } from "../../../common/gui/base/Dropdown.js"
 import { MobileBottomActionBar } from "../../../common/gui/MobileBottomActionBar.js"
-import { MailboxModel } from "../../../common/mailFunctionality/MailboxModel.js"
 import { MailModel } from "../model/MailModel.js"
 import { LabelsPopup } from "./LabelsPopup.js"
 import { allInSameMailbox } from "../model/MailUtils"
+import { ShowMoveMailsDropdownOpts } from "./MailGuiUtils"
 
 export interface MobileMailMultiselectionActionBarAttrs {
 	selectedMails: readonly Mail[]
 	mailModel: MailModel
-	mailboxModel: MailboxModel
 	selectNone: () => unknown
+	deleteMailsAction: (() => void) | null
+	moveMailsAction: ((origin: PosRect, opts?: ShowMoveMailsDropdownOpts) => void) | null
 	actionableMails: () => Promise<readonly IdTuple[]>
 }
 
@@ -23,31 +23,31 @@ export class MobileMailMultiselectionActionBar {
 	private dom: HTMLElement | null = null
 
 	view({ attrs }: Vnode<MobileMailMultiselectionActionBarAttrs>): Children {
-		const { selectedMails, selectNone, mailModel, mailboxModel, actionableMails } = attrs
+		const { selectedMails, selectNone, mailModel, actionableMails, moveMailsAction, deleteMailsAction } = attrs
 		return m(
 			MobileBottomActionBar,
 			{
 				oncreate: ({ dom }) => (this.dom = dom as HTMLElement),
 			},
 			[
-				m(IconButton, {
-					icon: Icons.Trash,
-					title: "delete_action",
-					click: () => promptAndDeleteMails(mailModel, selectedMails, selectNone),
-				}),
-				mailModel.isMovingMailsAllowed()
-					? m(IconButton, {
-							icon: Icons.Folder,
-							title: "move_action",
-							click: (e, dom) => {
-								const referenceDom = this.dom ?? dom
-								showMoveMailsDropdown(mailboxModel, mailModel, referenceDom.getBoundingClientRect(), selectedMails, {
-									onSelected: () => selectNone,
-									width: referenceDom.offsetWidth - DROPDOWN_MARGIN * 2,
-								})
-							},
-					  })
-					: null,
+				deleteMailsAction &&
+					m(IconButton, {
+						icon: Icons.Trash,
+						title: "delete_action",
+						click: deleteMailsAction,
+					}),
+				moveMailsAction &&
+					m(IconButton, {
+						icon: Icons.Folder,
+						title: "move_action",
+						click: (e, dom) => {
+							const referenceDom = this.dom ?? dom
+							moveMailsAction(referenceDom.getBoundingClientRect(), {
+								onSelected: () => selectNone,
+								width: referenceDom.offsetWidth - DROPDOWN_MARGIN * 2,
+							})
+						},
+					}),
 				mailModel.canAssignLabels() && allInSameMailbox(selectedMails)
 					? m(IconButton, {
 							icon: Icons.Label,
