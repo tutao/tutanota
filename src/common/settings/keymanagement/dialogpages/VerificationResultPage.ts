d8b53d1e85e8f90023b0d2865c43dbdb80ea6111
hec <hec@tutao.de>
2025-02-25 17:49:44 +0100

    fix scrollbar, fix close button, reload on success to see results

---
+++ src/common/settings/keymanagement/dialogpages/VerificationResultPage.ts
@@ -0,0 +1,32 @@
+import m, { Children, Component, Vnode } from "mithril"
+import { lang } from "../../../misc/LanguageViewModel"
+import { KeyVerificationModel } from "../KeyVerificationModel"
+import { TitleSection } from "../../../gui/TitleSection"
+import { FingerprintRow } from "../FingerprintRow"
+import { assertNotNull } from "@tutao/tutanota-utils"
+import { Icons } from "../../../gui/base/icons/Icons"
+
+export class VerificationResultPage implements Component<VerificationResultPageAttrs> {
+	view(vnode: Vnode<VerificationResultPageAttrs>): Children {
+		const { model } = vnode.attrs
+
+		return m(
+			"section.flex.flex-column.mt",
+			m(TitleSection, {
+				title: lang.get("keyManagement.contactVerificationConfirmationTitle_label"),
+				subTitle: lang.get("keyManagement.contactVerificationConfirmation_label"),
+				icon: Icons.Fingerprint,
+			}),
+			m(".mb"),
+			m(FingerprintRow, {
+				publicKeyFingerprint: assertNotNull(model.publicKeyFingerprint),
+				mailAddress: model.mailAddress,
+			}),
+		)
+	}
+}
+
+type VerificationResultPageAttrs = {
+	model: KeyVerificationModel
+	back: () => void
+}
