ff89ba84ee91ddd10f3fe6fa1c2d3f01bf54723c
FranzTutao <frm@tutao.de>
2025-02-27 18:00:45 +0100

    WIP: need to fix static layout; added preview; CAREFUL: changed to sdk 35!!! for preview

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -15,10 +15,10 @@ android {
 	namespace = "de.tutao.calendar"
 
 	defaultConfig {
-		compileSdk = 34
+		compileSdk = 35
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
-		targetSdk = 34
+		targetSdk = 35
 		versionCode = 108
 		versionName = "275.250324.1"
 
--- app-android/calendar/src/main/java/de/tutao/calendar/widget/MyAppWidget.kt
+++ app-android/calendar/src/main/java/de/tutao/calendar/widget/MyAppWidget.kt
@@ -1,22 +1,20 @@
 package de.tutao.calendar.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.Image
 import androidx.glance.ImageProvider
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.components.Scaffold
+import androidx.glance.appwidget.components.SquareIconButton
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.itemsIndexed
@@ -25,20 +23,20 @@ import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
-import androidx.glance.layout.absolutePadding
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.layout.width
 import androidx.glance.preview.ExperimentalGlancePreviewApi
+import androidx.glance.preview.Preview
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 import de.tutao.calendar.MainActivity
 import de.tutao.calendar.R
-import de.tutao.tutasdk.CalendarEventsList
+import de.tutao.tutasdk.CalendarEvent
 import de.tutao.tutasdk.Sdk
 import de.tutao.tutashared.AndroidNativeCryptoFacade
 import de.tutao.tutashared.SdkRestClient
@@ -64,18 +62,32 @@ class MyAppWidget : GlanceAppWidget() {
 
 		val crypto = AndroidNativeCryptoFacade(context)
 		val nativeCredentialsFacade = CredentialsEncryptionFactory.create(context, crypto, db)
-		val credentials = nativeCredentialsFacade.loadByUserId("OJXq2s3----0")!!
+		val credentials = nativeCredentialsFacade.loadByUserId("OK62Kzg----0")!!
 			.toSdkCredentials() // FIXME Get the user from config page
 
 		val sdk = Sdk(sseStorage.getSseOrigin()!!, SdkRestClient()).login(credentials)
-		var calendars = sdk.calendarFacade().getCalendarsRenderData();
-		var events = sdk.calendarFacade()
+		val calendars = sdk.calendarFacade().getCalendarsRenderData()
+		val events = sdk.calendarFacade()
 			.getCalendarEvents(calendars.keys.first()) // FIXME Change this to an static event array for UI development if needed
 
+		val allEvents: List<CalendarEvent> = events.shortEvents.plus(events.longEvents)
+
+		// Remember mutable state for the data
+//		val data = remember { mutableStateOf(allEvents) }
+
+		// Apply transformations to each item in the list
+		val eventsData = allEvents.map { event ->
+			val zoneId = ZoneId.systemDefault()
+			val start = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.startTime.toLong()), zoneId)
+			val end = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.startTime.toLong()), zoneId)
+			val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm")
+			EventData(event.summary, start.format(formatter), end.format(formatter))
+		}
 		provideContent {
 			GlanceTheme {
 				// create your AppWidget here
-				MyContent(events)
+				println(eventsData)
+				MyContent(eventsData)
 			}
 		}
 	}
@@ -87,107 +99,70 @@ data class EventData(
 	val endTime: String,
 )
 
-/**
- * idea: overload function to handle both CalendarEventsList and my DemoCalendarEventsList (needed for the preview)
- * main function should take the Demo class, second overloading function should take original class and cast down to Demo class
- * it should simulate this: fun MyContent(events: CalendarEventsList || DemoCalendarEventsList)
- */
-//@Composable
-//fun MyContent(events: CalendarEventsList) {
-//	val shortEvents: List<DummyCalendarEvent> = mutableListOf()
-//	val longEvents: List<DummyCalendarEvent> = mutableListOf()
-//
-//	// short
-//	for(shortEvent in events.shortEvents) {
-//
-//
-//		val dummyCalendarEvent = DummyCalendarEvent(shortEvent.summary, shortEvent.startTime, shortEvent.endTime)
-//
-//		shortEvents.add(dummyCalendarEvent)
-//	}
-//	// long
-//	for(longEvent in events.longEvents) {
-//
-//	}
-//
-//	val dummyCalendarEventsList: DummyCalendarEventsList = TODO()
-//}
-
-
+@SuppressLint("RestrictedApi")
 @Composable
-fun MyContent(events: CalendarEventsList) {
-	val allEvents = events.shortEvents.plus(events.longEvents)
+fun MyContent(eventsData: List<EventData>) {
 
-	// Remember mutable state for the data
-	val data = remember { mutableStateOf(allEvents) }
-
-	// Apply transformations to each item in the list
-	val eventsData = data.value.map { event ->
-		val zoneId = ZoneId.systemDefault()
-		val start = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.startTime.toLong()), zoneId)
-		val end = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.startTime.toLong()), zoneId)
-		val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm")
-		EventData(event.summary, start.format(formatter), end.format(formatter))
-	}
-
-	Scaffold(modifier = GlanceModifier.padding(vertical = 16.dp, horizontal = 20.dp).background(Color.LightGray),
+	Scaffold(
+		modifier = GlanceModifier
+			.padding(vertical = 16.dp, horizontal = 20.dp)
+			.background(Color.LightGray),
 		titleBar = {
-			Row(modifier = GlanceModifier.padding(bottom = 16.dp), verticalAlignment = Alignment.CenterVertically) {
-
-				Image(
-					provider = ImageProvider(R.mipmap.ic_launcher),
-					contentDescription = null,
-					modifier = GlanceModifier.width(60.dp).height(60.dp).absolutePadding(right = 8.dp)
-				)
+			Row(
+				modifier = GlanceModifier
+					.padding(bottom = 16.dp),
+				verticalAlignment = Alignment.CenterVertically
+			) {
 				Column(modifier = GlanceModifier.clickable { println("Open agenda") }) {
+					// date e.g. 20 February
 					Text(
-						style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp), text = "20 February"
+						style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp),
+						text = "20 February" // TODO() fetch data
+					)
+					// all day events
+					// TODO() fix border size and remove test color
+					BorderedContent(borderWidth = 5, borderColor = ColorProvider(R.color.red)) {
+						Text(
+							modifier = GlanceModifier
+								.cornerRadius(8.dp)
+								.width(122.dp)
+								.height(32.dp)
+								.padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp),
+							text = "5 all day events"
+						)
+					}
+				}
+				// create event button
+				Row(
+					modifier = GlanceModifier
+						.fillMaxWidth(),
+					horizontalAlignment = Alignment.End
+				) {
+					SquareIconButton(
+						imageProvider = ImageProvider(R.drawable.ic_add),
+						contentDescription = "Add event button",
+						modifier = GlanceModifier, // TODO() use correct size
+						onClick = { println("Open event creator") }, // TODO() open correct activity
+						backgroundColor = ColorProvider(R.color.blue) // TODO() proper color
+
 					)
 
-					Text(
-						modifier = GlanceModifier.cornerRadius(8.dp), text = "5 all day events"
-					)
-				}
-				Row(modifier = GlanceModifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
-					// FIXME Missing Icon button
 				}
 			}
 		}) {
+		// calendar events
 		Column {
 			LazyColumn(
-				modifier = GlanceModifier.fillMaxWidth()
+				modifier = GlanceModifier
+					.fillMaxSize()
 			) {
-				itemsIndexed(eventsData, itemId = { index, _ -> index.toLong() }) { index, event ->
+				itemsIndexed(eventsData, itemId = { index, _ -> index.toLong() }) { _, event ->
+					// events
 					Column(
-						modifier = GlanceModifier.padding(bottom = 8.dp) // Optional: Add padding
+						modifier = GlanceModifier
+							.padding(bottom = 8.dp)
 					) {
-						Column(
-							modifier = GlanceModifier.background(ColorProvider(R.color.white)).cornerRadius(8.dp)
-								.padding(8.dp) // Optional: Add padding
-						) {
-
-							Text(
-								"${event.summary}",
-								modifier = GlanceModifier.padding(8.dp).clickable(actionStartActivity<MainActivity>()),
-								style = TextStyle(
-									color = ColorProvider(R.color.darkDarkest), fontWeight = FontWeight.Bold
-								)
-							)
-							Row {
-								Text(
-									"${event.startTime}",
-									modifier = GlanceModifier.padding(8.dp)
-										.clickable(actionStartActivity<MainActivity>()),
-									style = TextStyle(color = ColorProvider(R.color.darkLighter))
-								)
-								Text(
-									"${event.endTime}",
-									modifier = GlanceModifier.padding(8.dp)
-										.clickable(actionStartActivity<MainActivity>()),
-									style = TextStyle(color = ColorProvider(R.color.darkLighter))
-								)
-							}
-						}
+						EventCard(event)
 					}
 				}
 			}
@@ -195,51 +170,123 @@ fun MyContent(events: CalendarEventsList) {
 	}
 }
 
-/**
- * simulate datastructure as we only need these three attributes
- */
-data class DummyCalendarEvent(
-	val summary: String, val startTime: Long, val endTime: Long
-)
-
-data class DummyCalendarEventsList(
-	var shortEvents: List<DummyCalendarEvent>, var longEvents: List<DummyCalendarEvent>
-)
-
-@Preview
+@SuppressLint("RestrictedApi")
 @Composable
-fun MyContentPreview() {
-	// preview cant use data from sdk so it needs dummy data to work
-	// TODO() use proper type for time
-	val dummyEvents = DummyCalendarEventsList(
-		shortEvents = listOf(
-			DummyCalendarEvent(summary = "Meeting", startTime = 800, endTime = 900)
-		), longEvents = listOf(
-			DummyCalendarEvent(summary = "Conference", startTime = 1000, endTime = 800)
-		)
-	)
+fun EventCard(event: EventData) {
+	// TODO() whats Scaffold and what is it needed for?
+	Scaffold(
+		modifier = GlanceModifier
+	) {
+		// circle icon and event text
+		Row(
+			modifier = GlanceModifier
+		) {
+			// event card
+			// TODO() fill full width
+			Column(
+				modifier = GlanceModifier
+					.background(ColorProvider(R.color.white)).cornerRadius(8.dp)
+					.padding(vertical = 8.dp)
+			) {
+				// circle icon
+				// TODO() align to left/ start
+				// TODO() align vertically centred
+				// TODO() fix padding
+				Row(
+					modifier = GlanceModifier,
+					verticalAlignment = Alignment.CenterVertically
+				) {
+					Circle()
+				}
+				// event title
+				// TODO() display limited amount of characters and handle overflow with dots e.g. "Hello Widget..."
+				Text(
+					event.summary,
+					modifier = GlanceModifier.padding(8.dp).clickable { println("Open event details") },
+					style = TextStyle(
+						color = ColorProvider(R.color.darkDarkest), fontWeight = FontWeight.Bold
+					)
+				)
+				// event start and end time
+				Row(
+					modifier = GlanceModifier
+						.clickable(actionStartActivity<MainActivity>()),
+					horizontalAlignment = Alignment.Start
+				) {
+					Text(
+						event.startTime + " - " + event.endTime,
+						modifier = GlanceModifier.padding(vertical = 8.dp),
+						style = TextStyle(color = ColorProvider(R.color.darkLighter))
+					)
+				}
+			}
+		}
+	}
+}
 
-	// MyContent(dummyEvents)
-	DummyStaticWidgetContent()
+@Composable
+fun Circle(radius: Int = 30, color: Color = Color.Blue) {
+	Column(
+		modifier = GlanceModifier
+			.width(radius.dp)
+			.height(radius.dp)
+			.background(color)
+			.cornerRadius((radius / 2).dp)
+	) {
+
+	}
 }
 
 /**
- * test how and if preview works
+ * get a boarder around your content as glance doesn't support this natively
  */
-@OptIn(ExperimentalGlancePreviewApi::class)
+@SuppressLint("RestrictedApi")
 @Composable
-fun DummyStaticWidgetContent() {
+fun BorderedContent(
+	borderColor: ColorProvider = ColorProvider(R.color.darkLighter),
+	borderWidth: Int,
+	content: @Composable () -> Unit
+
+) {
 	Column(
-		modifier = GlanceModifier.fillMaxSize().padding(16.dp),
-		verticalAlignment = Alignment.CenterVertically,
-		horizontalAlignment = Alignment.CenterHorizontally
+		modifier = GlanceModifier
+			.background(borderColor)
+			.cornerRadius(8.dp)
 	) {
-		Text("Hello Calendar Widget!")
-		Row {
-			Text("Button A here :)")
-			Text("  Button B here ;)")
+		Column(
+			modifier = GlanceModifier
+				.padding(borderWidth.dp)
+				.background(ColorProvider(R.color.white))
+		) {
+			content()
 		}
 	}
+
+}
+
+@OptIn(ExperimentalGlancePreviewApi::class)
+@Preview(widthDp = 130, heightDp = 422)
+@Preview(widthDp = 400, heightDp = 500)
+@Composable
+fun MyContentPreview() {
+	val eventData = ArrayList<EventData>()
+	for (i in 1..7) {
+		eventData.add(
+			EventData(
+				"Hello Widget $i",
+				"08:00",
+				"17:00"
+			)
+		)
+	}
+	eventData.add(
+		EventData(
+			"Summery",
+			"Start Time",
+			"End Time"
+		)
+	)
+	MyContent(eventData)
 }
 
 
