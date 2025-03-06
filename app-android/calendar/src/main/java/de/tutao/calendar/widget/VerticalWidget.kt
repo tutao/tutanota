a63457f5d255f61985ccdbff5d03d8e991c749e9
mup <mup@tutao.de>
2025-03-06 12:26:12 +0100

    Open app if tap on header

--- app-android/calendar/src/main/java/de/tutao/calendar/widget/VerticalWidget.kt
+++ app-android/calendar/src/main/java/de/tutao/calendar/widget/VerticalWidget.kt
@@ -1,12 +1,14 @@
 package de.tutao.calendar.widget
 
 import android.content.Context
+import android.content.Intent
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
@@ -35,8 +37,9 @@ import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
+import de.tutao.calendar.MainActivity
 import de.tutao.calendar.R
-import de.tutao.calendar.widget.VerticalWidget.MyAppWidgetGlanceColorScheme
+import de.tutao.calendar.widget.VerticalWidget.WidgetThemes
 import de.tutao.tutasdk.CalendarEvent
 import de.tutao.tutasdk.Sdk
 import de.tutao.tutashared.AndroidNativeCryptoFacade
@@ -44,17 +47,20 @@ import de.tutao.tutashared.SdkRestClient
 import de.tutao.tutashared.createAndroidKeyStoreFacade
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
+import de.tutao.tutashared.isAllDayEventByTimes
 import de.tutao.tutashared.push.SseStorage
 import de.tutao.tutashared.push.toSdkCredentials
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.Date
+
 
 class VerticalWidget : GlanceAppWidget() {
 
 
-	object MyAppWidgetGlanceColorScheme {
+	object WidgetThemes {
 		private val LightColors = lightColorScheme(
 			primary = Color(0xFF013E85),
 			onPrimary = Color(0xFFFFFFFF),
@@ -68,15 +74,15 @@ class VerticalWidget : GlanceAppWidget() {
 		)
 
 		private val DarkColors = darkColorScheme(
-			primary = Color(0xFF232323),
-			onPrimary = Color(0xFFDDDDDD),
-			secondary = Color(0xFF4D4D4D),
-			onSecondary = Color(0xFFDDDDDD),
+			primary = Color(0xFFACC7FF),
+			onPrimary = Color(0xFF232323),
+			secondary = Color(0xFFDDDDDD),
+			onSecondary = Color(0xFFACC7FF),
 			background = Color(0xFF232323),
 			onBackground = Color(0xFFDDDDDD),
-			surface = Color(0xFF4D4D4D),
+			surface = Color(0xFF111111),
 			onSurface = Color(0xFFDDDDDD),
-			primaryContainer = Color(0xFFFFFFFF)
+			primaryContainer = Color(0xFF111111)
 		)
 		val colors = ColorProviders(
 			light = LightColors,
@@ -91,16 +97,17 @@ class VerticalWidget : GlanceAppWidget() {
 		val db = AppDatabase.getDatabase(context, true)
 		val keyStoreFacade = createAndroidKeyStoreFacade()
 		val sseStorage = SseStorage(db, keyStoreFacade)
+		val userId = "OKV2uY3----4"
 
 		val crypto = AndroidNativeCryptoFacade(context)
 		val nativeCredentialsFacade = CredentialsEncryptionFactory.create(context, crypto, db)
-		val credentials = nativeCredentialsFacade.loadByUserId("OKV2uY3----4")!!
-			.toSdkCredentials() // FIXME Get the user from config page
+		val credentials = nativeCredentialsFacade.loadByUserId(userId)!!
+			.toSdkCredentials()
 
 		val sdk = Sdk(sseStorage.getSseOrigin()!!, SdkRestClient()).login(credentials)
 		val calendars = sdk.calendarFacade().getCalendarsRenderData()
 		val events = sdk.calendarFacade()
-			.getCalendarEvents(calendars.keys.first()) // FIXME Change this to an static event array for UI development if needed
+			.getCalendarEvents(calendars.keys.first())
 
 		val allEvents: List<CalendarEvent> = events.shortEvents.plus(events.longEvents)
 
@@ -108,19 +115,47 @@ class VerticalWidget : GlanceAppWidget() {
 //		val data = remember { mutableStateOf(allEvents) }
 
 		// Apply transformations to each item in the list
-		val eventsData = allEvents.map { event ->
+		val allDayEvents: MutableList<Event> = mutableListOf()
+		val normalEvents: MutableList<Event> = mutableListOf()
+
+		allEvents.forEach { ev ->
 			val zoneId = ZoneId.systemDefault()
-			val start = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.startTime.toLong()), zoneId)
-			val end = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.startTime.toLong()), zoneId)
+			val start = LocalDateTime.ofInstant(Instant.ofEpochMilli(ev.startTime.toLong()), zoneId)
+			val end = LocalDateTime.ofInstant(Instant.ofEpochMilli(ev.endTime.toLong()), zoneId)
 			val formatter = DateTimeFormatter.ofPattern("HH:mm")
-			Event(event.summary, start.format(formatter), end.format(formatter))
+			val isAllDay = isAllDayEventByTimes(
+				Date.from(Instant.ofEpochMilli(ev.startTime.toLong())),
+				Date.from(Instant.ofEpochMilli(ev.endTime.toLong()))
+			)
+
+			val event = Event(
+				ev.summary,
+				start.format(formatter),
+				end.format(formatter),
+				isAllDay
+			)
+
+			if (isAllDay) {
+				allDayEvents.add(event)
+			} else {
+				normalEvents.add(event)
+			}
 		}
+
 		provideContent {
 			GlanceTheme(
-				colors = MyAppWidgetGlanceColorScheme.colors
+				colors = WidgetThemes.colors
 			) {
 				// create your AppWidget here
-				WidgetBody(eventsData)
+				WidgetBody(WidgetData(normalEvents, allDayEvents)) {
+					val openCalendarEventIntent = Intent(context, MainActivity::class.java)
+					openCalendarEventIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+					openCalendarEventIntent.action = MainActivity.OPEN_CALENDAR_ACTION
+					openCalendarEventIntent.putExtra(MainActivity.OPEN_USER_MAILBOX_USERID_KEY, userId)
+
+					startActivity(context, openCalendarEventIntent, null)
+				}
 			}
 		}
 	}
@@ -130,10 +165,17 @@ data class Event(
 	val summary: String,
 	val startTime: String,
 	val endTime: String,
+	val isAllDay: Boolean
+)
+
+data class WidgetData(
+	val normalEvents: List<Event>,
+	val allDayEvents: List<Event>,
+	val allDayEventsCount: Int = allDayEvents.size
 )
 
 @Composable
-fun WidgetBody(eventsData: List<Event>) {
+fun WidgetBody(data: WidgetData, headerCallback: () -> Unit) {
 	Scaffold(
 		modifier = GlanceModifier
 			.padding(horizontal = 20.dp),
@@ -144,14 +186,14 @@ fun WidgetBody(eventsData: List<Event>) {
 				modifier = GlanceModifier.padding(vertical = 16.dp),
 				verticalAlignment = Alignment.CenterVertically
 			) {
-				Header()
+				Header(allDayEventsCount = data.allDayEventsCount, onTap = headerCallback)
 			}
 		}
 	) {
 		// calendar events
 		// we remove the scrollbar in res/values/styles.xml
 		LazyColumn {
-			itemsIndexed(eventsData, itemId = { index, _ -> index.toLong() }) { _, event ->
+			itemsIndexed(data.normalEvents, itemId = { index, _ -> index.toLong() }) { _, event ->
 				Column {
 					EventCard(event)
 					Spacer(modifier = GlanceModifier.height(4.dp))
@@ -162,10 +204,10 @@ fun WidgetBody(eventsData: List<Event>) {
 }
 
 @Composable
-private fun Header() {
+private fun Header(allDayEventsCount: Int, onTap: () -> Unit) {
 	Column(
 		modifier = GlanceModifier
-			.clickable { println("Open agenda") }
+			.clickable { onTap() }
 	) {
 		// date e.g. 20 February
 		Text(
@@ -174,22 +216,25 @@ private fun Header() {
 				fontSize = 16.sp,
 				color = GlanceTheme.colors.secondary
 			),
-			text = "20 February", // TODO() fetch data
+			text = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMMM")),
 			maxLines = 1,
 			modifier = GlanceModifier.padding(bottom = 4.dp)
 		)
 		// all day events
 		// TODO() border is thicker in the corners. should probably switch to xml or no border
 		Spacer(modifier = GlanceModifier.background(GlanceTheme.colors.primaryContainer).height(1.dp))
-		Text(
-			modifier = GlanceModifier
-				.padding(top = 4.dp),
-			style = TextStyle(
-				color = GlanceTheme.colors.secondary,
-				fontSize = 14.sp
-			),
-			text = "57 all day"
-		)
+
+		if (allDayEventsCount > 0) {
+			Text(
+				modifier = GlanceModifier
+					.padding(top = 4.dp),
+				style = TextStyle(
+					color = GlanceTheme.colors.secondary,
+					fontSize = 14.sp
+				),
+				text = "$allDayEventsCount all day event"
+			)
+		}
 	}
 	// create event button
 	Column(
@@ -287,29 +332,50 @@ fun BorderedContent(
 }
 
 @OptIn(ExperimentalGlancePreviewApi::class)
+@Preview(widthDp = 180, heightDp = 275)
 @Preview(widthDp = 200, heightDp = 422)
 @Preview(widthDp = 400, heightDp = 500)
 @Composable
 fun VerticalWidgetPreview() {
 	val eventData = ArrayList<Event>()
+	val allDayEvents = ArrayList<Event>()
 	for (i in 1..7) {
 		eventData.add(
 			Event(
 				"Hello Widget $i",
 				"08:00",
-				"17:00"
+				"17:00",
+				isAllDay = false
 			)
 		)
 	}
+
 	eventData.add(
 		Event(
 			"Summery",
 			"Start Time",
-			"End Time"
+			"End Time",
+			isAllDay = false
 		)
 	)
 
-	GlanceTheme(colors = MyAppWidgetGlanceColorScheme.colors) { WidgetBody(eventData) }
+	allDayEvents.add(
+		Event(
+			"Summery",
+			"Start Time",
+			"End Time",
+			isAllDay = true
+		)
+	)
+
+	GlanceTheme(colors = WidgetThemes.colors) {
+		WidgetBody(
+			WidgetData(
+				allDayEvents = allDayEvents,
+				normalEvents = eventData,
+			)
+		) {}
+	}
 }
 
 
