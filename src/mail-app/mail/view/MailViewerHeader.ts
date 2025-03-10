4ad50b379d41a796fd94c5fe07df1d63ccb4310b
Mo Solis <morony.solis@uconn.edu>
2025-03-10 16:00:50 -0400

    add popup on banner buttons press

--- src/mail-app/mail/view/MailViewerHeader.ts
+++ src/mail-app/mail/view/MailViewerHeader.ts
@@ -768,15 +768,15 @@ export class MailViewerHeader implements Component<MailViewerHeaderAttrs> {
 	//
 	//
 	private renderMobyPhishBanner(viewModel: MailViewerViewModel): Children | null {
-		// if (this.isSenderConfirmed) {			
-		// 	return m(InfoBanner, {
-		// 		message: "mobyPhish_sender_confirmed",
-		// 		icon: Icons.Warning,
-		// 		type: BannerType.Warning,
-		// 		helpLink: canSeeTutaLinks(viewModel.logins) ? InfoLink.Phishing : null,
-		// 		//buttons: [confirmButton, denyButton],
-		// 	});
-		// }
+		if (this.isSenderConfirmed) {			
+			return m(InfoBanner, {
+				message: "mobyPhish_sender_confirmed",
+				icon: Icons.Warning,
+				type: BannerType.Warning,
+				helpLink: canSeeTutaLinks(viewModel.logins) ? InfoLink.Phishing : null,
+				//buttons: [confirmButton, denyButton],
+			});
+		}
 
 		const confirmButton: BannerButtonAttrs = {
 			label: "mobyPhish_confirm",
