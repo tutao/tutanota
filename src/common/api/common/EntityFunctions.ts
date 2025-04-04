d1201811d1e2eb429352f6282689e5eaeec52ddd
sug <sug@tutao.de>
2025-04-04 18:39:18 +0200

    make ServerModelInfo less verbose

--- src/common/api/common/EntityFunctions.ts
+++ src/common/api/common/EntityFunctions.ts
@@ -1,6 +1,6 @@
 import { AssociationType, Cardinality, Type, ValueType } from "./EntityConstants.js"
 import { assert, assertNotNull, TypeRef } from "@tutao/tutanota-utils"
-import type { AttributeId, ModelAssociation, ModelValue, TypeId, TypeModel } from "./EntityTypes"
+import type { AttributeId, ModelAssociation, ModelValue, TypeModel } from "./EntityTypes"
 import { typeModels as baseTypeModels } from "../entities/base/TypeModels.js"
 import { typeModels as sysTypeModels } from "../entities/sys/TypeModels.js"
 import { typeModels as tutanotaTypeModels } from "../entities/tutanota/TypeModels.js"
@@ -62,30 +62,13 @@ export type ModelInfos = typeof modelInfos
 
 export type TypeReferenceResolver = typeof resolveTypeReference
 
+// Type model of each application with it's version
 export type ServerModels = {
-	base: { version: number; types: typeof baseTypeModels }
-	sys: { version: number; types: typeof sysTypeModels }
-	tutanota: { version: number; types: typeof tutanotaTypeModels }
-	monitor: { version: number; types: typeof monitorTypeModels }
-	accounting: { version: number; types: typeof accountingTypeModels }
-	gossip: { version: number; types: typeof gossipTypeModels }
-	storage: { version: number; types: typeof storageTypeModels }
-	usage: { version: number; types: typeof usageTypeModels }
-}
-
-export const EmptyTypeModels: ServerModels = {
-	base: { version: NaN, types: {} },
-	sys: { version: NaN, types: {} },
-	tutanota: { version: NaN, types: {} },
-	monitor: { version: NaN, types: {} },
-	accounting: { version: NaN, types: {} },
-	gossip: { version: NaN, types: {} },
-	storage: { version: NaN, types: {} },
-	usage: { version: NaN, types: {} },
+	[knownApps in AppName]: { version: number; types: typeof baseTypeModels }
 }
 
 export class ServerModelInfo {
-	public static typeModels: ServerModels = EmptyTypeModels
+	public static typeModels: ServerModels = {} as ServerModels
 	private static applicationVersionSum: number = NaN
 
 	public static getCurrentVersion(): number {
@@ -98,11 +81,10 @@ export class ServerModelInfo {
 	public static init(applicationVersionSum: number, parsedJson: Record<string, unknown>): void {
 		assert(applicationVersionSum > 0, "invalid version number")
 
-		let newTypeModels = EmptyTypeModels
+		let newTypeModels = {} as ServerModels
 		for (const appName of Object.values(AppNameEnum)) {
 			const appModels = parsedJson[appName]
-			assert(appModels != null, `app models for ${appModels} not found`)
-
+			assertNotNull(appModels, `app models for ${appModels} not found`)
 			newTypeModels[appName] = ServerModelInfo.parseAllTypesForModel(appModels)
 		}
 
@@ -117,13 +99,11 @@ export class ServerModelInfo {
 	public static initFromWrittenFile(fileContent: string) {
 		const parsedJson: ServerModels = JSON.parse(fileContent)
 		let versionSum = 0
-		let jsonInServerFormat = {}
+		let jsonInServerFormat: Record<string, any> = {}
 
 		Object.entries(parsedJson).map(([appName, { version, types }]) => {
 			versionSum += version
-
-			const modelInfo = { name: appName, version: version, types: types }
-			Object.assign(jsonInServerFormat, { [appName]: modelInfo })
+			jsonInServerFormat[appName] = { name: appName, version, types }
 		})
 
 		ServerModelInfo.init(versionSum, jsonInServerFormat)
@@ -136,11 +116,11 @@ export class ServerModelInfo {
 
 	private static parseAllTypesForModel(modelTypeInfo: unknown) {
 		const modelInfo = modelTypeInfo as Record<string, unknown>
-		const appName = ServerModelInfo.parseAppName(String(modelInfo.name))
-		const modelTypeInfoRecord = modelInfo.types as Record<string, unknown>
+		const appName = ServerModelInfo.ensureVariantOf(AppNameEnum, String(modelInfo.name))
 		const version = parseInt(String(modelInfo.version))
+		const modelTypeInfoRecord = modelInfo.types as Record<string, unknown>
 
-		let types: Record<TypeId, TypeModel> = {}
+		let types: Record<string, TypeModel> = {}
 		for (const typeInfo of Object.values(modelTypeInfoRecord)) {
 			const typeModel = ServerModelInfo.parseSingleTypeModel(appName, version, typeInfo)
 			types[typeModel.id] = typeModel
@@ -153,35 +133,22 @@ export class ServerModelInfo {
 
 	private static parseSingleTypeModel(app: AppName, appVersion: number, typeInfo: unknown): TypeModel {
 		const typeInfoRecord = typeInfo as Record<string, any>
-
-		const id = parseInt(typeInfoRecord.id)
-		const since = parseInt(typeInfoRecord.since)
-		const name = String(typeInfoRecord.name)
-		const version = appVersion.toString()
-		const rootId = String(typeInfoRecord.rootId)
-		const versioned = Boolean(typeInfoRecord.versioned)
-		const encrypted = typeInfoRecord.encrypted.toString() === "true"
-
-		const type = ServerModelInfo.parseEntityType(String(typeInfoRecord.type))
-
 		const valuesRecord = typeInfoRecord.values as Record<string, unknown>
 		const associationsRecord = typeInfoRecord.associations as Record<string, unknown>
-		const values = ServerModelInfo.parseModelValues(valuesRecord)
-		const associations = ServerModelInfo.parseModelAssociations(associationsRecord)
 
-		const typeModel = {
-			id,
-			since,
+		const typeModel: TypeModel = {
 			app,
-			version,
-			name,
-			type,
-			versioned,
-			encrypted,
-			rootId,
-			values,
-			associations,
-		} satisfies TypeModel
+			version: appVersion.toString(),
+			id: parseInt(typeInfoRecord.id),
+			since: parseInt(typeInfoRecord.since),
+			name: String(typeInfoRecord.name),
+			type: ServerModelInfo.ensureVariantOf(Type, String(typeInfoRecord.type)),
+			versioned: Boolean(typeInfoRecord.versioned),
+			encrypted: typeInfoRecord.encrypted.toString() === "true",
+			rootId: String(typeInfoRecord.rootId),
+			values: ServerModelInfo.parseModelValues(valuesRecord),
+			associations: ServerModelInfo.parseModelAssociations(associationsRecord),
+		}
 
 		ServerModelInfo.verifyNoNullValueInRecord(typeModel)
 		return typeModel
@@ -192,22 +159,14 @@ export class ServerModelInfo {
 
 		for (const modelValueInfo of Object.values(valuesRecord)) {
 			const modelValueInfoRecord = modelValueInfo as Record<string, unknown>
-
-			const final = Boolean(modelValueInfoRecord.final)
-			const name = String(modelValueInfoRecord.name)
-			const id = Number(modelValueInfoRecord.id)
-			const type = ServerModelInfo.parseValueType(String(modelValueInfoRecord.type))
-			const cardinality = ServerModelInfo.parseCardinality(String(modelValueInfoRecord.cardinality))
-			const encrypted = Boolean(modelValueInfoRecord.encrypted)
-
-			const modelValue = {
-				id,
-				name,
-				final,
-				type,
-				encrypted,
-				cardinality,
-			} satisfies ModelValue
+			const modelValue: ModelValue = {
+				id: parseInt(String(modelValueInfoRecord.id)),
+				name: String(modelValueInfoRecord.name),
+				final: Boolean(modelValueInfoRecord.final),
+				type: ServerModelInfo.ensureVariantOf(ValueType, String(modelValueInfoRecord.type)),
+				encrypted: Boolean(modelValueInfoRecord.encrypted),
+				cardinality: ServerModelInfo.ensureVariantOf(Cardinality, String(modelValueInfoRecord.cardinality)),
+			}
 
 			ServerModelInfo.verifyNoNullValueInRecord(modelValue)
 			Object.assign(values, { [modelValue.id]: modelValue })
@@ -221,28 +180,23 @@ export class ServerModelInfo {
 
 		for (const associationInfo of Object.values(modelAssociations)) {
 			const associationInfoRecord = associationInfo as Record<string, unknown>
-
-			const final = Boolean(associationInfoRecord.final)
-			const name = String(associationInfoRecord.name)
-			const id = Number(associationInfoRecord.id)
-			const type = ServerModelInfo.parseAssociationType(String(associationInfoRecord.type))
-			const cardinality = ServerModelInfo.parseCardinality(String(associationInfoRecord.cardinality))
-			const refTypeId = Number(associationInfoRecord.refTypeId)
-
-			const modelAssociation = {
-				id,
-				name,
-				final,
-				type,
-				cardinality,
-				refTypeId,
-			} satisfies ModelAssociation
+			const modelAssociation: ModelAssociation = {
+				id: parseInt(String(associationInfoRecord.id)),
+				name: String(associationInfoRecord.name),
+				final: Boolean(associationInfoRecord.final),
+				type: ServerModelInfo.ensureVariantOf(AssociationType, String(associationInfoRecord.type)),
+				cardinality: ServerModelInfo.ensureVariantOf(Cardinality, String(associationInfoRecord.cardinality)),
+				refTypeId: parseInt(String(associationInfoRecord.refTypeId)),
+			}
 
 			ServerModelInfo.verifyNoNullValueInRecord(modelAssociation)
 
 			// dependency can be null, so assign it after above `verifyNoNullValueInRecord` check. and check here instead
 			Object.assign(modelAssociation, {
-				dependency: typeof associationInfoRecord.dependency === "string" ? ServerModelInfo.parseAppName(associationInfoRecord.dependency) : null,
+				dependency:
+					typeof associationInfoRecord.dependency === "string"
+						? ServerModelInfo.ensureVariantOf(AppNameEnum, associationInfoRecord.dependency)
+						: null,
 			})
 
 			Object.assign(associations, { [modelAssociation.id]: modelAssociation })
@@ -251,43 +205,16 @@ export class ServerModelInfo {
 		return associations
 	}
 
-	private static parseAppName(appnameStr: string): AppName {
-		return assertNotNull(
-			Object.values(AppNameEnum).find((app) => app.toString() === appnameStr),
-			`Invalid app name in server model. Found: ${appnameStr}`,
-		)
-	}
-
-	private static parseEntityType(typeStr: string): Values<typeof Type> {
-		return assertNotNull(
-			Object.values(Type).find((elType) => elType.toString() === typeStr),
-			"Invalid entity type in server model",
-		)
-	}
-
-	private static parseValueType(typeStr: string): Values<typeof ValueType> {
-		return assertNotNull(
-			Object.values(ValueType).find((vType) => vType.toString() === typeStr),
-			"Invalid value type in server model",
-		)
-	}
-
-	private static parseAssociationType(typeStr: string): Values<typeof AssociationType> {
-		return assertNotNull(
-			Object.values(AssociationType).find((aType) => aType.toString() === typeStr),
-			"Invalid association type in server model",
-		)
-	}
-
-	private static parseCardinality(cardinalityStr: string): Values<typeof Cardinality> {
-		return assertNotNull(
-			Object.values(Cardinality).find((cardinality) => cardinality.toString() === cardinalityStr),
-			`Invalid cardinality type in server model. found: ${cardinalityStr}`,
-		)
-	}
-
 	private static computeApplicationVersionSum(models: ServerModels): number {
 		return Object.values(models).reduce((sum, model) => sum + model.version, 0)
+	}
+
+	private static ensureVariantOf<T extends string>(obj: Record<any, T>, inputStr: string): Values<typeof obj> {
+		const knownVariants = Object.values(obj)
+		return assertNotNull(
+			knownVariants.find((a) => a === inputStr),
+			`Unknown value ${inputStr}. Could be one of: ${knownVariants}`,
+		)
 	}
 
 	private static verifyNoNullValueInRecord(record: Record<any, any>) {
