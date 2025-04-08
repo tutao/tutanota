41a6d074225bdaa140586590ae1006aa3a54480d
paw <104824185+paw-hub@users.noreply.github.com>
2025-04-08 11:49:28 +0200

    Remove limit from OfflineStorageSearchFacade
    maxResults was intended to limit IndexedDbSearchFacade, and then the
    getMoreSearchResults would be called if more results were needed. If
    there are no more results, the results would be re-used.
    
    Since we don't have getMoreSearchResults on OfflineStorageSearchFacade,
    the incomplete result gets used. We can simply return everything upfront
    instead, though, when we do the initial search.
    
    Co-authored-by: bir <bir@tutao.de>
    Co-authored-by: hrb <hrb-hub@users.noreply.github.com>

--- src/mail-app/workerUtils/index/OfflineStorageSearchFacade.ts
+++ src/mail-app/workerUtils/index/OfflineStorageSearchFacade.ts
@@ -21,7 +21,7 @@ export class OfflineStorageSearchFacade implements SearchFacade {
 		private readonly contactIndexer: ContactIndexer,
 	) {}
 
-	async search(query: string, restriction: SearchRestriction, minSuggestionCount: number, maxResults?: number): Promise<SearchResult> {
+	async search(query: string, restriction: SearchRestriction, _minSuggestionCount: number, _maxResults?: number): Promise<SearchResult> {
 		// For FTS5, tokens with special characters or "AND", "OR", or "NOT" must be escaped with quotes, though we can
 		// also just escape all tokens with quotes for simplicity. Then, to include quotes mid-query like this, we
 		// replace " with "". Lastly, we add a `*` at the end of each token to match prefixes.
@@ -36,15 +36,15 @@ export class OfflineStorageSearchFacade implements SearchFacade {
 			.join(" ")
 
 		if (isSameTypeRef(restriction.type, MailTypeRef)) {
-			return this.searchMails(query, normalizedQuery, restriction, maxResults)
+			return this.searchMails(query, normalizedQuery, restriction)
 		} else if (isSameTypeRef(restriction.type, ContactTypeRef)) {
-			return this.searchContacts(query, normalizedQuery, restriction, maxResults)
+			return this.searchContacts(query, normalizedQuery, restriction)
 		} else {
 			throw new ProgrammingError(`cannot search ${restriction.type.type}`)
 		}
 	}
 
-	private async searchMails(originalQuery: string, normalizedQuery: string, restriction: SearchRestriction, maxResults?: number): Promise<SearchResult> {
+	private async searchMails(originalQuery: string, normalizedQuery: string, restriction: SearchRestriction): Promise<SearchResult> {
 		const preparedSqlQuery = sql`
             SELECT list_entities.listId,
                    list_entities.elementId
@@ -54,13 +54,12 @@ export class OfflineStorageSearchFacade implements SearchFacade {
                      INNER JOIN content_mail_index ON
                 list_entities.rowid = content_mail_index.rowid
             WHERE mail_index = ${normalizedQuery}
-            ORDER BY content_mail_index.receivedDate DESC
-                LIMIT ${maxResults ?? Number.MAX_SAFE_INTEGER}`
+            ORDER BY content_mail_index.receivedDate DESC`
 		const resultRows = await this.sqlCipherFacade.all(preparedSqlQuery.query, preparedSqlQuery.params)
 		const resultIds = resultRows.map(({ listId, elementId }) => {
 			return [untagSqlValue(listId) as string, untagSqlValue(elementId) as string] satisfies IdTuple
 		})
-		const result: SearchResult = {
+		return {
 			query: originalQuery,
 			restriction,
 			results: resultIds,
@@ -70,10 +69,9 @@ export class OfflineStorageSearchFacade implements SearchFacade {
 			moreResults: [],
 			moreResultsEntries: [],
 		}
-		return result
 	}
 
-	private async searchContacts(originalQuery: string, normalizedQuery: string, restriction: SearchRestriction, maxResults?: number): Promise<SearchResult> {
+	private async searchContacts(originalQuery: string, normalizedQuery: string, restriction: SearchRestriction): Promise<SearchResult> {
 		const preparedSqlQuery = sql`
             SELECT list_entities.listId,
                    list_entities.elementId
@@ -81,14 +79,13 @@ export class OfflineStorageSearchFacade implements SearchFacade {
                      INNER JOIN list_entities ON
                 contact_index.rowid = list_entities.rowid
             WHERE contact_index = ${normalizedQuery}
-            ORDER BY contact_index.lastName, contact_index.firstName
-                LIMIT ${maxResults ?? Number.MAX_SAFE_INTEGER}`
+            ORDER BY contact_index.lastName, contact_index.firstName`
 		const resultRows = await this.sqlCipherFacade.all(preparedSqlQuery.query, preparedSqlQuery.params)
 		const resultIds = resultRows.map(({ listId, elementId }) => {
 			return [untagSqlValue(listId) as string, untagSqlValue(elementId) as string] satisfies IdTuple
 		})
 		const indexTimestamp = (await this.contactIndexer.areContactsIndexed()) ? FULL_INDEXED_TIMESTAMP : NOTHING_INDEXED_TIMESTAMP
-		const result: SearchResult = {
+		return {
 			query: originalQuery,
 			restriction,
 			results: resultIds,
@@ -98,7 +95,6 @@ export class OfflineStorageSearchFacade implements SearchFacade {
 			moreResults: [],
 			moreResultsEntries: [],
 		}
-		return result
 	}
 
 	async getMoreSearchResults(searchResult: SearchResult, _: number): Promise<SearchResult> {
