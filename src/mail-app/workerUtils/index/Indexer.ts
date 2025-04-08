84396a7f3fd364edd8676519bb91f1dd1b60573b
paw <104824185+paw-hub@users.noreply.github.com>
2025-04-08 18:25:00 +0200

    Reduce mail index when needed
    Have SearchOfflineRangePostLoginAction call resizeMailIndex, which will
    extend or retract the mail index.
    
    Closes #8835
    
    Co-authored-by: bir <bir@tutao.de>

--- src/mail-app/workerUtils/index/Indexer.ts
+++ src/mail-app/workerUtils/index/Indexer.ts
@@ -19,7 +19,24 @@ export interface Indexer {
 
 	processEntityEvents(updates: readonly EntityUpdateData[], batchId: Id, groupId: Id): Promise<void>
 
+	/**
+	 * Extends the mail index to the given timestamp.
+	 *
+	 * Does nothing if it is already at least as far back as the timestamp.
+	 *
+	 * @param time timestamp
+	 */
 	extendMailIndex(time: number): Promise<void>
+
+	/**
+	 * Sets the mail index to the given timestamp.
+	 *
+	 * If the mail index would extend, then this will have the same effect as calling extendMailIndex.
+	 *
+	 * Otherwise, it sets the current timestamp to the new one.
+	 * @param time timestamp
+	 */
+	resizeMailIndex(time: number): Promise<void>
 
 	deleteIndex(userId: string): Promise<void>
 
