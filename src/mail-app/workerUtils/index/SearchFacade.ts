2da688d37c387f136b3e86ca57a5777443885139
paw <104824185+paw-hub@users.noreply.github.com>
2025-04-08 11:49:28 +0200

    Remove limit from OfflineStorageSearchFacade
    maxResults was intended to limit IndexedDbSearchFacade, and then the
    getMoreSearchResults would be called if more results were needed. If
    there are no more results, the results would be re-used.
    
    Since we don't have getMoreSearchResults on OfflineStorageSearchFacade,
    the incomplete result gets used. We can simply return everything upfront
    instead, though, when we do the initial search.
    
    Co-authored-by: bir <bir@tutao.de>
    Co-authored-by: hrb <hrb-hub@users.noreply.github.com>

--- src/mail-app/workerUtils/index/SearchFacade.ts
+++ src/mail-app/workerUtils/index/SearchFacade.ts
@@ -4,7 +4,28 @@ import type { SearchRestriction, SearchResult } from "../../../common/api/worker
  * Local search functions
  */
 export interface SearchFacade {
+	/**
+	 * Search for entries.
+	 *
+	 * The returned results will match {@link query} and {@link restriction}.
+	 *
+	 * Note that {@link maxResults} is intended to be used as an optimization and not a hard limit. The implementation
+	 * can freely ignore this value and return more or even all results.
+	 *
+	 * @param query search string
+	 * @param restriction search parameters
+	 * @param minSuggestionCount
+	 * @param maxResults desired number of results, or unlimited if left undefined
+	 */
 	search(query: string, restriction: SearchRestriction, minSuggestionCount: number, maxResults?: number): Promise<SearchResult>
 
+	/**
+	 * Get more search results in case search did not return everything.
+	 *
+	 * This may be a no-op on some implementations which would return everything upfront anyway.
+	 *
+	 * @param searchResult search result from search()
+	 * @param moreResultCount number of results
+	 */
 	getMoreSearchResults(searchResult: SearchResult, moreResultCount: number): Promise<SearchResult>
 }
