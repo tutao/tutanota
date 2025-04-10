bf5a90014e119df76992e41863296c251d1d29b7
vaf <vaf@tutao.de>
2025-04-10 12:05:12 +0200

    Do not initialize keyRotationFacade for temporary session
    Handle errors when loading pending roations gracefully if already logged out
    it is hard to decided whether to load because in the worker thread we do not
    know if the session is just temporary.

--- src/common/api/worker/facades/LoginFacade.ts
+++ src/common/api/worker/facades/LoginFacade.ts
@@ -298,7 +298,7 @@ export class LoginFacade {
 		}
 		this.loginListener.onFullLoginSuccess(sessionType, cacheInfo, credentials)
 
-		if (!isAdminClient()) {
+		if (!isAdminClient() && sessionType !== SessionType.Temporary) {
 			await this.keyRotationFacade.initialize(userPassphraseKey, modernKdfType)
 		}
 
