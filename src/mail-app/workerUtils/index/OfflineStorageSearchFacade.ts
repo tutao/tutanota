9fc226bcbfea39f836d28b8d0a608526b7fb253a
bir <bir@tutao.de>
2025-04-10 11:17:11 +0200

    Exact text search
    Combines phrases into individual tokens if surrounded with quotes.
    
    Also does not add a "*" to tokens surrounded with quotes.
    
    Close #8836
    
    Co-authored-by: paw <paw-hub@users.noreply.github.com>

--- src/mail-app/workerUtils/index/OfflineStorageSearchFacade.ts
+++ src/mail-app/workerUtils/index/OfflineStorageSearchFacade.ts
@@ -22,18 +22,7 @@ export class OfflineStorageSearchFacade implements SearchFacade {
 	) {}
 
 	async search(query: string, restriction: SearchRestriction, _minSuggestionCount: number, _maxResults?: number): Promise<SearchResult> {
-		// For FTS5, tokens with special characters or "AND", "OR", or "NOT" must be escaped with quotes, though we can
-		// also just escape all tokens with quotes for simplicity. Then, to include quotes mid-query like this, we
-		// replace " with "". Lastly, we add a `*` at the end of each token to match prefixes.
-		//
-		// See https://www.sqlite.org/fts5.html
-		//
-		// The code below is from https://github.com/signalapp/Signal-Desktop/blob/a714a31b3990c64801f1c43b3d465877719f30a4/ts/sql/Server.ts#L1873
-		// but simplified (we don't do tokenizing here)
-		const normalizedQuery = query
-			.split(/\s+/)
-			.map((token) => `"${token.replace(/"/g, '""')}"*`)
-			.join(" ")
+		const normalizedQuery = normalizeQuery(query)
 
 		if (isSameTypeRef(restriction.type, MailTypeRef)) {
 			return this.searchMails(query, normalizedQuery, restriction)
@@ -61,6 +50,14 @@ export class OfflineStorageSearchFacade implements SearchFacade {
 		// Otherwise, we pass the query string as-is.
 		const columnList = mailFieldToColumn(restriction.field)?.join(" ")
 		const queryString = columnList != null ? `{${columnList}} : ${normalizedQuery}` : normalizedQuery
+
+		// Do the actual search query.
+		//
+		// This will also filter based on date range and sets, and it will order by received date in descending order so
+		// that newer emails appear first in the returned list.
+		//
+		// Note: We use instr() for checking for sets, but there's a small gotcha: instr() is 1-indexed, returning 0
+		// if not found, 1 for the first char, etc.; see https://www.sqlitetutorial.net/sqlite-functions/sqlite-instr/
 		const preparedSqlQuery = sql`
             SELECT list_entities.listId,
                    list_entities.elementId
@@ -70,9 +67,9 @@ export class OfflineStorageSearchFacade implements SearchFacade {
                      INNER JOIN content_mail_index ON
                 list_entities.rowid = content_mail_index.rowid
             WHERE mail_index = ${queryString}
-			  AND instr(content_mail_index.sets, ${idToSearch}) > 0 -- instr() == 0 means not found, since instr() is 1-indexed
-			  AND content_mail_index.receivedDate <= ${restriction.end ?? Number.MAX_SAFE_INTEGER}
-			  AND content_mail_index.receivedDate >= ${restriction.start ?? 0}
+              AND instr(content_mail_index.sets, ${idToSearch}) > 0
+              AND content_mail_index.receivedDate <= ${restriction.end ?? Number.MAX_SAFE_INTEGER}
+              AND content_mail_index.receivedDate >= ${restriction.start ?? 0}
             ORDER BY content_mail_index.receivedDate DESC`
 		const resultRows = await this.sqlCipherFacade.all(preparedSqlQuery.query, preparedSqlQuery.params)
 		const resultIds = resultRows.map(({ listId, elementId }) => {
@@ -141,4 +138,44 @@ function mailFieldToColumn(field: string | null): string[] | null {
 		default:
 			throw new ProgrammingError(`Unknown field "${field}" passed into mailFieldToColumn`)
 	}
+}
+
+/**
+ * Split into FTS5 search tokens and return the query
+ *
+ * For words that are not in quotes, they will match the start of a string.
+ * Otherwise, they will be fully matched.
+ *
+ * For example, `hello world this is "my string"` becomes `"hello"* "world"* "this"* "is"* "my string"`
+ *
+ * See https://sqlite.org/fts5.html
+ *
+ * @param query query to check
+ * @return normalized query that can be used for FTS5
+ * @VisibleForTesting
+ */
+export function normalizeQuery(query: string): string {
+	const normalizedQuery: string[] = []
+
+	let quoted = false
+	for (const block of query.split('"')) {
+		if (quoted) {
+			// in quotes; match an exact token or phrase (e.g. "free" will not match "freedom")
+			const trimmed = block.trim()
+			if (trimmed !== "") {
+				normalizedQuery.push(`"${trimmed}"`)
+			}
+		} else {
+			// split into words and, for each word, match the start of a token (e.g. "free"* will match "freedom")
+			for (const word of block.split(/\s+/)) {
+				if (word !== "") {
+					normalizedQuery.push(`"${word}"*`)
+				}
+			}
+		}
+		quoted = !quoted
+	}
+	// if !quoted here, then the user is likely in the middle of typing a quoted string, which is fine
+
+	return normalizedQuery.join(" ")
 }
