6918f652f772085fbc5455ee0e95183ec9003b16
vaf <vaf@tutao.de>
2025-04-14 17:10:44 +0200

    export sign and verify
    Co-authored-by: hec <hec@tutao.de>

--- tuta-sdk/rust/crypto-primitives/src/lib.rs
+++ tuta-sdk/rust/crypto-primitives/src/lib.rs
@@ -7,12 +7,28 @@ mod randomizer_facade;
 use ed25519::Ed25519KeyPair;
 use randomizer_facade::RandomizerFacade;
 
+use crate::ed25519::{Ed25519PrivateKey, Ed25519PublicKey, Ed25519Signature};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
 #[wasm_bindgen]
-pub fn generate_ed25519_keypair() -> Ed25519KeyPair {
+pub fn ed25519_generate_keypair() -> Ed25519KeyPair {
 	let mut randomizer_facade = RandomizerFacade::from_core(rand_core::OsRng {});
 	Ed25519KeyPair::generate(&mut randomizer_facade)
+}
+
+#[wasm_bindgen]
+pub fn ed25519_sign(private_key: Ed25519PrivateKey, message: &[u8]) -> Ed25519Signature {
+	private_key.sign(message)
+}
+
+#[wasm_bindgen]
+pub fn ed25519_verify(
+	public_key: Ed25519PublicKey,
+	message: &[u8],
+	signature: Ed25519Signature,
+) -> bool {
+	match public_key.verify(message, &signature) {
+		Ok(_) => true,
+		Err(_) => false,
+	}
 }
