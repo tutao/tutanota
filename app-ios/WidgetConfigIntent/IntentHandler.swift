bff42a8a61c14c4f628d2ba2b296f066d8142f7f
Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>
2025-04-15 10:18:39 +0200

    [iOS] Adds AgendaWidget to xcodegen template
    Co-authored-by: André Dias <and@tutao.de>

---
+++ app-ios/WidgetConfigIntent/IntentHandler.swift
@@ -0,0 +1,19 @@
+//
+//  IntentHandler.swift
+//  WidgetConfigIntent
+//
+//  Created by Tutao GmbH on 15.04.25.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
