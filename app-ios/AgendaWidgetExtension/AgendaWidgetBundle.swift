fdf4a097d65c52216323e7c54aea85faa4410c7c
tutao-mac <hello@tutao.de>
2025-04-15 10:18:39 +0200

    Adds AgendaWidget to xcodegen template
    Co-authored-by: André Dias <and@tutao.de>

---
+++ app-ios/AgendaWidgetExtension/AgendaWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AgendaWidgetBundle.swift
+//  AgendaWidget
+//
+//  Created by Tutao GmbH on 15.04.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AgendaWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AgendaWidget()
+    }
+}
