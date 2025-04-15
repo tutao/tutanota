67f333ca4611df95b72e05b6a22b66b9f58b6c29
Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>
2025-04-15 10:18:39 +0200

    [iOS] Adds AgendaWidget to xcodegen template
    Co-authored-by: André Dias <and@tutao.de>

---
+++ app-ios/AgendaWidgetExtension/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  AgendaWidget
+//
+//  Created by Tutao GmbH on 15.04.25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
