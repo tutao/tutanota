b9741e61949963049fd4f5003b0c1c944bbcbd70
Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>
2025-04-29 13:33:43 +0200

    [iOS] Prompts the user to configure the Widget
    Co-authored-by: André Dias <and@tutao.de>

--- app-ios/AgendaWidgetExtension/Data/WidgetModel.swift
+++ app-ios/AgendaWidgetExtension/Data/WidgetModel.swift
@@ -19,9 +19,7 @@ struct WidgetModel {
 	private let urlSession: URLSession = makeUrlSession()
 	private let sdk: LoggedInSdk
 
-	init(userId: String) async throws {
-		self.sdk = try await SdkFactory.createSdk(userId: userId)
-	}
+	init(userId: String) async throws { self.sdk = try await SdkFactory.createSdk(userId: userId) }
 
 	func getEventsForCalendars(_ calendars: [CalendarEntity], date: Date) async throws -> ([CalendarEventData], [CalendarEventData]) {
 		let dateInMiliseconds = UInt64(date.timeIntervalSince1970) * 1000
@@ -32,29 +30,26 @@ struct WidgetModel {
 
 		for calendar in calendars {
 			let eventsList = await calendarFacade.getCalendarEvents(calendarId: calendar.id, date: dateInMiliseconds)
-			(eventsList.shortEvents + eventsList.longEvents).forEach { event in
-				let eventStart = Date(timeIntervalSince1970: Double(event.startTime) / 1000)
-				let eventEnd = Date(timeIntervalSince1970: Double(event.endTime) / 1000)
-				let isAllDay = isAllDayEvent(startDate: eventStart, endDate: eventEnd) || isAllDayOnReferenceDate(startDate: eventStart, endDate: eventEnd, referenceDate: date)
+			(eventsList.shortEvents + eventsList.longEvents)
+				.forEach { event in
+					let eventStart = Date(timeIntervalSince1970: Double(event.startTime) / 1000)
+					let eventEnd = Date(timeIntervalSince1970: Double(event.endTime) / 1000)
+					let isAllDay =
+						isAllDayEvent(startDate: eventStart, endDate: eventEnd)
+						|| isAllDayOnReferenceDate(startDate: eventStart, endDate: eventEnd, referenceDate: date)
 
-				let eventId = if let id = event.id {
-					id.listId + "/" + id.elementId
-				} else { "" }
+					let eventId = if let id = event.id { id.listId + "/" + id.elementId } else { "" }
 
-				let eventData = CalendarEventData(
-					id: eventId,
-					summary: event.summary,
-					startDate: eventStart,
-					endDate: eventEnd,
-					calendarColor: calendar.color == "" ? DEFAULT_CALENDAR_COLOR : calendar.color
-				)
+					let eventData = CalendarEventData(
+						id: eventId,
+						summary: event.summary,
+						startDate: eventStart,
+						endDate: eventEnd,
+						calendarColor: calendar.color.isEmpty ? DEFAULT_CALENDAR_COLOR : calendar.color
+					)
 
-				if isAllDay {
-					longEvents.append(eventData)
-				} else {
-					normalEvents.append(eventData)
+					if isAllDay { longEvents.append(eventData) } else { normalEvents.append(eventData) }
 				}
-			}
 		}
 
 		normalEvents.sort(by: { $0.startDate.timeIntervalSince1970 < $1.startDate.timeIntervalSince1970 })
