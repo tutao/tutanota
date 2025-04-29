5b89f7ccc3059565aa1b54ec97154b700f01d7f3
bir <bir@tutao.de>
2025-04-29 10:54:38 +0200

    Highlight additional fields (subject and sender/recipient)
    We use the same SearchToken list from #8883 and extend it to be used
    for the subject and mail viewer.
    
    For the mail viewer, we create mark components with Mithril. This uses
    a simple map() on the list returned by highlightTextInQuery function.
    
    For the list view, since we use HTMLElement, we do what we did with the
    mail body: create DOM elements, creating mark elements for the text that
    needs highlighted. We use Text nodes for non-highlighted elements to
    avoid malicious HTML injection, and we use textContent on highlighted
    elements more-or-less does the same thing as making a Text node.
    
    Closes #8939
    
    Co-authored-by: paw <paw-hub@users.noreply.github.com>

--- src/mail-app/search/view/SearchView.ts
+++ src/mail-app/search/view/SearchView.ts
@@ -293,6 +293,7 @@ export class SearchView extends BaseTopLevelView implements TopLevelView<SearchV
 			},
 			isFreeAccount: locator.logins.getUserController().isFreeAccount(),
 			getLabelsForMail: (mail) => this.searchViewModel.getLabelsForMail(mail),
+			highlightedStrings: this.searchViewModel.getHighlightedStrings(),
 		} satisfies SearchListViewAttrs)
 	}
 
