80857ce08ebd5b8d75cc3bf861b5c188ebac5709
paw <104824185+paw-hub@users.noreply.github.com>
2025-05-02 17:38:27 +0200

    Fix BulkMailLoader not resolving session keys for attachments
    Add a createOwnerEncSessionKeyProviderForAttachments to MailFacade that
    is also used for BulkMailLoader so that mail attachments are resolved
    the same way in both cases.
    
    Add tests for this function, as it did not seem to be tested in
    loadAttachments.
    
    Closes #8816

--- src/common/api/worker/facades/lazy/MailFacade.ts
+++ src/common/api/worker/facades/lazy/MailFacade.ts
@@ -94,6 +94,7 @@ import {
 	GroupInfoTypeRef,
 	GroupRootTypeRef,
 	GroupTypeRef,
+	type InstanceSessionKey,
 	User,
 	UserTypeRef,
 } from "../../../entities/sys/TypeRefs.js"
@@ -150,7 +151,7 @@ import { LoginFacade } from "../LoginFacade.js"
 import { ProgrammingError } from "../../../common/error/ProgrammingError.js"
 import { OwnerEncSessionKeyProvider } from "../../rest/EntityRestClient.js"
 import { KeyLoaderFacade, parseKeyVersion } from "../KeyLoaderFacade.js"
-import { encryptBytes, encryptKeyWithVersionedKey, encryptString, VersionedEncryptedKey, VersionedKey } from "../../crypto/CryptoWrapper.js"
+import { encryptBytes, encryptKeyWithVersionedKey, encryptString, VersionedKey } from "../../crypto/CryptoWrapper.js"
 import { PublicKeyProvider } from "../PublicKeyProvider.js"
 import { KeyVerificationMismatchError } from "../../../common/error/KeyVerificationMismatchError"
 import { EntityUpdateData, isUpdateForTypeRef } from "../../../common/utils/EntityUpdateUtils"
@@ -1033,20 +1034,7 @@ export class MailFacade {
 		const attachmentsListId = listIdPart(mail.attachments[0])
 		const attachmentElementIds = mail.attachments.map(elementIdPart)
 
-		const bucketKey = mail.bucketKey
-		let ownerEncSessionKeyProvider: OwnerEncSessionKeyProvider | undefined
-		if (bucketKey) {
-			const resolvedSessionKeys = await this.crypto.resolveWithBucketKey(mail)
-			ownerEncSessionKeyProvider = async (instanceElementId: Id): Promise<VersionedEncryptedKey> => {
-				const instanceSessionKey = assertNotNull(
-					resolvedSessionKeys.instanceSessionKeys.find((instanceSessionKey) => instanceElementId === instanceSessionKey.instanceId),
-				)
-				return {
-					key: instanceSessionKey.symEncSessionKey,
-					encryptingKeyVersion: parseKeyVersion(instanceSessionKey.symKeyVersion),
-				}
-			}
-		}
+		const ownerEncSessionKeyProvider = mail.bucketKey != null ? await this.createOwnerEncSessionKeyProviderForAttachments([mail]) : undefined
 		return await this.entityClient.loadMultiple(FileTypeRef, attachmentsListId, attachmentElementIds, ownerEncSessionKeyProvider)
 	}
 
@@ -1194,6 +1182,34 @@ export class MailFacade {
 			{ concurrency: 2 },
 		)
 		return result.flatMap((response) => response.mailIds).map((idTupleWrapper) => [idTupleWrapper.listId, idTupleWrapper.listElementId])
+	}
+
+	/**
+	 * Create an OwnerEncSessionKeyProvider that can be passed into an EntityClient for resolving files' session keys.
+	 *
+	 * This function CANNOT be called over IPC and MUST be called from the worker thread.
+	 *
+	 * @param mails an iterator of mails to resolve
+	 */
+	async createOwnerEncSessionKeyProviderForAttachments(mails: Iterable<Mail>): Promise<OwnerEncSessionKeyProvider> {
+		const sessionKeys: Map<Id, InstanceSessionKey> = new Map()
+
+		for (const mail of mails) {
+			if (mail.bucketKey != null) {
+				const resolvedSessionKeys = await this.crypto.resolveWithBucketKey(mail)
+				for (const key of resolvedSessionKeys.instanceSessionKeys) {
+					sessionKeys.set(key.instanceId, key)
+				}
+			}
+		}
+
+		return async (instanceElementId: Id) => {
+			const keyData = assertNotNull(sessionKeys.get(instanceElementId), `could not load session key for ${instanceElementId}`)
+			return {
+				key: keyData.symEncSessionKey,
+				encryptingKeyVersion: parseKeyVersion(keyData.symKeyVersion),
+			}
+		}
 	}
 }
 
