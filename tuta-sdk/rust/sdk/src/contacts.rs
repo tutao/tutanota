5fcca76a4448585f9dc5b205357fbfa899b090e9
mup <mup@tutao.de>
2025-05-02 11:23:13 +0200

    [SDK] Implements ContactFacade into SDK
    This commit adds the implementation of the ContactFacade to the SDK.
    The ContactFacade is read-only and doesn't fetch only contacts inside
    the default contact list.
    
    Co-authored-by: André Dias <and@tutao.de>

---
+++ tuta-sdk/rust/sdk/src/contacts.rs
@@ -0,0 +1,1 @@
+pub mod contact_facade;
---
+++ tuta-sdk/rust/sdk/tests/contact_facade.rs
@@ -0,0 +1,37 @@
+use std::sync::Arc;
+use tutasdk::bindings::test_file_client::TestFileClient;
+use tutasdk::contacts::contact_facade::ContactFacade;
+use tutasdk::net::native_rest_client::NativeRestClient;
+use tutasdk::Sdk;
+
+async fn create_contact_facade() -> ContactFacade {
+	const HOST: &str = "http://localhost:9000";
+
+	let rest_client = NativeRestClient::try_new().unwrap();
+	let file_client = TestFileClient::default();
+
+	let sdk = Sdk::new(
+		HOST.to_owned(),
+		Arc::new(rest_client),
+		Arc::new(file_client),
+	);
+	let session = sdk
+		.create_session("arm-free@tutanota.de", "arm")
+		.await
+		.unwrap();
+
+	session.contact_facade()
+}
+
+#[cfg_attr(
+	not(feature = "test-with-local-http-server"),
+	ignore = "require local http server."
+)]
+#[tokio::test]
+async fn load_user_contacts() {
+	let contact_facade = create_contact_facade().await;
+	let contacts = contact_facade.load_all_user_contacts().await;
+
+	assert_eq!(contacts.unwrap().iter().count(), 3);
+	log::info!("Test::Loaded user contacts correctly!");
+}
