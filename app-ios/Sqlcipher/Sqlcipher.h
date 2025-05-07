f7e561e9c53a26568a9b746b5feddbf0670a4a4c
tutao-mac <hello@tutao.de>
2025-05-07 15:02:37 +0200

    [ios] Use signal-tokenizer
    Introduce Sqlcipher.framework.
    
    Co-authored-by: paw <paw-hub@users.noreply.github.com>

---
+++ app-ios/Sqlcipher/Sqlcipher.h
@@ -0,0 +1,19 @@
+//
+//  Sqlcipher.h
+//  Sqlcipher
+//
+//  Created by Tutao GmbH on 5/13/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sqlcipher.
+FOUNDATION_EXPORT double SqlcipherVersionNumber;
+
+//! Project version string for Sqlcipher.
+FOUNDATION_EXPORT const unsigned char SqlcipherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sqlcipher/PublicHeader.h>
+
+#include <Sqlcipher/sqlite3.h>
+#include <Sqlcipher/signal_tokenizer.h>
