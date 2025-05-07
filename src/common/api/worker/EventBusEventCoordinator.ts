0b91e7def665d80f33230064c0fbea8d19013b01
vis <vis@tutao.de>
2025-05-07 17:53:20 +0200

    Create user group identity keys for existing users
    We add an event type to EventBusListener to be able to perform the migration in the worker once the synchronization is done.
    
    We send error any to the main thread so the user can reporet them, but we do not stop processing the event queue.
    
    tuta#2079

--- src/common/api/worker/EventBusEventCoordinator.ts
+++ src/common/api/worker/EventBusEventCoordinator.ts
@@ -7,7 +7,7 @@ import { isAdminClient, isTest } from "../common/Env.js"
 import { MailFacade } from "./facades/lazy/MailFacade.js"
 import { UserFacade } from "./facades/UserFacade.js"
 import { EntityClient } from "../common/EntityClient.js"
-import { AccountType, OperationType } from "../common/TutanotaConstants.js"
+import { AccountType, OperationType, RolloutType } from "../common/TutanotaConstants.js"
 import { lazyAsync } from "@tutao/tutanota-utils"
 import { isSameId } from "../common/utils/EntityUtils.js"
 import { ExposedEventController } from "../main/EventController.js"
@@ -15,6 +15,9 @@ import { ConfigurationDatabase } from "./facades/lazy/ConfigurationDatabase.js"
 import { KeyRotationFacade } from "./facades/KeyRotationFacade.js"
 import { CacheManagementFacade } from "./facades/lazy/CacheManagementFacade.js"
 import { EntityUpdateData, isUpdateForTypeRef } from "../common/utils/EntityUpdateUtils"
+import { RolloutFacade } from "./facades/RolloutFacade"
+import { GroupManagementFacade } from "./facades/lazy/GroupManagementFacade"
+import { SyncTracker } from "../main/SyncTracker"
 
 /** A bit of glue to distribute event bus events across the app. */
 export class EventBusEventCoordinator implements EventBusListener {
@@ -29,6 +32,9 @@ export class EventBusEventCoordinator implements EventBusListener {
 		private readonly cacheManagementFacade: lazyAsync<CacheManagementFacade>,
 		private readonly sendError: (error: Error) => Promise<void>,
 		private readonly appSpecificBatchHandling: (events: readonly EntityUpdateData[], batchId: Id, groupId: Id) => void,
+		private readonly rolloutFacade: RolloutFacade,
+		private readonly groupManagementFacade: lazyAsync<GroupManagementFacade>,
+		private readonly syncTracker: SyncTracker,
 	) {}
 
 	onWebsocketStateChanged(state: WsConnectionState) {
@@ -73,6 +79,23 @@ export class EventBusEventCoordinator implements EventBusListener {
 
 	onCounterChanged(counter: WebsocketCounterData) {
 		this.eventController.onCountersUpdateReceived(counter)
+	}
+
+	async onSyncDone(): Promise<void> {
+		this.syncTracker.markSyncAsDone()
+
+		if (this.userFacade.isLeader()) {
+			await this.rolloutFacade.processRollout(RolloutType.UserIdentityKeyCreation, async () => {
+				const userGroupId = this.userFacade.getUserGroupId()
+				const gmf = await this.groupManagementFacade()
+				try {
+					await gmf.createIdentityKeyPair(userGroupId)
+				} catch (error) {
+					console.log("error when creating user identity key pair", error)
+					this.sendError(error)
+				}
+			})
+		}
 	}
 
 	private async entityEventsReceived(data: readonly EntityUpdateData[]): Promise<void> {
