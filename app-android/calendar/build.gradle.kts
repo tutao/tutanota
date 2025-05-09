39b32ebf3baf990bed2498e78c2e35b65187a00c
map <mpfau@users.noreply.github.com>
2025-05-09 22:10:23 +0200

    v284.250509.1

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 123
-		versionName = "284.250509.0"
+		versionCode = 124
+		versionName = "284.250509.1"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
