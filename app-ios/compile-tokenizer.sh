3823b043a63e634bac734baa8d3aad8dbcdc1b8e
tutao <hello@tutao.de>
2025-05-09 18:15:57 +0200

    WIP compile tokenizer, lipo and link

--- app-ios/compile-tokenizer.sh
+++ app-ios/compile-tokenizer.sh
@@ -1,16 +1,45 @@
+# Add custom stage with this script, $SRCROOT/../libs/Signal-FTS5-Extension/libsignal_tokenizer.a as an output
+# Add the output file to the project, link against it
+# Modify library search path to include its directory
+# ...
+# PROFIT!
+
 SCRIPTS=$SRCROOT/../tuta-sdk/ios/build-scripts
-echo "SCRIPT=${SCRIPTS}"
+EXTENSION_DIR=$SRCROOT/../libs/Signal-FTS5-Extension
 
-echo "Temp dir exists: $(stat $PROJECT_TEMP_DIR)"
-export CARGO_TARGET_DIR="${PROJECT_TEMP_DIR}/cargo_target"
-echo "CARGO_TARGET_DIR=$CARGO_TARGET_DIR"
-mkdir -p "${CARGO_TARGET_DIR}"
+# export CARGO_TARGET_DIR="${PROJECT_TEMP_DIR}/cargo_target"
+#mkdir -p "${CARGO_TARGET_DIR}"
 
-bash $SCRIPTS/xc-universal-binary.sh signal-tokenizer $SRCROOT/../libs/Signal-FTS5-Extension "$CONFIGURATION" '--crate-type=staticlib'
-# FIXME: lipo
-for triple in $(bash "${SCRIPTS}/rust-triple.sh"); do
-	cp "$CARGO_TARGET_DIR/$triple/$CONFIGURATION/libsignal_tokenizer.a" "${TARGET_BUILD_DIR}/libsignal_tokenizer.a"
-	echo "Copying files: $CARGO_TARGET_DIR/$triple/$CONFIGURATION/libsignal_tokenizer.a ${TARGET_BUILD_DIR}/libsignal_tokenizer.a"
+IS_SIMULATOR=0
+if [ "${LLVM_TARGET_TRIPLE_SUFFIX-}" = "-simulator" ]; then
+  IS_SIMULATOR=1
+fi
+
+RELFLAG=debug
+if [[ "${BUILDVARIANT}" != debug* ]]; then
+    RELFLAG=release
+fi
+echo "RELFLAG: ${RELFLAG}"
+
+
+TRIPLES=$(bash "${SCRIPTS}/rust-triple.sh" $IS_SIMULATOR ${ARCHS[@]})
+LIPO_ARGS=""
+for triple in $TRIPLES; do
+	bash $SCRIPTS/xc-universal-binary.sh signal-tokenizer $EXTENSION_DIR "$CONFIGURATION" '--crate-type=staticlib' '--features=extension'
+	#cp "$CARGO_TARGET_DIR/$triple/$CONFIGURATION/libsignal_tokenizer.a" "${DERIVED_FILE_DIR}/libsignal_tokenizer.a"
+	#echo "Copying files: $CARGO_TARGET_DIR/$triple/$CONFIGURATION/libsignal_tokenizer.a ${DERIVED_FILE_DIR}/libsignal_tokenizer.a"
+	
+	# note the newline
+	LIPO_ARGS+="$EXTENSION_DIR/target/$triple/$RELFLAG/libsignal_tokenizer.a
+"
 done
+
+echo "LIPO_ARGS: $LIPO_ARGS"
+tr '\n' '\0' <<<"$LIPO_ARGS" | xargs -0 echo lipo -output "$SCRIPT_OUTPUT_FILE_0" -create
+tr '\n' '\0' <<<"$LIPO_ARGS" | xargs -0 lipo -output "$SCRIPT_OUTPUT_FILE_0" -create
+
+echo "lipo-ed"
+
+
 
 
--- tuta-sdk/ios/build-scripts/xc-universal-binary.sh
+++ tuta-sdk/ios/build-scripts/xc-universal-binary.sh
@@ -26,7 +26,7 @@ echo SRC_ROOT=$SRC_ROOT
 # buildvariant from our xcconfigs
 BUILDVARIANT=$(echo "${3}" | tr '[:upper:]' '[:lower:]')
 
-RUSTC_ARGS=${4}
+RUSTC_ARGS=${@:4}
 
 RELFLAG=
 if [[ "${BUILDVARIANT}" != debug* ]]; then
@@ -37,30 +37,6 @@ IS_SIMULATOR=0
 if [ "${LLVM_TARGET_TRIPLE_SUFFIX-}" = "-simulator" ]; then
   IS_SIMULATOR=1
 fi
-
-# for arch in $ARCHS; do
-#   case "$arch" in
-#     x86_64)
-#       if [ $IS_SIMULATOR -eq 0 ]; then
-#         echo "Building for x86_64, but not a simulator build. What's going on?" >&2
-#         exit 2
-#       fi
-
-#       # Intel iOS simulator
-#       export CFLAGS_x86_64_apple_ios="-target x86_64-apple-ios"
-#       cargo rustc --manifest-path="${SRC_ROOT}/Cargo.toml" -p "${FFI_TARGET}" --lib $RELFLAG --target x86_64-apple-ios -v
-#       ;;
-
-#     arm64)
-#       if [ $IS_SIMULATOR -eq 0 ]; then
-#         # Hardware iOS targets
-#         cargo rustc --manifest-path="${SRC_ROOT}/Cargo.toml" -p "${FFI_TARGET}" --lib $RELFLAG --target aarch64-apple-ios -v
-#       else
-#         # M1 iOS simulator
-#         cargo rustc --manifest-path="${SRC_ROOT}/Cargo.toml" -p "${FFI_TARGET}" --lib $RELFLAG --target aarch64-apple-ios-sim -v
-#       fi
-#   esac
-# done
 
 
 SCRIPT_DIR=$(dirname "$0")
