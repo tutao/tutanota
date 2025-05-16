b3f186f29fd3caeeb65df760df4801cc0b3a642d
yoy <yoy@tutao.de>
2025-05-16 12:15:16 +0200

    wip: surface modification

--- src/common/gui/theme.ts
+++ src/common/gui/theme.ts
@@ -36,7 +36,6 @@ export type Theme = {
 	surface: string
 	surface_dim: string
 	surface_container: string
-	surface_container_low: string
 	surface_container_high: string
 	on_surface_fade: string
 	on_surface: string
