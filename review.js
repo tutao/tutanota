e7aec7697f934c90bdaa1deb3359a2c96ad88f0b
ivk <ivk@tutao.de>
2025-06-27 12:59:44 +0200

    improve review script

--- review.js
+++ review.js
@@ -7,36 +7,42 @@ const rl = readline.createInterface(process.stdin, process.stdout)
 const deps = data[undefined]
 
 const reviewedPath = "reviewed.json"
-const reviewed = new Set()
+/** @type {Record<string, {who: string, when: string}>} */
+let reviewedData = {}
 try {
-	const reviewedData = JSON.parse(await fs.readFile(reviewedPath, { encoding: "utf8" }))
-	for (const item of reviewedData) {
-		reviewed.add(item)
-	}
+	reviewedData = JSON.parse(await fs.readFile(reviewedPath, { encoding: "utf8" }))
 } catch (e) {
 	console.log("Could not read reviewed data")
 }
 
+let reviewers = await rl.question("who is reviewing: ")
+
 async function markAsReviewed(currentDep) {
-	reviewed.add(currentDep)
-	const reviewedArray = Array.from(reviewed)
-	await fs.writeFile(reviewedPath, JSON.stringify(reviewedArray, null, 4), { encoding: "utf8" })
+	reviewedData[currentDep] = {
+		who: reviewers,
+		when: new Date().toISOString().slice(0, 10),
+	}
+	await fs.writeFile(reviewedPath, JSON.stringify(reviewedData, null, 4), { encoding: "utf8" })
 }
 
 async function review(currentDep, itsDeps) {
 	while (true) {
-		console.log(`Reviewing ${currentDep}`)
+		console.log(`\nReviewing ${currentDep}`)
 		const depsArray = Object.entries(itsDeps)
 		for (const [i, [key, value]] of depsArray.entries()) {
-			const mark = key.startsWith("node:") || reviewed.has(key) ? "✓" : ""
-			console.log(`${i}: ${mark} ${key}`)
+			const mark = key.startsWith("node:") || Object.hasOwn(reviewedData, key) ? "✅" : " "
+			const stats = calculateStats(key, value)
+			console.log(`${i}: ${mark} ${String(stats.reviewed).padStart(4)}/${String(stats.overall).padStart(4)} ${key}`)
 		}
 		console.log("d: mark as reviewed")
+		console.log("x: go up")
 		const answer = await rl.question("What to review?: ")
 		console.log(`(answered: ${answer})`)
 		if (answer === "d") {
 			console.log(`Marking ${currentDep} as reviewed`)
 			await markAsReviewed(currentDep)
+			break
+		} else if (answer === "x") {
 			break
 		}
 		const numAnswer = parseInt(answer)
@@ -46,6 +52,38 @@ async function review(currentDep, itsDeps) {
 			await review(dep, itsDeps)
 		}
 	}
+}
+
+const transitiveDepsCache = new Map()
+
+function transitiveDeps(currentDep, itsDeps) {
+	const cached = transitiveDepsCache.get(currentDep)
+	if (cached) {
+		return cached
+	}
+	const collectedDeps = Object.entries(itsDeps)
+		.map(([dep, depDeps]) => {
+			if (typeof depDeps === "string") {
+				return []
+			}
+			return transitiveDeps(dep, depDeps)
+		})
+		.flat()
+	const result = [currentDep, ...collectedDeps]
+	transitiveDepsCache.set(currentDep, result)
+	return result
+}
+
+function calculateStats(currentDep, itsDeps) {
+	const collectedTransitiveDeps = transitiveDeps(currentDep, itsDeps)
+	const dedupedDeps = new Set(collectedTransitiveDeps)
+	let reviewed = 0
+	for (const dep of dedupedDeps) {
+		if (Object.hasOwn(reviewedData, dep)) {
+			reviewed += 1
+		}
+	}
+	return { reviewed, overall: dedupedDeps.size }
 }
 
 await review("entry", deps)
