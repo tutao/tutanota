29af134bd4a89517c62f4dc96d54f10f4f8d188a
ivk <ivk@tutao.de>
2025-06-27 20:32:35 +0200

    WIP disable some eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,84 @@
 import typescriptEslint from "typescript-eslint"
 import unicorn from "eslint-plugin-unicorn"
 import globals from "globals"
-import js from "@eslint/js"
 import { defineConfig, globalIgnores } from "eslint/config"
 import eslintPluginPrettierRecommended from "eslint-config-prettier/flat"
 
 export default defineConfig([
-	js.configs.recommended,
-	...typescriptEslint.configs.recommended,
-	// this one just disables some rules so it makes sense to put it after
-	eslintPluginPrettierRecommended,
+	// js.configs.recommended,
+	{
+		rules: {
+			"for-direction": "error",
+			"no-async-promise-executor": "error",
+			"no-compare-neg-zero": "warn",
+			"no-cond-assign": "error",
+			"no-constant-binary-expression": "warn",
+			"no-debugger": "error",
+			"no-dupe-else-if": "warn",
+			"no-duplicate-case": "error",
+			"no-empty-character-class": "warn",
+			"no-empty-pattern": "warn",
+			"no-ex-assign": "warn",
+			"no-fallthrough": "error",
+			"no-invalid-regexp": "error",
+			"no-irregular-whitespace": "error",
+			"no-loss-of-precision": "error",
+			"no-misleading-character-class": "warn",
+			"no-prototype-builtins": "error",
+			"no-self-assign": "error",
+			// TODO: would be cool to enable
+			// "no-self-compare": "error",
+			"no-setter-return": "error",
+			"no-sparse-arrays": "error",
+			"no-unexpected-multiline": "error",
+			"no-unreachable": "error",
+			"no-unsafe-finally": "error",
+			"no-unsafe-negation": "error",
+			"no-unused-private-class-members": "warn",
+			"no-useless-backreference": "warn",
+			"use-isnan": "error",
+			"valid-typeof": "error",
+			// TODO: would be cool to enable
+			// eqeqeq: ["error", "always", { null: "ignore" }],
+			"no-case-declarations": "error",
+			"no-delete-var": "error",
+			"no-empty": "warn",
+			"no-empty-static-block": "error",
+			"no-eval": "error",
+			"no-global-assign": "error",
+			"no-implied-eval": "error",
+			"no-nonoctal-decimal-escape": "error",
+			"no-octal": "error",
+			"no-octal-escape": "error",
+			"no-proto": "error",
+			"no-regex-spaces": "error",
+			"no-shadow-restricted-names": "error",
+			"no-unused-labels": "warn",
+			"no-useless-catch": "warn",
+			"no-useless-escape": "error",
+			"no-var": "error",
+			// TODO: would be cool to enable
+			// "no-void": "error",
+			"no-with": "error",
+			"require-yield": "error",
+		},
+	},
+	// FIXME
+	// ...typescriptEslint.configs.recommended,
+	// // this one just disables some rules so it makes sense to put it after
+	// eslintPluginPrettierRecommended,
 	{
 		rules: {
 			"no-control-regex": 0,
 			"@typescript-eslint/no-non-null-asserted-optional-chain": 0,
 			"@typescript-eslint/no-this-alias": 0,
-			"no-async-promise-executor": 2,
-			"no-empty-pattern": 0,
-			"no-inner-declarations": 0,
-			"no-irregular-whitespace": 0,
-			"no-constant-condition": 0,
+			// "no-empty-pattern": 0,
 			"prefer-rest-params": 2,
 			"prefer-spread": 0,
-			"no-prototype-builtins": 2,
-			"no-var": 2,
-			// does not take into account declared globals, not useful with ts
-			"no-undef": 0,
-			"no-fallthrough": 2,
-			"no-case-declarations": 2,
-			// enable it separately
+			// FIXME: where it comes from?
 			"prefer-const": 0,
+			// does not take into account declared globals, not useful with ts
+			// enable it separately
 			"@typescript-eslint/no-empty-function": 0,
 			"@typescript-eslint/no-non-null-assertion": 0,
 			"@typescript-eslint/ban-ts-comment": 0,
