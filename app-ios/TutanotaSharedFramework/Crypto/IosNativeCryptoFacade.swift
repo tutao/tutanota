e9e19aa8e6b25f688886fe4e724e025c38193dc7
tutao-mac <hello@tutao.de>
2025-07-02 11:48:25 +0200

    ed25519 native implementation for ios

--- app-ios/TutanotaSharedFramework/Crypto/IosNativeCryptoFacade.swift
+++ app-ios/TutanotaSharedFramework/Crypto/IosNativeCryptoFacade.swift
@@ -82,20 +82,20 @@ public actor IosNativeCryptoFacade: NativeCryptoFacade {
 	}
   
   
-  
-  public func generateEd25519Keypair() async throws -> IPCEd25519KeyPair {
-	throw CryptoError(message: "not implemented")
-  }
 
-  public func ed25519Sign(_ privateKey: IPCEd25519PrivateKey, _ data: DataWrapper) async throws -> IPCEd25519Signature {
-	throw CryptoError(message: "not implemented")
-  }		
+	public func generateEd25519Keypair() async throws -> IPCEd25519KeyPair {
+		let keyPair = tutasdk.ed25519GenerateKeyPair()
+		return IPCEd25519KeyPair(publicKey: IPCEd25519PublicKey(raw: DataWrapper(data: keyPair.publicKey)), privateKey: IPCEd25519PrivateKey(raw: DataWrapper(data: keyPair.privateKey)))
+	}
 
-  public func ed25519Verify(_ publicKey: IPCEd25519PublicKey, _ data: DataWrapper, _ signature: IPCEd25519Signature) async throws -> Bool {
-	throw CryptoError(message: "not implemented")
-  }
+	public func ed25519Sign(_ privateKey: IPCEd25519PrivateKey, _ data: DataWrapper) async throws -> IPCEd25519Signature {
+		let signature = tutasdk.ed25519Sign(privateKey: privateKey.raw.data, message: data.data)
+		return IPCEd25519Signature(signature: DataWrapper(data: signature))
+	}
 
-  
+	public func ed25519Verify(_ publicKey: IPCEd25519PublicKey, _ data: DataWrapper, _ signature: IPCEd25519Signature) async throws -> Bool {
+		return tutasdk.ed25519Verify(publicKey: publicKey.raw.data, message: data.data, signature: signature.signature.data)
+	}
   
 }
 
