95ef007fdcccba01915aa715d9cb3e73b3641d55
paw <104824185+paw-hub@users.noreply.github.com>
2025-07-11 16:51:07 +0200

    Fix DesktopNotifier tests

--- test/tests/desktop/notifications/DesktopNotifierTest.ts
+++ test/tests/desktop/notifications/DesktopNotifierTest.ts
@@ -1,6 +1,6 @@
 import o from "@tutao/otest"
 import type { NotificationFactory } from "../../../../src/common/desktop/notifications/NotificationFactory.js"
-import { defer, delay, downcast, LazyLoaded } from "@tutao/tutanota-utils"
+import { defer, DeferredObject, delay, downcast, LazyLoaded } from "@tutao/tutanota-utils"
 import { DesktopNotifier } from "../../../../src/common/desktop/notifications/DesktopNotifier.js"
 import type { DesktopTray } from "../../../../src/common/desktop/tray/DesktopTray.js"
 import type { NativeImage } from "electron"
@@ -16,9 +16,10 @@ o.spec("DesktopNotifier", function () {
 	let desktopTray: DesktopTray
 	let notificationFactory: NotificationFactory
 	let notificationFactoryLazy: LazyLoaded<NotificationFactory>
-	let notificationMade = defer<void>()
+	let notificationMade: DeferredObject<void>
 	o.beforeEach(function () {
 		createdNotifications = []
+		notificationMade = defer<void>()
 		desktopTray = downcast({
 			getAppIcon: () => appIcon,
 			update: spy(() => {}),
@@ -38,7 +39,7 @@ o.spec("DesktopNotifier", function () {
 		})
 		notificationFactoryLazy = new LazyLoaded(() => Promise.resolve(notificationFactory))
 	})
-	o("show no notifications before call to start()", async function () {
+	o.test("show no notifications before call to start()", async function () {
 		const notifier = new DesktopNotifier(desktopTray, notificationFactoryLazy)
 		notifier.showOneShot({
 			title: "Title1",
@@ -46,20 +47,21 @@ o.spec("DesktopNotifier", function () {
 			icon: icon1,
 		})
 		notifier.submitGroupedNotification("Title2", "Message", "gn1", () => {})
-		o(notificationFactory.makeNotification.calls).deepEquals([])
+		o.check(notificationFactory.makeNotification.calls).deepEquals([])
 		notifier.start(notificationStartDelay)
 		setImmediate(() => {
-			o(notificationFactory.makeNotification.calls).deepEquals([])
+			o.check(notificationFactory.makeNotification.calls).deepEquals([])
 		})
 		await delay(notificationStartDelay * 3)
-		o(notificationFactory.makeNotification.calls[0][0]).deepEquals({
+		o.check(notificationFactory.makeNotification.calls[0][0]).deepEquals({
 			title: "Title1",
+			group: "oneshot",
 			body: "Body1",
 			icon: icon1,
 		})
-		o(desktopTray.update.callCount).equals(1)
+		o.check(desktopTray.update.callCount).equals(1)
 	})
-	o("show no notifications when no notifications available", async function () {
+	o.test("show no notifications when no notifications available", async function () {
 		downcast(notificationFactory).isSupported = () => false
 
 		const notifier = new DesktopNotifier(desktopTray, notificationFactoryLazy)
@@ -74,9 +76,9 @@ o.spec("DesktopNotifier", function () {
 		notifier.submitGroupedNotification("Title2", "Message", "gn1", () => {})
 		notifier.start(notificationStartDelay)
 		await delay(notificationStartDelay * 1.1)
-		o(notificationFactory.makeNotification.callCount).equals(0)
+		o.check(notificationFactory.makeNotification.callCount).equals(0)
 	})
-	o("grouped notifications replace each other", async function () {
+	o.test("grouped notifications replace each other", async function () {
 		const notifier = new DesktopNotifier(desktopTray, notificationFactoryLazy)
 		notifier.start(notificationStartDelay)
 		await delay(notificationStartDelay * 1.1)
@@ -89,33 +91,33 @@ o.spec("DesktopNotifier", function () {
 		notificationMade = defer()
 		notifier.submitGroupedNotification("Title3", "Message3", "gn2", () => {})
 		await notificationMade.promise
-		o(createdNotifications.length).equals(3)
-		o(createdNotifications[0].close.callCount).equals(1)
-		o(createdNotifications[1].close.callCount).equals(0)
-		o(createdNotifications[1].close.callCount).equals(0)
-		o(desktopTray.update.callCount).equals(3)
-		o(desktopTray.setBadge.callCount).equals(3)
-		o(notifier.hasNotificationForId("gn1")).equals(true)
-		o(notifier.hasNotificationForId("gn2")).equals(true)
+		o.check(createdNotifications.length).equals(3)
+		o.check(createdNotifications[0].close.callCount).equals(1)
+		o.check(createdNotifications[1].close.callCount).equals(0)
+		o.check(createdNotifications[1].close.callCount).equals(0)
+		o.check(desktopTray.update.callCount).equals(3)
+		o.check(desktopTray.setBadge.callCount).equals(3)
+		o.check(notifier.hasNotificationForId("gn1")).equals(true)
+		o.check(notifier.hasNotificationForId("gn2")).equals(true)
 	})
-	o("grouped notification disappear after clicking", async function () {
+	o.test("grouped notification disappear after clicking", async function () {
 		const notifier = new DesktopNotifier(desktopTray, notificationFactoryLazy)
 		notifier.start(notificationStartDelay)
 		const clickHandler = spy(() => notifier.resolveGroupedNotification("gn1"))
 		notifier.submitGroupedNotification("Title1", "Message1", "gn1", clickHandler)
 		// not shown yet
-		o(createdNotifications.length).equals(0)
-		o(desktopTray.update.callCount).equals(0)
-		o(notifier.hasNotificationForId("gn1")).equals(false)
-		o(clickHandler.callCount).equals(0)
+		o.check(createdNotifications.length).equals(0)
+		o.check(desktopTray.update.callCount).equals(0)
+		o.check(notifier.hasNotificationForId("gn1")).equals(false)
+		o.check(clickHandler.callCount).equals(0)
 		await delay(notificationStartDelay * 2)
 		createdNotifications[0].click()
 		// shown and removed
-		o(clickHandler.callCount).equals(1)
-		o(createdNotifications.length).equals(1)
-		o(createdNotifications[0].close.callCount).equals(1)
-		o(desktopTray.update.callCount).equals(2)
-		o(desktopTray.setBadge.callCount).equals(1)
-		o(notifier.hasNotificationForId("gn1")).equals(false)
+		o.check(clickHandler.callCount).equals(1)
+		o.check(createdNotifications.length).equals(1)
+		o.check(createdNotifications[0].close.callCount).equals(1)
+		o.check(desktopTray.update.callCount).equals(2)
+		o.check(desktopTray.setBadge.callCount).equals(1)
+		o.check(notifier.hasNotificationForId("gn1")).equals(false)
 	})
 })
