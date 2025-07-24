6d400f4fbbf1d71ceb69d1aa294765e7f63c94f0
paw <104824185+paw-hub@users.noreply.github.com>
2025-07-24 19:39:13 +0200

    Temporarily close the dns checker when loading new results
    domainStatus is not valid for renderCheckResult once we reload it.
    
    Closes #6818

--- src/mail-app/settings/CheckDomainDnsStatusDialog.ts
+++ src/mail-app/settings/CheckDomainDnsStatusDialog.ts
@@ -1,9 +1,8 @@
-import m from "mithril"
-import { lang } from "../../common/misc/LanguageViewModel"
 import { Dialog, DialogType } from "../../common/gui/base/Dialog"
 import { DomainDnsStatus } from "./DomainDnsStatus"
 import { renderCheckResult } from "./emaildomain/VerifyDnsRecordsPage"
 import { assertMainOrNode } from "../../common/api/common/Env"
+import { showProgressDialog } from "../../common/gui/dialogs/ProgressDialog"
 
 assertMainOrNode()
 
@@ -11,20 +10,22 @@ assertMainOrNode()
  * @pre domainStatus.status.isLoaded() == true
  */
 export function showDnsCheckDialog(domainStatus: DomainDnsStatus) {
-	let dialog = Dialog.showActionDialog({
+	const dialog = Dialog.showActionDialog({
 		type: DialogType.EditLarger,
 		title: "checkDnsRecords_action",
 		okActionTextId: "checkAgain_action",
 		cancelActionTextId: "close_alt",
 		child: () => renderCheckResult(domainStatus, true),
-		okAction: () => {
-			domainStatus.loadCurrentStatus().then(() => {
-				if (domainStatus.areRecordsFine()) {
-					dialog.close()
-				} else {
-					m.redraw()
-				}
-			})
+		okAction: async () => {
+			// Close the dialog, since once loadCurrentStatus is called, domainStatus will be invalidated until it's
+			// finished loading
+			dialog.close()
+
+			await showProgressDialog("loadingDNSRecords_msg", domainStatus.loadCurrentStatus())
+
+			if (!domainStatus.areRecordsFine()) {
+				dialog.show()
+			}
 		},
 	})
 }
