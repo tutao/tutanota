fb0e3feb4beae198ea4ac878f9303a1c3ce7cd00
toj <toj@tutao.de>
2025-07-30 16:24:16 +0200

    Add BotD Library and implement clientside webdriver detection.
    Co-authored-by: yoy <yoy@tutao.de>
    Co-authored-by: nig <nig@tutao.de>
    Co-authored-by: toj <toj@tutao.de>

--- test/tests/subscription/CaptchaTest.ts
+++ test/tests/subscription/CaptchaTest.ts
@@ -6,17 +6,13 @@ o.spec("SolvePowChallenge", () => {
 	o(
 		"solve valid challenge",
 		browser(async () => {
-			const captchaGetOut: TimelockCaptchaGetOut = createTimelockCaptchaGetOut({ base: "2", difficulty: "2", modulus: "2" })
+			const captchaGetOut: TimelockCaptchaGetOut = createTimelockCaptchaGetOut({
+				base: "2",
+				difficulty: "2",
+				modulus: "2",
+			})
 			const solution = await solvePowChallengeInWorker(captchaGetOut)
 			o(solution).equals(0n)
-		}),
-	)
-
-	o(
-		"solve invalid challenge with modulo 0",
-		browser(async () => {
-			const captchaGetOut: TimelockCaptchaGetOut = createTimelockCaptchaGetOut({ base: "2", difficulty: "2", modulus: "0" })
-			await o(() => solvePowChallengeInWorker(captchaGetOut)).asyncThrows("Uncaught RangeError: Division by zero")
 		}),
 	)
 })
