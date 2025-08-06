9dd9e44eec867de5025116c941d3ba9db7b9c594
vaf <vaf@tutao.de>
2025-08-06 16:39:54 +0200

    Do not always recompile the crypto-primitives wasm
    Only recompile and optimize in case of clean build, i.e. when the folder does not exist yet.
    
    tuta#2238

--- packages/tutanota-crypto/make.js
+++ packages/tutanota-crypto/make.js
@@ -1,6 +1,7 @@
 import { program } from "commander"
 import { CRYPTO_PRIMITIVES_CRATE, WASM_PACK_OUT_DIR } from "../../buildSrc/cryptoPrimitivesUtils.js"
 import { $, usePowerShell } from "zx"
+import fs from "fs-extra"
 
 await program.usage("").action(run).parseAsync(process.argv)
 
@@ -8,5 +9,9 @@ async function run() {
 	if (process.platform === "win32") {
 		usePowerShell()
 	}
-	await $`npx wasm-pack build --target web ../../${CRYPTO_PRIMITIVES_CRATE} --out-dir ../../../${WASM_PACK_OUT_DIR}`
+	// we only want to build this again in case it does not exist yet.
+	// use node make clean to force rebuilding
+	if (!fs.existsSync(`../../${WASM_PACK_OUT_DIR}`)) {
+		await $`npx wasm-pack build --target web ../../${CRYPTO_PRIMITIVES_CRATE} --out-dir ../../../${WASM_PACK_OUT_DIR}`
+	}
 }
