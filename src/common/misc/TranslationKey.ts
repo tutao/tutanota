ebc6d46def8ae0fc6d70a492b4e4311d9c2f77f1
mup <mup@tutao.de>
2025-08-08 14:45:14 +0200

    Replaces the guest status icon by characters without circle

--- src/common/misc/TranslationKey.ts
+++ src/common/misc/TranslationKey.ts
@@ -2090,5 +2090,5 @@ export type TranslationKeyType =
 	| "duplicateEvent_label"
 	| "addComment_label"
 	| "copyOf_title"
-	| "declinedInviteSubject_msg"
-	| "acceptedInviteSubject_msg"
+	| "declined_label"
+	| "accepted_label"
