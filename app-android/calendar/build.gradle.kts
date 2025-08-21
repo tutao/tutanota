2099ddd3009da7de3171ab9d0381e23224ba1e5c
vis <vis@tutao.de>
2025-08-21 12:16:52 +0200

    v303.250821.0

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 173
-		versionName = "303.250820.1"
+		versionCode = 174
+		versionName = "303.250821.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
