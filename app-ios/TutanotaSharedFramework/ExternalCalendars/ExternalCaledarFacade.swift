5a2836868a8d3cfb8e06554227870e69a20babbf
mup <mup@tutao.de>
2025-09-03 12:57:52 +0200

    Fixes External Calendar synchronization
    Some providers require that we provide a valid User-Agent to
    be able to fetch ics content. This commit injects the current
    User-Agent from the WebView in the fetch request to solve this
    problem.

--- app-ios/TutanotaSharedFramework/ExternalCalendars/ExternalCaledarFacade.swift
+++ app-ios/TutanotaSharedFramework/ExternalCalendars/ExternalCaledarFacade.swift
@@ -2,7 +2,12 @@ import Foundation
 
 public class ExternalCalendarFacadeImpl: ExternalCalendarFacade {
 	private let urlSession: URLSession
-	public init(urlSession: URLSession) { self.urlSession = urlSession }
+	private let userAgent: String
+
+	public init(urlSession: URLSession, userAgent: String) {
+		self.urlSession = urlSession
+		self.userAgent = userAgent
+	}
 
 	public func fetchExternalCalendar(_ url: String) async throws -> String {
 		enum FetchExternalError: Error {
@@ -11,8 +16,11 @@ public class ExternalCalendarFacadeImpl: ExternalCalendarFacade {
 		}
 
 		guard let sourceUrl = URL(string: url) else { throw FetchExternalError.URLError("Error while creating the URL") }
+		var request = URLRequest(url: sourceUrl)
+		request.httpMethod = "GET"
+		request.setValue(self.userAgent, forHTTPHeaderField: "User-Agent")
 
-		let (data, _) = try await self.urlSession.data(from: sourceUrl)
+		let (data, _) = try await self.urlSession.data(for: request)
 		let str = String(data: data, encoding: .utf8) ?? ""
 
 		return str
