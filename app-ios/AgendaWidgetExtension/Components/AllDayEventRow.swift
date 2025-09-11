e2a7b70de44d49243265289c98917370966977fd
and <and@tutao.de>
2025-09-11 12:32:00 +0200

    [iOS] Adjusts spacing and style to match last revision
    Also extract dimensions to its own struct.
    
    Co-authored-by: Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>

--- app-ios/AgendaWidgetExtension/Components/AllDayEventRow.swift
+++ app-ios/AgendaWidgetExtension/Components/AllDayEventRow.swift
@@ -14,7 +14,9 @@ struct AllDayEventRow: View {
 	var textColor: Color?
 	var body: some View {
 		let (allDayImage, allDayPadding): (ImageResource, CGFloat) =
-			if let firstEvent = allDayEventsData.event, firstEvent.isBirthdayEvent { (.giftIcon, 4) } else { (.allDayIcon, 2) }
+			if let firstEvent = allDayEventsData.event, firstEvent.isBirthdayEvent { (.giftIcon, Dimensions.Spacing.XS) } else {
+				(.allDayIcon, Dimensions.Spacing.XS / 2)
+			}
 		let eventTitle: String =
 			if let title: String = allDayEventsData.event?.summary, !title.isEmpty { allDayEventsData.event!.summary } else {
 				translate("TutaoNoTitleLabel", default: "<No Title>")
@@ -22,9 +24,9 @@ struct AllDayEventRow: View {
 		let backgroundColor: UIColor = UIColor(hex: allDayEventsData.event?.calendarColor ?? DEFAULT_CALENDAR_COLOR) ?? UIColor(.primary)
 		let foregroundColor: Color = if backgroundColor.getLuminance() > 0.5 { .black } else { .white }
 
-		return HStack(alignment: .center, spacing: 4) {
+		return HStack(alignment: .center, spacing: Dimensions.Spacing.XS) {
 			Image(allDayImage).foregroundStyle(foregroundColor).font(.system(size: 14)).padding(allDayPadding).background(Color(backgroundColor.cgColor))
-				.clipShape(.rect(cornerRadii: .init(topLeading: 12, bottomLeading: 12, bottomTrailing: 12, topTrailing: 12)))
+				.clipShape(.rect(cornerRadius: 12))
 			Text(eventTitle).lineLimit(1).font(.system(size: 12)).if(textColor != nil) { $0.foregroundStyle(textColor!) }
 
 			if allDayEventsData.count > 1 {
