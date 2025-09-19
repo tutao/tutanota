5a29f6e0088d6d41e72469740dd0c5a011c6d278
yoy <yoy@tutao.de>
2025-09-19 11:56:58 +0200

    Move font tokens to font_size object

--- src/mail-app/templates/view/TemplateSearchBar.ts
+++ src/mail-app/templates/view/TemplateSearchBar.ts
@@ -1,7 +1,7 @@
 import m, { Children, ClassComponent, Vnode } from "mithril"
 import type { MaybeTranslation } from "../../../common/misc/LanguageViewModel"
 import { lang } from "../../../common/misc/LanguageViewModel"
-import { inputLineHeight, px } from "../../../common/gui/size"
+import { font_size, px } from "../../../common/gui/size"
 import { keyboardEventToKeyPress, keyHandler } from "../../../common/misc/KeyManager"
 import { theme } from "../../../common/gui/theme"
 import { assertNotNull } from "@tutao/tutanota-utils"
@@ -48,7 +48,7 @@ export class TemplateSearchBar implements ClassComponent<TemplateSearchBarAttrs>
 				a.oninput?.(domInput.value, domInput)
 			},
 			style: {
-				lineHeight: px(inputLineHeight),
+				lineHeight: px(font_size.line_height_input),
 			},
 		})
 	}
