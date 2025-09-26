f08ed981934f19f4b88810a346109d4f99fd9d23
and <and@tutao.de>
2025-09-26 13:16:16 +0200

    [Android] Fix widget error when device is offline
    When the device is offline the widget fails to log in into the sdk and throws a LoginException not handled before during calendar colors update attempt. This was preventing the cache fallback.

--- app-android/calendar/src/main/java/de/tutao/calendar/widget/model/WidgetUIViewModel.kt
+++ app-android/calendar/src/main/java/de/tutao/calendar/widget/model/WidgetUIViewModel.kt
@@ -18,6 +18,7 @@ import de.tutao.calendar.widget.data.WidgetRepository
 import de.tutao.calendar.widget.data.WidgetUIData
 import de.tutao.calendar.widget.error.WidgetError
 import de.tutao.calendar.widget.error.WidgetErrorType
+import de.tutao.tutasdk.LoginException
 import de.tutao.tutasdk.Sdk
 import de.tutao.tutashared.AndroidNativeCryptoFacade
 import de.tutao.tutashared.IdTuple
@@ -38,7 +39,6 @@ import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
-import java.util.Calendar
 import java.util.Date
 
 class WidgetUIViewModel(
@@ -62,40 +62,26 @@ class WidgetUIViewModel(
 		val allDayEvents: HashMap<Long, List<UIEvent>> = HashMap()
 		val normalEvents: HashMap<Long, List<UIEvent>> = HashMap()
 
-		val todayMidnight = Calendar.getInstance()
-		todayMidnight.set(Calendar.HOUR_OF_DAY, 0)
-		todayMidnight.set(Calendar.MINUTE, 0)
-		todayMidnight.set(Calendar.SECOND, 0)
-		todayMidnight.set(Calendar.MILLISECOND, 0)
-
-		val tomorrowMidnight = Calendar.getInstance()
-		tomorrowMidnight.add(Calendar.DAY_OF_YEAR, 1)
-		tomorrowMidnight.set(Calendar.HOUR_OF_DAY, 0)
-		tomorrowMidnight.set(Calendar.MINUTE, 0)
-		tomorrowMidnight.set(Calendar.SECOND, 0)
-		tomorrowMidnight.set(Calendar.MILLISECOND, 0)
+		val zone = ZoneId.systemDefault()
+		val todayMidnight = LocalDate.now(zone).atStartOfDay(zone).toInstant()
+		val tomorrowMidnight = LocalDate.now(zone)
+			.plusDays(1)
+			.atStartOfDay(zone)
+			.toInstant()
 
 		val settings: SettingsDao?
 		val lastSync: LastSyncDao?
-		var calendars = listOf<String>()
+		val calendars: List<String>
 
 		try {
 			settings = repository.loadSettings(context, widgetId) ?: return WidgetUIData(normalEvents, allDayEvents)
 			lastSync = repository.loadLastSync(context, widgetId)
-
-			if (sdk != null) {
-				val loadedCalendars = repository.loadCalendars(settings.userId, credentialsFacade, sdk)
-				for (key in loadedCalendars.keys) {
-					settings.calendars[key]?.color = loadedCalendars[key]?.color ?: continue
-				}
-				repository.storeSettings(context, widgetId, settings)
-			}
-
+			sdk?.let { sdk -> loadCalendars(context, sdk, settings) }
 			calendars = settings.calendars.keys.toList()
 		} catch (e: Exception) {
 			// We couldn't load widget settings, so we must show an error to User
 			_error.value = WidgetError(
-				"Something went wrong when reading from DataStore, WidgetId $widgetId",
+				"Error reading from DataStore (WidgetId $widgetId)",
 				e.stackTraceToString(),
 				WidgetErrorType.UNEXPECTED
 			)
@@ -164,7 +150,7 @@ class WidgetUIViewModel(
 				val isAllDay = isAllDayEventByTimes(
 					Date.from(Instant.ofEpochMilli(loadedEvent.startTime.toLong())),
 					Date.from(Instant.ofEpochMilli(loadedEvent.endTime.toLong()))
-				) || (loadedEvent.startTime.toLong() < todayMidnight.timeInMillis && loadedEvent.endTime.toLong() >= tomorrowMidnight.timeInMillis)
+				) || (loadedEvent.startTime.toLong() < todayMidnight.toEpochMilli() && loadedEvent.endTime.toLong() >= tomorrowMidnight.toEpochMilli())
 
 				val event = UIEvent(
 					calendarId,
@@ -254,6 +240,27 @@ class WidgetUIViewModel(
 		return uiState.value
 	}
 
+	private suspend fun loadCalendars(context: Context, sdk: Sdk, settings: SettingsDao) {
+		try {
+			val loadedCalendars = repository.loadCalendars(settings.userId, credentialsFacade, sdk)
+			for (key in loadedCalendars.keys) {
+				settings.calendars[key]?.color = loadedCalendars[key]?.color ?: continue
+			}
+			repository.storeSettings(context, widgetId, settings)
+			Log.d(TAG, "Calendars loaded successfully!")
+		} catch (e: LoginException.ApiCall) {
+			// Failed to login into SDK, probably because of connection issues
+			Log.w(TAG, "Calendar colors could not be loaded. Falling back to cached values.", e)
+		} catch (e: IOException) {
+			// We couldn't load widget settings, so we must show an error to User
+			_error.value = WidgetError(
+				"Error writing to DataStore (WidgetId $widgetId)",
+				e.stackTraceToString(),
+				WidgetErrorType.UNEXPECTED
+			)
+		}
+	}
+
 	private fun buildBirthdayEventTitle(event: BirthdayEventDao, context: Context): String {
 		if (event.contact.age == null) {
 			return context.getString(R.string.birthdayEvent_title).replace("{name}", event.contact.name)
@@ -286,8 +293,6 @@ class WidgetUIViewModel(
 
 		return null
 	}
-
-
 }
 
 fun openCalendarAgenda(
