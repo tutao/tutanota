6cd18ec33a2dbddfcbf3a10ab3a3683562aaabbe
and <and@tutao.de>
2025-09-29 13:17:35 +0200

    Rename CalendarInfo 'shared' field to 'hasMultipleMembers'

--- src/calendar-app/calendar/gui/eventeditor-view/CalendarEventEditView.ts
+++ src/calendar-app/calendar/gui/eventeditor-view/CalendarEventEditView.ts
@@ -293,7 +293,7 @@ export class CalendarEventEditView implements Component<CalendarEventEditViewAtt
 		const availableCalendars = model.editModels.whoModel.getAvailableCalendars()
 
 		const options: CalendarSelectItem[] = availableCalendars.map((calendarInfo) => {
-			const name = getSharedGroupName(calendarInfo.groupInfo, model.userController.userSettingsGroupRoot, calendarInfo.shared)
+			const name = getSharedGroupName(calendarInfo.groupInfo, model.userController.userSettingsGroupRoot, calendarInfo.hasMultipleMembers)
 			return {
 				name,
 				color: "#" + (groupColors.get(calendarInfo.group._id) ?? defaultCalendarColor),
@@ -303,7 +303,11 @@ export class CalendarEventEditView implements Component<CalendarEventEditViewAtt
 		})
 
 		const selectedCalendarInfo = model.editModels.whoModel.selectedCalendar
-		const selectedCalendarName = getSharedGroupName(selectedCalendarInfo.groupInfo, model.userController.userSettingsGroupRoot, selectedCalendarInfo.shared)
+		const selectedCalendarName = getSharedGroupName(
+			selectedCalendarInfo.groupInfo,
+			model.userController.userSettingsGroupRoot,
+			selectedCalendarInfo.hasMultipleMembers,
+		)
 		let selected: CalendarSelectItem = {
 			name: selectedCalendarName,
 			color: "#" + (groupColors.get(selectedCalendarInfo.group._id) ?? defaultCalendarColor),
