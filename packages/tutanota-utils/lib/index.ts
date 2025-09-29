8cd879c267432506bea69e49e30b1c9218b5c8c8
yoy <yoy@tutao.de>
2025-09-29 14:42:44 +0200

    Separate csv writer into header and body
    Co-authored-by: nig <nig@tutao.de>

--- packages/tutanota-utils/lib/index.ts
+++ packages/tutanota-utils/lib/index.ts
@@ -215,7 +215,7 @@ export {
 
 export { mod, clamp } from "./MathUtils.js"
 
-export { renderCsv } from "./Csv.js"
+export { renderCsv, renderCsvBody, renderCsvHeader } from "./Csv.js"
 
 export { tokenize } from "./Tokenizer.js"
 export { memoizedWithHiddenArgument, memoized, deepMemoized, lazyMemoized } from "./memoized.js"
