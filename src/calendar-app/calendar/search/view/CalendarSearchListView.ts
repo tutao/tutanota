1ba268707718e39c974063a8fa4c4adf266a78f2
bed <bed@tutao.de>
2025-10-07 15:08:12 +0200

    remove TODO about highlighting search results and create a task for it.

--- src/calendar-app/calendar/search/view/CalendarSearchListView.ts
+++ src/calendar-app/calendar/search/view/CalendarSearchListView.ts
@@ -27,7 +27,7 @@ export interface CalendarSearchListViewAttrs {
 	listModel: ListElementListModel<CalendarSearchResultListEntry>
 	onSingleSelection: (item: CalendarSearchResultListEntry) => unknown
 	isFreeAccount: boolean
-	cancelCallback: () => unknown | null // TODO add search highlights?
+	cancelCallback: () => unknown | null
 	availableCalendars: ReadonlyArray<CalendarInfoBase>
 }
 
