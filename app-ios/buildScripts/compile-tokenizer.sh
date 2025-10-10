d78682e37eac21ba7e8253e05970b79ae5fa1bbf
tutao-mac <hello@tutao.de>
2025-10-10 15:57:54 +0200

    [ios, build] Fix using artifacts for the wrong target
    When building static libraries we cache the lipo'd output
    but did not include the target information. This would
    result in linking error if the file was considered up-to
    -date but was for the wrong target.
    
    We fixed this by including by including the target info
    in the cached file path.
    
    Co-authored-by: wrd <wrd@tutao.de>

--- app-ios/buildScripts/compile-tokenizer.sh
+++ app-ios/buildScripts/compile-tokenizer.sh
@@ -30,15 +30,20 @@ for triple in $TRIPLES; do
 "
 done
 
+TRIPLE_FOLDER=$(tr '\n' '_' <<<"$TRIPLES")
+LIPO_OUTPUT_DIR="$CARGO_TARGET_DIR/$TRIPLE_FOLDER/$RELFLAG"
+LIPO_OUTPUT_FILE="$LIPO_OUTPUT_DIR/libsignal_tokenizer.a"
+mkdir -p $LIPO_OUTPUT_DIR
+
 # We want to avoid running lipo if it's up-to-date.
 # So we do some ad-hoc make-like timestamp check.
 NEEDS_TO_RUN_LIPO=0
 # If lipo-ed file is there and is not older than any of the inputs
 # then we don't need to do anything.
 # Cargo is nice enough where it won't update the timestamp for something that didn't change.
-if [ -e "$SCRIPT_OUTPUT_FILE_0" ]; then
+if [ -e "$LIPO_OUTPUT_FILE" ]; then
 	for LIB_PATH in $LIPO_ARGS; do
-		if [ "$LIB_PATH" -nt "$SCRIPT_OUTPUT_FILE_0" ]; then
+		if [ "$LIB_PATH" -nt "$LIPO_OUTPUT_FILE" ]; then
 			echo "I have found a NEW lib! $LIB_PATH"
 			NEEDS_TO_RUN_LIPO=1
 		fi
@@ -50,7 +55,9 @@ fi
 if [ $NEEDS_TO_RUN_LIPO = 1 ]; then
 	# Replace newline with \0
 	# Pass LIPO_ARGS to lipo, splitting on \0
-	tr '\n' '\0' <<<"$LIPO_ARGS" | xargs -0 lipo -create -output "$SCRIPT_OUTPUT_FILE_0"
+	tr '\n' '\0' <<<"$LIPO_ARGS" | xargs -0 lipo -create -output "$LIPO_OUTPUT_FILE"
 else
-	echo "$SCRIPT_OUTPUT_FILE_0 is up to date"
+	echo "$LIPO_OUTPUT_FILE is up to date"
 fi
+
+cp -p $LIPO_OUTPUT_FILE $SCRIPT_OUTPUT_FILE_0
