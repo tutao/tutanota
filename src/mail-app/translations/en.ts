7de3dcd79febf381a07880cbbb92ce108e8a30f3
bir <bir@tutao.de>
2025-10-13 15:52:36 +0200

    Fix contacts `relationships` field is confusing
    Fixed by changing the heading to `Related people` because the field is
     between a contact and another person, not between the contact book
     owner and the contact
    
    Close #8681

--- src/mail-app/translations/en.ts
+++ src/mail-app/translations/en.ts
@@ -2128,6 +2128,7 @@ export default {
 		"yourMessage_label": "Your message",
 		"you_label": "You",
 		"zoomIn_action": "Zoom In",
-		"zoomOut_action": "Zoom Out"
+		"zoomOut_action": "Zoom Out",
+		"relatedPeople_label": "Related people"
 	}
 }
