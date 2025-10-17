112e92a1bd1b1117ae9cccd357fdc0b69aaf34c2
ivk <ivk@tutao.de>
2025-10-17 13:42:49 +0200

    Improve settings navigation

--- src/common/settings/SettingsFolder.ts
+++ src/common/settings/SettingsFolder.ts
@@ -1,5 +1,5 @@
 import type { lazyIcon } from "../gui/base/Icon.js"
-import type { TranslationKey, MaybeTranslation } from "../misc/LanguageViewModel.js"
+import type { MaybeTranslation } from "../misc/LanguageViewModel.js"
 import { isSelectedPrefix } from "../gui/base/NavButton.js"
 import type { lazy } from "@tutao/tutanota-utils"
 import { assertMainOrNode } from "../api/common/Env.js"
@@ -7,22 +7,36 @@ import { UpdatableSettingsViewer } from "./Interfaces.js"
 
 assertMainOrNode()
 
-type SettingsFolderPath = string | { folder: string; id: string }
+interface SettingsFolderPath {
+	folder: string
+	id: string | null
+}
 
 export class SettingsFolder<T> {
 	readonly url: string
+	private readonly path: SettingsFolderPath
 
 	private _isVisibleHandler: lazy<boolean>
 
+	/**
+	 * @param name Displayed as the folder name
+	 * @param icon Displayed as the folder icon
+	 * @param path Either a string which means URL like `/settings/mail` or an object which means URL like `/settings/templates/1`
+	 * @param viewerCreator A function to produce instances of {@link UpdatableSettingsViewer}.
+	 * @param data Additional data that the folder can carry
+	 */
 	constructor(
 		readonly name: () => MaybeTranslation,
 		readonly icon: lazyIcon,
-		readonly path: SettingsFolderPath,
+		path: string | SettingsFolderPath,
 		readonly viewerCreator: lazy<UpdatableSettingsViewer>,
 		readonly data: T,
 	) {
+		this.path = typeof path === "string" ? { folder: path, id: null } : path
 		this.url =
-			typeof path === "string" ? `/settings/${encodeURIComponent(path)}` : `/settings/${encodeURIComponent(path.folder)}/${encodeURIComponent(path.id)}`
+			this.path.id == null
+				? `/settings/${encodeURIComponent(this.path.folder)}`
+				: `/settings/${encodeURIComponent(this.path.folder)}/${encodeURIComponent(this.path.id)}`
 
 		this._isVisibleHandler = () => true
 	}
@@ -40,11 +54,20 @@ export class SettingsFolder<T> {
 		return this
 	}
 
-	get folder(): string | null {
-		return typeof this.path === "string" ? null : this.path.folder
+	get folder(): string {
+		return this.path.folder
 	}
 
 	get id(): string | null {
-		return typeof this.path === "string" ? null : this.path.id
+		return this.path.id
+	}
+
+	matches(folder: string, id: string | undefined | null): boolean {
+		// eslint-disable-next-line eqeqeq -- this.id can be null and id can be undefined
+		return folder === this.folder && id == this.id
+	}
+
+	isSameFolder(anotherFolder: SettingsFolder<unknown>): boolean {
+		return this.matches(anotherFolder.folder, anotherFolder.id)
 	}
 }
