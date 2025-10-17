397b644ced3d0a4ac305cfbd122947c5188a23c9
das <das@tutao.de>
2025-10-17 09:20:59 +0200

    wip

--- src/common/desktop/db/DesktopSqlCipher.ts
+++ src/common/desktop/db/DesktopSqlCipher.ts
@@ -92,7 +92,6 @@ export class DesktopSqlCipher implements SqlCipherFacade {
 		}
 
 		const key = `x'${uint8ArrayToHex(databaseKey)}'`
-		console.log("THE KEY", key)
 		this.db.pragma(`KEY = "${key}"`)
 
 		// We are using the auto_vacuum=incremental mode to allow for a faster vacuum execution
