tutao/tutanota

commit c7a8749a710f4759e2bc2884fc52bf1859312061

change handling of Entities with decryption errors when updating range
This commit makes a distinction between temporary and permanent crypto
errors. We do not write entities to the offline storage if they have
a SessionKeyNotFoundError, since this is a temporary error and is
expected to go away the next time the range is updated. We now do
cache entities with other errors, since we cannot do anything about
them.

Writing entities with permanent errors will not cause UI issues, since
the CryptoMapper replaces the affected fields with default values.

Co-authored-by: map <mpfau@users.noreply.github.com>
Co-authored-by: jhm <17314077+jomapp@users.noreply.github.com>
Co-authored-by: hrb-hub <181954414+hrb-hub@users.noreply.github.com>

src/common/api/worker/crypto/CryptoMapper.ts
@@ -8,7 +8,7 @@ import {
 	ServerModelParsedInstance,
 	ServerTypeModel,
 } from "../../common/EntityTypes"
-import { Base64, base64ToUint8Array, stringToUtf8Uint8Array, TypeRef, uint8ArrayToBase64, utf8Uint8ArrayToString } from "@tutao/tutanota-utils"
+import { assertNotNull, Base64, base64ToUint8Array, stringToUtf8Uint8Array, TypeRef, uint8ArrayToBase64, utf8Uint8ArrayToString } from "@tutao/tutanota-utils"
 import { AssociationType, Cardinality, ValueType } from "../../common/EntityConstants"
 import { CryptoError } from "@tutao/tutanota-crypto/error.js"
 import { Nullable } from "@tutao/tutanota-utils/dist/Utils"
@@ -17,6 +17,8 @@ import { convertDbToJsType, convertJsToDbType, decompressString, isDefaultValue,
 import { ClientTypeReferenceResolver, ServerTypeReferenceResolver } from "../../common/EntityFunctions"
 import { isWebClient } from "../../common/Env"
 import { ProgrammingError } from "../../common/error/ProgrammingError"
+import { SessionKeyNotFoundError } from "../../common/error/SessionKeyNotFoundError"
+import { AttributeModel } from "../../common/AttributeModel"
 
 // Exported for testing
 export function encryptValue(
@@ -92,7 +94,9 @@ export class CryptoMapper {
 					const encryptedString = encryptedValue as Base64
 					decrypted[valueId] = decryptValue(encryptedValueInfo, encryptedString, sk)
 				} else {
-					throw new CryptoError("session key is null, but value is encrypted. valueName: " + valueName + " valueType: " + JSON.stringify(valueInfo))
+					throw new SessionKeyNotFoundError(
+						"session key is null, but value is encrypted. valueName: " + valueName + " valueType: " + JSON.stringify(valueInfo),
+					)
 				}
 				if (valueInfo.encrypted) {
 					if (encryptedValue === "") {
@@ -113,9 +117,13 @@ export class CryptoMapper {
 					decrypted._errors = {}
 				}
 				decrypted[valueId] = valueToDefault(valueInfo.type)
-
-				decrypted._errors[valueId] = JSON.stringify(e)
-				console.log("error when decrypting value on type:", `[${serverTypeModel.app},${serverTypeModel.name}]`, "valueName:", valueName, e)
+				if (e instanceof SessionKeyNotFoundError) {
+					const skAttrId = assertNotNull(AttributeModel.getAttributeId(serverTypeModel, "_ownerEncSessionKey"))
+					decrypted._errors[skAttrId] = "Probably temporary SessionKeyNotFound"
+				} else {
+					decrypted._errors[valueId] = JSON.stringify(e)
+					console.log("error when decrypting value on type:", `[${serverTypeModel.app},${serverTypeModel.name}]`, "valueName:", valueName, e)
+				}
 			}
 		}
 

src/common/api/common/EntityTypes.ts
@@ -191,7 +191,7 @@ export type ParsedAssociation = EncryptedParsedAssociation
 
 /** a parsed instance after/before going through decryption/encryption */
 export type ParsedInstance = Record<AttributeId, Nullable<ParsedValue> | ParsedAssociation> & {
-	/** crypto errors that happened during serialization are stored here for debugging purposes */
+	/** crypto errors that happened during deserialization/serialization */
 	_errors?: Record<AttributeId, string>
 	/** the ivs used to encrypt final fields on the instance */
 	_finalIvs: Record<AttributeId, Nullable<Uint8Array>>

src/common/api/worker/rest/DefaultEntityRestCache.ts
@@ -8,7 +8,7 @@ import {
 	OwnerEncSessionKeyProvider,
 } from "./EntityRestClient"
 import { OperationType } from "../../common/TutanotaConstants"
-import { assertNotNull, downcast, getFirstOrThrow, getTypeString, isSameTypeRef, lastThrow, TypeRef } from "@tutao/tutanota-utils"
+import { assertNotNull, downcast, getFirstOrThrow, getTypeString, isNotEmpty, isSameTypeRef, lastThrow, TypeRef } from "@tutao/tutanota-utils"
 import {
 	AuditLogEntryTypeRef,
 	BucketPermissionTypeRef,
@@ -628,40 +628,60 @@ export class DefaultEntityRestCache implements EntityRestCache {
 		wasReverseRequest: boolean,
 		receivedEntities: ServerModelParsedInstance[],
 	) {
-		// Filter out parsed instances after the first instances with _errors (i.e. decryption errors),
-		// because we should NEVER store instances in the storage that failed to be decrypted!
-		const elementsToAdd = wasReverseRequest ? receivedEntities.reverse() : receivedEntities
-		const firstElementToAddIndexWithError = elementsToAdd.findIndex((elementToAdd) => hasError(elementToAdd))
-		const elementsToAddWithoutErrors = firstElementToAddIndexWithError !== -1 ? elementsToAdd.slice(0, firstElementToAddIndexWithError) : elementsToAdd
-		const elementsToAddWithoutErrorsCount = elementsToAddWithoutErrors.length
-		await this.storage.putMultiple(typeRef, elementsToAddWithoutErrors)
+		// Filter out parsed instances after the first instances with SessionKeyNotFoundErrors in _errors,
+		// because we should NEVER store instances in the storage that have a temporary decryption error
+		// for example because the session key was not found. This is usually happening e.g. for attachments (file type)
+		// where the _ownerEncSessionKey has not been written to the instance yet.
+		// Since this is only a temporary error, we do not want to update the full range yet, leading the client to think the instance is already cached.
+		// Entities with permanent errors (_errors but not SessionKeyNotFoundErrors) are written to the offline storage.
+		// The corrupted fields in such cases are replace with default values and causes therefore not UI issues (See CryptoMapper.decryptParsedInstance).
+
+		let allInstances = wasReverseRequest ? receivedEntities.reverse() : receivedEntities
 
 		const typeModel = await this.typeModelResolver.resolveServerTypeReference(typeRef)
+
+		const ownerEncSessionKeyAttributeId = AttributeModel.getAttributeId(typeModel, "_ownerEncSessionKey")
+
+		// look for first instance with a SessionKeyNotFoundError. See CryptoMapper.decryptParsedInstance.
+		const errorRangeBound = allInstances.findIndex((instance) => hasError(instance, ownerEncSessionKeyAttributeId))
+
+		const instancesWithoutSessionKeyNotFoundErrors = errorRangeBound !== -1 ? allInstances.slice(0, errorRangeBound) : allInstances
+		const instancesWithoutErrors = instancesWithoutSessionKeyNotFoundErrors.filter((instance) => true)
+
+		await this.storage.putMultiple(typeRef, instancesWithoutErrors)
+
 		const isCustomId = isCustomIdType(await this.typeModelResolver.resolveClientTypeReference(typeRef))
+
+		const isFinishedLoading = instancesWithoutErrors.length === receivedEntities.length && receivedEntities.length < countRequested
 		if (wasReverseRequest) {
 			// Ensure that elements are cached in ascending (not reverse) order
-			if (elementsToAddWithoutErrorsCount === receivedEntities.length && receivedEntities.length < countRequested) {
+
+			if (isFinishedLoading) {
 				console.log("finished loading, setting min id")
 				await this.storage.setLowerRangeForList(typeRef, listId, isCustomId ? CUSTOM_MIN_ID : GENERATED_MIN_ID)
-			} else {
+			} else if (isNotEmpty(instancesWithoutSessionKeyNotFoundErrors)) {
+				// When all receivedEntities have SessionKeyNotFound errors, and therefore instancesWithoutSessionKeyNotFoundErrors is empty, do nothing
+
 				// After reversing the list the first element in the list is the lower range limit
 				await this.storage.setLowerRangeForList(
 					typeRef,
 					listId,
-					elementIdPart(AttributeModel.getAttribute(getFirstOrThrow(elementsToAddWithoutErrors), "_id", typeModel)),
+					elementIdPart(AttributeModel.getAttribute(getFirstOrThrow(instancesWithoutSessionKeyNotFoundErrors), "_id", typeModel)),
 				)
 			}
 		} else {
+			// When all receivedEntities have SessionKeyNotFound errors, and therefore instancesWithoutSessionKeyNotFoundErrors is empty, do nothing
+
 			// Last element in the list is the upper range limit
-			if (elementsToAddWithoutErrorsCount === receivedEntities.length && receivedEntities.length < countRequested) {
+			if (isFinishedLoading) {
 				// all elements have been loaded, so the upper range must be set to MAX_ID
 				console.log("finished loading, setting max id")
 				await this.storage.setUpperRangeForList(typeRef, listId, isCustomId ? CUSTOM_MAX_ID : GENERATED_MAX_ID)
-			} else {
+			} else if (isNotEmpty(instancesWithoutSessionKeyNotFoundErrors)) {
 				await this.storage.setUpperRangeForList(
 					typeRef,
 					listId,
-					elementIdPart(AttributeModel.getAttribute(lastThrow(elementsToAddWithoutErrors), "_id", typeModel)),
+					elementIdPart(AttributeModel.getAttribute(lastThrow(instancesWithoutSessionKeyNotFoundErrors), "_id", typeModel)),
 				)
 			}
 		}

test/tests/api/worker/crypto/CryptoMapperTest.ts
@@ -277,7 +277,7 @@ o.spec("CryptoMapper", function () {
 		} as any as ServerModelEncryptedParsedInstance
 		const instance = await cryptoMapper.decryptParsedInstance(testTypeModel as ServerTypeModel, encryptedInstance, null)
 		o(instance[1]).equals("") // default value is assigned in case of crypto errors
-		o(typeof instance._errors?.[1]).equals("string")
+		o(instance._errors?.[14]).equals("Probably temporary SessionKeyNotFound")
 	})
 
 	o("encryptParsedInstance with missing sk throws", async function () {

test/tests/api/worker/crypto/InstancePipelineTestUtils.ts
@@ -52,6 +52,14 @@ export const testTypeModel: TypeModel = {
 			final: false,
 			encrypted: false,
 		},
+		"14": {
+			id: 14,
+			name: "_ownerEncSessionKey",
+			type: ValueType.Bytes,
+			cardinality: Cardinality.ZeroOrOne,
+			final: true,
+			encrypted: false,
+		},
 	},
 	associations: {
 		"3": {

commit dd0efb907a78f616986d9612c52578f0a1468db8

do not assertNotNull for _ownerEncSessionKey in temporary crypto errors
Since there is at least one case where there is no _ownerEncSessionKey
on an encrypted (aggregated) type (GroupInvitationPostReturn and its
MailAddress aggregates), we cannot assertNotNull for the attribute.
This fixes the error that was introduced by
c7a8749a710f4759e2bc2884fc52bf1859312061.

Co-authored-by: jomapp <17314077+jomapp@users.noreply.github.com>

src/common/api/worker/crypto/CryptoMapper.ts
@@ -118,8 +118,10 @@ export class CryptoMapper {
 				}
 				decrypted[valueId] = valueToDefault(valueInfo.type)
 				if (e instanceof SessionKeyNotFoundError) {
-					const skAttrId = assertNotNull(AttributeModel.getAttributeId(serverTypeModel, "_ownerEncSessionKey"))
-					decrypted._errors[skAttrId] = "Probably temporary SessionKeyNotFound"
+					const skAttrId = AttributeModel.getAttributeId(serverTypeModel, "_ownerEncSessionKey")
+					if (skAttrId) {
+						decrypted._errors[skAttrId] = "Probably temporary SessionKeyNotFound"
+					}
 				} else {
 					decrypted._errors[valueId] = JSON.stringify(e)
 					console.log("error when decrypting value on type:", `[${serverTypeModel.app},${serverTypeModel.name}]`, "valueName:", valueName, e)