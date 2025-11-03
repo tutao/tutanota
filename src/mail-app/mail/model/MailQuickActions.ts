d3dc69f258fc88e217c76ee8f90fd14d02f3d35f
ivk <ivk@tutao.de>
2025-11-03 17:57:49 +0100

    Improve mail folder display in quick actions

--- src/mail-app/mail/model/MailQuickActions.ts
+++ src/mail-app/mail/model/MailQuickActions.ts
@@ -2,7 +2,7 @@ import { MailModel } from "./MailModel"
 import { MailboxDetail, MailboxModel } from "../../../common/mailFunctionality/MailboxModel"
 import { getMailboxName } from "../../../common/mailFunctionality/SharedMailUtils"
 import { LoginController } from "../../../common/api/main/LoginController"
-import { getIndentedFolderNameForDropdown } from "./MailUtils"
+import { getIndentedFolderNameForDropdown, getPathToFolderString } from "./MailUtils"
 import { Router } from "../../../common/gui/ScopedRouter"
 import { getElementId } from "../../../common/api/common/utils/EntityUtils"
 import { lang } from "../../../common/misc/LanguageViewModel"
@@ -41,12 +41,13 @@ export async function quickMailActions(
 		if (fs == null) {
 			folderActions = []
 		} else {
-			folderActions = fs.getIndentedList().map((folderInfo: IndentedFolder) => {
+			folderActions = fs.getIndentedList().map(({ folder }) => {
 				return {
-					description: `${mailboxName} ${getIndentedFolderNameForDropdown(folderInfo)}`,
+					// this is not the most performant thing, but we are doing this once so it's okay
+					description: `${mailboxName} ${getPathToFolderString(fs, folder)}`,
 					// TODO: this is not ideal as this will forget the selected mail in that folder. We could pull it
 					//   up from somewhere.
-					exec: () => router.routeTo(`${MAIL_PREFIX}/:folder`, { folder: getElementId(folderInfo.folder) }),
+					exec: () => router.routeTo(`${MAIL_PREFIX}/:folder`, { folder: getElementId(folder) }),
 				}
 			})
 		}
