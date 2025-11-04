59d8d25f1bebce6e3a0d3bdecf88b4e80a3a3888
hrb-hub <hrb-hub@users.noreply.github.com>
2025-11-04 10:21:49 +0100

    TutanotaModelV99 (Send Later)

--- src/common/api/worker/facades/lazy/MailFacade.ts
+++ src/common/api/worker/facades/lazy/MailFacade.ts
@@ -70,6 +70,7 @@ import {
 	createResolveConversationsServiceGetIn,
 	createSecureExternalRecipientKeyData,
 	createSendDraftData,
+	createSendDraftParameters,
 	createSimpleMoveMailPostIn,
 	createUnreadMailStatePostIn,
 	createUpdateMailFolderData,
@@ -592,7 +593,7 @@ export class MailFacade {
 	async sendDraft(draft: Mail, recipients: Array<Recipient>, language: string): Promise<void> {
 		const senderMailGroupId = await this._getMailGroupIdForMailAddress(this.userFacade.getLoggedInUser(), draft.sender.address)
 		const bucketKey = aes256RandomKey()
-		const sendDraftData = createSendDraftData({
+		const parameters = createSendDraftParameters({
 			language: language,
 			mail: draft._id,
 			mailSessionKey: null,
@@ -605,6 +606,11 @@ export class MailFacade {
 			secureExternalRecipientKeyData: [],
 			symEncInternalRecipientKeyData: [],
 			sessionEncEncryptionAuthStatus: null,
+		})
+		const sendDraftData = createSendDraftData({
+			...parameters,
+			parameters,
+			sendAt: null,
 		})
 
 		const attachments = await this.getAttachmentIds(draft)
