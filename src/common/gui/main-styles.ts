29b2b8be53e2f928e177c399b73ab23655922039
bir <bir@tutao.de>
2025-11-24 14:42:51 +0100

    [Android] Fix background issue when exporting mail as PDF
    close #9984

--- src/common/gui/main-styles.ts
+++ src/common/gui/main-styles.ts
@@ -2793,7 +2793,7 @@ styles.registerStyle("main", () => {
 			},
 			'.mail-viewer, [data-testid="collapsed-mail-view"]': {
 				color: `${lightTheme.on_surface} !important`,
-				"background-color": `${lightTheme.on_surface}`,
+				"background-color": `${lightTheme.surface}`,
 			},
 			"#mail-body": {
 				overflow: "visible",
