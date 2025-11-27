1eec9b388850a7f52702312d6133b02cbf0e819e
and <and@tutao.de>
2025-11-27 10:26:31 +0100

    Model v99 - Introduction of Ghost Bubbles and default calendars

--- tuta-sdk/rust/sdk/src/entities/generated/tutanota.rs
+++ tuta-sdk/rust/sdk/src/entities/generated/tutanota.rs
@@ -1885,6 +1885,8 @@ pub struct CalendarEvent {
 	pub recurrenceId: Option<DateTime>,
 	#[serde(rename = "1401")]
 	pub _ownerKeyVersion: Option<i64>,
+	#[serde(rename = "1788")]
+	pub sender: Option<String>,
 	#[serde(rename = "945")]
 	pub repeatRule: Option<CalendarRepeatRule>,
 	#[serde(rename = "946")]
@@ -1931,6 +1933,8 @@ pub struct CalendarGroupRoot {
 	pub longEvents: GeneratedId,
 	#[serde(rename = "1103")]
 	pub index: Option<CalendarEventIndexRef>,
+	#[serde(rename = "1786")]
+	pub pendingEvents: Option<CalendarEventsRef>,
 
 	#[serde(default)]
 	pub _errors: Errors,
@@ -2061,6 +2065,8 @@ pub struct UserSettingsGroupRoot {
 	pub birthdayCalendarColor: Option<String>,
 	#[serde(rename = "979")]
 	pub groupSettings: Vec<GroupSettings>,
+	#[serde(rename = "1787")]
+	pub defaultCalendar: Option<GeneratedId>,
 
 	#[serde(default)]
 	pub _errors: Errors,
@@ -2079,14 +2085,16 @@ impl Entity for UserSettingsGroupRoot {
 
 #[derive(uniffi::Record, Clone, Serialize, Deserialize)]
 #[cfg_attr(any(test, feature = "testing"), derive(PartialEq, Debug))]
-pub struct CalendarDeleteData {
+pub struct CalendarDeleteIn {
 	#[serde(rename = "983")]
 	pub _format: i64,
+	#[serde(rename = "1789")]
+	pub deleteEventsOnly: bool,
 	#[serde(rename = "984")]
 	pub groupRootId: GeneratedId,
 }
 
-impl Entity for CalendarDeleteData {
+impl Entity for CalendarDeleteIn {
 	fn type_ref() -> TypeRef {
 		TypeRef {
 			app: AppName::Tutanota,
@@ -4312,6 +4320,24 @@ impl Entity for PopulateClientSpamTrainingDataPostIn {
 		TypeRef {
 			app: AppName::Tutanota,
 			type_id: TypeId::from(1778),
+		}
+	}
+}
+
+#[derive(uniffi::Record, Clone, Serialize, Deserialize)]
+#[cfg_attr(any(test, feature = "testing"), derive(PartialEq, Debug))]
+pub struct CalendarEventsRef {
+	#[serde(rename = "1784")]
+	pub _id: Option<CustomId>,
+	#[serde(rename = "1785")]
+	pub list: GeneratedId,
+}
+
+impl Entity for CalendarEventsRef {
+	fn type_ref() -> TypeRef {
+		TypeRef {
+			app: AppName::Tutanota,
+			type_id: TypeId::from(1783),
 		}
 	}
 }
