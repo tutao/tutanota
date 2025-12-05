0c9285a44055c93a75e7192d76e48ee72f4f9d9d
toj <toj@tutao.de>
2025-12-05 18:02:02 +0100

    wip: initial payment page implementation

--- src/common/signup/SignupPaymentPage.ts
+++ src/common/signup/SignupPaymentPage.ts
@@ -1,20 +1,45 @@
 import m, { ClassComponent, Vnode } from "mithril"
 import { WizardStepComponentAttrs } from "../gui/base/wizard/WizardStep"
 import { SignupViewModel } from "./SignupView"
-import { InvoiceAndPaymentDataPageNew } from "../subscription/InvoiceAndPaymentDataPageNew"
+import { InvoiceAndPaymentDataPageNew } from "./components/InvoiceAndPaymentDataPageNew"
+import { px, size } from "../gui/size"
+import { theme } from "../gui/theme"
+import { RadioSelectorOption } from "../gui/base/RadioSelectorItem"
+import { RadioSelector, RadioSelectorAttrs } from "../gui/base/RadioSelector"
 
 export class SignupPaymentPage implements ClassComponent<WizardStepComponentAttrs<SignupViewModel>> {
+	private currentOption = 0
 	view(vnode: Vnode<WizardStepComponentAttrs<SignupViewModel>>) {
 		const { ctx } = vnode.attrs
 
-		return m(
-			".flex.col.justify-center",
+		const boxAttr = { style: { width: px(500), height: px(500), background: theme.primary_container, padding: size.spacing_16 } }
+		const options: ReadonlyArray<RadioSelectorOption<number>> = [
 			{
-				style: {
-					"max-width": "50%",
-				},
+				name: "paymentMethodCreditCard_label",
+				value: 0,
+				renderChild: () =>
+					m(
+						"div.flex.flex-column",
+						{
+							style: {
+								width: px(500),
+							},
+						},
+
+						m(InvoiceAndPaymentDataPageNew, ctx),
+					),
 			},
-			m(InvoiceAndPaymentDataPageNew, ctx),
-		)
+			{ name: "credit_label", value: 1, renderChild: () => m("div", boxAttr, "2") },
+		]
+		return m(".flex.items-center.flex-center", [
+			m(RadioSelector, {
+				groupName: "credentialsEncryptionMode_label",
+				options,
+				selectedOption: this.currentOption,
+				onOptionSelected: (mode: number) => {
+					this.currentOption = mode
+				},
+			} satisfies RadioSelectorAttrs<number>),
+		])
 	}
 }
