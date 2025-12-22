c6af8db0b82b84dede308b86399d905e235148ab
vaf <vaf@tutao.de>
2025-12-22 15:11:36 +0100

    propagate decryption errors in nested aggregates to the parent instance
    previously errors in nested aggregates were not visible at the top instance level. in case there is not value at the top level that fails decryption (which is the case for example for MailDetailsBlob) these instances would not be treated like errors. this leads for example to the case that we would cache such instances and then never properly retry decryption although this would help with temporary errors.
    
    now we bubble the _errors up to the top level of the instance, which allows for an easy check.
    
    we use this check in case of MailDetails in MailViewerViewModel to decide how to render an instance
    
    also we now properly error out on patches that contain decryption _errors

--- src/common/api/worker/crypto/CryptoMapper.ts
+++ src/common/api/worker/crypto/CryptoMapper.ts
@@ -118,11 +118,23 @@ export class CryptoMapper {
 			if (associationType.type === AssociationType.Aggregation) {
 				const appName = associationType.dependency ?? serverTypeModel.app
 				const associationTypeModel = await this.serverTypeReferenceResolver(new TypeRef(appName, associationType.refTypeId))
-				decrypted[associationId] = await this.decryptAggregateAssociation(
+				const decryptedAggregates = await this.decryptAggregateAssociation(
 					associationTypeModel,
 					encryptedInstanceValue as Array<ServerModelEncryptedParsedInstance>,
 					sk,
 				)
+				decrypted[associationId] = decryptedAggregates
+				if (this.containErrors(decryptedAggregates)) {
+					// we must propagate up to the top level of the instance that there is an error somewhere in an aggregated type.
+					// this indicates to the caller whether decryption succeeded.
+					// e.g. in order to decide whether an instance should be cached or not.
+					if (decrypted._errors == null) {
+						decrypted._errors = {}
+					}
+					// we cannot leave the object empty here, because empty objects are not consistently treated as errors
+					// see the _errors properties in the nested aggregates for more details about the error
+					decrypted._errors[associationId] = "Aggregated type decrypted with errors"
+				}
 			} else {
 				decrypted[associationId] = encryptedInstanceValue
 			}
@@ -130,6 +142,18 @@ export class CryptoMapper {
 		return decrypted
 	}
 
+	/**
+	 * Returns true if at least one of the instances contains _errors at the top level.
+	 * Useful for ATs.
+	 */
+	public containErrors(instances: ServerModelParsedInstance[]): boolean {
+		return instances.some((instance) => instance._errors != null)
+	}
+
+	/**
+	 * Returns an array of the decrypted aggregates, each of which may contain decryption errors.
+	 * The caller is responsible for handling the _errors property on each aggregate if it is set.
+	 */
 	public async decryptAggregateAssociation(
 		associationServerTypeModel: ServerTypeModel | ClientTypeModel,
 		encryptedInstanceValues: Array<ServerModelEncryptedParsedInstance>,
