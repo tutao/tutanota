828a5c7f39b5a9995a799f9943d2f1299928ec67
toj <toj@tutao.de>
2025-12-29 13:52:40 +0100

    remove redundant code

--- src/common/signup/SignupFormPage.ts
+++ src/common/signup/SignupFormPage.ts
@@ -58,23 +58,6 @@ export class SignupFormPage implements ClassComponent<WizardStepComponentAttrs<S
 						passwordInputStore: data.passwordInputStore,
 					}),
 				),
-				// !styles.isMobileLayout() &&
-				// 	m(
-				// 		".flex-grow",
-				// 		{
-				// 			style: {
-				// 				width: `calc(50% - ${px(size.spacing_32)})`,
-				// 			},
-				// 		},
-				// 		m("img.block.full-width", {
-				// 			style: { "max-width": px(400), "margin-inline": "auto" },
-				// 			src: `${window.tutao.appState.prefixWithoutFile}/images/signup/placeholder.svg`,
-				// 			alt: "",
-				// 			rel: "noreferrer",
-				// 			loading: "lazy",
-				// 			decoding: "async",
-				// 		}),
-				// 	),
 			]),
 		])
 	}
