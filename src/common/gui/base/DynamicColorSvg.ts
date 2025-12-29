9fb9fb2be2853272ae42b26c6f9ed2819dc4951c
toj <toj@tutao.de>
2025-12-29 11:15:18 +0100

    Allow DynamicColorSvg to change svg images

--- src/common/gui/base/DynamicColorSvg.ts
+++ src/common/gui/base/DynamicColorSvg.ts
@@ -7,18 +7,33 @@ interface DynamicColorSvgAttrs {
 }
 export class DynamicColorSvg implements Component<DynamicColorSvgAttrs> {
 	private svg: stream<string | undefined> = stream()
+	private loadSeq = 0
 
-	async oncreate({ attrs: { path } }: Vnode<DynamicColorSvgAttrs>) {
+	oncreate({ attrs: { path } }: Vnode<DynamicColorSvgAttrs>) {
+		void this.loadSvg(path)
+	}
+
+	onbeforeupdate(vnode: Vnode<DynamicColorSvgAttrs>, old: Vnode<DynamicColorSvgAttrs>) {
+		if (vnode.attrs.path !== old.attrs.path) {
+			void this.loadSvg(vnode.attrs.path)
+		}
+		return true
+	}
+
+	private async loadSvg(path: string) {
+		const loadId = ++this.loadSeq
 		try {
 			const res = await fetch(path)
 			const content = await res.text()
 			const contentType = res.headers.get("content-type")
 			if (!contentType?.includes("svg")) throw new Error()
+			if (loadId !== this.loadSeq) return
 			this.svg(content)
 		} catch (e) {
+			if (loadId !== this.loadSeq) return
 			throw new ProgrammingError(`Failed to fetch SVG file: ${e}`)
 		} finally {
-			m.redraw()
+			if (loadId === this.loadSeq) m.redraw()
 		}
 	}
 
