4af3460144541af36fde7b3882c8e935c9174dd2
wrd <wrd@tutao.de>
2026-01-02 15:58:30 +0100

    Catch when moving a mail results in a BadRquest error and show a message
    Close #9601

--- src/mail-app/translations/de.ts
+++ src/mail-app/translations/de.ts
@@ -2125,6 +2125,7 @@ export default {
 		"yourFolders_action": "DEINE ORDNER",
 		"yourMessage_label": "Deine Nachricht",
 		"zoomIn_action": "Hereinzoomen",
-		"zoomOut_action": "Herauszoomen"
+		"zoomOut_action": "Herauszoomen",
+		"couldNotMoveMail_msg": "Could not move mail."
 	}
 }
