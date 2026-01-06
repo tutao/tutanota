2fca79b6b9d92b4e46c82542fb5e23064af74043
mab <mab@tutao.de>
2026-01-06 17:34:20 +0100

    [Android] Show download progress
    Co-authored-by: ivk <ivk@tutao.de>

---
+++ app-android/tutashared/src/main/java/de/tutao/tutashared/ProgressResponseBody.kt
@@ -0,0 +1,50 @@
+package de.tutao.tutashared
+
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import okio.Buffer
+import okio.BufferedSource
+import okio.ForwardingSource
+import okio.Source
+import okio.buffer
+
+typealias ProgressListener = (bytesRead: Long, contentLength: Long, done: Boolean) -> Unit
+
+/**
+ * Wrapper for OkHttp's [okhttp3.ResponseBody] that can report how much of it was read to [ProgressListener].
+ */
+class ProgressResponseBody(
+	private val responseBody: ResponseBody,
+	private val progressListener: ProgressListener
+) : ResponseBody() {
+	private var bufferedSource: BufferedSource? = null
+
+	override fun contentType(): MediaType? {
+		return responseBody.contentType()
+	}
+
+	override fun contentLength(): Long {
+		return responseBody.contentLength()
+	}
+
+	override fun source(): BufferedSource {
+		if (bufferedSource == null) {
+			bufferedSource = source(responseBody.source()).buffer()
+		}
+		return bufferedSource!!
+	}
+
+	private fun source(source: Source): Source {
+		return object : ForwardingSource(source) {
+			var totalBytesRead: Long = 0L
+
+			override fun read(sink: Buffer, byteCount: Long): Long {
+				val bytesRead: Long = super.read(sink, byteCount)
+				// read() returns the number of bytes read, or -1 if this source is exhausted.
+				totalBytesRead += if (bytesRead != -1L) bytesRead else 0
+				progressListener(totalBytesRead, responseBody.contentLength(), bytesRead == -1L)
+				return bytesRead
+			}
+		}
+	}
+}
