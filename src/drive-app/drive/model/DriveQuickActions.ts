602a76c82d4a8097b498cf62b598289e59461d42
mab <mab@tutao.de>
2026-01-07 17:26:33 +0100

    Use translated strings in DriveQuickActions
    Co-authored-by: ivk <ivk@tutao.de>

--- src/drive-app/drive/model/DriveQuickActions.ts
+++ src/drive-app/drive/model/DriveQuickActions.ts
@@ -3,21 +3,21 @@ import { QuickAction } from "../../../common/misc/quickactions/QuickActionsModel
 import { DRIVE_PREFIX } from "../../../common/misc/RouteChange"
 import { showNewFileDialog, showNewFolderDialog } from "../view/DriveGuiUtils"
 import { DriveViewModel } from "../view/DriveViewModel"
+import { lang } from "../../../common/misc/LanguageViewModel"
 
 export async function quickDriveActions(router: Router, driveViewModel: DriveViewModel): Promise<readonly QuickAction[]> {
 	const driveTabAction: QuickAction = {
-		description: "Go to Drive", // FIXME
+		description: lang.getTranslationText("driveView_action"),
 		exec: () => router.routeTo(DRIVE_PREFIX, {}),
 	}
 
-	// FIXME: there's probably a better way to do this.
 	const inDrive = router.getFullPath().startsWith(DRIVE_PREFIX)
 
 	let driveActions: readonly QuickAction[] = []
 	if (inDrive) {
 		driveActions = [
 			{
-				description: "Drive: Create new folder", // FIXME: translate
+				description: `${lang.getTranslationText("driveView_action")}: ${lang.getTranslationText("createFolder_action")}`,
 				exec: () =>
 					showNewFolderDialog(
 						(folderName) => driveViewModel.createNewFolder(folderName),
@@ -25,7 +25,7 @@ export async function quickDriveActions(router: Router, driveViewModel: DriveVie
 					),
 			},
 			{
-				description: "Drive: Upload files", // FIXME: translate
+				description: `${lang.getTranslationText("driveView_action")}: ${lang.getTranslationText("uploadFile_action")}`,
 				exec: () => showNewFileDialog((files) => driveViewModel.uploadFiles(files)),
 			},
 		]
