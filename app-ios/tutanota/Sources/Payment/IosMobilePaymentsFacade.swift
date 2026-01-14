4d0764ab8f959d92a9763c0f15d1ad9566dd8498
mac-github <mac-github@tutao.de>
2026-01-14 16:59:06 +0100

    [ios] apps with test plan capability

--- app-ios/tutanota/Sources/Payment/IosMobilePaymentsFacade.swift
+++ app-ios/tutanota/Sources/Payment/IosMobilePaymentsFacade.swift
@@ -161,8 +161,9 @@ public class IosMobilePaymentsFacade: MobilePaymentsFacade {
 			case 12: "yearly"
 			default: fatalError("invalid plan (\(plan)) interval (\(interval))")
 			}
-
-		return "plans.\(plan).\(intervalString)"
+		let bundleID: String = Bundle.main.bundleIdentifier ?? "de.tutao.tutanota"
+		let stagingLevelString = (bundleID == "de.tutao.tutanota.test") ? "testplans" : "plans"
+		return "\(stagingLevelString).\(plan).\(intervalString)"
 	}
 
 	static func checkVerified<T>(_ result: VerificationResult<T>) -> T {
