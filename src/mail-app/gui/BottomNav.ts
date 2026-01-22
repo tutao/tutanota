5e0eb96cec99e9a8d3892fb79929a42d630da0f8
mab <mab@tutao.de>
2026-01-22 13:40:15 +0100

    improve BottomNav items spacing
    Co-authored-by: ivk <ivk@tutao.de>

--- src/mail-app/gui/BottomNav.ts
+++ src/mail-app/gui/BottomNav.ts
@@ -1,6 +1,6 @@
 import m, { Children, Component, Vnode } from "mithril"
 import { NavButton } from "../../common/gui/base/NavButton.js"
-import { font_size, size } from "../../common/gui/size"
+import { font_size } from "../../common/gui/size"
 import { CALENDAR_PREFIX, CONTACTS_PREFIX, MAIL_PREFIX, SEARCH_PREFIX } from "../../common/misc/RouteChange"
 import { FeatureType } from "../../common/api/common/TutanotaConstants"
 import { BootIcons } from "../../common/gui/base/icons/BootIcons"
@@ -25,7 +25,7 @@ function getHrefForSearch(): string {
 export class BottomNav implements Component<Attrs> {
 	view(vnode: Vnode<Attrs>): Children {
 		// Using bottom-nav class too to match it inside media queries like @print, otherwise it's not matched
-		return m("nav.bottom-nav.flex.items-center.z1", [
+		return m("nav.bottom-nav.flex.items-center.z1.gap-4.plr-4", [
 			m(NavButton, {
 				label: "emails_label",
 				icon: () => BootIcons.Mail,
