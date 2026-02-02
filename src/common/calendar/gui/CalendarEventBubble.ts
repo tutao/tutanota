140ab426f389280d6183a83fd9b55e9970c36464
and <and@tutao.de>
2026-02-02 16:06:47 +0100

    AllDay events style and styling resolution for event bubble
    - Apply ghost bubble style to continuation indicator for all-day
    like events
    - Improve styling resolution for event bubble
    
    Closes #10262
    
    Co-authored-by: jas <jas@tutao.de>

--- src/common/calendar/gui/CalendarEventBubble.ts
+++ src/common/calendar/gui/CalendarEventBubble.ts
@@ -49,16 +49,10 @@ export type CalendarEventBubbleAttrs = {
 
 export class CalendarEventBubble implements Component<CalendarEventBubbleAttrs> {
 	view({ attrs }: Vnode<CalendarEventBubbleAttrs>): Children {
-		// This helps us stop flickering in certain cases where we want to disable and re-enable fade in (ie. when dragging events)
-		// Reapplying the animation to the element will cause it to trigger instantly, so we don't want to do that
-		// const doFadeIn = !this.hasFinishedInitialRender && attrs.fadeIn
-		// const enablePointerEvents = attrs.enablePointerEvents
-		const { gridInfo, eventWrapper, verticalOverflowInfo, interactions, canReceiveFocus, baseDate, horizontalOverflowInfo, height } = attrs
-
+		const { gridInfo, eventWrapper, interactions, canReceiveFocus, baseDate, horizontalOverflowInfo } = attrs
 		const calendarEvent = eventWrapper.event
 
 		const isLongNormalEvent = !isAllDayEvent(calendarEvent) && getDiffIn60mIntervals(calendarEvent.startTime, calendarEvent.endTime) >= 24
-
 		const timeFormat = isLongNormalEvent
 			? EventTextTimeOption.START_END_TIME
 			: getTimeTextFormatForLongEvent(calendarEvent, baseDate, baseDate, getTimeZone())
@@ -66,17 +60,12 @@ export class CalendarEventBubble implements Component<CalendarEventBubbleAttrs> 
 		const eventTime = timeFormat ? formatEventTime(calendarEvent, timeFormat) : ""
 		const eventTitle = isLongNormalEvent ? `${eventTime} ${getDisplayEventTitle(calendarEvent.summary)}` : getDisplayEventTitle(calendarEvent.summary)
 
-		let textColor = !eventWrapper.flags?.isFeatured ? colorForBg(`#${eventWrapper.color}`) : undefined
-		let border = eventWrapper.flags?.isFeatured
-			? `1.5px dashed ${eventWrapper.flags?.isConflict ? theme.on_warning_container : theme.on_success_container}`
-			: "none"
-		let backgroundColor = eventWrapper.color.includes("#") ? eventWrapper.color : `#${eventWrapper.color}`
-
-		if (eventWrapper.flags?.isGhost) {
-			textColor = theme.on_surface_variant
-			border = `1px dashed ${theme.outline}`
-			backgroundColor = theme.surface_container_high
-		}
+		const resolvedStyles = this.resolveStyles(attrs)
+		const overflowIndicatorStyle = {
+			backgroundColor: resolvedStyles.backgroundColor,
+			borderRight: `1px solid ${resolvedStyles.backgroundColor}`,
+			width: px(6),
+		} satisfies Partial<CSSStyleDeclaration>
 
 		return m(
 			".flex.z2.b.darker-hover.small",
@@ -87,22 +76,14 @@ export class CalendarEventBubble implements Component<CalendarEventBubbleAttrs> 
 					minWidth: px(0),
 					gridColumn: `${gridInfo.column.start} / span ${gridInfo.column.span}`,
 					gridRow: `${gridInfo.row.start} / ${gridInfo.row.end}`,
-					color: textColor, // Why is this logic only applied for Featured bubbles? (Or is it also applied elsewhere?)
+					color: resolvedStyles.color,
 					opacity: `${eventWrapper.flags?.isTransientEvent ? TEMPORARY_EVENT_OPACITY : 1}`,
 				} satisfies Partial<CSSStyleDeclaration>,
 			},
 			[
-				horizontalOverflowInfo.start
-					? m(".event-continues-left-indicator.height-100p", {
-							style: {
-								backgroundColor: `#${eventWrapper.color}`,
-								borderRight: `1px solid #${eventWrapper.color}`,
-								width: px(6),
-							} satisfies Partial<CSSStyleDeclaration>,
-						})
-					: null,
+				horizontalOverflowInfo.start ? m(".event-continues-left-indicator.height-100p", { style: overflowIndicatorStyle }) : null,
+				// EventBubble
 				m(
-					// EventBubble
 					".pl-4.pr-4.height-100p.full-width.border-radius-4",
 					{
 						onclick: (e: MouseEvent) => {
@@ -121,23 +102,7 @@ export class CalendarEventBubble implements Component<CalendarEventBubbleAttrs> 
 						},
 						tabIndex: canReceiveFocus ? TabIndex.Default : TabIndex.Programmatic,
 						class: interactions?.click ? "cursor-pointer" : "",
-						style: {
-							height: height ? px(height) : undefined,
-							borderTop: verticalOverflowInfo.start ? "none" : undefined,
-							borderBottom: verticalOverflowInfo.end ? "none" : undefined,
-
-							"border-top-left-radius": verticalOverflowInfo.start || horizontalOverflowInfo.start ? "0" : undefined,
-							"border-top-right-radius": verticalOverflowInfo.start || horizontalOverflowInfo.end ? "0" : undefined,
-							"border-bottom-left-radius": verticalOverflowInfo.end || horizontalOverflowInfo.start ? "0" : undefined,
-							"border-bottom-right-radius": verticalOverflowInfo.end || horizontalOverflowInfo.end ? "0" : undefined,
-
-							backgroundColor: backgroundColor,
-
-							border: border,
-
-							paddingTop: "2px",
-							paddingBottom: "2px",
-						} satisfies Partial<CSSStyleDeclaration> & Record<string, any>,
+						style: resolvedStyles,
 					},
 					eventWrapper.flags.isFeatured
 						? this.renderFeaturedTexts(eventTitle, eventWrapper.flags)
@@ -149,17 +114,45 @@ export class CalendarEventBubble implements Component<CalendarEventBubbleAttrs> 
 								eventWrapper.flags,
 							),
 				),
-				horizontalOverflowInfo.end
-					? m(".event-continues-right-indicator.height-100p", {
-							style: {
-								backgroundColor: `#${eventWrapper.color}`,
-								borderLeft: `1px solid #${eventWrapper.color}`,
-								width: px(6),
-							} satisfies Partial<CSSStyleDeclaration>,
-						})
-					: null,
+				horizontalOverflowInfo.end ? m(".event-continues-right-indicator.height-100p", { style: overflowIndicatorStyle }) : null,
 			],
 		)
+	}
+
+	private resolveStyles({ eventWrapper, height, horizontalOverflowInfo, verticalOverflowInfo }: CalendarEventBubbleAttrs): Partial<CSSStyleDeclaration> {
+		const normalizedEventWrapperColor = eventWrapper.color.includes("#") ? eventWrapper.color : `#${eventWrapper.color}`
+		const defaultStyle = {
+			height: height ? px(height) : undefined,
+			color: colorForBg(normalizedEventWrapperColor),
+			backgroundColor: normalizedEventWrapperColor,
+			borderTop: verticalOverflowInfo.start ? "none" : undefined,
+			borderBottom: verticalOverflowInfo.end ? "none" : undefined,
+			"border-top-left-radius": verticalOverflowInfo.start || horizontalOverflowInfo.start ? "0" : undefined,
+			"border-top-right-radius": verticalOverflowInfo.start || horizontalOverflowInfo.end ? "0" : undefined,
+			"border-bottom-left-radius": verticalOverflowInfo.end || horizontalOverflowInfo.start ? "0" : undefined,
+			"border-bottom-right-radius": verticalOverflowInfo.end || horizontalOverflowInfo.end ? "0" : undefined,
+			paddingTop: "2px",
+			paddingBottom: "2px",
+		}
+
+		const featuredEventStyle = {
+			border: `1.5px dashed ${eventWrapper.flags?.isConflict ? theme.on_warning_container : theme.on_success_container}`,
+			color: undefined,
+		}
+
+		const ghostStyle = {
+			color: theme.on_surface_variant,
+			border: `1px dashed ${theme.outline}`,
+			borderLeft: horizontalOverflowInfo.start ? "none" : undefined,
+			borderRight: horizontalOverflowInfo.end ? "none" : undefined,
+			backgroundColor: theme.surface_container_high,
+		}
+
+		return {
+			...defaultStyle,
+			...(eventWrapper.flags?.isFeatured ? featuredEventStyle : {}),
+			...(eventWrapper.flags?.isGhost ? ghostStyle : {}),
+		}
 	}
 
 	private renderFeaturedTexts(title: string, flags: EventWrapperFlags) {
