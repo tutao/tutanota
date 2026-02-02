4ae1b5c91c4b3344ee7fa8fd6a51decdbddb4250
and <and@tutao.de>
2026-02-02 10:09:33 +0100

    Re-add logic to prevent editing ghost bubbles
    Co-authored-by: jas <jas@tutao.de>

--- src/calendar-app/calendar/gui/eventpopup/CalendarEventPreviewViewModel.ts
+++ src/calendar-app/calendar/gui/eventpopup/CalendarEventPreviewViewModel.ts
@@ -93,10 +93,11 @@ export class CalendarEventPreviewViewModel {
 		} else {
 			// partially editable (adding alarms) counts as editable.
 			this.canEdit =
-				this.eventType === EventType.OWN ||
-				this.eventType === EventType.SHARED_RW ||
-				this.eventType === EventType.LOCKED ||
-				this.eventType === EventType.INVITE
+				!this.calendarEvent.pendingInvitation &&
+				(this.eventType === EventType.OWN ||
+					this.eventType === EventType.SHARED_RW ||
+					this.eventType === EventType.LOCKED ||
+					this.eventType === EventType.INVITE)
 			this.canDelete = this.canEdit || this.eventType === EventType.INVITE
 			this.canSendUpdates = hasBusinessFeature && this.eventType === EventType.OWN && getNonOrganizerAttendees(calendarEvent).length > 0
 		}
