3c0a73620d75b3fe3e91e33a2dbc0603303eeef3
jhm <17314077+jomapp@users.noreply.github.com>
2026-02-05 11:58:38 +0100

    add tutamail and tutacalendar thunderbird add-ons v1.0.0

---
+++ thunderbird-add-ons/tutacalendar/app-calendar.js
@@ -0,0 +1,5 @@
+browser.spacesToolbar.addButton("tutacalendar", {
+	title: "Tuta Calendar",
+	defaultIcons: "icon-calendar.svg",
+	url: "https://app.tuta.com/calendar/",
+})
---
+++ thunderbird-add-ons/tutamail/app-mail.js
@@ -0,0 +1,5 @@
+browser.spacesToolbar.addButton("tutamail", {
+	title: "Tuta Mail",
+	defaultIcons: "icon-mail.svg",
+	url: "https://app.tuta.com/mail/",
+})
