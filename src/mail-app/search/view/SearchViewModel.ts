f03a890af6076b88f46bba049015efa1a2e3f26e
hrb-hub <hrb-hub@users.noreply.github.com>
2026-02-20 13:12:33 +0100

    Rename `isDeletableMail` and `areMailsDeletable`
    Rename `MailViewerViewModel#isDeletableMail` and
    `SearchViewModel#areMailsDeletable` to differentiate between a mail that
    is not deletable because the server will not allow it, and a mail that
    can be deleted but is not allowed by the client.

--- src/mail-app/search/view/SearchViewModel.ts
+++ src/mail-app/search/view/SearchViewModel.ts
@@ -830,7 +830,7 @@ export class SearchViewModel {
 		},
 	)
 
-	readonly areMailsDeletable: () => boolean = memoizedWithHiddenArgument(
+	readonly isPermanentDeleteAllowed: () => boolean = memoizedWithHiddenArgument(
 		() => this.getSelectedMails(),
 		(selectedMails) => {
 			return selectedMails.every((mail) => {
