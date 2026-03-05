397ac3207f6d7ff63c83a54ef6a42d150bfeceb8
sas <sas@tutao.de>
2026-03-05 16:26:37 +0100

    Add CTR mode to sjcl
    Fix overflow handling in sjcl counter mode
    Document changes made to CTR in SJCL
    
    tuta#2724

--- packages/tutanota-crypto/lib/internal/sjcl.js
+++ packages/tutanota-crypto/lib/internal/sjcl.js
@@ -13,7 +13,10 @@
 // - added option to not use padding in encrypt/decrypt in cbc mode
 // CHANGED (tutao.ivk)
 // - added byteOffset and byteLength to codec.arrayBuffer.toBits
-// Configured with: ./configure --with-codecArrayBuffer --with-cbc --with-sha1 --with-sha512 --with-codecBytes --without-ccm --without-ocb2 --without-pbkdf2 --without-convenience --compress=none
+// CHANGED (tutao.vaf/tutao.sas)
+// - added counter mode (CTR) from version 1.0.8 (and added it to the configuration right below)
+// - fixed counter overflow handling in CTR mode
+// Configured with: ./configure --with-codecArrayBuffer --with-cbc --with-ctr --with-sha1 --with-sha512 --with-codecBytes --without-ccm --without-ocb2 --without-pbkdf2 --without-convenience --compress=none
 
 /*jslint indent: 2, bitwise: false, nomen: false, plusplus: false, white: false, regexp: false */
 /*global document, window, escape, unescape, module, require, Uint32Array */
@@ -1660,6 +1663,82 @@ sjcl.hash.sha1.prototype = {
 		h[4] = (h[4] + e) | 0
 	},
 }
+
+/** @fileOverview CTR mode implementation
+ *
+ * @author Torben Haase
+ */
+
+/**
+ * Dangerous: CTR mode.
+ * @namespace
+ * @author Torben Haase
+ */
+sjcl.mode.ctr = {
+	/** The name of the mode.
+	 * @constant
+	 */
+	name: "ctr",
+
+	/** Encrypt in CTR mode.
+	 * @param {Object} prf The pseudorandom function.  It must have a block size of 16 bytes.
+	 * @param {bitArray} plaintext The plaintext data.
+	 * @param {bitArray} iv The initialization value.  It must be 128 bits.
+	 * @param {bitArray} [adata=[]] The authenticated data.  Must be empty.
+	 * @return The encrypted data, an array of bytes.
+	 * @throws {sjcl.exception.invalid} if the IV isn't exactly 128 bits or if any adata is specified.
+	 */
+	encrypt: function(prf, plaintext, iv, adata) {
+		return sjcl.mode.ctr._calculate(prf, plaintext, iv, adata);
+	},
+
+	/** Decrypt in CTR mode.
+	 * @param {Object} prf The pseudorandom function.  It must have a block size of 16 bytes.
+	 * @param {bitArray} ciphertext The ciphertext data.
+	 * @param {bitArray} iv The initialization value.  It must be 128 bits.
+	 * @param {bitArray} [adata=[]] The authenticated data.  It must be empty.
+	 * @return The decrypted data, an array of bytes.
+	 * @throws {sjcl.exception.invalid} if the IV isn't exactly 128 bits or if any adata is specified.
+	 * @throws {sjcl.exception.corrupt} if if the message is corrupt.
+	 */
+	decrypt: function(prf, ciphertext, iv, adata) {
+		return sjcl.mode.ctr._calculate(prf, ciphertext, iv, adata);
+	},
+
+	_calculate: function(prf, data, iv, adata) {
+		var l, bl, res, c, d, e, i;
+		if (adata && adata.length) {
+			throw new sjcl.exception.invalid("ctr can't authenticate data");
+		}
+		if (sjcl.bitArray.bitLength(iv) !== 128) {
+			throw new sjcl.exception.invalid("ctr iv must be 128 bits");
+		}
+		if (!(l = data.length)) {
+			return [];
+		}
+		c = iv.slice(0);
+		d = data.slice(0);
+		bl = sjcl.bitArray.bitLength(d);
+		for (i=0; i<l; i+=4) {
+			e = prf.encrypt(c);
+			d[i] ^= e[0];
+			d[i+1] ^= e[1];
+			d[i+2] ^= e[2];
+			d[i+3] ^= e[3];
+			for(var carry = 3; carry >= 0; carry--) {
+				// TUTAO: fix handling of counter overflow
+				// javascript numbers can be more than 4 byte
+				if (++c[carry] > 0xffffffff ) {
+					c[carry] = 0
+				} else {
+					break;
+				}
+			}
+		}
+		return sjcl.bitArray.clamp(d, bl);
+	}
+};
+
 /** @fileOverview CBC mode implementation
  *
  * @author Emily Stark
