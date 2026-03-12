18df698c1227a27fe1baf06b222b55365d30f9b5
wrd <wrd@tutao.de>
2026-03-12 12:12:05 +0100

    Keep track of removed attachments so undoing a removed attachment works
    Close #10468
    
    Co-authored-by: hrb-hub <hrb-hub@users.noreply.github.com>

--- src/mail-app/mail/editor/MailEditor.ts
+++ src/mail-app/mail/editor/MailEditor.ts
@@ -179,7 +179,6 @@ export class MailEditor implements Component<MailEditorAttrs> {
 		bcc: stream(""),
 	}
 
-	mentionedInlineImages: Array<{ cid: string; url: string }>
 	templateModel: TemplatePopupModel | null
 	knowledgeBaseInjection: DialogInjectionRightAttrs<KnowledgebaseDialogContentAttrs> | null = null
 	sendMailModel: SendMailModel
@@ -199,7 +198,6 @@ export class MailEditor implements Component<MailEditorAttrs> {
 	constructor(vnode: Vnode<MailEditorAttrs>) {
 		const a = vnode.attrs
 		this.attrs = a
-		this.mentionedInlineImages = []
 		const model = a.model
 		this.sendMailModel = model
 		this.templateModel = a.templateModel
@@ -247,7 +245,7 @@ export class MailEditor implements Component<MailEditorAttrs> {
 		)
 
 		const onEditorChanged = () => {
-			cleanupInlineAttachments(this.editor.getDOM(), this.mentionedInlineImages, model.getAttachments())
+			cleanupInlineAttachments(this.editor.getDOM(), model.getAttachments(), model.getRemovedInlineImages())
 			model.markAsChangedIfNecessary(true)
 			m.redraw()
 		}
@@ -542,7 +540,7 @@ export class MailEditor implements Component<MailEditorAttrs> {
 			oninput: (val) => model.setSubject(val),
 		}
 
-		const attachmentBubbleAttrs = createAttachmentBubbleAttrs(model, this.mentionedInlineImages, () => {
+		const attachmentBubbleAttrs = createAttachmentBubbleAttrs(model, () => {
 			return this.editor.getDOM()
 		})
 
@@ -870,7 +868,7 @@ export class MailEditor implements Component<MailEditorAttrs> {
 	}
 
 	private processInlineImages() {
-		this.mentionedInlineImages = replaceCidsWithInlineImages(this.editor.getDOM(), this.sendMailModel.loadedInlineImages, (cid, event, dom) => {
+		replaceCidsWithInlineImages(this.editor.getDOM(), this.sendMailModel.loadedInlineImages, (cid, event, dom) => {
 			const downloadClickHandler = createDropdown({
 				lazyButtons: () => [
 					{
@@ -943,7 +941,6 @@ export class MailEditor implements Component<MailEditorAttrs> {
 		for (const file of files) {
 			const img = createInlineImage(file as DataFile)
 			model.loadedInlineImages.set(img.cid, img)
-			this.mentionedInlineImages.push({ cid: img.cid, url: img.objectUrl })
 			this.editor.insertImage(img.objectUrl, {
 				cid: img.cid,
 				style: "max-width: 100%",
