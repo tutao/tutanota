f859863c8e8976c0a68327f4c01f3450c21e9c1b
mab <mab@tutao.de>
2026-03-12 12:10:35 +0100

    Drive: rework mime type processing
    Before, mime types were separately matched to both a file format and
    an icon.
    
    Now, mime types are matched to `DisplayFileType`, a bundle containing
    both a file type and a file format, with an option to provide overrides
    of file format depending on the file's suffix. Then, the file type is
    used for getting a matching icon.

---
+++ src/drive-app/drive/model/DriveMimeUtils.ts
@@ -0,0 +1,95 @@
+import { getFileExtension } from "../../../common/api/common/utils/FileUtils"
+import { Icons } from "../../../common/gui/base/icons/Icons"
+
+export enum FileType {
+	Default,
+	Document,
+	Image,
+	Audio,
+	Video,
+}
+
+export type FileFormat =
+	// Document
+	| "PDF"
+	| "Text"
+
+	// Image
+	| "JPEG"
+	| "PNG"
+	| "SVG"
+
+	// Audio
+	| "MPEG"
+	| "AAC"
+	| "Wave"
+	| "MP3"
+
+	// Video
+	| "MP4"
+
+// Heads up: when adding new mime types, test with different browsers, as they
+// sometimes have their own opinions on which mime type a file should get.
+const mimeTypes: Record<string, DisplayFileType> = {
+	// Document
+	"application/pdf": { fileType: FileType.Document, fileFormat: "PDF" },
+	"text/plain": { fileType: FileType.Document, fileFormat: "Text" },
+
+	// Image
+	"image/jpeg": { fileType: FileType.Image, fileFormat: "JPEG" },
+	"image/png": { fileType: FileType.Image, fileFormat: "PNG" },
+	"image/svg+xml": { fileType: FileType.Image, fileFormat: "SVG" },
+
+	// Audio
+	"audio/mpeg": { fileType: FileType.Audio, fileFormat: "MPEG", overrides: [{ extension: ".mp3", fileFormat: "MP3" }] },
+	"audio/mp4": { fileType: FileType.Audio, fileFormat: "AAC" },
+	"audio/wav": { fileType: FileType.Audio, fileFormat: "Wave" },
+
+	// Video
+	// browsers cannot distinguish e.g. H.264 or AV1 from "classic" MPEG-4 video, so we always show "MP4". Sigh.
+	"video/mp4": { fileType: FileType.Video, fileFormat: "MP4" },
+}
+
+// Sometimes the browser only provides a generic mime type but more information
+// can be inferred by looking at the file extension, e.g. for MP3.
+export type FileFormatOverride = {
+	extension: string
+	fileFormat: FileFormat
+}
+
+export type DisplayFileType = {
+	fileType: FileType
+	fileFormat: FileFormat
+
+	overrides?: FileFormatOverride[]
+}
+
+export function getFileIcon(displayFileType: DisplayFileType): Icons {
+	if (displayFileType.fileType === FileType.Document && displayFileType.fileFormat === "PDF") {
+		return Icons.PDFFilled
+	} else if (displayFileType.fileType === FileType.Document && displayFileType.fileFormat === "Text") {
+		return Icons.TextFilled
+	} else if (displayFileType.fileType === FileType.Image) {
+		return Icons.PictureFilled
+	} else if (displayFileType.fileType === FileType.Audio) {
+		return Icons.AudioFilled
+	} else if (displayFileType.fileType === FileType.Video) {
+		return Icons.VideoFilled
+	} else {
+		return Icons.EmptyDocumentFilled
+	}
+}
+
+export function getDisplayType(mimeType: string, fileName?: string) {
+	const displayType = mimeTypes[mimeType]
+
+	if (fileName && displayType?.overrides) {
+		const fileExtension = getFileExtension(fileName)
+		const override = displayType.overrides.filter((override) => override.extension === fileExtension).at(0)
+		if (override) {
+			return { fileType: displayType.fileType, fileFormat: override.fileFormat }
+		}
+	}
+
+	return displayType ?? { fileType: FileType.Default, fileFormat: "File" }
+}
