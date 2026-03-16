b22606c5403ee8f48a490cdec4f10f162536d872
bir <bir@tutao.de>
2026-03-16 15:57:20 +0100

    Add `Undo Send` in quick actions
    Close: #10492

--- src/common/settings/SettingsQuickActions.ts
+++ src/common/settings/SettingsQuickActions.ts
@@ -137,6 +137,10 @@ function emailSettings(router: Router): readonly QuickAction[] {
 			description: `${emailSettingsLabel} ${lang.getTranslationText("outOfOfficeNotification_title")}`,
 			exec: () => routeToFolderSection(router, folder, "outofoffice"),
 		},
+		{
+			description: `${emailSettingsLabel} ${lang.getTranslationText("undoSend_label")}`,
+			exec: () => routeToFolderSection(router, folder, "undoSend"),
+		},
 		!isBrowser() && !(env.mode === Mode.Admin)
 			? {
 					description: `${emailSettingsLabel} ${lang.getTranslationText("localDataSection_label")}`,
