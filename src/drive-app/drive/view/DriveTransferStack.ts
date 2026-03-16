5bf289fcd0398f8bec152a64ea28a8e7af2663f0
wrd <wrd@tutao.de>
2026-03-16 12:06:15 +0100

    Make Drive transfer progress bar generic

--- src/drive-app/drive/view/DriveTransferStack.ts
+++ src/drive-app/drive/view/DriveTransferStack.ts
@@ -1,8 +1,10 @@
 import m, { Children, Component, Vnode } from "mithril"
 import { DriveTransferState } from "./DriveTransferController"
-import { DriveTransferBox, DriveTransferBoxAttrs } from "./DriveTransferBox"
 import { px, size } from "../../../common/gui/size"
 import { TransferId } from "../../../common/api/common/drive/DriveTypes"
+import { ProgressSnackBar, ProgressSnackBarAttrs, ProgressState } from "../../../common/gui/ProgressSnackBar"
+import { TranslationKeyType } from "../../../common/misc/TranslationKey"
+import { lang, Translation } from "../../../common/misc/LanguageViewModel"
 
 export interface DriveTransferStackAttrs {
 	transfers: readonly DriveTransferState[]
@@ -22,12 +24,42 @@ export class DriveTransferStack implements Component<DriveTransferStackAttrs> {
 				},
 			},
 			transfers.map((transferState) => {
-				return m(DriveTransferBox, {
+				return m(ProgressSnackBar, {
 					key: transferState.id,
-					transferState,
+					mainText: transferState.filename,
+					infoText: this.getStatusText(transferState.type, transferState.state),
+					progressState: this.getProgressState(transferState.state),
+					percentage: Math.min(Math.round((transferState.transferredSize / transferState.totalSize) * 100), 100),
 					onCancel: () => cancelTransfer(transferState.id),
-				} satisfies DriveTransferBoxAttrs & { key: string })
+				} satisfies ProgressSnackBarAttrs & { key: string })
 			}),
 		)
+	}
+
+	private getProgressState(state: DriveTransferState["state"]): ProgressState {
+		switch (state) {
+			case "active":
+			case "waiting":
+				return ProgressState.running
+			case "failed":
+				return ProgressState.error
+			case "finished":
+				return ProgressState.done
+		}
+	}
+
+	private getStatusText(type: "upload" | "download", state: DriveTransferState["state"]): Translation {
+		let translationKey: TranslationKeyType
+		if (state === "failed") {
+			translationKey = "transferFailed_msg"
+		} else if (state === "waiting") {
+			translationKey = "transferWaiting_msg"
+		} else if (state === "active") {
+			translationKey = type === "upload" ? "uploadInProgress_msg" : "downloadInProgress_msg"
+		} else {
+			translationKey = type === "upload" ? "uploadCompleted_msg" : "downloadCompleted_msg"
+		}
+
+		return lang.getTranslation(translationKey)
 	}
 }
