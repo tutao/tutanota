b017c068ef95b9c418a4b78def0680836ee09a05
jenkins build server <jenkins@next.tutao.de>
2026-03-16 15:05:16 +0100

    v336.260316.0

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 246
-		versionName = "336.260312.0"
+		versionCode = 247
+		versionName = "336.260316.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
