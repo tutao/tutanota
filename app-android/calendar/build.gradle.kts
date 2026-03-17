dc616170b745e0a10ac8b73ebf448f79619cd43e
nig <nig@tutao.de>
2026-03-17 14:51:31 +0100

    v338.260317.0

--- app-android/calendar/build.gradle.kts
+++ app-android/calendar/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 		applicationId = "de.tutao.calendar"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 248
-		versionName = "336.260317.0"
+		versionCode = 249
+		versionName = "338.260317.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
