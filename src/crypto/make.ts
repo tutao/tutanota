2bb2f377017c69f41ed3e7035bad3946b2111efc
sug <sug@tutao.de>
2026-03-19 11:01:23 +0100

    Add make file to genetrate wasm for src/crypto module
    Signed-off-by: sug <sug@tutao.de>

---
+++ src/crypto/make.ts
@@ -0,0 +1,16 @@
+import { program } from "commander"
+import { $, usePowerShell } from "zx"
+
+const CRYPTO_PRIMITIVES_CRATE = "../../tuta-sdk/rust/crypto-primitives"
+const WASM_PACK_OUT_DIR = "../../../src/crypto/encryption/ed25519wasm"
+
+async function run() {
+	if (process.platform === "win32") {
+		usePowerShell()
+	}
+
+	// note: --out-dir is relative to the rust package in `tuta-sdk/rust/crypto_primitives` (CRYPTO_PRIMITIVES_CRATE)
+	await $`npx wasm-pack build --target web --profile release-wasm ${CRYPTO_PRIMITIVES_CRATE} --out-dir ${WASM_PACK_OUT_DIR}`
+}
+
+await program.usage("").action(run).parseAsync(process.argv)
