513b154389c88fed66aef96169ffe279cd8af819
mab <mab@tutao.de>
2026-03-19 17:02:50 +0100

    restore updated design for ProgressSnackBar
    somehow got lost during a rewrite
    
    Co-authored-by: ivk <ivk@tutao.de>
    Co-authored-by: usa <usa@tutao.de>

--- src/common/gui/ProgressSnackBar.ts
+++ src/common/gui/ProgressSnackBar.ts
@@ -4,8 +4,9 @@ import { boxShadowHigh } from "./main-styles"
 import { component_size, px, size } from "./size"
 import { IconButton } from "./base/IconButton"
 import { Icons } from "./base/icons/Icons"
-import { lang, Translation } from "../misc/LanguageViewModel"
+import { Translation } from "../misc/LanguageViewModel"
 import { Icon, IconSize } from "./base/Icon"
+import { ButtonSize } from "./base/ButtonSize"
 
 export enum ProgressState {
 	done,
@@ -22,41 +23,37 @@ export interface ProgressSnackBarAttrs {
 }
 
 export class ProgressSnackBar implements Component<ProgressSnackBarAttrs> {
-	view({ attrs }: Vnode<ProgressSnackBarAttrs>): Children {
+	view({ attrs: { infoText, mainText, onCancel, progressState, percentage } }: Vnode<ProgressSnackBarAttrs>): Children {
 		return m(
 			".flex.col.border-radius",
 			{
 				style: {
 					background: theme.surface,
 					"box-shadow": boxShadowHigh,
-					paddingTop: px(size.spacing_16),
-					paddingBottom: px(size.spacing_16),
-					paddingLeft: px(size.spacing_12),
-					paddingRight: px(size.spacing_12),
+					padding: px(size.spacing_12),
 				},
 			},
 			[
 				m(".flex.row.items-center.justify-between.items-center", [
 					m(".flex.items-center.gap-16.overflow-hidden", [
-						attrs.progressState === ProgressState.running
-							? this.renderProgress(attrs.percentage)
-							: this.renderTerminateStateIcon(attrs.progressState),
+						this.renderProgress(progressState, percentage),
 						m(".flex.col.gap-8.flex-shrink.overflow-hidden", [
-							m(".font-weight-500.text-ellipsis", attrs.mainText),
-							attrs.infoText ? m(".small", { "data-testid": attrs.infoText.testId }, attrs.infoText.text) : null,
+							m(".font-weight-500.text-ellipsis", mainText),
+							infoText ? m(".small", { "data-testid": infoText.testId }, infoText.text) : null,
 						]),
 					]),
 
-					attrs.progressState === ProgressState.running
+					progressState === ProgressState.running
 						? m(IconButton, {
-								click: () => attrs.onCancel(),
+								click: () => onCancel(),
 								icon: Icons.X,
-								title: lang.makeTranslation("cancel", () => "cancel"),
+								title: "cancel_action",
+								size: ButtonSize.Large,
 							})
 						: m("", {
 								style: {
-									width: px(component_size.button_height),
-									height: px(component_size.button_height),
+									width: px(component_size.button_floating_size),
+									height: px(component_size.button_floating_size),
 								},
 							}),
 				]),
@@ -64,7 +61,16 @@ export class ProgressSnackBar implements Component<ProgressSnackBarAttrs> {
 		)
 	}
 
-	private renderProgress(percentage: number): Children {
+	private renderProgress(state: ProgressState, percentage: number): Children {
+		let indicatorColor = theme.on_surface_variant
+		if (state === ProgressState.error) {
+			indicatorColor = theme.error
+			percentage = 100
+		} else if (state === ProgressState.done) {
+			indicatorColor = theme.success
+			percentage = 100
+		}
+
 		return m(
 			".flex.justify-center.items-center.no-shrink",
 			{
@@ -78,32 +84,23 @@ export class ProgressSnackBar implements Component<ProgressSnackBarAttrs> {
 					height: px(component_size.button_height),
 					borderRadius: "50%",
 					// drawing a circle on the inside and a colored circle on the outside (with the rest filled with transparent)
-					background: `radial-gradient(closest-side, ${theme.surface} 79%, transparent 80% 100%), conic-gradient(${theme.on_surface_variant} calc(var(--progress-value) * 1%), transparent 0)`,
+					background: `radial-gradient(closest-side, ${theme.surface} 79%, transparent 80% 100%), conic-gradient(${indicatorColor} calc(var(--progress-value) * 1%), transparent 0)`,
 					transition: "--progress-value 200ms",
 				},
 			},
-			m(".small.font-weight-500", `${percentage}%`),
+			state === ProgressState.running ? m(".small.font-weight-500", `${percentage}%`) : this.renderTerminateStateIcon(state),
 		)
 	}
 
 	private renderTerminateStateIcon(state: ProgressState): Children {
-		const [color, icon] = state === ProgressState.done ? [theme.success, Icons.SuccessFilled] : [theme.error, Icons.FailureFilled]
+		const [color, icon] = state === ProgressState.done ? [theme.success, Icons.Checkmark] : [theme.error, Icons.X]
 
-		return m(
-			".flex.justify-center.items-center",
-			{
-				style: {
-					width: px(component_size.button_height),
-					height: px(component_size.button_height),
-				},
+		return m(Icon, {
+			icon,
+			size: IconSize.PX32,
+			style: {
+				fill: color,
 			},
-			m(Icon, {
-				icon,
-				size: IconSize.PX32,
-				style: {
-					fill: color,
-				},
-			}),
-		)
+		})
 	}
 }
