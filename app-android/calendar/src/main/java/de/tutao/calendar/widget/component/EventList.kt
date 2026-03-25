c76a1220ec686dba13c13b607607d0dc7f9f98a3
and <and@tutao.de>
2026-03-25 14:38:00 +0100

    [Android] Fix white spacing when widget has 5+ events on future day
    Closes #10552
    
    Co-authored-by: jas <jas@tutao.de>

--- app-android/calendar/src/main/java/de/tutao/calendar/widget/component/EventList.kt
+++ app-android/calendar/src/main/java/de/tutao/calendar/widget/component/EventList.kt
@@ -20,30 +20,29 @@ fun EventList(
 	currentDay: LocalDateTime,
 	paddingEndForFirstElement: Int = 0
 ) {
-	// we need to chunk events because columns inside scrollable elements doesn't support more than five children
-	// lazy lists also doesn't support more than 10 elements
-	val chunked = normalEvents.chunked(5)
-	val eventGroups = chunked.subList(
-		0,
-		if (chunked.size > 10) 10 else chunked.size
-	)
-	eventGroups.forEachIndexed { chunkIndex, events ->
-		Column {
-			events.forEachIndexed { elementIndexInChung, event ->
-				var paddingEnd = 0
-				if (chunkIndex + elementIndexInChung == 0) {
-					paddingEnd = paddingEndForFirstElement
+	// we need to chunk events because columns  can only have 10 children
+	val chunked = normalEvents.chunked(10)
+
+	Column {
+		chunked.forEachIndexed { chunkIndex, events ->
+			Column {
+				events.forEachIndexed { elementIndexInChunk, event ->
+					var paddingEnd = 0
+					if (chunkIndex + elementIndexInChunk == 0) {
+						paddingEnd = paddingEndForFirstElement
+					}
+
+					EventRow(
+						modifier = GlanceModifier.defaultWeight()
+							.padding(
+								top = Dimensions.Spacing.space_4.dp,
+								bottom = Dimensions.Spacing.space_4.dp,
+								end = paddingEnd.dp
+							),
+						event,
+						openEventAction = openCalendarAgenda(LocalContext.current, userId, currentDay, event.eventId)
+					)
 				}
-				EventRow(
-					modifier = GlanceModifier.defaultWeight()
-						.padding(
-							top = Dimensions.Spacing.space_4.dp,
-							bottom = Dimensions.Spacing.space_4.dp,
-							end = paddingEnd.dp
-						),
-					event,
-					openEventAction = openCalendarAgenda(LocalContext.current, userId, currentDay, event.eventId)
-				)
 			}
 		}
 	}
