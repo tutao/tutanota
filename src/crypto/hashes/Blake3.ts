e18a7b0bc1043322ee7e8a4d2ab5ddf2c3321c05
Kinan <104761667+kibibytium@users.noreply.github.com>
2026-03-31 10:55:39 +0200

    split and move tutanota-crypto to src/crypto and test/crypto
    Co-authored-by: map <mpfau@users.noreply.github.com>
    Co-authored-by: sug <sug@tutao.de>
    Co-authored-by: nif <nif@tutao.de>

---
+++ src/crypto/hashes/Blake3.ts
@@ -0,0 +1,42 @@
+import { blake3 } from "../internal/noble-hashes-2.0.1.js"
+import sjcl from "../internal/sjcl.js"
+import { CryptoError } from "@tutao/crypto/error"
+import { MacTag } from "../misc/Constants.js"
+import { stringToUtf8Uint8Array } from "@tutao/utils"
+
+export const DEFAULT_BLAKE3_OUTPUT_LENGTH_BYTES = 32
+
+/**
+ * Compute a 32 byte BLAKE3 hash.
+ */
+export function blake3Hash(data: Uint8Array) {
+	return blake3(data, { dkLen: DEFAULT_BLAKE3_OUTPUT_LENGTH_BYTES })
+}
+
+/**
+ * Create a 32 byte BLAKE3 tag over the given data using the given key.
+ */
+export function blake3Mac(keyBytes: Uint8Array, data: Uint8Array): MacTag {
+	return blake3(data, { dkLen: DEFAULT_BLAKE3_OUTPUT_LENGTH_BYTES, key: keyBytes }) as MacTag
+}
+
+/**
+ * Verify a BLAKE3 tag against the given data and key.
+ * @throws CryptoError if the tag does not match the data and key.
+ */
+export function blake3MacVerify(keyBytes: Uint8Array, data: Uint8Array, tag: MacTag) {
+	const computedTag = blake3Mac(keyBytes, data)
+	if (!sjcl.bitArray.equal(computedTag, tag)) {
+		throw new CryptoError("invalid mac")
+	}
+}
+
+/**
+ * Derive key bytes from the given input key material and context
+ * @param inputKeyMaterial Input Key Material
+ * @param context
+ * @param desiredLengthBytes
+ */
+export function blake3Kdf(inputKeyMaterial: Uint8Array, context: string, desiredLengthBytes: number): Uint8Array {
+	return blake3(inputKeyMaterial, { dkLen: desiredLengthBytes, context: stringToUtf8Uint8Array(context) })
+}
