2d6c1a06e727df39bc92b779c7ea0774f83b96cd
jas <jas@tutao.de>
2026-04-01 10:48:00 +0200

    Simplify UI state processing & fix DST date rendering issue.
    Events happening on the first day of DST were rendering
    as though they were 1 day earlier.
    
    Also refactored widget loading with helper function for
    readability.
    
    Fixes #10565
    
    Co-authored-by: and <and@tutao.de>

--- app-android/calendar/src/main/java/de/tutao/calendar/widget/data/UIEvent.kt
+++ app-android/calendar/src/main/java/de/tutao/calendar/widget/data/UIEvent.kt
@@ -3,13 +3,19 @@ package de.tutao.calendar.widget.data
 import de.tutao.tutasdk.GeneratedId
 import de.tutao.tutashared.IdTuple
 
+
+/**
+ * UI representation of an calendar event
+ *
+ * Contains all the necessary information to render an event in the Widget UI
+ */
 data class UIEvent(
 	val calendarId: GeneratedId,
 	val eventId: IdTuple?,
 	val calendarColor: String,
 	val summary: String,
-	val startTime: String,
-	val endTime: String,
-	val isAllDay: Boolean,
+	val formattedStartTime: String,
+	val formattedEndTime: String,
+	val isDisplayedAsAllDay: Boolean,
 	val isBirthday: Boolean = false
 )
