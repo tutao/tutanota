387fdb1a4a49ac105ed33eda6b1c67570bb39878
sug <sug@tutao.de>
2026-04-16 13:44:02 +0200

    Fix typescript warnings
    - export typescript types as type
    - do not statically import IdentityKeyTrustDatabase which was
    meant to be imported dynamically
    
    Signed-off-by: sug <sug@tutao.de>

--- src/common/api/worker/offline/migrations/offline-v8.ts
+++ src/common/api/worker/offline/migrations/offline-v8.ts
@@ -1,11 +1,12 @@
 import { OfflineMigration } from "../OfflineStorageMigrator.js"
-import { OfflineStorage, TableDefinitions } from "../OfflineStorage.js"
-import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade"
-import { KeyVerificationTableDefinitions } from "../../facades/IdentityKeyTrustDatabase"
+import { OfflineStorage } from "../OfflineStorage.js"
+import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade.js"
 
 export const offline8: OfflineMigration = {
 	version: 8,
 	async migrate(storage: OfflineStorage, sqlCipherFacade: SqlCipherFacade) {
+		const { KeyVerificationTableDefinitions } = await import("../../facades/IdentityKeyTrustDatabase.js")
+
 		console.log("migrating from trusted_identities to identity_store")
 		await sqlCipherFacade.run(`DROP TABLE IF EXISTS trusted_identities`, [])
 		await sqlCipherFacade.run(KeyVerificationTableDefinitions.identity_store.definition, [])
