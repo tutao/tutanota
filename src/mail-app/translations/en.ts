aadc5ea5a40d2c0814035785e0fddf040792e35e
jenkins build server <jenkins@next.tutao.de>
2026-04-29 18:04:21 +0200

    update translations

--- src/mail-app/translations/en.ts
+++ src/mail-app/translations/en.ts
@@ -2268,6 +2268,6 @@ export default {
 		"yourFolders_action": "YOUR FOLDERS",
 		"yourMessage_label": "Your message",
 		"zoomIn_action": "Zoom In",
-		"zoomOut_action": "Zoom Out",
+		"zoomOut_action": "Zoom Out"
 	}
 }
