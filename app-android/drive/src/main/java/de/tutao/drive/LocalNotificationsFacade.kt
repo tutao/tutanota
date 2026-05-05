119bff6ad8f978c99884bb923c8d8873ec2d6234
usa <usa@tutao.de>
2026-05-05 15:51:12 +0200

    [android] Drive: update showDownloadNotifications for older versions
    #3395
    
    Co-authored-by: mab <mab@tutao.de>

--- app-android/drive/src/main/java/de/tutao/drive/LocalNotificationsFacade.kt
+++ app-android/drive/src/main/java/de/tutao/drive/LocalNotificationsFacade.kt
@@ -6,12 +6,18 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.pm.PackageManager
+import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import de.tutao.tutashared.file.FileNotificationSender
+import de.tutao.tutashared.file.getMimeType
 import de.tutao.tutashared.push.SseStorage
 import java.io.File
 import kotlin.math.abs
+
+private const val DOWNLOAD_NOTIFICATION_CHANNEL_ID = "downloads"
 
 class LocalNotificationsFacade(private val context: Context, private val sseStorage: SseStorage) :
 	FileNotificationSender {
@@ -20,7 +26,7 @@ class LocalNotificationsFacade(private val context: Context, private val sseStor
 	override fun sendDownloadFinishedNotification(fileName: String?) {
 		val notificationManager = NotificationManagerCompat.from(context)
 		val channel = NotificationChannel(
-			"downloads",
+			DOWNLOAD_NOTIFICATION_CHANNEL_ID,
 			"Downloads",
 			NotificationManager.IMPORTANCE_DEFAULT
 		)
@@ -35,10 +41,21 @@ class LocalNotificationsFacade(private val context: Context, private val sseStor
 		if (checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
 			return
 		}
-		notificationManager.notify(driveNotificationId("downloads"), notification)
+		notificationManager.notify(driveNotificationId(DOWNLOAD_NOTIFICATION_CHANNEL_ID), notification)
 	}
 
 	override fun showDownloadNotification(file: File) {
-		TODO("Not yet implemented")
+		val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+		val uri = FileProvider.getUriForFile(context, BuildConfig.FILE_PROVIDER_AUTHORITY, file)
+		val mimeType = getMimeType(file.toUri(), context)
+		notificationManager.notify(
+			System.currentTimeMillis().toInt(),
+			NotificationCompat.Builder(context, DOWNLOAD_NOTIFICATION_CHANNEL_ID)
+				.setSmallIcon(R.drawable.ic_download)
+				.setContentTitle(context.getString(R.string.downloadCompleted_msg))
+				.setContentText(file.name)
+				.setAutoCancel(true)
+				.build()
+		)
 	}
 }
