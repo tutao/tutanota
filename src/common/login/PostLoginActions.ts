82a0afa7e64feb7a9836f2872786ee7abc384032
mab <mab@tutao.de>
2026-05-08 17:46:33 +0200

    move registering push service into its own PostLoginAction
    Co-authored-by: ivk <ivk@tutao.de>

--- src/common/login/PostLoginActions.ts
+++ src/common/login/PostLoginActions.ts
@@ -34,7 +34,6 @@ import { showSnackBar } from "../gui/base/SnackBar"
 import { SyncDonePriority, SyncTracker } from "../api/main/SyncTracker"
 import { showRequestPasswordDialog } from "../misc/passwords/PasswordRequestDialog"
 import { LoginFacade } from "../api/worker/facades/LoginFacade"
-import { client } from "../misc/ClientDetector"
 
 /**
  * This is a collection of all things that need to be initialized/global state to be set after a user has logged in successfully.
@@ -140,20 +139,6 @@ export class PostLoginActions implements PostLoginAction {
 		}
 
 		if (isApp() || isDesktop()) {
-			// Do not try to register for notifications while the setup dialog
-			// is being shown because we might not have a permission yet and
-			// we don't want to ask for it while dialog is shown, we will ask in
-			// the dialog anyway.
-			// After dialog is finished or dismissed the setup is "complete".
-			//FIXME
-			if ((isApp() && deviceConfig.getIsSetupComplete() && !client.isDriveApp()) || isDesktop()) {
-				// Await the push service registration so `storePushIdentifierLocally()` can set the extended notification mode on Android
-				// before `loadNewsIds()` runs the `isShown()` check of the `RichNotificationsNews` news item
-				await locator.pushService.register()
-			} else {
-				console.log("Skipping registering for notifications while setup dialog is shown")
-			}
-
 			this.handleExternalSync()
 		}
 
---
+++ src/common/native/main/RegisterPushServicePostLoginAction.ts
@@ -0,0 +1,26 @@
+import { LoggedInEvent, PostLoginAction } from "../../api/main/LoginController"
+import { NativePushServiceApp } from "./NativePushServiceApp"
+import { DeviceConfig } from "../../misc/DeviceConfig"
+
+export class RegisterPushServicePostLoginAction implements PostLoginAction {
+	constructor(
+		private readonly deviceConfig: DeviceConfig,
+		private readonly pushService: NativePushServiceApp,
+	) {}
+
+	async onPartialLoginSuccess(_: LoggedInEvent): Promise<void> {}
+	async onFullLoginSuccess(_: LoggedInEvent): Promise<void> {
+		// Do not try to register for notifications while the setup dialog
+		// is being shown because we might not have a permission yet and
+		// we don't want to ask for it while dialog is shown, we will ask in
+		// the dialog anyway.
+		// After dialog is finished or dismissed the setup is "complete".
+		if (this.deviceConfig.getIsSetupComplete()) {
+			// Await the push service registration so `storePushIdentifierLocally()` can set the extended notification mode on Android
+			// before `loadNewsIds()` runs the `isShown()` check of the `RichNotificationsNews` news item
+			await this.pushService.register()
+		} else {
+			console.log("Skipping registering for notifications while setup dialog is shown")
+		}
+	}
+}
