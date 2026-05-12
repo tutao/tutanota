22dd85a1159f3a900e044866ad6dea2837b8b331
paw <104824185+paw-hub@users.noreply.github.com>
2026-05-12 13:31:42 +0200

    Create new mail indexer for full mailbox archival
    Also remove OfflineStorageCleaner
    
    Co-authored-by: bir <bir@tutao.de>
    Co-authored-by: hrb-hub <hrb-hub@users.noreply.github.com>
    
    Close #10722

--- test/tests/api/worker/search/IndexedDbIndexerTest.ts
+++ test/tests/api/worker/search/IndexedDbIndexerTest.ts
@@ -29,6 +29,7 @@ import { CachingStatus, EntityUpdateData, entityUpdateToUpdateData } from "../..
 import { GroupType } from "../../../../../src/entities/sys/Utils"
 import { decryptKey, encryptKey } from "../../../../../src/platform-kit/crypto/instance-pipeline-crypto/KeyEncryption"
 import { aesEncrypt } from "../../../../../src/platform-kit/crypto/instance-pipeline-crypto/Aes"
+import { WebMailIndexer } from "../../../../../src/applications/mail-app/workerUtils/index/WebMailIndexer"
 
 const SERVER_TIME = new Date("1994-06-08").getTime()
 const serverDateProvider: DateProvider = {
@@ -56,7 +57,7 @@ o.spec("IndexedDbIndexer", () => {
 	}
 
 	let keyLoaderFacade: KeyLoaderFacade
-	let mailIndexer: MailIndexer
+	let mailIndexer: WebMailIndexer
 	let contactIndexer: ContactIndexer
 	let idbStub = new DbStub()
 	let dbWithStub: EncryptedDbWrapper
@@ -591,7 +592,7 @@ o.spec("IndexedDbIndexer", () => {
 
 			await indexer._processEntityEvents(batch)
 			verify(core.putLastBatchIdForGroup(groupId, batchId))
-			verify(mailIndexer.processEntityEvents(events, groupId, batchId), { times: 1 })
+			verify(mailIndexer.processEntityEvents(events), { times: 1 })
 			verify(mailIndexer.afterMailCreated(["mail-list", "mail-create"]), { times: 1 })
 			verify(mailIndexer.afterMailUpdated(["mail-list", "mail-update"]), { times: 1 })
 			verify(mailIndexer.afterMailDeleted(["mail-list", "mail-delete"]), { times: 1 })
@@ -724,11 +725,11 @@ o.spec("IndexedDbIndexer", () => {
 			await indexer.eventQueue.waitForEmptyQueue()
 
 			verify(core.putLastBatchIdForGroup(groupId, batchId1))
-			verify(mailIndexer.processEntityEvents(events1, groupId, batchId1))
+			verify(mailIndexer.processEntityEvents(events1))
 			verify(mailIndexer.afterMailCreated(["list-id", "id-1"]))
 
 			verify(core.putLastBatchIdForGroup(groupId, batchId2))
-			verify(mailIndexer.processEntityEvents(events2, groupId, batchId2))
+			verify(mailIndexer.processEntityEvents(events2))
 			verify(mailIndexer.afterMailCreated(["list-id", "id-2"]))
 		})
 
@@ -1004,20 +1005,20 @@ o.spec("IndexedDbIndexer", () => {
 						...noPatchesAndInstance,
 					},
 				]
-				when(mailIndexer.processEntityEvents(updates, matchers.anything(), matchers.anything())).thenDo(() => processDeferred.resolve())
+				when(mailIndexer.processEntityEvents(updates)).thenDo(() => processDeferred.resolve())
 				indexer.enableMailIndexing()
 
 				await initialIndexingCalled.promise
 				// dispatch an event while initial indexing is running and see that it is not immediately processed
 				await indexer.processEntityEvents(updates, "batchId", userGroupId, true)
 				// not processed yet
-				verify(mailIndexer.processEntityEvents(matchers.anything(), matchers.anything(), matchers.anything()), { times: 0 })
+				verify(mailIndexer.processEntityEvents(matchers.anything()), { times: 0 })
 
 				// allow initial indexing to finish
 				initialIndexingDone.resolve()
 				// wait until process callback is called
 				await processDeferred.promise
-				verify(mailIndexer.processEntityEvents(updates, userGroupId, "batchId"))
+				verify(mailIndexer.processEntityEvents(updates))
 				verify(mailIndexer.doInitialMailIndexing(user))
 			})
 
@@ -1044,19 +1045,19 @@ o.spec("IndexedDbIndexer", () => {
 						...noPatchesAndInstance,
 					},
 				]
-				when(mailIndexer.processEntityEvents(updates, matchers.anything(), matchers.anything())).thenDo(() => processDeferred.resolve())
+				when(mailIndexer.processEntityEvents(updates)).thenDo(() => processDeferred.resolve())
 				indexer.extendMailIndex(time.getTime())
 
 				await extendingMailIndexingCalled.promise
 				await indexer.processEntityEvents(updates, "batchId", userGroupId, true)
 				// not processed yet
-				verify(mailIndexer.processEntityEvents(matchers.anything(), matchers.anything(), matchers.anything()), { times: 0 })
+				verify(mailIndexer.processEntityEvents(matchers.anything()), { times: 0 })
 
 				// allow extending indexing to finish
 				extendMailIndexingDone.resolve()
 				// wait until process callback is called
 				await processDeferred.promise
-				verify(mailIndexer.processEntityEvents(updates, userGroupId, "batchId"))
+				verify(mailIndexer.processEntityEvents(updates))
 				verify(mailIndexer.extendIndexIfNeeded(user, time.getTime()))
 			})
 
