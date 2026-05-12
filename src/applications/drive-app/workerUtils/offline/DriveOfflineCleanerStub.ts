792de156d8c1271e95061dd0e2866acb6d4d457a
paw <104824185+paw-hub@users.noreply.github.com>
2026-05-12 13:31:42 +0200

    Create new mail indexer for full mailbox archival
    Also remove OfflineStorageCleaner
    
    Co-authored-by: bir <bir@tutao.de>
    Co-authored-by: hrb-hub <hrb-hub@users.noreply.github.com>
    
    Close #10722

--- src/applications/drive-app/workerUtils/offline/DriveOfflineCleanerStub.ts
+++
@@ -1,8 +0,0 @@
-import { ProgrammingError } from "../../../../platform-kit/app-env"
-import { OfflineStorage, OfflineStorageCleaner } from "../../../../app-kit/local-store/OfflineStorage"
-
-export class DriveOfflineCleanerStub implements OfflineStorageCleaner {
-	cleanOfflineDb(offlineStorage: OfflineStorage, timeRangeDate: Date | null, userId: Id, now: number): Promise<void> {
-		throw new ProgrammingError("not implemented")
-	}
-}
