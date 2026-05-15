fb57b9ea59a917e573803ad5b9c0c7004ca027cd
ivk <ivk@tutao.de>
2026-05-15 16:49:59 +0200

    [build] Adjust android.js and bump-version for Drive
    Co-authored-by: mab <mab@tutao.de>

--- app-android/drive/build.gradle.kts
+++ app-android/drive/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 		minSdk = 26
 		targetSdk = 35
 		versionCode = 1
-		versionName = "348.260506.0"
+		versionName = "348.260508.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
