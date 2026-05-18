abf79828121fa0d8f99ad058f9386de21c10cb93
mab <mab@tutao.de>
2026-05-18 18:32:32 +0200

    Drive: show file size also on mobile layout
    tuta#3474

--- src/applications/drive-app/drive/view/DriveFolderContentMobile.ts
+++ src/applications/drive-app/drive/view/DriveFolderContentMobile.ts
@@ -8,12 +8,12 @@ import { Icons, IconsSvg } from "../../../../ui/base/icons/Icons"
 import { theme } from "../../../../ui/theme"
 import { IconButton } from "../../../../ui/base/IconButton"
 import { attachDropdown } from "../../../../ui/base/Dropdown"
-import { formatDateTime } from "../../../../ui/utils/Formatter"
+import { formatDateTime, formatStorageSize } from "../../../../ui/utils/Formatter"
 import { FileActions } from "./DriveFolderContentEntry"
 import { getContextActions } from "./DriveGuiUtils"
 import { DriveFolderSelectionEvents } from "./DriveFolderContent"
 import { getDisplayType, getFileIcon, getItemIconFill } from "../model/DriveMimeUtils"
-import { assertNotNull } from "../../../../platform-kit/utils"
+import { assertNotNull, filterInt } from "@tutao/utils"
 
 export interface DriveFolderContentMobileAttrs {
 	listState: ListState<FolderItem>
@@ -71,7 +71,7 @@ const ROW_HEIGHT_PX = 56
 class DriveFolderItemRow implements ViewHolder<FolderItem> {
 	private iconDom!: HTMLElement
 	private filenameDom!: HTMLElement
-	private dateDom!: HTMLElement
+	private metadataDom!: HTMLElement
 	private item: FolderItem | null = null
 	private selectionSetter!: SelectableRowSelectedSetter
 
@@ -109,7 +109,7 @@ class DriveFolderItemRow implements ViewHolder<FolderItem> {
 						m("", {
 							style: { fontSize: "10px", color: theme.on_surface_variant },
 							oncreate: (vnode) => {
-								this.dateDom = vnode.dom as HTMLElement
+								this.metadataDom = vnode.dom as HTMLElement
 							},
 						}),
 					]),
@@ -140,7 +140,12 @@ class DriveFolderItemRow implements ViewHolder<FolderItem> {
 
 		this.filenameDom.innerText = item.type === "file" ? item.file.name : item.folder.name
 		const updatedDate = item.type === "file" ? item.file.updatedDate : item.folder.updatedDate
-		this.dateDom.innerText = formatDateTime(updatedDate)
+		this.metadataDom.innerText = formatDateTime(updatedDate)
+
+		if (item.type === "file") {
+			const readableSize = formatStorageSize(filterInt(item.file.size))
+			this.metadataDom.innerText += ` · ${readableSize}`
+		}
 
 		const displayType = item.type === "file" ? getDisplayType(item.file.mimeType) : null
 		const icon = item.type === "file" ? getFileIcon(assertNotNull(displayType)) : Icons.FolderFilled
