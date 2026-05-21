529abf44d564b1ab16323fc69e2b9874ebea8c29
nig <nig@tutao.de>
2026-05-21 15:10:06 +0200

    extract PriceChangeModel to its own file

--- src/applications/common/subscription/BuyDialog.ts
+++ src/applications/common/subscription/BuyDialog.ts
@@ -12,6 +12,7 @@ import { asPaymentInterval, formatPrice, getPriceItem, PaymentInterval } from ".
 import { showProgressDialog } from "../../../ui/dialogs/ProgressDialog.js"
 import { locator } from "../api/main/CommonLocator.js"
 import { NotAuthorizedError } from "@tutao/rest-client/error"
+import { PriceChangeModel } from "./PriceChangeModel"
 
 assertMainOrNode()
 
@@ -155,107 +156,5 @@ class ConfirmSubscriptionView implements Component<ConfirmAttrs> {
 		} else {
 			return ""
 		}
-	}
-}
-
-class PriceChangeModel {
-	readonly currentItem: PriceItemData | null
-	readonly futureItem: PriceItemData | null
-	readonly currentPrice: number
-	readonly futurePrice: number
-	readonly additionalFeatures: ReadonlySet<BookingItemFeatureType>
-
-	constructor(
-		private readonly price: PriceServiceReturn,
-		readonly featureType: BookingItemFeatureType,
-	) {
-		this.currentItem = getPriceItem(price.currentPriceNextPeriod, featureType)
-		this.futureItem = getPriceItem(price.futurePriceNextPeriod, featureType)
-		this.currentPrice = this.getPriceFromPriceData(price.currentPriceNextPeriod, featureType)
-		this.futurePrice = this.getPriceFromPriceData(price.futurePriceNextPeriod, featureType)
-
-		if (this.featureType === BookingItemFeatureType.LegacyUsers) {
-			this.additionalFeatures = new Set(
-				[BookingItemFeatureType.Whitelabel, BookingItemFeatureType.Sharing, BookingItemFeatureType.Business].filter((f) => this.getFuturePrice(f) > 0),
-			)
-		} else {
-			this.additionalFeatures = new Set()
-		}
-	}
-
-	getActionLabel(): TranslationKey {
-		if (!this.isPriceChange()) {
-			return "accept_action"
-		}
-		if (this.isBuy()) {
-			return "buy_action"
-		}
-		return "order_action"
-	}
-
-	isBuy() {
-		return this.currentPrice < this.futurePrice
-	}
-
-	isUnbuy() {
-		return this.currentPrice > this.futurePrice
-	}
-
-	isPriceChange() {
-		return this.currentPrice !== this.futurePrice
-	}
-
-	isSinglePriceType() {
-		return this.anyItem().singleType
-	}
-
-	getCurrentCount(): number {
-		return filterInt(assertNotNull(this.currentItem).count)
-	}
-
-	getFutureCount(): number {
-		return filterInt(assertNotNull(this.futureItem).count)
-	}
-
-	isYearly(): boolean {
-		const period = assertNotNull(this.price.futurePriceNextPeriod ?? this.price.currentPriceNextPeriod)
-		return asPaymentInterval(period.paymentInterval) === PaymentInterval.Yearly
-	}
-
-	taxIncluded(): boolean {
-		return assertNotNull(this.price.futurePriceNextPeriod).taxIncluded
-	}
-
-	periodEndDate(): Date {
-		// return a copy to prevent the date from being changed by the caller
-		return new Date(this.price.periodEndDate)
-	}
-
-	addedPriceForCurrentPeriod(): number {
-		return this.price.currentPeriodAddedPrice ? filterInt(this.price.currentPeriodAddedPrice) : 0
-	}
-
-	private anyItem() {
-		return assertNotNull(this.futureItem ?? this.currentItem)
-	}
-
-	private getFuturePrice(featureType: BookingItemFeatureType) {
-		return this.getPriceFromPriceData(this.price.futurePriceNextPeriod, featureType)
-	}
-
-	/**
-	 * Returns the price for the feature type from the price data if available, otherwise 0.
-	 */
-	private getPriceFromPriceData(priceData: PriceData | null, featureType: NumberString): number {
-		let item = getPriceItem(priceData, featureType)
-		let itemPrice = item ? Number(item.price) : 0
-
-		if (featureType === BookingItemFeatureType.LegacyUsers) {
-			itemPrice += this.getPriceFromPriceData(priceData, BookingItemFeatureType.Whitelabel)
-			itemPrice += this.getPriceFromPriceData(priceData, BookingItemFeatureType.Sharing)
-			itemPrice += this.getPriceFromPriceData(priceData, BookingItemFeatureType.Business)
-		}
-
-		return itemPrice
 	}
 }
