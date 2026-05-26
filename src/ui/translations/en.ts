53a6133ccd299316bc62ac98c51584ad2644ac4d
hrb-hub <hrb-hub@users.noreply.github.com>
2026-05-26 15:10:41 +0200

    Use appropriate label for showQuickActionBar shortcut

--- src/ui/translations/en.ts
+++ src/ui/translations/en.ts
@@ -2309,6 +2309,7 @@ export default {
 		"yourFolders_action": "YOUR FOLDERS",
 		"yourMessage_label": "Your message",
 		"zoomIn_action": "Zoom In",
-		"zoomOut_action": "Zoom Out"
+		"zoomOut_action": "Zoom Out",
+		"showQuickActionsBar_action": "Show quick actions bar",
 	}
 }
