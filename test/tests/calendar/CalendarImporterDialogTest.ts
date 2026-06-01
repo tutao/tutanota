1dcba3956c73976adf16e05556bd0e631ea5a8fd
and <and@tutao.de>
2026-06-01 12:32:06 +0200

    Docs, test and improvements for EventSeriesResolver.ts
    - Add error cases and improved edge case handling for
    resolveAllExcludedDatesForNewProgenitors.
    - Throw errors when finding 2 progenitors with same uid.
    - Handle cases where new exclusionDates are not already in progenitors.
    
    Co-authored-by: jas <jas@tutao.de>

--- test/tests/calendar/CalendarImporterDialogTest.ts
+++
@@ -1,9 +0,0 @@
-import o from "@tutao/otest"
-import { addExclusionsToNewProgenitors } from "../../../src/common/calendar/gui/CalendarImporterDialog"
-
-o.spec("CalendarImporterDialog", function () {
-	o.spec("handleCalendarImport", function () {})
-	o.spec("findProgenitorsToAddExcludedDates", function () {
-		o.test("correctly adds ")
-	})
-})
