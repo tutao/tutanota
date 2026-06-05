8c2b36ea5755adc4aa2c6c8d1edebb31efbb8420
sug <sug@tutao.de>
2026-06-05 16:36:26 +0200

    [simplification] TutanotaError should Extend Error
    previously there was a hack to get around babel limitation

--- src/platform-kit/app-env/TutanotaError.ts
+++ src/platform-kit/app-env/TutanotaError.ts
@@ -1,7 +1,5 @@
 /**
  * Base class for all errors in Tutanota. Provides the handling of error stacks for chrome (captureStackTrace) and others.
- * Implemented using ES5 inheritance as babel does not support extending builtin types
- * @see http://stackoverflow.com/questions/33870684/why-doesnt-instanceof-work-on-instances-of-error-subclasses-under-babel-node
  *
  * Note that passing errors between worker and main thread requires all fields of the error to be serializable.
  * Currently those are name, message, stack and data. See (errorToObj() and objToError()).
@@ -10,18 +8,8 @@
  * (needed for e instanceof CustomError to work), the error class needs to be
  * added to the ErrorNameToType map in Utils.js.
  */
-const ExtendableErrorF = function ExtendableError(...args: any[]) {
-	// @ts-ignore
-	Error.apply(this, args)
-}
 
-// Warning: huge type hack
-// You can't import downcast here
-ExtendableErrorF.prototype = Object.create(Error.prototype)
-// @ts-ignore
-const ExtendableError: Class<Error> = ExtendableErrorF as any
-
-export class TutanotaError extends ExtendableError {
+export class TutanotaError extends Error {
 	name: string
 	message: string
 
@@ -35,7 +23,7 @@ export class TutanotaError extends ExtendableError {
 		} else {
 			let error = new Error()
 
-			if (!error.stack) {
+			if (error.stack == null) {
 				// fill the stack trace on ios devices
 				try {
 					throw error
@@ -46,11 +34,11 @@ export class TutanotaError extends ExtendableError {
 
 			this.stack = this.name + ". " + this.message
 
-			if (error.stack) {
+			if (error.stack != null) {
 				// not existing in IE9
 				let stackLines = error.stack.split("\n")
 
-				while (stackLines[0] && !stackLines[0].match(this.name)) {
+				while (stackLines[0] != null && !stackLines[0].match(this.name)) {
 					stackLines = stackLines.slice(1) // removes line from stack
 				}
 
