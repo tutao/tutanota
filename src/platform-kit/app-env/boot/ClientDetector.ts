58950ca4e79021a79c5af525190782ad5683adc4
sug <sug@tutao.de>
2026-06-08 09:43:13 +0200

    [simplification] Make PlatformId an enum instead of string union

--- src/platform-kit/app-env/boot/ClientDetector.ts
+++ src/platform-kit/app-env/boot/ClientDetector.ts
@@ -1,4 +1,4 @@
-import { assertMainOrNodeBoot, envProvider, EnvType, Mode } from "../Env"
+import { assertMainOrNodeBoot, envProvider, EnvType, Mode, PlatformId } from "../Env"
 import { BrowserData, BrowserType, DeviceType } from "./ClientConstants"
 import { BotKind, load } from "@fingerprintjs/botd"
 import { AppType } from "../AppType"
@@ -293,11 +293,11 @@ export class ClientDetector {
 			return `${ClientDetector.get().device} ${appType} App`
 		} else if (envProvider.isBrowser()) {
 			return ClientDetector.get().browser + " Browser"
-		} else if (this.env.platformId === "linux") {
+		} else if (this.env.platformId === PlatformId.Linux) {
 			return "Linux Desktop"
-		} else if (this.env.platformId === "darwin") {
+		} else if (this.env.platformId === PlatformId.Darwin) {
 			return "Mac Desktop"
-		} else if (this.env.platformId === "win32") {
+		} else if (this.env.platformId === PlatformId.Win32) {
 			return "Windows Desktop"
 		}
 
@@ -344,9 +344,9 @@ export class ClientDetector {
 
 	getClientPlatform(): ClientPlatform {
 		if (envProvider.isDesktop()) {
-			if (this.env.platformId === "darwin") return ClientPlatform.DESKTOP_MAC
-			if (this.env.platformId === "linux") return ClientPlatform.DESKTOP_LINUX
-			if (this.env.platformId === "win32") return ClientPlatform.DESKTOP_WINDOWS
+			if (this.env.platformId === PlatformId.Darwin) return ClientPlatform.DESKTOP_MAC
+			if (this.env.platformId === PlatformId.Linux) return ClientPlatform.DESKTOP_LINUX
+			if (this.env.platformId === PlatformId.Win32) return ClientPlatform.DESKTOP_WINDOWS
 			return ClientPlatform.DESKTOP_UNKNOWN
 		}
 		if (!envProvider.isApp()) return ClientPlatform.WEB
