f0e6f54973e169fb55a578e2ec975be52f2ba9ab
wrd <wrd@tutao.de>
2026-06-11 15:19:29 +0200

    Fix logic when downloading attachment from draft
    Fixes #10915
    
    Add AttachmentDownloader to unify logic when downloading attachments
    
    Co-authored-by: hak <hak@tutao.de>

--- src/applications/mail-app/app.ts
+++ src/applications/mail-app/app.ts
@@ -68,6 +68,7 @@ import { initClientModels } from "../common/api/common/ClientModelInfoInitialize
 import { CacheMode, DEFAULT_ENTITY_RESTCLIENT_LOAD_OPTIONS } from "../../platform-kit/instance-pipeline/RestClientOptions"
 import { RevocationView, RevocationViewAttrs } from "../common/revocation/RevocationView"
 import { RevocationViewModel } from "../common/revocation/RevocationViewModel"
+import { AttachmentDownloader } from "./mail/view/MailGuiUtils"
 
 assertMainOrNodeBoot()
 bootFinished()
@@ -322,11 +323,19 @@ import("../../ui/translations/en.js")
 			const { OpenLocallySavedDraftAction } = await import("./mail/editor/OpenLocallySavedDraftAction.js")
 			const { newMailEditorFromLocalDraftData } = await import("./mail/editor/MailEditor.js")
 			const { createEditDraftDialog } = await import("./mail/view/MailViewerUtils.js")
-			return new OpenLocallySavedDraftAction(mailLocator.autosaveFacade, mailLocator.mailboxModel, mailLocator.entityClient, {
-				newMailEditorFromLocalDraftData,
-				createEditDraftDialog,
-				mailViewerViewModelFactory: () => mailLocator.mailViewerViewModelFactory(),
-			})
+			const { AttachmentDownloader } = await import("./mail/view/MailGuiUtils.js")
+			const fileApp = isBrowser() ? null : mailLocator.fileApp
+			return new OpenLocallySavedDraftAction(
+				mailLocator.autosaveFacade,
+				mailLocator.mailboxModel,
+				new AttachmentDownloader(mailLocator.fileController, fileApp, mailLocator.transferProgressDispatcher),
+				mailLocator.entityClient,
+				{
+					newMailEditorFromLocalDraftData,
+					createEditDraftDialog,
+					mailViewerViewModelFactory: () => mailLocator.mailViewerViewModelFactory(),
+				},
+			)
 		})
 
 		if (isDesktop()) {
