e9313edd369b4200821bb6153f51da3e06fe3354
hrb-hub <hrb-hub@users.noreply.github.com>
2026-06-12 16:43:48 +0200

    Refactor PromiseUtilTest to use o.test and o.check

--- test/tests/utils/PromiseUtilTest.ts
+++ test/tests/utils/PromiseUtilTest.ts
@@ -9,71 +9,71 @@ function mockDeferMapper() {
 	return mapper
 }
 
-o.spec("PromiseUtils", function () {
-	o.spec("mapInCallContexnt", function () {
-		o("works for values", function () {
-			o(mapInCallContext([1, 2, 3, 4], (v) => v + 1).value).deepEquals([2, 3, 4, 5])
+o.spec("PromiseUtils", () => {
+	o.spec("mapInCallContexnt", () => {
+		o.test("works for values", () => {
+			o.check(mapInCallContext([1, 2, 3, 4], (v) => v + 1).value).deepEquals([2, 3, 4, 5])
 		})
-		o("works for promises", async function () {
-			o(await mapInCallContext([1, 2, 3, 4], (v) => Promise.resolve(v + 1)).value).deepEquals([2, 3, 4, 5])
+		o.test("works for promises", async () => {
+			o.check(await mapInCallContext([1, 2, 3, 4], (v) => Promise.resolve(v + 1)).value).deepEquals([2, 3, 4, 5])
 		})
-		o("works for mix of promises & values", async function () {
-			o(await mapInCallContext([1, 2, 3, 4], (v) => (v % 2 === 0 ? Promise.resolve(v + 1) : v + 1)).value).deepEquals([2, 3, 4, 5])
+		o.test("works for mix of promises & values", async () => {
+			o.check(await mapInCallContext([1, 2, 3, 4], (v) => (v % 2 === 0 ? Promise.resolve(v + 1) : v + 1)).value).deepEquals([2, 3, 4, 5])
 		})
 	})
-	o.spec("PromiseableWrapper", function () {
-		o.spec("thenOrApply", function () {
-			o("value + value = value", function () {
-				o(new PromiseableWrapper("tuta").thenOrApply((v) => v + "nota").value).equals("tutanota")
+	o.spec("PromiseableWrapper", () => {
+		o.spec("thenOrApply", () => {
+			o.test("value + value = value", () => {
+				o.check(new PromiseableWrapper("tuta").thenOrApply((v) => v + "nota").value).equals("tutanota")
 			})
-			o("value + promise = promise", async function () {
+			o.test("value + promise = promise", async () => {
 				const value: any = new PromiseableWrapper("tuta").thenOrApply((v) => Promise.resolve(v + "nota")).value
-				o(typeof value.then).equals("function")
-				o(await value).equals("tutanota")
+				o.check(typeof value.then).equals("function")
+				o.check(await value).equals("tutanota")
 			})
-			o("promise + value = promise", async function () {
+			o.test("promise + value = promise", async () => {
 				const value: any = new PromiseableWrapper(Promise.resolve("tuta")).thenOrApply((v) => v + "nota").value
-				o(typeof value.then).equals("function")
-				o(await value).equals("tutanota")
+				o.check(typeof value.then).equals("function")
+				o.check(await value).equals("tutanota")
 			})
-			o("proimse + promise = promise", async function () {
+			o.test("proimse + promise = promise", async () => {
 				const value: any = new PromiseableWrapper(Promise.resolve("tuta")).thenOrApply((v) => Promise.resolve(v + "nota")).value
-				o(typeof value.then).equals("function")
-				o(await value).equals("tutanota")
+				o.check(typeof value.then).equals("function")
+				o.check(await value).equals("tutanota")
 			})
-			o("promise + wrapper = promise", async function () {
+			o.test("promise + wrapper = promise", async () => {
 				const value: any = new PromiseableWrapper(Promise.resolve("tuta")).thenOrApply((v) => new PromiseableWrapper(Promise.resolve(v + "nota"))).value
-				o(typeof value.then).equals("function")
-				o(await value).equals("tutanota")
+				o.check(typeof value.then).equals("function")
+				o.check(await value).equals("tutanota")
 			})
-			o("promise + promised wrapper = promise", async function () {
+			o.test("promise + promised wrapper = promise", async () => {
 				const value: any = new PromiseableWrapper(Promise.resolve("tuta")).thenOrApply((v) => Promise.resolve(new PromiseableWrapper(v + "nota"))).value
-				o(typeof value.then).equals("function")
-				o(await value).equals("tutanota")
+				o.check(typeof value.then).equals("function")
+				o.check(await value).equals("tutanota")
 			})
 		})
-		o.spec("toPromise", function () {
-			o("from value", async function () {
+		o.spec("toPromise", () => {
+			o.test("from value", async () => {
 				const promise = new PromiseableWrapper("tuta").toPromise()
-				o(typeof promise.then).equals("function")
-				o(await promise.then()).equals("tuta")
+				o.check(typeof promise.then).equals("function")
+				o.check(await promise.then()).equals("tuta")
 			})
-			o("from promise", async function () {
+			o.test("from promise", async () => {
 				const promise = new PromiseableWrapper(Promise.resolve("tuta")).toPromise()
-				o(typeof promise.then).equals("function")
-				o(await promise.then()).equals("tuta")
+				o.check(typeof promise.then).equals("function")
+				o.check(await promise.then()).equals("tuta")
 			})
 		})
 	})
-	o.spec("promiseMap Array<T>", function () {
-		o("empty", async function () {
-			o(await promiseMap([], (n) => n + 1)).deepEquals([])
+	o.spec("promiseMap Array<T>", () => {
+		o.test("empty", async () => {
+			o.check(await promiseMap([], (n) => n + 1)).deepEquals([])
 		})
-		o("non-empty", async function () {
-			o(await promiseMap([1, 2, 3], (n) => n + 1)).deepEquals([2, 3, 4])
+		o.test("non-empty", async () => {
+			o.check(await promiseMap([1, 2, 3], (n) => n + 1)).deepEquals([2, 3, 4])
 		})
 
-		o("parallel", async function () {
+		o.test("parallel", async () => {
 			const defer1 = defer<void>()
 			const defer2 = defer<void>()
 			const defer3 = defer<void>()
@@ -95,7 +95,7 @@ o.spec("PromiseUtils", function () {
 			verify(mapper(matchers.anything(), matchers.anything()), { times: 3 })
 		})
 
-		o("async in order", async function () {
+		o.test("async in order", async () => {
 			const defer1 = defer()
 			const defer2 = defer()
 			const mapper = mockDeferMapper()
@@ -107,9 +107,9 @@ o.spec("PromiseUtils", function () {
 			verify(mapper(matchers.anything(), matchers.anything()), { times: 2 })
 			defer2.resolve(2)
 			await Promise.resolve()
-			o(await resultPromise).deepEquals([1, 2])
+			o.check(await resultPromise).deepEquals([1, 2])
 		})
-		o("stops on rejection", async function () {
+		o.test("stops on rejection", async () => {
 			const defer1 = defer()
 			const defer2 = defer()
 			const mapper = mockDeferMapper()
@@ -117,40 +117,40 @@ o.spec("PromiseUtils", function () {
 			await Promise.resolve()
 			verify(mapper(matchers.anything(), matchers.anything()), { times: 1 })
 			defer1.reject(new Error("test"))
-			await o(() => resultPromise).asyncThrows(Error)
+			await o.check(() => resultPromise).asyncThrows(Error)
 			verify(mapper(matchers.anything(), matchers.anything()), { times: 1 })
 		})
-		o("stops on exception", async function () {
+		o.test("stops on exception", async () => {
 			const mapper = func<(number: number, idx: number) => Promise<number>>()
 			when(mapper(matchers.anything(), matchers.anything()), { ignoreExtraArgs: true }).thenDo(() => {
 				throw new Error("test")
 			})
-			await o(() => promiseMap([1, 2], mapper)).asyncThrows(Error)
+			await o.check(() => promiseMap([1, 2], mapper)).asyncThrows(Error)
 			// we would test that the mapper is called here but testdouble is not supporting that and are being very stubborn about it
 			//https://github.com/testdouble/testdouble.js/issues/412
 		})
 	})
-	o.spec("promiseFilter", function () {
+	o.spec("promiseFilter", () => {
 		function isEven(n) {
 			return n % 2 === 0
 		}
 
-		o("sync", async function () {
+		o.test("sync", async () => {
 			const arr = [1, 2, 3, 4]
 			const result = await promiseFilter(arr, isEven)
-			o(result).deepEquals([2, 4])
+			o.check(result).deepEquals([2, 4])
 		})
-		o("async", async function () {
+		o.test("async", async () => {
 			const arr = [1, 2, 3, 4]
 			const result = await promiseFilter(arr, (n) => Promise.resolve(isEven(n)))
-			o(result).deepEquals([2, 4])
+			o.check(result).deepEquals([2, 4])
 		})
-		o("index", async function () {
+		o.test("index", async () => {
 			const arr = [1, 2, 3, 4]
 			const result = await promiseFilter(arr, (_, i) => Promise.resolve(isEven(i)))
-			o(result).deepEquals([1, 3])
+			o.check(result).deepEquals([1, 3])
 		})
-		o("no concurrency", async function () {
+		o.test("no concurrency", async () => {
 			// One deferred per each item we want to check
 			const arr = [1, 2, 3, 4]
 			const deferred = [defer<boolean>(), defer<boolean>(), defer<boolean>(), defer<boolean>()]
@@ -175,7 +175,7 @@ o.spec("PromiseUtils", function () {
 			deferred[3].resolve(true)
 			await delay(1)
 			verify(mapper(matchers.anything(), matchers.anything()), { times: 4 })
-			o(await resultP).deepEquals([1, 4])
+			o.check(await resultP).deepEquals([1, 4])
 		})
 	})
 
