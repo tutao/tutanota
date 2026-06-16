c3863ee3daa8c16fdb1d1dbf52d95b6c2f73310f
bir <bir@tutao.de>
2026-06-16 15:41:21 +0200

    Remove `OfflineStorageSettingsModel`
    Co-authored-by: paw <paw-hub@users.noreply.github.com>

--- src/applications/mail-app/mailLocator.ts
+++ src/applications/mail-app/mailLocator.ts
@@ -136,7 +136,6 @@ import { SyncTracker } from "../common/api/main/SyncTracker.js"
 import { Indexer } from "./workerUtils/index/Indexer"
 import { SearchFacade } from "./workerUtils/index/SearchFacade"
 import { getEventWithDefaultTimes, setNextHalfHour } from "../common/api/common/utils/CommonCalendarUtils.js"
-import { OfflineStorageSettingsModel } from "../common/offline/OfflineStorageSettingsModel"
 import { SearchToken } from "../../ui/utils/QueryTokenUtils"
 import type { ContactSearchFacade } from "./workerUtils/index/ContactSearchFacade"
 import PublicEncryptionKeyProvider from "../../platform-kit/base/base-crypto/PublicEncryptionKeyProvider"
@@ -334,7 +333,6 @@ class MailLocator implements CommonLocator {
 		const redraw = await this.redraw()
 		const searchRouter = await this.scopedSearchRouter()
 		const calendarEventsRepository = await this.calendarEventsRepository()
-		const offlineStorageSettings = await this.offlineStorageSettingsModel()
 		const calendarModel = await this.calendarModel()
 		return () => {
 			return new SearchViewModel(
@@ -353,7 +351,6 @@ class MailLocator implements CommonLocator {
 				calendarModel,
 				redraw,
 				deviceConfig.getMailAutoSelectBehavior(),
-				offlineStorageSettings,
 			)
 		}
 	}
@@ -1384,14 +1381,6 @@ class MailLocator implements CommonLocator {
 			this.mailModel,
 		)
 	})
-
-	async offlineStorageSettingsModel(): Promise<OfflineStorageSettingsModel | null> {
-		if (!isBrowser() && !isAdminClient()) {
-			return new OfflineStorageSettingsModel(this.logins.getUserController(), deviceConfig)
-		} else {
-			return null
-		}
-	}
 
 	readonly undoModel: lazyAsync<UndoModel> = lazyMemoized(async () => {
 		const { UndoModel } = await import("./UndoModel.js")
