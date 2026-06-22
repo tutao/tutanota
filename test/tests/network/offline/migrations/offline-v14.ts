9b6ef876a5afb268b8873457cce9a727afcde007
paw <104824185+paw-hub@users.noreply.github.com>
2026-06-22 17:14:37 +0200

    Add migration for new columns for indexer
    Closes #11015

---
+++ test/tests/network/offline/migrations/offline-v14.ts
@@ -0,0 +1,53 @@
+import o from "@tutao/otest"
+import { offline14 } from "../../../../../src/app-kit/local-store/migrations/offline-v14"
+import { SqlCipherFacade } from "../../../../../src/app-kit/native-bridge/common/generatedipc/types"
+import { DesktopSqlCipher } from "../../../../../src/applications/common/desktop/db/DesktopSqlCipher"
+
+o.spec("offline-v14", () => {
+	let migration: offline14
+	let sqlCipherFacade: SqlCipherFacade
+
+	o.beforeEach(async () => {
+		const offlineDatabaseTestKey = new Uint8Array([3957386659, 354339016, 3786337319, 3366334248])
+		sqlCipherFacade = new DesktopSqlCipher(":memory:", false)
+		await sqlCipherFacade.openDb("user id", offlineDatabaseTestKey)
+		migration = new offline14(sqlCipherFacade)
+	})
+
+	o.test("no table", async () => {
+		await migration.migrate()
+	})
+
+	o.test("full migration", async () => {
+		await sqlCipherFacade.run(
+			"CREATE TABLE IF NOT EXISTS search_group_data (groupId TEXT NOT NULL PRIMARY KEY, groupType NUMBER NOT NULL, indexedTimestamp NUMBER NOT NULL)",
+			[],
+		)
+		await migration.migrate()
+	})
+
+	o.spec("partial migration", () => {
+		o.test("list but no element", async () => {
+			await sqlCipherFacade.run(
+				"CREATE TABLE IF NOT EXISTS search_group_data (groupId TEXT NOT NULL PRIMARY KEY, groupType NUMBER NOT NULL, indexedTimestamp NUMBER NOT NULL, lastIndexedEntityListId TEXT NOT NULL)",
+				[],
+			)
+			await migration.migrate()
+		})
+		o.test("element but no list", async () => {
+			await sqlCipherFacade.run(
+				"CREATE TABLE IF NOT EXISTS search_group_data (groupId TEXT NOT NULL PRIMARY KEY, groupType NUMBER NOT NULL, indexedTimestamp NUMBER NOT NULL, lastIndexedEntityElementId TEXT NOT NULL)",
+				[],
+			)
+			await migration.migrate()
+		})
+	})
+
+	o.test("no migration", async () => {
+		await sqlCipherFacade.run(
+			"CREATE TABLE IF NOT EXISTS search_group_data (groupId TEXT NOT NULL PRIMARY KEY, groupType NUMBER NOT NULL, indexedTimestamp NUMBER NOT NULL, lastIndexedEntityListId TEXT NOT NULL, lastIndexedEntityElementId TEXT NOT NULL)",
+			[],
+		)
+		await migration.migrate()
+	})
+})
