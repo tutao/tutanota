f2f53339395183892051f7d8a0d9abb43836cecb
paw <104824185+paw-hub@users.noreply.github.com>
2026-06-22 17:14:37 +0200

    Add migration for new columns for indexer
    Closes #11015

---
+++ src/app-kit/local-store/migrations/offline-v13.ts
@@ -0,0 +1,44 @@
+import { SqlCipherFacade } from "@tutao/native-bridge/generatedIpc/types"
+import { OfflineMigration } from "../OfflineMigration"
+import { untagSqlObject } from "../SqlValue"
+import { GENERATED_MAX_ID } from "@tutao/meta"
+import { isEmpty } from "@tutao/utils"
+
+const VERSION = 13
+
+/**
+ * Adds lastIndexedEntityListId and lastIndexedEntityElementId to search_group_data
+ */
+export class offline13 extends OfflineMigration {
+	constructor(private readonly sqlCipherFacade: SqlCipherFacade) {
+		super(VERSION)
+	}
+
+	async migrate() {
+		const tableInfo = await this.sqlCipherFacade.all(`PRAGMA table_info(search_group_data)`, [])
+		if (isEmpty(tableInfo)) {
+			// the whole table doesn't exist; that's fine, we'll add it when we initialize offline storage
+			return
+		}
+
+		const existingColumns: Set<string> = new Set(tableInfo.map((t) => untagSqlObject(t).name as string))
+
+		async function addColumnIfNotExists(column: string, sqlCipherFacade: SqlCipherFacade) {
+			if (!existingColumns.has(column)) {
+				// 🦀 ⚠️ this is not a prepared statement (we can't do it here); ye be warned of the consequences ⚠️ 🦀
+				await sqlCipherFacade.run(
+					`ALTER TABLE search_group_data
+						ADD COLUMN ${column} TEXT NOT NULL DEFAULT '${GENERATED_MAX_ID}'`,
+					[],
+				)
+			}
+		}
+
+		// the columns may or may not already exist
+		//
+		// they *shouldn't*, but the migration might have been partially completed, as the version is not written until
+		// completion
+		await addColumnIfNotExists("lastIndexedEntityElementId", this.sqlCipherFacade)
+		await addColumnIfNotExists("lastIndexedEntityListId", this.sqlCipherFacade)
+	}
+}
