b22e041f6a1142e43f41a9703e7e1b9d41f291bb
paw <104824185+paw-hub@users.noreply.github.com>
2026-06-22 17:14:37 +0200

    Add migration for new columns for indexer
    Closes #11015

--- src/app-kit/local-store/OfflineStorageMigrator.ts
+++ src/app-kit/local-store/OfflineStorageMigrator.ts
@@ -11,13 +11,14 @@ import { offline10 } from "./migrations/offline-v10"
 import { offline11 } from "./migrations/offline-v11"
 import { offline12 } from "./migrations/offline-v12"
 import { offline13 } from "./migrations/offline-v13"
+import { offline14 } from "./migrations/offline-v14"
 import { ApplicationTypesFacade } from "../../platform-kit/instance-pipeline"
 import { SqlCipherFacade } from "@tutao/native-bridge/generatedIpc/types"
 import { OfflineMigration } from "./OfflineMigration"
 
 // in cases where the actual migration is not there anymore (we clean up old migrations no client would apply anymore)
 // and we create a new offline database, we still need to set the offline version to the current value.
-export const CURRENT_OFFLINE_VERSION = 13
+export const CURRENT_OFFLINE_VERSION = 14
 
 /**
  * List of migrations that will be run when needed. Please add your migrations to the list.
@@ -39,6 +40,7 @@ export function createOfflineStorageMigrations(
 		new offline11(applicationTypesFacade),
 		new offline12(sqlCipherFacade),
 		new offline13(sqlCipherFacade),
+		new offline14(sqlCipherFacade),
 	] as ReadonlyArray<OfflineMigration>
 }
 
