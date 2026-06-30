cfd0771202ac981110e525641cd6c40b71350714
osb <osb@tutao.de>
2026-06-30 18:29:17 +0200

    Use fixed time zone list for time zone selection
    Instead of relying on the timezone list supported by the browser (Intl.supportedValuesOf("timeZone")) we introduce a fixed time zone list that we generate from the IANA time zone database. The list contains major time zone names from continental regions and important aliases so the user can choose the time zone based on a major city that is close to them
    (Most time zones are in the format '<continental region>/<major city>').
    
    We want to accept as many time zones in our import logic as possible. To achieve this are
    import logic is as follows. We first check if the time zone is in the fixed list of time
    zones, next we check our Windows time zone to IANA time zone mapping. Finally, if it's not
    in the mapping we use `Intl.DateTimeFormat(... {timeZone: timeZone}).resolvedOptions().timeZone` induce errors if it's a time zone that is not understood by the Intl API and to
    normalize the time zone identifier using the API.
    
    This commit also moves logic to a new TimeZoneProvider class and adds tests for the utility
    methods that this class provides.

---
+++ src/applications/common/calendar/TimeZoneProvider.ts
@@ -0,0 +1,52 @@
+import { availableIANATimeZones, windowsToIANATimeZones } from "./TimeZoneData"
+import { DateTimeFormatterWrapper } from "./DateTimeFormatterWrapper"
+import { DateTime } from "luxon"
+import { buildGmtOffset } from "../../calendar-app/calendar/gui/DateTimeTextFormatterUtils"
+
+export type IANATimeZoneStrings = {
+	timeZone: string
+	name: string
+	offsetLongName: string
+	gmtOffset: string
+}
+
+export class TimeZoneProvider {
+	private readonly availableIANATimeZoneSet = new Set(availableIANATimeZones)
+
+	constructor(private readonly dateTimeFormatterWrapper: DateTimeFormatterWrapper) {}
+
+	resolveTimeZoneForImport(timeZone: string) {
+		if (this.availableIANATimeZoneSet.has(timeZone)) {
+			return timeZone
+		}
+
+		const timeZoneFromWindowsMap = windowsToIANATimeZones[timeZone]
+		if (timeZoneFromWindowsMap) {
+			return timeZoneFromWindowsMap
+		}
+
+		return this.dateTimeFormatterWrapper.resolveTimezone(timeZone)
+	}
+
+	getTimeZonesStrings(dateTime: DateTime): Array<IANATimeZoneStrings> {
+		return availableIANATimeZones.map((timeZone) => {
+			return this.createTimeZoneStrings(timeZone, dateTime)
+		})
+	}
+
+	createTimeZoneStrings(timeZone: string, dateTime: DateTime): IANATimeZoneStrings {
+		const dateTimeInTimeZone = dateTime.setZone(timeZone)
+		if (!dateTimeInTimeZone.isValid) {
+			throw new Error(`Unexpected invalid time zone = '${timeZone}'`)
+		}
+		const timeZoneName = timeZone.replaceAll("_", " ")
+		return {
+			timeZone: timeZone,
+			name: timeZoneName,
+			offsetLongName: dateTimeInTimeZone.offsetNameLong ?? "",
+			gmtOffset: buildGmtOffset(dateTimeInTimeZone),
+		}
+	}
+}
+
+export const timeZoneProvider = new TimeZoneProvider(new DateTimeFormatterWrapper())
