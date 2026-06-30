9b3c1cc9b9934fd2f72f6afb7248debece77fe3b
ivk <ivk@tutao.de>
2026-06-30 17:31:34 +0200

    Add test ids to DriveBreadcrumbs

--- src/applications/drive-app/drive/view/DriveBreadcrumbs.ts
+++ src/applications/drive-app/drive/view/DriveBreadcrumbs.ts
@@ -74,69 +74,79 @@ function folderRoute(entry: DriveFolder): string {
 
 export class DriveBreadcrumbs implements Component<DriveBreadcrumbsAttrs> {
 	view({ attrs: { currentFolder, parents, loadParents, onDropInto, onClick } }: Vnode<DriveBreadcrumbsAttrs>): Children {
-		return m("div.flex.items-center.column-gap-4", [
-			parents
-				.map((entry, index) => {
-					return [
-						// if it's the first item and it has a parent it means we don't have the full path
-						index === 0 && entry.parent
-							? [
-									m(BaseButton, {
-										class: "click state-bg pl-8 pr-8 pt-4 pb-4 border-radius-4",
-										text: "…",
-										onclick: (_, dom) => this.onLoadParents(dom, loadParents, onClick, undefined),
-										ondragover: (event: DragEvent) => {
-											if (event.target && isDraggingDriveItems(event.dataTransfer)) {
-												const dom = event.target as HTMLElement
-												this.onLoadParents(dom, loadParents, undefined, onDropInto)
-											}
-										},
-										label: "showParentFolders_action",
-									} satisfies BaseButtonAttrs),
-									m(Icon, {
-										icon: Icons.ChevronRight,
-										size: IconSize.PX24,
-										style: { fill: theme.on_surface_variant },
-									}),
-								]
-							: null,
-						m(BreadcrumbLink, {
-							label: driveFolderName(entry),
-							href: folderRoute(entry),
-							onClick: (e: MouseEvent) => {
-								onClick?.(entry, e)
-							},
-							onDrop: onDropInto
-								? (event) => {
-										onDropInto({ type: "folder", folder: entry }, event)
-									}
-								: undefined,
-						}),
-						m(Icon, {
-							icon: Icons.ChevronRight,
-							size: IconSize.PX24,
-							style: { fill: theme.on_surface_variant },
-						}),
-					]
-				})
-				.flat(),
-			currentFolder
-				? [
-						m(
-							".pl-8.pr-8.pt-4.pb-4.text-ellipsis.font-weight-500",
-							{
-								style: {
-									// match the border of breadcrumb links
-									border: BREADCRUMB_BORDER_NONE,
-									"max-width": BREADCRUMBS_ITEM_MAXWIDTH,
+		return m(
+			"div.flex.items-center.column-gap-4",
+			{
+				"data-testid": "section:breadcrumbs",
+			},
+			[
+				parents
+					.map((entry, index) => {
+						return [
+							// if it's the first item and it has a parent it means we don't have the full path
+							index === 0 && entry.parent
+								? [
+										m(BaseButton, {
+											class: "click state-bg pl-8 pr-8 pt-4 pb-4 border-radius-4",
+											text: "…",
+											onclick: (_, dom) => this.onLoadParents(dom, loadParents, onClick, undefined),
+											ondragover: (event: DragEvent) => {
+												if (event.target && isDraggingDriveItems(event.dataTransfer)) {
+													const dom = event.target as HTMLElement
+													this.onLoadParents(dom, loadParents, undefined, onDropInto)
+												}
+											},
+											label: "showParentFolders_action",
+										} satisfies BaseButtonAttrs),
+										m(Icon, {
+											icon: Icons.ChevronRight,
+											size: IconSize.PX24,
+											style: { fill: theme.on_surface_variant },
+										}),
+									]
+								: null,
+							m(BreadcrumbLink, {
+								label: driveFolderName(entry),
+								href: folderRoute(entry),
+								onClick: (e: MouseEvent) => {
+									onClick?.(entry, e)
 								},
-							},
-							" " + driveFolderName(currentFolder).text,
-						),
-					]
-				: null,
-		])
+								onDrop: onDropInto
+									? (event) => {
+											onDropInto({ type: "folder", folder: entry }, event)
+										}
+									: undefined,
+							}),
+							m(Icon, {
+								icon: Icons.ChevronRight,
+								size: IconSize.PX24,
+								style: { fill: theme.on_surface_variant },
+							}),
+						]
+					})
+					.flat(),
+				// it has to be an array to satisfy mithil key requirements
+				currentFolder ? [this.renderCurrentFolder(currentFolder)] : null,
+			],
+		)
 	}
+	private renderCurrentFolder(currentFolder: DriveFolder): Children {
+		const name = driveFolderName(currentFolder)
+		return m(
+			".pl-8.pr-8.pt-4.pb-4.text-ellipsis.font-weight-500",
+			{
+				"data-testid": `breadcrumb:current`,
+				"data-foldername": lang.getTestId(name),
+				style: {
+					// match the border of breadcrumb links
+					border: BREADCRUMB_BORDER_NONE,
+					"max-width": BREADCRUMBS_ITEM_MAXWIDTH,
+				},
+			},
+			" " + lang.getTranslationText(name),
+		)
+	}
+
 	private async onLoadParents(
 		dom: HTMLElement,
 		loadParents: () => Promise<DriveFolder[]>,
