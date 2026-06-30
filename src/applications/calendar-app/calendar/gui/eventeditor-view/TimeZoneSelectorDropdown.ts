551545af7764fafa8bb5a4601f93ef29c88fd647
osb <osb@tutao.de>
2026-06-30 18:29:17 +0200

    Use fixed time zone list for time zone selection
    Instead of relying on the timezone list supported by the browser (Intl.supportedValuesOf("timeZone")) we introduce a fixed time zone list that we generate from the IANA time zone database. The list contains major time zone names from continental regions and important aliases so the user can choose the time zone based on a major city that is close to them
    (Most time zones are in the format '<continental region>/<major city>').
    
    We want to accept as many time zones in our import logic as possible. To achieve this are
    import logic is as follows. We first check if the time zone is in the fixed list of time
    zones, next we check our Windows time zone to IANA time zone mapping. Finally, if it's not
    in the mapping we use `Intl.DateTimeFormat(... {timeZone: timeZone}).resolvedOptions().timeZone` induce errors if it's a time zone that is not understood by the Intl API and to
    normalize the time zone identifier using the API.
    
    This commit also moves logic to a new TimeZoneProvider class and adds tests for the utility
    methods that this class provides.

--- src/applications/calendar-app/calendar/gui/eventeditor-view/TimeZoneSelectorDropdown.ts
+++ src/applications/calendar-app/calendar/gui/eventeditor-view/TimeZoneSelectorDropdown.ts
@@ -1,73 +1,44 @@
 import m, { Children, Component, Vnode } from "mithril"
-import { Card } from "../../../../../ui/base/Card"
-import { px } from "../../../../../ui/size"
-import { Select, SelectAttributes, SelectOption } from "../../../../../ui/base/Select"
-import stream from "mithril/stream"
-import { getTimeZoneLongName, getTimeZoneOffset, IANATimeZone, IANATimeZonesList } from "../DateTimeTextFormatterUtils"
-import { Icon, IconSize } from "../../../../../ui/base/Icon"
 import { Icons } from "../../../../../ui/base/icons/Icons"
 import { theme } from "../../../../../ui/theme"
+import { DateTime } from "luxon"
+import { DropDownSelectorNew, DropDownSelectorNewAttrs } from "../../../../../ui/base/DropDownSelectorNew"
+import { lang } from "../../../../../ui/utils/LanguageViewModel"
+import { IANATimeZoneStrings, timeZoneProvider } from "../../../../common/calendar/TimeZoneProvider"
 
 export type TimeZoneSelectorDropdownAttrs = {
-	selectedTimeZone: IANATimeZone
-	onSelectionChanged: (selectedTimezone: IANATimeZone) => void
-}
-
-interface TimeZoneSelectOption extends SelectOption<IANATimeZone> {
-	timeZoneName: string
-	timeZoneLongName: string
-	timeZoneOffset: string
+	dateTime: DateTime
+	selectedTimeZone: string
+	onSelectionChanged: (selectedTimezone: string) => void
 }
 
 export class TimeZoneSelectorDropdown implements Component<TimeZoneSelectorDropdownAttrs> {
-	private timeZoneOptionsList: TimeZoneSelectOption[] = []
+	private timeZonesStrings: IANATimeZoneStrings[]
 
-	oninit({ attrs }: Vnode<TimeZoneSelectorDropdownAttrs>) {
-		this.timeZoneOptionsList = IANATimeZonesList.map((timeZone) => {
-			const timeZoneName = timeZone.replaceAll("_", " ")
-			return {
-				value: timeZone,
-				timeZoneName: timeZoneName,
-				ariaValue: timeZoneName,
-				timeZoneLongName: getTimeZoneLongName(new Date(), timeZone),
-				timeZoneOffset: getTimeZoneOffset(new Date(), timeZone),
-			}
-		})
+	constructor({ attrs }: Vnode<TimeZoneSelectorDropdownAttrs>) {
+		this.timeZonesStrings = timeZoneProvider.getTimeZonesStrings(attrs.dateTime)
 	}
 
 	public view({ attrs }: Vnode<TimeZoneSelectorDropdownAttrs>): Children {
-		return m(
-			Card,
-			{ style: { padding: px(0) } },
-			m(Select<TimeZoneSelectOption, string>, {
-				onchange: (val) => {
-					attrs.onSelectionChanged(val.value)
-				},
-				options: stream(this.timeZoneOptionsList),
-				expanded: true,
-				selected: this.getSelected(attrs.selectedTimeZone),
-				renderOption: this.renderOption,
-				renderDisplay: this.renderOption,
-				ariaLabel: "Calendar",
-				classes: ["pr-8"],
-			} satisfies SelectAttributes<TimeZoneSelectOption, string>),
-		)
-	}
-
-	private renderOption(option: TimeZoneSelectOption) {
-		return m(".flex.items-center.gap-8.plr-12.pt-8.pb-8", [
-			m(Icon, {
+		let selectedTimeZoneStrings = this.timeZonesStrings.find((strings) => strings.timeZone === attrs.selectedTimeZone)
+		if (!selectedTimeZoneStrings) {
+			selectedTimeZoneStrings = timeZoneProvider.createTimeZoneStrings(attrs.selectedTimeZone, attrs.dateTime)
+		}
+		return m(DropDownSelectorNew, {
+			label: lang.makeTranslation("selectedTimeZone", `${selectedTimeZoneStrings.offsetLongName} (${selectedTimeZoneStrings.gmtOffset})`),
+			items: this.timeZonesStrings.map((strings) => ({
+				name: strings.name,
+				value: strings.timeZone,
 				icon: Icons.GlobeOutline,
-				size: IconSize.PX24,
-				style: {
-					fill: theme.on_surface_variant,
-				},
-			}),
-			m(".flex.col", [m("small.faded", option.timeZoneLongName), m("span", option.timeZoneName)]),
-		])
-	}
-
-	private getSelected(startTimeZone: string) {
-		return this.timeZoneOptionsList.find((timeZoneOption) => timeZoneOption.value === startTimeZone)
+				secondaryTextLine: `${strings.offsetLongName} (${strings.gmtOffset})`,
+			})),
+			selectedValue: selectedTimeZoneStrings.timeZone,
+			selectedValueDisplay: selectedTimeZoneStrings.name,
+			icon: {
+				icon: Icons.GlobeOutline,
+				color: theme.on_surface_variant,
+			},
+			selectionChangedHandler: attrs.onSelectionChanged,
+		} satisfies DropDownSelectorNewAttrs<string>)
 	}
 }
---
+++ test/tests/calendar/TimeZoneProviderTest.ts
@@ -0,0 +1,119 @@
+import o from "@tutao/otest"
+import { TimeZoneProvider } from "../../../src/applications/common/calendar/TimeZoneProvider"
+import { DateTimeFormatterWrapper } from "../../../src/applications/common/calendar/DateTimeFormatterWrapper"
+import { matchers, object, verify, when } from "testdouble"
+import { DateTime } from "luxon"
+
+o.spec("TimeZoneProvider", () => {
+	let dateTimeFormatterWrapperMock: DateTimeFormatterWrapper
+	let provider: TimeZoneProvider
+	o.before(function () {
+		dateTimeFormatterWrapperMock = object()
+		provider = new TimeZoneProvider(dateTimeFormatterWrapperMock)
+	})
+
+	o.spec("resolveTimeZoneForImport", () => {
+		o.test("Primary IANA time zone resolves to same identifier", function () {
+			o(provider.resolveTimeZoneForImport("Europe/Berlin")).equals("Europe/Berlin")
+		})
+		o.test("IANA time zone alias (referred to as Links in tzdb) resolves to same identifier", () => {
+			o(provider.resolveTimeZoneForImport("Europe/Stockholm")).equals("Europe/Stockholm")
+		})
+		o.test("IANA time zone with 2 slashes resolves to time zone our Intl.DateTimeFormatter wrapper", function () {
+			//
+			// Test Chrome-like behaviour, were time zones with 2 slashes resolve to an alias with 1 slash
+			//
+			when(dateTimeFormatterWrapperMock.resolveTimezone("America/Argentina/Buenos_Aires")).thenReturn("America/Buenos_Aires")
+			when(dateTimeFormatterWrapperMock.resolveTimezone("America/Buenos_Aires")).thenReturn("America/Buenos_Aires")
+
+			o(provider.resolveTimeZoneForImport("America/Argentina/Buenos_Aires")).equals("America/Buenos_Aires")
+			o(provider.resolveTimeZoneForImport("America/Buenos_Aires")).equals("America/Buenos_Aires")
+
+			verify(dateTimeFormatterWrapperMock.resolveTimezone("America/Argentina/Buenos_Aires"), { times: 1 })
+
+			//
+			// Test Firefox-like behaviour, were time zones with 1 slash may resolve to an alias with 2 slashes
+			//
+			when(dateTimeFormatterWrapperMock.resolveTimezone("America/Argentina/Buenos_Aires")).thenReturn("America/Argentina/Buenos_Aires")
+			when(dateTimeFormatterWrapperMock.resolveTimezone("America/Buenos_Aires")).thenReturn("America/Argentina/Buenos_Aires")
+
+			o(provider.resolveTimeZoneForImport("America/Argentina/Buenos_Aires")).equals("America/Argentina/Buenos_Aires")
+			o(provider.resolveTimeZoneForImport("America/Buenos_Aires")).equals("America/Buenos_Aires")
+
+			verify(dateTimeFormatterWrapperMock.resolveTimezone("America/Argentina/Buenos_Aires"), { times: 2 })
+		})
+		o.test("Handles Windows time zones", function () {
+			o(provider.resolveTimeZoneForImport("Central Europe Standard Time")).equals("Europe/Budapest")
+			o(provider.resolveTimeZoneForImport("Argentina Standard Time")).equals("America/Buenos_Aires")
+		})
+		o.test("Handles UTC-<offset> Windows time zones", function () {
+			o(provider.resolveTimeZoneForImport("UTC-02")).equals("Etc/GMT+2")
+		})
+		o.test("Handles null-return from DateTimeFormatterWrapper", function () {
+			when(dateTimeFormatterWrapperMock.resolveTimezone(matchers.anything())).thenReturn(null)
+
+			o(provider.resolveTimeZoneForImport("bogus")).equals(null)
+
+			verify(dateTimeFormatterWrapperMock.resolveTimezone("bogus"), { times: 1 })
+		})
+	})
+	o.spec("createTimeZoneStrings", () => {
+		o.test("Returns correct strings for Europe/Berlin during daylight saving time", () => {
+			const timeZone = "Europe/Berlin"
+			const dateTime = DateTime.fromISO("2026-07-07T12:00:00", { zone: "UTC" })
+			o(provider.createTimeZoneStrings(timeZone, dateTime)).deepEquals({
+				timeZone: "Europe/Berlin",
+				name: "Europe/Berlin",
+				offsetLongName: "Central European Summer Time",
+				gmtOffset: "GMT+2",
+			})
+		})
+		o.test("Returns correct strings for Europe/Berlin during standard time", () => {
+			const timeZone = "Europe/Berlin"
+			const dateTime = DateTime.fromISO("2026-01-01T12:00:00", { zone: "UTC" })
+			o(provider.createTimeZoneStrings(timeZone, dateTime)).deepEquals({
+				timeZone: "Europe/Berlin",
+				name: "Europe/Berlin",
+				offsetLongName: "Central European Standard Time",
+				gmtOffset: "GMT+1",
+			})
+		})
+		o.test("Replaces underscores with spaces in name", () => {
+			const timeZone = "America/Buenos_Aires"
+			const dateTime = DateTime.fromISO("2026-01-01T12:00:00", { zone: "UTC" })
+			o(provider.createTimeZoneStrings(timeZone, dateTime).name).deepEquals("America/Buenos Aires")
+		})
+		o.test("Handles 'UTC' time zone", () => {
+			const timeZone = "UTC"
+			const dateTime = DateTime.fromISO("2026-01-01T12:00:00", { zone: "UTC" })
+			o(provider.createTimeZoneStrings(timeZone, dateTime)).deepEquals({
+				timeZone: "UTC",
+				name: "UTC",
+				offsetLongName: "UTC",
+				gmtOffset: "GMT+0",
+			})
+		})
+		o.test("Handles 'Etc/GMT+11' time zone", () => {
+			const timeZone = "Etc/GMT+11"
+			const dateTime = DateTime.fromISO("2026-01-01T12:00:00", { zone: "UTC" })
+			o(provider.createTimeZoneStrings(timeZone, dateTime)).deepEquals({
+				timeZone: "Etc/GMT+11",
+				name: "Etc/GMT+11",
+				// Beware, 'Etc/GMT+<offset>' flip the sign of the GMT-offset because they were standardized
+				// in an old POSIX standard; i.e. Etc/GMT+1 != UTC+1 && Etc/GMT+1 == UTC-1!
+				offsetLongName: "GMT-11:00",
+				gmtOffset: "GMT-11",
+			})
+		})
+		o.test("Handles time zones with 2 slashes", () => {
+			const timeZone = "America/Argentina/Buenos_Aires"
+			const dateTime = DateTime.fromISO("2026-01-01T12:00:00", { zone: "UTC" })
+			o(provider.createTimeZoneStrings(timeZone, dateTime)).deepEquals({
+				timeZone: "America/Argentina/Buenos_Aires",
+				name: "America/Argentina/Buenos Aires",
+				offsetLongName: "Argentina Standard Time",
+				gmtOffset: "GMT-3",
+			})
+		})
+	})
+})
