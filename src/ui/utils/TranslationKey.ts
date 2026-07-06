7e23cd0fda5a4f240b65fec1bccd9da2725d7b80
usa <usa@tutao.de>
2026-07-06 17:16:07 +0200

    Drive: Show choice dialog for duplicate file names on upload
    tuta#3674
    
    Co-authored-by: mab <mab@tutao.de>
    Co-authored-by: ivk <ivk@tutao.de>

--- src/ui/utils/TranslationKey.ts
+++ src/ui/utils/TranslationKey.ts
@@ -2492,4 +2492,8 @@ export type TranslationKeyType =
 	| "subscriptionSettingAppleWebsite_action"
 	| "paymentMethodOnAccountHowItWorks_msg"
 	| "emptyString_msg"
+	| "duplicateFileName_msg"
+	| "keepBothFiles_action"
+	| "replaceFile_action"
+	| "applyToAllFiles_label"
 	| "transferTimeRemaining_msg"
