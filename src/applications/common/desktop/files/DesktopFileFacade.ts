tutao/tutanota

commit ac6ba0a87f5c718649354a8f5a589dcfa570515e

[Desktop] use file urls on Desktop instead of paths
We are now using streams and tmp uri to read chunks. So file urls are
more suitable

tuta#3627

Co-authored-by: ivk <ivk@tutao.de>
Co-authored-by: mab <mab@tutao.de>

src/applications/common/desktop/files/DesktopFileFacade.ts
@@ -283,13 +283,13 @@ export class DesktopFileFacade implements FileFacade {
 
 	async putFileIntoDownloadsFolder(localFileUri: string, fileNameToUse: string): Promise<string> {
 		const url = this.tfs.assertInTmpDir(localFileUri)
-		const savePath = await this.pickSavePath(fileNameToUse)
-		await this.fs.promises.mkdir(path.dirname(savePath), {
+		const savedFileUrl = await this.pickSavePath(fileNameToUse)
+		await this.fs.promises.mkdir(path.dirname(fileURLToPath(savedFileUrl)), {
 			recursive: true,
 		})
-		await this.fs.promises.copyFile(url, savePath)
-		await this.showInFileExplorer(savePath)
-		return pathToFileURL(savePath).toString()
+		await this.fs.promises.copyFile(url, savedFileUrl)
+		await this.showInFileExplorer(savedFileUrl)
+		return savedFileUrl.toString()
 	}
 
 	async upload(fileUri: string, targetUrl: string, method: HttpMethod, headers: Record<string, string>, fileId: string): Promise<UploadTaskResponse> {
@@ -424,12 +424,13 @@ export class DesktopFileFacade implements FileFacade {
 	 * Select a non-colliding name in the configured downloadPath, preferably with the given file name
 	 * public for testing
 	 */
-	private async pickSavePath(filename: string): Promise<string> {
+	private async pickSavePath(filename: string): Promise<URL> {
 		const defaultDownloadPath = await this.conf.getVar(DesktopConfigKey.defaultDownloadPath)
 
 		if (defaultDownloadPath != null) {
 			const fileName = path.basename(filename)
-			return path.join(defaultDownloadPath, nonClobberingFilename(await this.fs.promises.readdir(defaultDownloadPath), fileName))
+			const destinationPath = path.join(defaultDownloadPath, nonClobberingFilename(await this.fs.promises.readdir(defaultDownloadPath), fileName))
+			return pathToFileURL(destinationPath)
 		} else {
 			const { canceled, filePath } = await this.electron.dialog.showSaveDialog({
 				defaultPath: path.join(this.electron.app.getPath("downloads"), filename),
@@ -438,20 +439,20 @@ export class DesktopFileFacade implements FileFacade {
 			if (canceled) {
 				throw new CancelledError("Path selection cancelled")
 			} else {
-				return filePath
+				return pathToFileURL(filePath)
 			}
 		}
 	}
 
 	/** public for testing */
-	async showInFileExplorer(savePath: string): Promise<void> {
+	async showInFileExplorer(fileUrl: URL): Promise<void> {
 		// See doc for _lastOpenedFileManagerAt on why we do this throttling.
 		const lastOpenedFileManagerAt = this.lastOpenedFileManagerAt
 		const fileManagerTimeout = await this.conf.getConst(BuildConfigKey.fileManagerTimeout)
 
 		if (lastOpenedFileManagerAt == null || this.dateProvider.now() - lastOpenedFileManagerAt > fileManagerTimeout) {
 			this.lastOpenedFileManagerAt = this.dateProvider.now()
-			this.electron.shell.showItemInFolder(savePath)
+			this.electron.shell.showItemInFolder(fileURLToPath(fileUrl))
 		}
 	}
 	/** can be used with arbitrary paths, is run on the selected file locations before the files are read */

test/tests/desktop/files/DesktopFileFacadeTest.ts
@@ -426,18 +426,18 @@ o.spec("DesktopFileFacade", function () {
 
 	o.spec("showInFileExplorer", function () {
 		o("two downloads, open two filemanagers", async function () {
-			await ff.showInFileExplorer("file:///path/to/file.txt")
+			await ff.showInFileExplorer(new URL("file:///path/to/file.txt"))
 			verify(electron.shell.showItemInFolder("/path/to/file.txt"), { times: 1 })
 		})
 
 		o("two downloads, open two filemanagers after a pause", async function () {
 			const time = 1629115820468
-			await ff.showInFileExplorer("file:///path/to/file.txt")
+			await ff.showInFileExplorer(new URL("file:///path/to/file.txt"))
 			when(dp.now()).thenReturn(time)
 			when(conf.getConst(BuildConfigKey.fileManagerTimeout)).thenResolve(2)
 			verify(electron.shell.showItemInFolder("/path/to/file.txt"), { times: 1 })
 			when(dp.now()).thenReturn(time + 10)
-			await ff.showInFileExplorer("file:///path/to/file.txt")
+			await ff.showInFileExplorer(new URL("file:///path/to/file.txt"))
 			verify(electron.shell.showItemInFolder("/path/to/file.txt"), { times: 2 })
 		})
 	})

commit d7dd34f9c13121062a9323c888f00c08a0725511

[desktop] fix default download path for saving attachments not working
Since we were not converting the file URLs we saved to the config
database to paths before joining, the destination path we created was
not a proper path. We now convert the file URL to path before joining.

This fixes a regression introduced in revision
ac6ba0a87f5c718649354a8f5a589dcfa570515e

src/applications/common/desktop/files/DesktopFileFacade.ts
@@ -426,9 +426,14 @@ export class DesktopFileFacade implements FileFacade {
 	 * public for testing
 	 */
 	private async pickSavePath(filename: string): Promise<URL> {
-		const defaultDownloadPath = await this.conf.getVar(DesktopConfigKey.defaultDownloadPath)
+		const defaultDownloadPathMaybeFileUrl = await this.conf.getVar(DesktopConfigKey.defaultDownloadPath)
 
-		if (defaultDownloadPath != null) {
+		if (defaultDownloadPathMaybeFileUrl != null) {
+			// the value saved to the config database changed from a file path to a file URL starting with the client version 353.260626.0
+			// (see revision ac6ba0a87f5c718649354a8f5a589dcfa570515e), so we need to check if the value is a file URL and if so, convert it to a file path
+			const defaultDownloadPath = defaultDownloadPathMaybeFileUrl.startsWith("file://")
+				? fileURLToPath(defaultDownloadPathMaybeFileUrl)
+				: defaultDownloadPathMaybeFileUrl
 			const fileName = path.basename(filename)
 			const destinationPath = path.join(defaultDownloadPath, nonClobberingFilename(await this.fs.promises.readdir(defaultDownloadPath), fileName))
 			return pathToFileURL(destinationPath)

test/tests/desktop/files/DesktopFileFacadeTest.ts
@@ -447,7 +447,8 @@ o.spec("DesktopFileFacade", function () {
 			const src = "file:///path/random.pdf"
 			when(tfs.assertInTmpDir(src)).thenReturn(new URL(src))
 			const filename = "fileName.pdf"
-			const defaultDownloadPath = "/some/downloads"
+			// we save the default download path as the return value of FileFacade#openFolderChooser in DesktopSettingsViewer, which returns a fileURL
+			const defaultDownloadPath = "file:///some/downloads"
 			when(conf.getVar(DesktopConfigKey.defaultDownloadPath)).thenResolve(defaultDownloadPath)
 			when(fs.promises.readdir(matchers.anything())).thenResolve([])
 			const copiedFileUri = await ff.putFileIntoDownloadsFolder(src, filename)