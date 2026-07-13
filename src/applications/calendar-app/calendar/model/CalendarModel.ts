b92fa99bae0a298e3cec8fcd732596617e3875d8
osb <osb@tutao.de>
2026-07-13 16:14:38 +0200

    Remove organiser address is same as sender to fix google invite updates
    Currently, google calendar uses a random string for the email address in
    the ORGANIZER field in .ics files if the event is part of a custom
    calendar.
    That does not match the sender's email address since it correstponds to
    their calendar id, which for anything but their default user calendar
    will be a random 64 char string.
    
    This is technically similar to spec compliant behaviour described in
    RFC 6047, Section 2.3:
    - https://www.rfc-editor.org/info/rfc6047/#section-2.3
    '"Sender" value may not equal that of the "Organizer"'.
    
    This commit attempt to fix this we simply remove the check that the
    email in the field matches the email's sender address.
    
    Because we have NO cryptographic signatures for .ics files, relying on
    the contents of a .ics file for permission control or access management
    does not protect against malicious actors in any way. In consequence,
    we are only concerned with negative effects that the removal of this
    check could have on existing trusted user flows. As of writing this
    commit message, we can't think of any major ones.

--- src/applications/calendar-app/calendar/model/CalendarModel.ts
+++ src/applications/calendar-app/calendar/model/CalendarModel.ts
@@ -1173,12 +1173,11 @@ export class CalendarModel {
 		uidIndexEntry: ResolvedUidIndexEntry,
 	): Promise<void> {
 		const calendarEvent = makeCalendarEventFromIcsCalendarEvent(icsCalendarEvent)
-		const sentByOrganizer: boolean = resolvedPersistedCalendarEvent.organizer != null && resolvedPersistedCalendarEvent.organizer.address === sender
 		if (method === CalendarMethod.REPLY) {
 			return this.processCalendarReply(sender, resolvedPersistedCalendarEvent, calendarEvent) // TODO: why are alarms NOT passed in here
-		} else if (sentByOrganizer && method === CalendarMethod.REQUEST) {
+		} else if (method === CalendarMethod.REQUEST) {
 			return await this.processUpdateToCalendarEventFromIcs(uidIndexEntry, resolvedPersistedCalendarEvent, calendarEvent)
-		} else if (sentByOrganizer && method === CalendarMethod.CANCEL) {
+		} else if (method === CalendarMethod.CANCEL) {
 			return await this.processCalendarCancel(uidIndexEntry, resolvedPersistedCalendarEvent)
 		} else {
 			console.log(TAG, `${method} update sent not by organizer, ignoring.`)
