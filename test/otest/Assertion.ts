bf82bbfcef302c63fc4c6ce21cd719222c04624a
sug <sug@tutao.de>
2026-07-23 16:46:28 +0200

    re-add deepEquals on otest's Assertion.ts
    browsers tests cannot import deepEquals from platform-kit/utils
    for some reason.
    
    Co-authored-by: kib <104761667+kibibytium@users.noreply.github.com>
    Co-authored-by: nif <nif@tutao.de>

--- test/otest/Assertion.ts
+++ test/otest/Assertion.ts
@@ -1,5 +1,4 @@
 import { TestError, TestResult } from "./TestResult.js"
-import { deepEqual } from "../../src/platform-kit/utils"
 
 export type AssertionDescriber = (description: string) => void
 
@@ -176,6 +175,88 @@ ${result.message}`)
 	private errorName(error: unknown): string {
 		return typeof error === "string" ? error : typeof error === "function" ? error.name : String(error)
 	}
+}
+
+/**
+ * modified deepEquals from ospec is only needed as long as we use custom classes (TypeRef) and Date is not properly handled
+ */
+function deepEqual(a: any, b: any): boolean {
+	if (a === b) return true
+	if (xor(a === null, b === null) || xor(a === undefined, b === undefined)) return false
+
+	if (typeof a === "object" && typeof b === "object") {
+		const aIsArgs = isArguments(a),
+			bIsArgs = isArguments(b)
+
+		if (a.length === b.length && ((a instanceof Array && b instanceof Array) || (aIsArgs && bIsArgs))) {
+			const aKeys = Object.getOwnPropertyNames(a),
+				bKeys = Object.getOwnPropertyNames(b)
+			if (aKeys.length !== bKeys.length) return false
+
+			for (let i = 0; i < aKeys.length; i++) {
+				if (!Object.hasOwn(b, aKeys[i]) || !deepEqual(a[aKeys[i]], b[aKeys[i]])) return false
+			}
+
+			return true
+		}
+
+		if (a instanceof Map && b instanceof Map) {
+			for (const key of a.keys()) {
+				if (!deepEqual(a.get(key), b.get(key))) {
+					return false
+				}
+			}
+			for (const key of b.keys()) {
+				if (!deepEqual(a.get(key), b.get(key))) {
+					return false
+				}
+			}
+
+			return true
+		}
+
+		// See: DeepEquals interface
+		if (typeof a.deepEquals === "function" && typeof b.deepEquals === "function") {
+			return a.deepEquals(b)
+		}
+
+		if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime()
+
+		if (a instanceof Uint8Array && b instanceof Uint8Array) {
+			if (a.length !== b.length) return false
+			for (let i = 0; i < a.length; i++) {
+				if (a[i] !== b[i]) return false
+			}
+
+			return true
+		}
+
+		if (a instanceof Object && b instanceof Object && !aIsArgs && !bIsArgs) {
+			for (let i in a) {
+				if (!(i in b) || !deepEqual(a[i], b[i])) return false
+			}
+
+			for (let i in b) {
+				if (!(i in a)) return false
+			}
+
+			return true
+		}
+
+		// @ts-ignore: we would need to include all @types/node for this to work or import it explicitly. Should probably be rewritten for all typed arrays.
+		if (typeof Buffer === "function" && a instanceof Buffer && b instanceof Buffer) {
+			if (a.length !== b.length) return false
+			for (let i = 0; i < a.length; i++) {
+				if (a[i] !== b[i]) return false
+			}
+
+			return true
+		}
+
+		if (a.valueOf() === b.valueOf()) return true
+	}
+
+	return false
 }
 
 class AssertionError extends Error {}
