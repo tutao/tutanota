b3382c447c6b36320f8c1d8457a77181bdd9cc99
mab <mab@tutao.de>
2026-07-27 16:29:09 +0200

    tests for calendar search
    Co-authored-by: usa <usa@tutao.de>

--- test/tests/calendar/CalendarSearchTest.ts
+++ test/tests/calendar/CalendarSearchTest.ts
@@ -1,22 +1,45 @@
 import o from "@tutao/otest"
 import { createTestEntity } from "../TestUtils"
 import { CalendarEvent, CalendarEventTypeRef } from "@tutao/entities/tutanota"
-import { SearchModel } from "../../../src/applications/mail-app/search/model/SearchModel"
-import { object, when } from "testdouble"
-import { CalendarModel } from "../../../src/applications/calendar-app/calendar/model/CalendarModel"
+import { SearchModel, SearchQuery } from "../../../src/applications/mail-app/search/model/SearchModel"
+import { matchers, object, when } from "testdouble"
 import { CalendarEventsRepository, DaysToEvents } from "../../../src/applications/common/calendar/date/CalendarEventsRepository"
+import { SearchFacade } from "../../../src/applications/mail-app/workerUtils/index/SearchFacade"
+import { EventController } from "../../../src/applications/common/api/main/EventController"
+import { EntityClient } from "../../../src/platform-kit/network/EntityClient"
+import { ProgressTracker } from "../../../src/applications/common/api/main/ProgressTracker"
+import { SearchCategoryType, SearchRestriction } from "../../../src/applications/common/api/worker/search/SearchTypes"
+import { ProgressMonitorInterface } from "../../../src/platform-kit/network/ProgressMonitorInterface"
+import { EventWrapper } from "../../../src/applications/calendar-app/calendar/view/CalendarViewModel"
 import { getStartOfDay } from "../../../src/platform-kit/utils"
+import Stream from "mithril/stream"
+import stream from "mithril/stream"
 
-o.spec("CalendarSearchTest", function () {
+o.spec("CalendarSearch", function () {
 	let search: SearchModel
-	let calendarRepo: CalendarEventsRepository
+	let searchFacade: SearchFacade
+	let eventController: EventController
+	let entityClient: EntityClient
+	let calendarEventsRepository: CalendarEventsRepository
+	let progressTracker: ProgressTracker
+	let abort: AbortController
 
 	o.beforeEach(function () {
-		search = object()
-		calendarRepo = object()
+		searchFacade = object()
+		eventController = object()
+		entityClient = object()
+		calendarEventsRepository = object()
+
+		progressTracker = object()
+		const progressMonitorInterface: ProgressMonitorInterface = object()
+		when(progressTracker.getMonitor(matchers.anything())).thenReturn(progressMonitorInterface)
+
+		abort = new AbortController()
+
+		search = new SearchModel(searchFacade, eventController, entityClient, async () => calendarEventsRepository, progressTracker)
 	})
 	o.spec("Event Search", function () {
-		o.test("searching an event should return all the events that contain query in summary or description", function () {
+		o.test("searching an event should return all the events that contain query in summary or description", async function () {
 			const eventStartDate = new Date(2026, 7, 27)
 			const eventEndDate = new Date(2026, 7, 28)
 			const event1 = createTestEntity(CalendarEventTypeRef, {
@@ -25,12 +48,53 @@ o.spec("CalendarSearchTest", function () {
 				startTime: eventStartDate,
 				endTime: eventEndDate,
 			})
+			event1._id = ["ListID", "Event1ID"]
 			const event2 = createTestEntity(CalendarEventTypeRef, {
 				summary: "no summary",
 				description: "test event",
 				startTime: eventStartDate,
 				endTime: eventEndDate,
 			})
+			event2._id = ["ListID", "Event2ID"]
+
+			const rangeStart = new Date(2026, 7, 20)
+			const rangeEnd = new Date(2026, 8, 1)
+
+			const restriction: SearchRestriction = {
+				start: rangeStart.getTime(),
+				end: rangeEnd.getTime(),
+				folderIds: [],
+				attributeIds: null,
+				eventSeries: null,
+				field: null,
+				type: SearchCategoryType.calendar,
+			}
+
+			const query: SearchQuery = {
+				query: "event",
+				restriction,
+				maxResults: null,
+			}
+
+			when(calendarEventsRepository.getDaysToEvents()).thenReturn(makeDaysToEvents(event1, event2))
+
+			const { tokens, resultItems } = await search.runCalendarSearch(query, abort.signal)
+			console.log(tokens, resultItems)
 		})
 	})
 })
+
+function makeDaysToEvents(...events: CalendarEvent[]): Stream<DaysToEvents> {
+	const daysToEventsMap: Map<number, EventWrapper[]> = new Map()
+
+	for (const event of events) {
+		const startOfDay = getStartOfDay(event.startTime).getTime()
+		if (!daysToEventsMap.has(startOfDay)) {
+			daysToEventsMap.set(startOfDay, [])
+		}
+		const a = daysToEventsMap.get(startOfDay)
+		a?.push({ event, flags: { isAlteredInstance: false, hasAlarms: false }, color: "whatever" })
+	}
+
+	return stream(daysToEventsMap)
+}
