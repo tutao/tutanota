aa20db8e7027c9792aa7e1295564153e4e4b2a7e
bir <bir@tutao.de>
2026-07-29 08:54:28 +0200

    Add `Mark as read` Result to Inbox rule dialog
    close: #11073

--- src/applications/mail-app/settings/AddInboxRuleDialog.ts
+++ src/applications/mail-app/settings/AddInboxRuleDialog.ts
@@ -39,7 +39,7 @@ import { onbeforeremoveColapseAnimation, oncreateExpandAnimation } from "../../.
 import { IconButton } from "../../../ui/base/IconButton"
 import { ButtonSize } from "../../../ui/base/ButtonSize"
 import { SelectorItem } from "../../../ui/base/DropDownSelector"
-import { getInboxRuleResultTypeNameMapping, getInboxRuleConditionTypeNameMapping } from "../mail/model/InboxRuleHandler"
+import { getInboxRuleConditionTypeNameMapping, getInboxRuleResultTypeNameMapping } from "../mail/model/InboxRuleHandler"
 
 assertMainOrNode()
 
@@ -517,6 +517,7 @@ function getRuleResultValueInputByType(ruleResult: InboxRuleResultField) {
 					class: "",
 				})
 		case InboxRuleResultType.EXCLUDE_SPAM:
+		case InboxRuleResultType.READ:
 			return null
 		default:
 			throw new ProgrammingError(`No Input specified for rule result of type: ${ruleResult.type()}`)
