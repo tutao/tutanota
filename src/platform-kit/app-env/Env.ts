1c1ea51ea783f78fed9b013ecc3e43fbbb20bba4
sug <sug@tutao.de>
2026-07-31 11:47:42 +0200

    [simplification] use envProvider in ClientDetector instead of env

--- src/platform-kit/app-env/Env.ts
+++ src/platform-kit/app-env/Env.ts
@@ -1,4 +1,5 @@
 import { ProgrammingError } from "./ProgrammingError"
+import { _isNode, _isWorker } from "./boot/TsPlatformConstants"
 
 // keep in sync with LaunchHtml.js meta tag title
 export const LOGIN_TITLE = "Mail. Done. Right. Tuta Mail Login & Sign up for an Ad-free Mailbox"
@@ -15,6 +16,7 @@ export type EnvType = {
 	networkDebugging: boolean
 	clientName: string | null
 }
+
 export const enum PlatformId {
 	Ios = "ios",
 	Android = "android",
@@ -77,11 +79,17 @@ export const enum Mode {
  * If not set defaults to true
  */
 const assertionsEnabled = false
+
 export class EnvProvider {
 	private boot: boolean
-	constructor(private readonly env: EnvType) {
+
+	constructor(public readonly env: EnvType) {
 		const isDesktopMainThread = _isNode && this.env != null && (this.isDesktop() || this.isAdminClient())
 		this.boot = !isDesktopMainThread && !_isWorker
+	}
+
+	public getPlatformId(): PlatformId | null {
+		return this.env.platformId
 	}
 
 	isIOSApp(): boolean {
@@ -156,6 +164,7 @@ export class EnvProvider {
 	isOfflineStorageAvailable(): boolean {
 		return !this.isBrowser() && !this.isAdminClient()
 	}
+
 	bootFinished() {
 		this.boot = false
 	}
@@ -207,71 +216,26 @@ export class EnvProvider {
 		}
 	}
 }
+
 export const envProvider = new EnvProvider(env)
 
 // ========================================================
 // TODO: Inline these (CTRL+ALT+N), it will just change all files that import it
-export function assertMainOrNode() {
-	return envProvider.assertMainOrNode()
-}
-export function assertMainOrNodeBoot() {
-	return envProvider.assertMainOrNodeBoot()
-}
-export function assertWorkerOrNode() {
-	return envProvider.assertWorkerOrNode()
-}
-export function isIOSApp() {
-	return envProvider.isIOSApp()
-}
-export function isAppleDevice() {
-	return envProvider.isAppleDevice()
-}
-export function isAndroidApp() {
-	return envProvider.isAndroidApp()
-}
-export function isApp() {
-	return envProvider.isApp()
-}
-export function isDesktop() {
-	return envProvider.isDesktop()
-}
-export function isBrowser() {
-	return envProvider.isBrowser()
-}
-export function isWebClient() {
-	return envProvider.isWebClient()
-}
-export function isAdminClient() {
-	return envProvider.isAdminClient()
-}
-export function isElectronClient() {
-	return envProvider.isElectronClient()
-}
-export function isMainOrNode() {
-	return envProvider.isMainOrNode()
-}
-export function isWorkerOrNode() {
-	return envProvider.isWorkerOrNode()
-}
-export function isWorker() {
-	return envProvider.isWorker()
-}
-export function isMain() {
-	return envProvider.isMain()
-}
-export const isTest = () => {
-	return envProvider.isTest()
-}
-export const isOfflineStorageAvailable = () => {
-	return envProvider.isOfflineStorageAvailable()
-}
-export const getApiBaseUrl = (dc: DomainConfig) => {
-	return envProvider.getApiBaseUrl(dc)
-}
-export const getWebsocketBaseUrl: typeof envProvider.getWebsocketBaseUrl = (dc: DomainConfig) => {
-	return envProvider.getWebsocketBaseUrl(dc)
-}
-
-export const bootFinished = () => {
-	envProvider.bootFinished()
-}
+export const assertMainOrNode = envProvider.assertMainOrNode
+export const assertMainOrNodeBoot = envProvider.assertMainOrNodeBoot
+export const assertWorkerOrNode = envProvider.assertWorkerOrNode
+export const isIOSApp = envProvider.isIOSApp
+export const isAppleDevice = envProvider.isAppleDevice
+export const isAndroidApp = envProvider.isAndroidApp
+export const isApp = envProvider.isApp
+export const isDesktop = envProvider.isDesktop
+export const isBrowser = envProvider.isBrowser
+export const isWebClient = envProvider.isWebClient
+export const isAdminClient = envProvider.isAdminClient
+export const isMainOrNode = envProvider.isMainOrNode
+export const isWorker = envProvider.isWorker
+export const isTest = envProvider.isTest
+export const isOfflineStorageAvailable = envProvider.isOfflineStorageAvailable
+export const getApiBaseUrl = envProvider.getApiBaseUrl
+export const getWebsocketBaseUrl = envProvider.getWebsocketBaseUrl
+export const bootFinished = envProvider.bootFinished
