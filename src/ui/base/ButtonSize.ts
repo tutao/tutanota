affb8a95a5795943d721cd5de9cfebed98af3373
wrd <wrd@tutao.de>
2026-08-06 14:11:12 +0200

    Change inbox rule list to new design
    Close #11076
    Co-authored-by: hnb <hnb@tutao.de>

--- src/ui/base/ButtonSize.ts
+++ src/ui/base/ButtonSize.ts
@@ -2,6 +2,10 @@
  * Buttons have an interactive area that is larger than the visual size
  * this size is too large to fit into some injections, so we have
  * a compact size for that.
+ * Large = 56px button, 32px icon
+ * Normal = 44px button, 24px icon
+ * Small = 32px button, 24px icon
+ * Compact = 30px button, 24px icon
  */
 export const enum ButtonSize {
 	Small,
