b14efabb1fa70359fc08b13d815c48147d7cd9b0
abp <abp@tutao.de>
2026-08-14 09:00:03 +0200

    init connectivity listeners correctly in drive and contact ViewModels
    We were initializing the connectivity listeners in contact and drive
    view models only when we call init() for the first time, and they were
    not initialized again for the further calls, because of early returns.

--- src/applications/mail-app/contacts/view/ContactViewModel.ts
+++ src/applications/mail-app/contacts/view/ContactViewModel.ts
@@ -47,6 +47,7 @@ export class ContactViewModel {
 
 	/** init is called every time the view is opened */
 	async init(contactListId?: Id) {
+		this.connectivityModel.addConnectionStateListener(this.connectivityListener)
 		// update url if the view was just opened
 		if (contactListId == null) this.updateUrl()
 		if (this.contactListId) return
@@ -54,8 +55,6 @@ export class ContactViewModel {
 		this.contactListId = assertNotNull(await this.contactModel.getContactListId(), "not available for external users")
 
 		this.initOnce()
-
-		this.connectivityModel.addConnectionStateListener(this.connectivityListener)
 
 		await this.listModel.loadInitial()
 	}
