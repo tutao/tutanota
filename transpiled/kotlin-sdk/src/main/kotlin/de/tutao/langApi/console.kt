e7511139042065c9971ef3bb75c9e181ff6e847c
sug <sug@tutao.de>
2026-08-18 17:24:16 +0200

    add more files

---
+++ transpiled/kotlin-sdk/src/main/kotlin/de/tutao/langApi/console.kt
@@ -0,0 +1,25 @@
+package de.tutao.langApi
+
+class console {
+	companion object {
+		fun log(vararg items: Any) {
+			println(items)
+		}
+
+		fun warn(vararg items: Any) {
+			println(items)
+		}
+
+		fun debug(vararg items: Any) {
+			println(items)
+		}
+
+		fun error(vararg items: Any) {
+			println(items)
+		}
+
+		fun trace(vararg items: Any) {
+			println(items)
+		}
+	}
+}
