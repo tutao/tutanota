e8761800094336796fc3a06942af169edf8a13c6
sug <sug@tutao.de>
2026-08-18 11:51:47 +0200

    map nullable type argument to T?

--- transpiled/kotlin-sdk/src/main/kotlin/de/tutao/langApi/types/KtObject.kt
+++ transpiled/kotlin-sdk/src/main/kotlin/de/tutao/langApi/types/KtObject.kt
@@ -1,3 +1,13 @@
 package de.tutao.langApi.types
 
-class KtObject
+class KtObject {
+	companion object {
+		fun <T> freeze(list: List<T>): List<T> {
+			return list.toList()
+		}
+
+		fun <T> freeze(list: Array<T>): List<T> {
+			return list.toList()
+		}
+	}
+}
