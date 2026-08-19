eea1b741689aaf1cc1fd27965a94af69038d8286
Kinan <104761667+kibibytium@users.noreply.github.com>
2026-08-19 17:56:52 +0200

    [transpiler] start swift-sdk project
    Co-authored-by: sug <sug@tutao.de>

---
+++ transpiled/swift-sdk/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+let package = Package(
+    name: "swift-sdk",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swift-sdk",
+            targets: ["AppEnv", "LangApi"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "LangApi", dependencies: [], path: "Sources/LangApi"),
+        .target(name: "AppEnv",dependencies: ["LangApi"], path: "Sources/PlatformKit/AppEnv"),
+    ]
+)
