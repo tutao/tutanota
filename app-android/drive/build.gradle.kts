cfffe33730bd5f0144528e29fcdba1f4840d0910
jenkins build server <jenkins@next.tutao.de>
2026-08-20 15:20:36 +0200

    v357.260820.0

--- app-android/drive/build.gradle.kts
+++ app-android/drive/build.gradle.kts
@@ -20,8 +20,8 @@ android {
 		applicationId = "de.tutao.drive"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 44
-		versionName = "357.260818.1"
+		versionCode = 45
+		versionName = "357.260820.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
