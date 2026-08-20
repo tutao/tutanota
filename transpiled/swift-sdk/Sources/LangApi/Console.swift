dd5c7f43e0f7e91280ddb33fdf69df4b8e6724cb
Kinan <104761667+kibibytium@users.noreply.github.com>
2026-08-20 13:50:35 +0200

    implement LangApi::console in swift
    Co-authored-by: sug <sug@tutao.de>

---
+++ transpiled/swift-sdk/Sources/LangApi/Console.swift
@@ -0,0 +1,26 @@
+
+public class console {
+	private static func write(_ items: Any...) {
+		print(items.map { "\($0)" }.joined(separator:  " "))
+	}
+
+	public static func log(_ items: Any...) {
+		write(items)
+	}
+
+	public static func warn(_ items: Any...) {
+		write(["W"] + items)
+	}
+
+	public static func error(_ items: Any...) {
+		write(["E"] + items)
+	}
+
+	public static func debug(_ items: Any...) {
+		write(["D"] + items)
+	}
+
+	public static func trace(_ items: Any...) {
+		write(["T"] + items)
+	}
+}
