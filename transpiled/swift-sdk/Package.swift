92d1dae1a5cf724bee95f3e3702753101c2f0f32
Kinan <104761667+kibibytium@users.noreply.github.com>
2026-08-20 14:32:03 +0200

    swift import statements
    Co-authored-by: sug <sug@tutao.de>

--- transpiled/swift-sdk/Package.swift
+++ transpiled/swift-sdk/Package.swift
@@ -8,15 +8,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "swift-sdk",
-            targets: ["AppEnv", "LangApi"]
+            targets: ["Tutao_AppEnv", "Tutao_LangApi"]
         ),
         .executable(name: "Cli", targets: ["Cli"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "LangApi", dependencies: [], path: "Sources/LangApi"),
-        .target(name: "AppEnv",dependencies: ["LangApi"], path: "Sources/PlatformKit/AppEnv"),
-        .target(name: "Cli", dependencies: ["LangApi"])
+        .target(name: "Tutao_LangApi", dependencies: [], path: "Sources/LangApi"),
+        .target(name: "Tutao_AppEnv", dependencies: ["Tutao_LangApi"], path: "Sources/PlatformKit/AppEnv"),
+        .target(name: "Tutao_FingerPrintJs", path: "Sources/LangApi/FingerPrintJs"),
+        .target(name: "Cli", dependencies: ["Tutao_LangApi", "Tutao_AppEnv"])
     ]
 )
