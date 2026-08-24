d665d14839681fc9a028f2d4ede1b57786437e10
and <and@tutao.de>
2026-08-24 10:07:36 +0200

    Introduce better UI state handling for the widget
    This commit changes the internal data structured used by the view model
    to a more descriptive type.
    
    It also changes when we load the initial state of a widget moving the
    coroutine to outside provide content.

--- app-android/calendar/src/main/java/de/tutao/calendar/widget/data/WidgetUIData.kt
+++
@@ -1,8 +0,0 @@
-package de.tutao.calendar.widget.data
-
-import java.time.LocalDate
-
-data class WidgetUIData(
-	val normalEvents: HashMap<LocalDate, List<UIEvent>>,
-	val allDayEvents: HashMap<LocalDate, List<UIEvent>>,
-)
