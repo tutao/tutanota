tutao/tutanota

commit eff866c1acb03c88275a067407fa2078891546af

Streamed file uploads in apps
On Android it is not possible to seek data obtained via content resolver
in an efficient way.

We have changed file handling to keep the file opened and read chunks
from it sequentially.

Adjust iOS code to exchange paths via file URIs

tuta#3602

Co-authored-by: mab <mab@tutao.de>
Co-authored-by: ivk <ivk@tutao.de>

app-ios/TutanotaSharedFramework/Files/TempFs.swift
@@ -1,50 +1,119 @@
 public actor TempFs {
-	var tutaUriToNativeUri = [URL: String]()
+	private var inMemoryFiles = [Filename: Data]()
+	private var openStreams = [Filename: SimpleInputStream]()
 	public init() {}
-	func createFileChunkUri(fileUri: String, start: Int64, length: Int64) throws -> URL {
-		let chunkId = base64ToBase64Url(inputText: (secureRandomData(ofLength: 16).base64EncodedString()))
-		let tutaUriString = "tuta-chunk://\(chunkId)?start=\(start)&length=\(length)"
-		let tutaUri = try urlFromString(string: tutaUriString)
-		self.tutaUriToNativeUri[tutaUri] = fileUri
-		return tutaUri
-	}
-	func fileStream(tutaUri: String) throws -> Data {
-		let chunk = try parseChunkUri(uri: tutaUri)
-		let handle = try FileHandle(forReadingFrom: try urlFromString(string: chunk.nativeUri))
-		try handle.seek(toOffset: chunk.start)
-		// Conversion from Int64 to Int should be OK here as we're on 64 bit-platforms anyway.
-		let readCount = Int(chunk.length)
-		// Apple's documentation for FileHandle.read() does not explain *at all* when the optional type
-		// returning Data is empty. It seems that in all cases it either returns a valid (potentially
-		// empty) Data object or throws. So we'll just assume it's OK to force-unwrap.
-		return try handle.read(upToCount: readCount)!
-	}
 
-	func fileInfo(uri: String) throws -> FileInfo {
-		let chunk = try parseChunkUri(uri: uri)
-		let fileInfo = try getFileInfo(fileUri: try urlFromString(string: chunk.nativeUri))
-		return FileInfo(name: "\(fileInfo.name).\(chunk.start).chunk", size: chunk.length)
-	}
-
-	func fileExists(uri: String) throws -> Bool {
-		let chunk = try self.parseChunkUri(uri: uri)
-		return FileUtils.fileExists(atPath: chunk.nativeUri)
-	}
-	func parseChunkUri(uri: String) throws -> ChunkUri {
-		guard let parsed = URL(string: uri), parsed.scheme == "tuta-chunk", let startString = parsed.queryParameter("start"),
-			let lengthString = parsed.queryParameter("length")
-		else { throw FileError(message: "uri cannot be parsed: \(uri)") }
-
-		guard let start = UInt64(startString), let length = Int64(lengthString) else {
-			throw FileError(message: "invalid start or length: \(startString), \(lengthString)")
+	func fileStream(tutaUri: String) async throws -> SimpleInputStream {
+		let tutaUrl = try TutaUrl(url: tutaUri)
+		switch tutaUrl {
+		case .tmp(let name):
+			let data = try self.readInMemoryFile(name)
+			return DataSimpleInputStream(data: data)
+		case .stream(let name): return try self.openedStream(fileName: name)
+		case .file(let url):
+			try assertInTmp(url: url)
+			return try FileHandleSimpleInputStream(fileHandle: FileHandle(forReadingFrom: url))
 		}
-		guard let nativeUri = self.tutaUriToNativeUri[parsed] else { throw FileError(message: "tuta uri does not map to native: \(parsed)") }
-		return ChunkUri(start: start, length: length, nativeUri: nativeUri)
 	}
+	func fileInfo(uri: String) throws -> FileInfo {
+		let tutaUrl = try TutaUrl(url: uri)
+		switch tutaUrl {
+		case .tmp(let name):
+			let chunk = try self.readInMemoryFile(name)
+			return FileInfo(name: name.fileName, size: Int64(chunk.count))
+		case .stream: throw GenericTutanotaError(message: "fileInfo is not available for tuta-stream uris")
+		case .file(let url):
+			try self.assertInTmp(url: url)
+			let fileInfo = try getFileInfo(fileUri: url)
+			return fileInfo
+		}
+	}
+
+	func openFileForReading(uri: String) throws -> String {
+		let fileUrl = try URL.from(fileUrl: uri)
+		let stream = try FileHandleSimpleInputStream(fileHandle: FileHandle(forReadingFrom: fileUrl))
+		let fileName = self.generateFilename()
+		self.openStreams[fileName] = stream
+		return TutaUrl.stream(name: fileName).asString()
+	}
+	func closeFile(streamUri: String) throws {
+		guard case .stream(let name) = try TutaUrl(url: streamUri) else { throw GenericTutanotaError(message: "not a valid URL: \(streamUri)") }
+		self.openStreams.removeValue(forKey: name)  // deinit will close it
+	}
+	func deleteFile(uri: String) throws {
+		let tutaUrl = try TutaUrl(url: uri)
+		switch tutaUrl {
+		case .tmp(let name): self.deleteInMemoryFile(name: name)
+		case .stream(let name): throw GenericTutanotaError(message: "cannot delete stream \(name)")
+		case .file(let url):
+			try assertInTmp(url: url)
+			do { try FileManager.default.removeItem(at: url) } catch {
+				if (error as NSError).code == NSFileNoSuchFileError { return printLog("Tried to delete file \(uri) that does not exist.") }
+				throw FileError(message: "Failed to delete file \(uri)", underlyingError: error)
+			}
+		}
+	}
+	func createInMemoryFile(data: Data) -> String {
+		let fileName = self.generateFilename()
+		self.inMemoryFiles[fileName] = data
+		return TutaUrl.tmp(name: fileName).asString()
+	}
+	func readAsData(uri: String) async throws -> Data {
+		let tutaUrl = try TutaUrl(url: uri)
+		switch tutaUrl {
+		case .tmp(let name): return try self.readInMemoryFile(name)
+		case .stream(let name):
+			var stream = try self.openedStream(fileName: name)
+			return try stream.readUntilEnd() ?? Data()
+		case .file(let url):
+			try assertInTmp(url: url)
+			return try Data(contentsOf: url)
+		}
+	}
+	private func readInMemoryFile(_ fileName: Filename) throws -> Data {
+		guard let buffer = self.inMemoryFiles[fileName] else { throw FileError(message: "File not found \(fileName.fileName)") }
+		return buffer
+	}
+	private func openedStream(fileName: Filename) throws -> SimpleInputStream {
+		guard let stream = self.openStreams[fileName] else { throw FileError(message: "stream not found: \(fileName.fileName)") }
+		return stream
+	}
+	func closeFileStream(stream: SimpleInputStream) {
+		// deinit will close it
+	}
+	private func assertInTmp(url: URL) throws {
+		if !url.path(percentEncoded: false).starts(with: (FileManager().temporaryDirectory.path(percentEncoded: false))) {
+			throw FileError(message: "file does not exist on this url \(url)")
+		}
+	}
+	private func deleteInMemoryFile(name: Filename) { self.inMemoryFiles.removeValue(forKey: name) }
+	private func generateFilename() -> Filename { Filename(fileName: secureRandomData(ofLength: 16).hexEncodedString()) }
 }
 
-struct ChunkUri {
-	let start: UInt64
-	let length: Int64
-	let nativeUri: String
+private struct Filename: Equatable, Hashable { let fileName: String }
+
+private enum TutaUrl {
+	case file(url: URL)
+	case tmp(name: Filename)
+	case stream(name: Filename)
+	init(url urlString: String) throws {
+		guard let url = URL(string: urlString) else { throw GenericTutanotaError(message: "Invalid URL: \(urlString)") }
+		switch url.scheme {
+		case "tuta-tmp":
+			let name = Filename(fileName: url.path(percentEncoded: false))
+			self = .tmp(name: name)
+		case "tuta-stream":
+			let name = Filename(fileName: url.path(percentEncoded: false))
+			self = .stream(name: name)
+		case "file": self = .file(url: url)
+		default: throw GenericTutanotaError(message: "Invalid URL: \(urlString)")
+		}
+	}
+	func asString() -> String {
+		switch self {
+		case .file(let url): return url.absoluteString
+		case .tmp(let name): return "tuta-tmp:\(name.fileName)"
+		case .stream(let name): return "tuta-stream:\(name.fileName)"
+		}
+	}
 }

app-android/app/src/main/java/de/tutao/tutanota/MainActivity.kt
@@ -70,6 +70,7 @@ import de.tutao.tutashared.AppType
 import de.tutao.tutashared.CancelledError
 import de.tutao.tutashared.DateProviderImpl
 import de.tutao.tutashared.NetworkUtils
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.Theme
 import de.tutao.tutashared.WebViewReloader
 import de.tutao.tutashared.alarms.AlarmNotificationsManager
@@ -78,6 +79,7 @@ import de.tutao.tutashared.createAndroidKeyStoreFacade
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
 import de.tutao.tutashared.file.AndroidFileFacade
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.ipc.AndroidGlobalDispatcher
 import de.tutao.tutashared.ipc.CalendarOpenAction
 import de.tutao.tutashared.ipc.CommonNativeFacade
@@ -162,6 +164,8 @@ class MainActivity : FragmentActivity(), ActivityUtils, WebViewReloader, Webauth
 
 		//we need to manually enable edge-to-edge to get `windowInsets` in Android ≤ 14
 		enableEdgeToEdge()
+		val tempDir = TempDir(applicationContext)
+		val tempFs = TempFs(applicationContext, SecureRandom(), tempDir)
 
 		val localNotificationsFacade = LocalNotificationsFacade(this, sseStorage)
 		val fileFacade =
@@ -170,6 +174,7 @@ class MainActivity : FragmentActivity(), ActivityUtils, WebViewReloader, Webauth
 				this,
 				localNotificationsFacade,
 				SecureRandom(),
+				tempFs,
 				NetworkUtils.defaultClient,
 				{ fileId, bytes ->
 					lifecycleScope.launch {
@@ -184,7 +189,7 @@ class MainActivity : FragmentActivity(), ActivityUtils, WebViewReloader, Webauth
 				BuildConfig.FILE_PROVIDER_AUTHORITY
 			)
 		val calendarFacade = AndroidCalendarFacade(NetworkUtils.defaultClient, webView.settings.userAgentString)
-		val cryptoFacade = AndroidNativeCryptoFacade(this, fileFacade.tempDir)
+		val cryptoFacade = AndroidNativeCryptoFacade(this, tempFs)
 
 
 		val alarmNotificationsManager = AlarmNotificationsManager(
@@ -208,7 +213,7 @@ class MainActivity : FragmentActivity(), ActivityUtils, WebViewReloader, Webauth
 
 		sqlCipherFacade = AndroidSqlCipherFacade(this)
 		commonSystemFacade =
-			AndroidCommonSystemFacade(this, sqlCipherFacade, fileFacade.tempDir, NetworkUtils.defaultClient)
+			AndroidCommonSystemFacade(this, sqlCipherFacade, tempDir, NetworkUtils.defaultClient)
 
 		val webauthnFacade = AndroidWebauthnFacade(this, ipcJson, "tutanota", BuildConfig.APPLICATION_ID)
 
@@ -225,7 +230,8 @@ class MainActivity : FragmentActivity(), ActivityUtils, WebViewReloader, Webauth
 				db,
 				BuildConfig.FILE_PROVIDER_AUTHORITY,
 				AppType.MAIL,
-				null
+				null,
+				tempDir
 			),
 			CredentialsEncryptionFactory.create(this, cryptoFacade, db),
 			cryptoFacade,

app-android/app/src/main/java/de/tutao/tutanota/push/MailNotificationActionReceiver.kt
@@ -1,6 +1,5 @@
 package de.tutao.tutanota.push
 
-import android.app.Notification
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -12,17 +11,19 @@ import de.tutao.tutasdk.Sdk
 import de.tutao.tutashared.AndroidNativeCryptoFacade
 import de.tutao.tutashared.SdkFileClient
 import de.tutao.tutashared.SdkRestClient
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.createAndroidKeyStoreFacade
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.push.SseStorage
-import de.tutao.tutashared.toSdkIdTupleGenerated
 import de.tutao.tutashared.push.toSdkCredentials
+import de.tutao.tutashared.toSdkIdTupleGenerated
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.security.SecureRandom
 
 /**
  * Performs mail notification actions
@@ -41,11 +42,15 @@ class MailNotificationActionReceiver : BroadcastReceiver() {
 				val keyStoreFacade = createAndroidKeyStoreFacade()
 				val sseStorage = SseStorage(db, keyStoreFacade)
 
-				val crypto = AndroidNativeCryptoFacade(context)
+				val crypto = AndroidNativeCryptoFacade(context, TempFs(context, SecureRandom(), TempDir(context)))
 				val nativeCredentialsFacade = CredentialsEncryptionFactory.create(context, crypto, db)
 				val credentials = nativeCredentialsFacade.loadByUserId(notificationInfo.userId)!!.toSdkCredentials()
 
-				val sdk = Sdk(sseStorage.getSseOrigin()!!, SdkRestClient(), SdkFileClient(context.filesDir)).login(credentials)
+				val sdk = Sdk(
+					sseStorage.getSseOrigin()!!,
+					SdkRestClient(),
+					SdkFileClient(context.filesDir)
+				).login(credentials)
 
 				when (action) {
 					TRASH_ACTION -> sendMailToTrash(sdk, notificationInfo)

app-android/app/src/main/java/de/tutao/tutanota/push/PushNotificationService.kt
@@ -12,12 +12,14 @@ import de.tutao.tutashared.DateProviderImpl
 import de.tutao.tutashared.LifecycleJobService
 import de.tutao.tutashared.NetworkUtils
 import de.tutao.tutashared.SuspensionHandler
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.alarms.AlarmNotificationsManager
 import de.tutao.tutashared.alarms.SystemAlarmFacade
 import de.tutao.tutashared.createAndroidKeyStoreFacade
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
 import de.tutao.tutashared.data.SseInfo
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.ipc.NativeCredentialsFacade
 import de.tutao.tutashared.offline.AndroidSqlCipherFacade
 import de.tutao.tutashared.push.SseStorage
@@ -26,6 +28,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import java.io.File
+import java.security.SecureRandom
 import java.util.TimeZone
 import java.util.concurrent.TimeUnit
 
@@ -80,7 +83,7 @@ class PushNotificationService : LifecycleJobService() {
 		finishJobThread.start()
 
 		val appDatabase: AppDatabase = AppDatabase.getDatabase(this, allowMainThreadAccess = true)
-		val crypto = AndroidNativeCryptoFacade(this)
+		val crypto = AndroidNativeCryptoFacade(this, TempFs(this, SecureRandom(), TempDir(this)))
 		val keyStoreFacade = createAndroidKeyStoreFacade()
 		val nativeCredentialsFacade = CredentialsEncryptionFactory.create(this, crypto, appDatabase)
 		val sseStorage = SseStorage(appDatabase, keyStoreFacade)

app-android/calendar/src/main/java/de/tutao/calendar/MainActivity.kt
@@ -63,12 +63,14 @@ import de.tutao.tutashared.AppType
 import de.tutao.tutashared.CancelledError
 import de.tutao.tutashared.DateProviderImpl
 import de.tutao.tutashared.NetworkUtils
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.alarms.AlarmNotificationsManager
 import de.tutao.tutashared.alarms.SystemAlarmFacade
 import de.tutao.tutashared.createAndroidKeyStoreFacade
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
 import de.tutao.tutashared.file.AndroidFileFacade
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.ipc.AndroidGlobalDispatcher
 import de.tutao.tutashared.ipc.CalendarOpenAction
 import de.tutao.tutashared.ipc.CommonNativeFacade
@@ -147,6 +149,8 @@ class MainActivity : FragmentActivity(), ActivityUtils {
 
 		//we need to manually enable edge-to-edge to get `windowInsets` in Android ≤ 14
 		enableEdgeToEdge()
+		val tempDir = TempDir(applicationContext)
+		val tempFs = TempFs(applicationContext, SecureRandom(), tempDir)
 
 		val localNotificationsFacade = LocalNotificationsFacade(this, sseStorage)
 		val fileFacade =
@@ -155,6 +159,7 @@ class MainActivity : FragmentActivity(), ActivityUtils {
 				this,
 				localNotificationsFacade,
 				SecureRandom(),
+				tempFs,
 				NetworkUtils.defaultClient,
 				{ fileId, bytes ->
 					lifecycleScope.launch {
@@ -168,7 +173,7 @@ class MainActivity : FragmentActivity(), ActivityUtils {
 				}, "unused"
 			)
 		val calendarFacade = AndroidCalendarFacade(NetworkUtils.defaultClient, webView.settings.userAgentString)
-		val cryptoFacade = AndroidNativeCryptoFacade(this, fileFacade.tempDir)
+		val cryptoFacade = AndroidNativeCryptoFacade(this, tempFs)
 
 
 		val alarmNotificationsManager = AlarmNotificationsManager(
@@ -192,7 +197,7 @@ class MainActivity : FragmentActivity(), ActivityUtils {
 
 		sqlCipherFacade = AndroidSqlCipherFacade(this)
 		commonSystemFacade =
-			AndroidCommonSystemFacade(this, sqlCipherFacade, fileFacade.tempDir, NetworkUtils.defaultClient)
+			AndroidCommonSystemFacade(this, sqlCipherFacade, tempDir, NetworkUtils.defaultClient)
 
 		val webauthnFacade = AndroidWebauthnFacade(this, ipcJson)
 
@@ -209,7 +214,8 @@ class MainActivity : FragmentActivity(), ActivityUtils {
 				db,
 				BuildConfig.FILE_PROVIDER_AUTHORITY,
 				AppType.CALENDAR,
-				WidgetRefresher()
+				WidgetRefresher(),
+				tempDir
 			),
 			CredentialsEncryptionFactory.create(this, cryptoFacade, db),
 			cryptoFacade,

app-android/calendar/src/main/java/de/tutao/calendar/push/PushNotificationService.kt
@@ -12,16 +12,19 @@ import de.tutao.tutashared.DateProviderImpl
 import de.tutao.tutashared.LifecycleJobService
 import de.tutao.tutashared.NetworkUtils
 import de.tutao.tutashared.SuspensionHandler
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.alarms.AlarmNotificationsManager
 import de.tutao.tutashared.alarms.SystemAlarmFacade
 import de.tutao.tutashared.createAndroidKeyStoreFacade
 import de.tutao.tutashared.data.AppDatabase
 import de.tutao.tutashared.data.SseInfo
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.push.SseStorage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
+import java.security.SecureRandom
 import java.util.TimeZone
 import java.util.concurrent.TimeUnit
 
@@ -74,9 +77,10 @@ class PushNotificationService : LifecycleJobService() {
 		state = State.CREATED
 
 		finishJobThread.start()
-
+		val tempDir = TempDir(applicationContext, SecureRandom())
+		val tempFs = TempFs(applicationContext, SecureRandom(), tempDir)
 		val appDatabase: AppDatabase = AppDatabase.getDatabase(this, allowMainThreadAccess = true)
-		val crypto = AndroidNativeCryptoFacade(this)
+		val crypto = AndroidNativeCryptoFacade(this, tempFs)
 		val keyStoreFacade = createAndroidKeyStoreFacade()
 		val dateProvider = DateProviderImpl()
 		val suspensionHandler = SuspensionHandler(dateProvider)

app-android/calendar/src/main/java/de/tutao/calendar/widget/Agenda.kt
@@ -53,10 +53,13 @@ import de.tutao.tutashared.AndroidNativeCryptoFacade
 import de.tutao.tutashared.IdTuple
 import de.tutao.tutashared.SdkFileClient
 import de.tutao.tutashared.SdkRestClient
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.ipc.CalendarOpenAction
 import de.tutao.tutashared.remote.RemoteStorage
+import java.security.SecureRandom
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -129,7 +132,9 @@ class Agenda : GlanceAppWidget() {
 	): Pair<WidgetUIViewModel, String?> {
 		val db = AppDatabase.getDatabase(context, true)
 		val remoteStorage = RemoteStorage(db)
-		val crypto = AndroidNativeCryptoFacade(context)
+		val tempDir = TempDir(context)
+		val tempFs = TempFs(context, SecureRandom(), tempDir)
+		val crypto = AndroidNativeCryptoFacade(context, tempFs)
 		val nativeCredentialsFacade = CredentialsEncryptionFactory.create(context, crypto, db)
 
 		val sdk = try {

app-android/calendar/src/main/java/de/tutao/calendar/widget/WidgetConfigActivity.kt
@@ -106,8 +106,10 @@ import de.tutao.tutashared.AndroidNativeCryptoFacade
 import de.tutao.tutashared.CredentialType
 import de.tutao.tutashared.SdkFileClient
 import de.tutao.tutashared.SdkRestClient
+import de.tutao.tutashared.TempDir
 import de.tutao.tutashared.credentials.CredentialsEncryptionFactory
 import de.tutao.tutashared.data.AppDatabase
+import de.tutao.tutashared.file.TempFs
 import de.tutao.tutashared.ipc.CalendarOpenAction
 import de.tutao.tutashared.ipc.CredentialsInfo
 import de.tutao.tutashared.ipc.DataWrapper
@@ -116,6 +118,7 @@ import de.tutao.tutashared.parseColor
 import de.tutao.tutashared.remote.RemoteStorage
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.launch
+import java.security.SecureRandom
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -138,8 +141,9 @@ class WidgetConfigActivity : AppCompatActivity() {
 			val remoteStorage = RemoteStorage(db)
 
 			val serverURL = remoteStorage.getRemoteUrl()
-
-			val crypto = AndroidNativeCryptoFacade(baseContext)
+			val tempDir= TempDir(applicationContext)
+			val tempFs= TempFs(applicationContext, SecureRandom(),tempDir)
+			val crypto = AndroidNativeCryptoFacade(baseContext, tempFs)
 			val sdk =
 				if (serverURL == null) {
 					null

commit bddd4fead941251bf7991f5ce3d4d1c607974244

[ios] Fix sharing files to create draft
Close #11317

Co-authored-by: hnb <hnb@tutao.de>
Co-authored-by: bir <bir@tutao.de>

Was broken since eff866c1acb03c88275a067407fa2078891546af.

app-ios/TutanotaSharedFramework/Files/TempFs.swift
@@ -23,7 +23,6 @@ public actor TempFs {
 			return FileInfo(name: name.fileName, size: Int64(chunk.count))
 		case .stream: throw GenericTutanotaError(message: "fileInfo is not available for tuta-stream uris")
 		case .file(let url):
-			try self.assertInTmp(url: url)
 			let fileInfo = try getFileInfo(fileUri: url)
 			return fileInfo
 		}

app-ios/calendar/Sources/ViewController.swift
@@ -259,7 +259,7 @@ class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelega
 			return
 		}
 
-		do { try await self.commonNativeFacade.createMailEditor(info.fileUrls.map { $0.path }, info.text, [], "", "") } catch {
+		do { try await self.commonNativeFacade.createMailEditor(info.fileUrls.map { $0.absoluteString }, info.text, [], "", "") } catch {
 			printLog("failed to open mail editor to share: \(error)")
 			try FileUtils.deleteSharedStorage(subDir: info.identifier)
 		}

app-ios/drive/Sources/ViewController.swift
@@ -242,8 +242,7 @@ class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelega
 			return
 		}
 
-		// FIXME
-		do { try await self.commonNativeFacade.createMailEditor(info.fileUrls.map { $0.path }, info.text, [], "", "") } catch {
+		do { try await self.commonNativeFacade.createMailEditor(info.fileUrls.map { $0.absoluteString }, info.text, [], "", "") } catch {
 			printLog("failed to open mail editor to share: \(error)")
 			try FileUtils.deleteSharedStorage(subDir: info.identifier)
 		}

app-ios/tutanota/Sources/ViewController.swift
@@ -286,7 +286,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UISc
 			return
 		}
 
-		do { try await self.commonNativeFacade.createMailEditor(info.fileUrls.map { $0.path }, info.text, [], "", "") } catch {
+		do { try await self.commonNativeFacade.createMailEditor(info.fileUrls.map { $0.absoluteString }, info.text, [], "", "") } catch {
 			TUTSLog("failed to open mail editor to share: \(error)")
 			try FileUtils.deleteSharedStorage(subDir: info.identifier)
 		}