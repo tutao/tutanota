1db2e9ea60e8a31ad25325f8c9d2f9638808c3f5
Kinan <104761667+kibibytium@users.noreply.github.com>
2026-09-11 17:07:03 +0200

    [plugin-kit] upload file to nextcloud remotely

--- src/plugin-kit/plugins/nextcloud/Plugin.ts
+++ src/plugin-kit/plugins/nextcloud/Plugin.ts
@@ -1,6 +1,11 @@
 import { PluginApi, PluginMetadata } from "../../sdk/PluginApi"
-import { ButtonConfiguration, ButtonExtensionPoint, ButtonRef, PluginHostApi } from "../../sdk/PluginHostApi"
+import { ButtonConfiguration, ButtonExtensionPoint, PluginHostApi } from "../../sdk/PluginHostApi"
 import { AttachmentButtonExtension, PluginDataFile } from "../../sdk/AttachmentButtonExtensionPoint"
+import { default as ncAxios } from "@nextcloud/axios"
+import { DownloadReturn } from "../../../applications/common/file/FileController"
+import { TransferId } from "../../../entities/drive/Utils"
+import { Dialog } from "../../../ui/base/Dialog"
+import { LanguageViewModel } from "../../../ui/utils/LanguageViewModel"
 
 export class Plugin extends PluginApi implements AttachmentButtonExtension {
 	public readonly attachmentButton: ButtonConfiguration
@@ -26,7 +31,37 @@ export class Plugin extends PluginApi implements AttachmentButtonExtension {
 
 	async unload(): Promise<void> {}
 
-	attachmentButtonClicked(dataFile: PluginDataFile): void {
+	async attachmentButtonClicked(dataFile: PluginDataFile): Promise<void> {
 		console.log("data file " + dataFile.name)
+
+		const davFileName = `dav/files/admin/tuta/${dataFile.name}`
+		const url = `http://nextcloud.tuta/remote.php/${davFileName}`
+
+		const { transferIds, promise } = makePutRequestToNextcloud(url, dataFile.data)
+		await promise
+		console.log("uploaded to nextcloud?")
 	}
+}
+
+function makePutRequestToNextcloud(saveDirUri: string, fileContent: Uint8Array): DownloadReturn {
+	const loginName = "admin"
+	const appPassword = "APP_PASSWORD" // The generated Nextcloud App Password
+	const token = btoa(`${loginName}:${appPassword}`)
+	console.log(token)
+	const filePutHeaders = {
+		headers: {
+			"If-None-Match": "*", // do not override already existing files,
+			Authorization: `Basic ${token}`,
+		},
+	}
+	const transferIds: TransferId[] = []
+	const promise = ncAxios
+		.put(saveDirUri, fileContent, filePutHeaders)
+		.then((_) => {
+			Dialog.message(LanguageViewModel.makeTranslation("nextcloud-ok-msg", "Your attachment is saved to nextcloud"))
+		})
+		.catch((err) => {
+			Dialog.message(LanguageViewModel.makeTranslation("nextcloud-err-msg", "You attachment could not be saved to nextcloud"))
+		})
+	return { transferIds, promise }
 }
